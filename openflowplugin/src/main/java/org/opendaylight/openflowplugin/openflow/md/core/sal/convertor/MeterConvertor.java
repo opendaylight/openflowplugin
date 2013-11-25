cf99e9a3cc98b1eeef6b18f6efa2b02dbc9b4acd
usha <usha.m.s@ericsson.com>
2013-11-25 23:57:20 +0530

    comments: get version from session context and setting it corresponding builders(flow,action,meter,group and port) for the OF Plugin usage
    Signed-off-by: usha <usha.m.s@ericsson.com>
    Change-Id: I696f98adf7d9dd0f919c529018afad87afe9a087

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/MeterConvertor.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/MeterConvertor.java
@@ -1,4 +1,5 @@
 package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor;
+
 /****
  *
  * This class is used for converting the data from SAL layer to OF Library Layer for Meter Mod Command.
@@ -9,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterFlags.Flags;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.band.type.band.type.Drop;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.band.type.band.type.DscpRemark;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.band.type.band.type.Experimenter;
@@ -27,126 +27,98 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.meter.band.header.meter.band.MeterBandExperimenterBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.meter.mod.Bands;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.meter.mod.BandsBuilder;
-//import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.meter.meter.band.headers.MeterBandHeaderBuilder;
 
+//import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.meter.meter.band.headers.MeterBandHeaderBuilder;
 
 public final class MeterConvertor {
 
-	 private static List<Bands> bands;
-	 private static MeterModInputBuilder meterModInputBuilder;
-	 private static MeterFlags flags;
-	 private MeterConvertor(){
+    private MeterConvertor() {
 
-	 }
+    }
 
-	 //Get all the data for the meter from the Yang/SAL-Layer
-	 /**
-	 * @param Yang Data source
-	 * @return MeterModInput required by OF Library
-	 */
-	public static MeterModInput  toMeterModInput(org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.Meter source ) {
+    // Get all the data for the meter from the Yang/SAL-Layer
+    /**
+     * @param version
+     * @param Yang
+     *            Data source
+     * @return MeterModInput required by OF Library
+     */
+    public static MeterModInput toMeterModInput(
+            org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.Meter source, short version) {
 
-		 meterModInputBuilder = new MeterModInputBuilder();
+        MeterModInputBuilder meterModInputBuilder = new MeterModInputBuilder();
 
+        List<Bands> bands = new ArrayList<Bands>();
+        MeterFlags flags = null;
+        if (source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInput)
+            meterModInputBuilder.setCommand(MeterModCommand.OFPMCADD);
+        if (source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInput)
+            meterModInputBuilder.setCommand(MeterModCommand.OFPMCDELETE);
+        if (source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInput)
+            meterModInputBuilder.setCommand(MeterModCommand.OFPMCMODIFY);
 
-		 if(source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInput)
-				 meterModInputBuilder.setCommand(MeterModCommand.OFPMCADD);
-			if(source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInput)
-				meterModInputBuilder.setCommand(MeterModCommand.OFPMCDELETE);
-			if(source instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInput)
-				meterModInputBuilder.setCommand(MeterModCommand.OFPMCMODIFY);
+        meterModInputBuilder.setMeterId(new MeterId(source.getMeterId().getValue()));
 
-			meterModInputBuilder.setMeterId(new MeterId(source.getMeterId().getValue()));
+        meterModInputBuilder.setFlags(flags = new MeterFlags(source.getFlags().isMeterBurst(), source.getFlags()
+                .isMeterKbps(), source.getFlags().isMeterPktps(), source.getFlags().isMeterStats()));
+        getBandsFromSAL(source.getMeterBandHeaders(), bands);
+        meterModInputBuilder.setBands(bands);
+        meterModInputBuilder.setVersion(version);
+        return meterModInputBuilder.build();
+    }
 
-			getFlagsFromSAL(source.getFlags());
-				meterModInputBuilder.setFlags(flags);
-			getBandsFromSAL(source.getMeterBandHeaders());
-		 		 meterModInputBuilder.setBands(bands);
-		 return meterModInputBuilder.build();
-	 }
+    private static void getBandsFromSAL(MeterBandHeaders meterBandHeaders, List<Bands> bands) {
 
+        Iterator<MeterBandHeader> bandHeadersIterator = meterBandHeaders.getMeterBandHeader().iterator();
+        MeterBandHeader meterBandHeader;
 
+        BandsBuilder bandsB = null;
 
-	 private static void getBandsFromSAL(MeterBandHeaders meterBandHeaders) {
+        while (bandHeadersIterator.hasNext()) {
+            meterBandHeader = bandHeadersIterator.next();
+            MeterBand meterBandItem = null;
+            // The band types :drop,DSCP_Remark or experimenter.
 
-		Iterator <MeterBandHeader> bandHeadersIterator  = meterBandHeaders.getMeterBandHeader().iterator();
-		MeterBandHeader meterBandHeader;
+            if (meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtDrop()) {
 
-		BandsBuilder bandsB = null;
-		//MeterBandHeaderBuilder meterBandHeaderBuilder = null ;
-		bands = new ArrayList<Bands>();
-		while(bandHeadersIterator.hasNext())
-		{
-				meterBandHeader = bandHeadersIterator.next();
-				MeterBand meterBandItem = null;
-				//The band types :drop,DSCP_Remark or experimenter.
-				//meterBandHeaderBuilder = new MeterBandHeaderBuilder();
+                MeterBandDropBuilder meterBandDropBuilder = new MeterBandDropBuilder();
+                meterBandDropBuilder.setType(MeterBandType.OFPMBTDROP);
 
+                Drop drop = (Drop) meterBandHeader.getBandType();
 
-				if(meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtDrop()){
+                meterBandDropBuilder.setBurstSize(drop.getRate());
+                meterBandDropBuilder.setRate(drop.getBurstSize());
+                meterBandItem = meterBandDropBuilder.build();
 
-					MeterBandDropBuilder meterBandDropBuilder = new MeterBandDropBuilder();
-					meterBandDropBuilder.setType(MeterBandType.OFPMBTDROP);
+            } else if (meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtDscpRemark()) {
+                MeterBandDscpRemarkBuilder meterBandDscpRemarkBuilder = new MeterBandDscpRemarkBuilder();
+                meterBandDscpRemarkBuilder.setType(MeterBandType.OFPMBTDSCPREMARK);
 
-					Drop drop = (Drop)meterBandHeader.getBandType();
+                DscpRemark dscpRemark = (DscpRemark) meterBandHeader.getBandType();
 
-					meterBandDropBuilder.setBurstSize(drop.getRate());
-					meterBandDropBuilder.setRate(drop.getBurstSize());
-					meterBandItem= meterBandDropBuilder.build();
+                meterBandDscpRemarkBuilder.setBurstSize(dscpRemark.getBurstSize());
+                meterBandDscpRemarkBuilder.setRate(dscpRemark.getRate());
+                meterBandDscpRemarkBuilder.setPrecLevel(dscpRemark.getPercLevel());
+                meterBandItem = meterBandDscpRemarkBuilder.build();
 
-				}
-				else if(meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtDscpRemark())
-				{
-					MeterBandDscpRemarkBuilder meterBandDscpRemarkBuilder = new MeterBandDscpRemarkBuilder();
-					meterBandDscpRemarkBuilder.setType(MeterBandType.OFPMBTDSCPREMARK);
+            } else if (meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtExperimenter()) {
+                MeterBandExperimenterBuilder meterBandExperimenterBuilder = new MeterBandExperimenterBuilder();
+                meterBandExperimenterBuilder.setType(MeterBandType.OFPMBTEXPERIMENTER);
+                Experimenter experimenter = (Experimenter) meterBandHeader.getBandType();
+                meterBandExperimenterBuilder.setBurstSize(experimenter.getBurstSize());
+                meterBandExperimenterBuilder.setRate(experimenter.getRate());
+                meterBandExperimenterBuilder.setExperimenter(experimenter.getExperimenter());
+                meterBandItem = meterBandExperimenterBuilder.build();
 
-					DscpRemark dscpRemark = (DscpRemark)meterBandHeader.getBandType();
+            }
 
-					meterBandDscpRemarkBuilder.setBurstSize(dscpRemark.getBurstSize());
-					meterBandDscpRemarkBuilder.setRate(dscpRemark.getRate());
-					meterBandDscpRemarkBuilder.setPrecLevel(dscpRemark.getPercLevel());
-					meterBandItem = meterBandDscpRemarkBuilder.build();
+            bandsB = new BandsBuilder();
+            bandsB.setMeterBand(meterBandItem).build();
 
-				}
-				else if(meterBandHeader.getMeterBandTypes().getFlags().isOfpmbtExperimenter())
-				{
-					MeterBandExperimenterBuilder meterBandExperimenterBuilder = new MeterBandExperimenterBuilder();
-					meterBandExperimenterBuilder.setType(MeterBandType.OFPMBTEXPERIMENTER);
-				        Experimenter experimenter = (Experimenter)meterBandHeader.getBandType();
-					meterBandExperimenterBuilder.setBurstSize(experimenter.getBurstSize());
-					meterBandExperimenterBuilder.setRate(experimenter.getRate());
-					meterBandExperimenterBuilder.setExperimenter(experimenter.getExperimenter());
-					meterBandItem = meterBandExperimenterBuilder.build();
+            bands.add(bandsB.build()); // Bands list
 
-				}
+        }
 
-				bandsB = new BandsBuilder();
-				bandsB.setMeterBand(meterBandItem).build();
-
-				bands.add(bandsB.build()); //Bands list
-
-
-			}
-
-	}
-
-	 //get it from plugin(SAL) layer
-	 private static void getFlagsFromSAL(Flags flags2) {
-	                        boolean meterBurst_SAL = false;
-	                        boolean meterKbps_SAL = false;
-				boolean meterPktps_SAL = false;
-				boolean meterStats_SAL = false;
-
-
-				if(flags2.isMeterBurst())	meterBurst_SAL = true;
-				if(flags2.isMeterKbps())  meterKbps_SAL = true;
-				if(flags2.isMeterPktps()) meterPktps_SAL = true;
-				if(flags2.isMeterStats()) meterStats_SAL = true;
-
-				flags = new MeterFlags(meterBurst_SAL,meterKbps_SAL,meterPktps_SAL,meterStats_SAL);
-
-
-		}
-
+    }
 
 }
