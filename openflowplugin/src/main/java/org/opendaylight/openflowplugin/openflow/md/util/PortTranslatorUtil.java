c00943154273a4f6e69bb9e003f07f9ca663242d
Ed Warnicke <eaw@cisco.com>
2014-01-04 14:01:44 -0800

    Fixed port discovery for OF 1.0
    1)  Consolodated translation from Port to NodeConnectorUpdated into
    PortTranslatorUtils.java
    2)  Updated PortTranslatorUtils.translatePort to correctly handle
    OF 1.0
    3)  Switched MultiPartReplyPortToNodeConnectorUpdatedTranslator.java
    to using PortTranslatorUtils.translatePort
    4)  Switched PortStatusMessageToNodeConnectorUpdatedTranslator.java
    to using PortTranslatorUtils.translatePort
    5)  Write FeaturesV10ToNodeConnectorUpdatedTranslator.java to
    translate OF 1.0 GetFeaturesOutput to a NodeConnectorUpdated
    as thats where in OF 1.0 we learn about ports
    6)  Changed MDController.java to register the new translator for OF 1.0
    7)  Changed ConnectionConductorImpl.java to queue up the
    GetFeaturesOutput for OF 1.0 so we can process it like any other
    message.
    
    Change-Id: Ie6dbd961c5ec609bd881a60f7f2c11826dfb781d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/util/PortTranslatorUtil.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/util/PortTranslatorUtil.java
@@ -1,10 +1,21 @@
 package org.opendaylight.openflowplugin.openflow.md.util;
 
+import java.math.BigInteger;
+
+import org.opendaylight.openflowplugin.openflow.md.OFConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnectorUpdated;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnectorUpdatedBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.flow.capable.port.State;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.flow.capable.port.StateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorUpdated;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorUpdatedBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortConfigV10;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortFeatures;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortFeaturesV10;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortStateV10;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.Port;
 
 public class PortTranslatorUtil {
     public static  org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortFeatures translatePortFeatures(PortFeatures apf) {
@@ -17,8 +28,30 @@ public class PortTranslatorUtil {
         }
         return napf;
     }
+    
+    public static org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortFeatures translatePortFeatures(
+            PortFeaturesV10 apf) {
+        org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortFeatures napf = null;
+        if(apf != null){
+                napf = new org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortFeatures(
+                        false,apf.is_100mbFd(),apf.is_100mbHd(),apf.is_10gbFd(),apf.is_10mbFd(),apf.is_10mbHd(),
+                        apf.is_1gbFd(),apf.is_1gbHd(),false,false,apf.isAutoneg(),apf.isCopper(),apf.isFiber(),false,
+                        apf.isPause(), apf.isPauseAsym());
+        }
+        return napf;
+    }
 
     public static  State translatePortState(PortState state) {
+        StateBuilder nstate = new StateBuilder();
+        if(state !=null) {
+            nstate.setBlocked(state.isBlocked());
+            nstate.setLinkDown(state.isLinkDown());
+            nstate.setLive(state.isLive());
+        }
+        return nstate.build();
+    }
+    
+    public static State translatePortState(PortStateV10 state) {
         StateBuilder nstate = new StateBuilder();
         if(state !=null) {
             nstate.setBlocked(state.isBlocked());
@@ -35,5 +68,43 @@ public class PortTranslatorUtil {
                         pc.isNoPacketIn(), pc.isNoRecv(), pc.isPortDown());
         }
         return npc;
+    }
+    
+    public static org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortConfig translatePortConfig(
+            PortConfigV10 pc) {
+        org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortConfig npc = null;
+        if(pc != null) {
+                npc = new org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortConfig(pc.isNoFwd(),
+                        pc.isNoPacketIn(), pc.isNoRecv(), pc.isPortDown());
+        }
+        return npc;
+    }
+    
+    public static NodeConnectorUpdated translatePort(Short version,BigInteger datapathId,Long portNo, Port port) {
+        NodeConnectorUpdatedBuilder builder = InventoryDataServiceUtil
+                .nodeConnectorUpdatedBuilderFromDatapathIdPortNo(datapathId,port.getPortNo());
+        FlowCapableNodeConnectorUpdatedBuilder fcncub = new FlowCapableNodeConnectorUpdatedBuilder();
+        if(version == OFConstants.OFP_VERSION_1_3) {
+            fcncub.setAdvertisedFeatures(PortTranslatorUtil.translatePortFeatures(port.getAdvertisedFeatures()));
+            fcncub.setConfiguration(PortTranslatorUtil.translatePortConfig(port.getConfig()));
+            fcncub.setCurrentFeature(PortTranslatorUtil.translatePortFeatures(port.getCurrentFeatures()));
+            fcncub.setPeerFeatures(PortTranslatorUtil.translatePortFeatures(port.getPeerFeatures()));
+            fcncub.setState(PortTranslatorUtil.translatePortState(port.getState()));
+            fcncub.setSupported(PortTranslatorUtil.translatePortFeatures(port.getSupportedFeatures()));
+        } else if (version == OFConstants.OFP_VERSION_1_0) {
+            fcncub.setAdvertisedFeatures(PortTranslatorUtil.translatePortFeatures(port.getAdvertisedFeaturesV10()));
+            fcncub.setConfiguration(PortTranslatorUtil.translatePortConfig(port.getConfigV10()));
+            fcncub.setCurrentFeature(PortTranslatorUtil.translatePortFeatures(port.getCurrentFeaturesV10()));
+            fcncub.setPeerFeatures(PortTranslatorUtil.translatePortFeatures(port.getPeerFeaturesV10()));
+            fcncub.setState(PortTranslatorUtil.translatePortState(port.getStateV10()));
+            fcncub.setSupported(PortTranslatorUtil.translatePortFeatures(port.getSupportedFeaturesV10()));
+        }
+        fcncub.setCurrentSpeed(port.getCurrSpeed());
+        fcncub.setHardwareAddress(port.getHwAddr());
+        fcncub.setMaximumSpeed(port.getMaxSpeed());
+        fcncub.setName(port.getName());
+        fcncub.setPortNumber(port.getPortNo());
+        builder.addAugmentation(FlowCapableNodeConnectorUpdated.class, fcncub.build());
+        return builder.build();
     }
 }
