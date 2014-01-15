opendaylight/openflowplugin

commit c9d12e93d94189aca02073079db55e92abfed6e4

Pass additional fields from OF lib to apps Added a null check Signed-off-by: Srikar Rajamani <srikar.rajamani@ericsson.com>

openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/PacketInTranslator.java
@@ -7,6 +7,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.opendaylight.openflowplugin.openflow.md.core.IMDMessageTranslator;
 import org.opendaylight.openflowplugin.openflow.md.core.SwitchConnectionDistinguisher;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match.MatchConvertorImpl;
 import org.opendaylight.openflowplugin.openflow.md.core.session.SessionContext;
 import org.opendaylight.openflowplugin.openflow.md.util.InventoryDataServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.PortNumberMatchEntry;
@@ -17,6 +18,10 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.Cookie;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceivedBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketInReason;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.NoMatch;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.InvalidTtl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.SendToController;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,12 +87,33 @@ public class PacketInTranslator implements IMDMessageTranslator<OfHeader, List<D
                }else{
                    LOG.info("Receive packet_in from {} on port {}", dpid, port);
                }
+               org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match match = 
+            		   (org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match)
+            		   MatchConvertorImpl.fromOFMatchToSALMatch(message.getMatch(),dpid);
+               pktInBuilder.setMatch((org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.Match)match);
+               pktInBuilder.setPacketInReason(getPacketInReason(message.getReason()));
+               pktInBuilder.setTableId(new org.opendaylight.yang.gen.v1.urn.opendaylight.table.types.rev131026.TableId(message.getTableId().getValue().shortValue()));
                pktInBuilder.setIngress(InventoryDataServiceUtil.nodeConnectorRefFromDatapathIdPortno(dpid,port));
                PacketReceived pktInEvent = pktInBuilder.build();
                list.add(pktInEvent);
-                return list;
+               return list;
            } 
         } 
         return Collections.emptyList();
+    }
+    
+    private Class <?extends PacketInReason> getPacketInReason(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PacketInReason reason) {
+    
+    	if (reason.equals(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PacketInReason.OFPRNOMATCH)) {
+    		return NoMatch.class;
+    	}
+    	else if (reason.equals(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PacketInReason.OFPRINVALIDTTL)) {
+    		return InvalidTtl.class;
+    	}
+    	else if (reason.equals(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PacketInReason.OFPRACTION)) {
+    		return SendToController.class;
+    	}
+    
+    	return PacketInReason.class;
     }
 }

commit d9a979f5cb31e220e93c6bfcff6e29a657c8a08a

Commented out a call to PacketReceivedBuilder.setMatch because it causes a ClassCastException
This is a problem that seems to have been introduced in commit c9d12e93d94189aca02073079db55e92abfed6e4
where an attempt was made to set the match on PacketReceivedBuilder.

Change-Id: I39b53fe176b78cd635f96fb3953f33fd82763fbc
Signed-off-by: Moiz Raja <moraja@cisco.com>

openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/PacketInTranslator.java
@@ -1,10 +1,5 @@
 package org.opendaylight.openflowplugin.openflow.md.core.translator;
 
-import java.math.BigInteger;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 import org.opendaylight.openflowplugin.openflow.md.core.IMDMessageTranslator;
 import org.opendaylight.openflowplugin.openflow.md.core.SwitchConnectionDistinguisher;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match.MatchConvertorImpl;
@@ -16,15 +11,20 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketInMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.Cookie;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.InvalidTtl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.NoMatch;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketInReason;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceivedBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketInReason;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.NoMatch;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.InvalidTtl;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.SendToController;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PacketInTranslator implements IMDMessageTranslator<OfHeader, List<DataObject>> {
 
@@ -90,7 +90,17 @@ public class PacketInTranslator implements IMDMessageTranslator<OfHeader, List<D
                org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match match = 
             		   (org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match)
             		   MatchConvertorImpl.fromOFMatchToSALMatch(message.getMatch(),dpid);
-               pktInBuilder.setMatch((org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.Match)match);
+
+               // The following setMatch is completely wrong and will never work (you will always get a ClassCastException)
+               // because org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match and
+               // org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.Match are
+               // unrelated types
+               // To fix this properly it may be neccessary to implement a new converter method on MatchConvertor
+               // which returns org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.Match
+               // and then pass it to setMatch
+
+               //pktInBuilder.setMatch((org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.packet.received.Match)match);
+
                pktInBuilder.setPacketInReason(getPacketInReason(message.getReason()));
                pktInBuilder.setTableId(new org.opendaylight.yang.gen.v1.urn.opendaylight.table.types.rev131026.TableId(message.getTableId().getValue().shortValue()));
                pktInBuilder.setIngress(InventoryDataServiceUtil.nodeConnectorRefFromDatapathIdPortno(dpid,port));