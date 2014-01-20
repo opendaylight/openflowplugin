4d8bace720f326d872d43bb58b9a38c242e74b84
Michal Rehak <mirehak@cisco.com>
2014-01-20 19:35:36 +0100

    copyright header added
    Change-Id: I1169e1761a5505a6a79e58893837753602d26e92
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/session/FeaturesV13Bandwidth.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/session/FeaturesV13Bandwidth.java
@@ -4,8 +4,7 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- */ 	  				 	 	 
-
+ */
 package org.opendaylight.openflowplugin.openflow.md.core.session;
 
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.Port;
