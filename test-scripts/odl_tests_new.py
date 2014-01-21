f915ce0b7a97e1be1eb64210b8a72b3c1b9b3693
Juraj Sebin <jsebin@cisco.com>
2014-01-21 15:28:19 +0100

    updates in test scripts -tests scripts are running in sequence -new validation method -structure of xml is validated before test
    Change-Id: I6931f6449370f65df9a081e3089f9f8d0c0e20e8
    Signed-off-by: Juraj Sebin <jsebin@cisco.com>

--- test-scripts/odl_tests_new.py
+++ test-scripts/odl_tests_new.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import time
 import logging
@@ -18,10 +19,139 @@ from mininet.node import RemoteController
 from mininet.node import OVSKernelSwitch
 
 import xmltodict
+from xmlvalidator import XMLValidator
+
+class TestOpenFlowXml_Base(unittest.TestCase):
+    
+    def __init__(self, methodName='runTest', path_to_xml=None):
+        super(TestOpenFlowXml_Base, self).__init__(methodName)
+        self.path_to_xml = path_to_xml
+        
+    @staticmethod
+    def load_file_name(clazz, path_to_xml=None):
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(clazz)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(clazz(name, path_to_xml=path_to_xml))
+        return suite
 
 
-class ParseTools():
+class ConvertorTools():
+    
+    CONVERTORS = {
+        'cookie': hex, 
+    }  
+    
+    @staticmethod
+    def base_tag_values_conversion(key, value):
+        convertor = ConvertorTools.CONVERTORS.get(key, None)
+        return convertor(int(value)) if convertor > None else value
 
+
+class ParseTools(): 
+
+    @staticmethod
+    def get_element_alias_by_key(element,key_dict):
+        return key_dict.get(element.tag) if (key_dict.get(element.tag, None) > None) else None
+
+    @staticmethod
+    def sort_ordered_dict_to_array(x_dict=None):
+        if (x_dict > None):
+            out_put = None
+            for val in map(lambda val: x_dict.get(val), sorted(x_dict.keys())) : 
+                if (out_put > None) :
+                    out_put += ', %s' %val
+                else :
+                    out_put = val
+            return out_put
+        return
+
+    @staticmethod
+    def get_element_value(element):
+        return (re.sub('[\s]+', '', element.text, count=1)).lower() if element.text > None else ''
+
+    @staticmethod
+    def __parse_ordered_tags_from_xml(element, kwd, p_elm_n=None, ikwd=None, ord_value=None):
+        a_dict = {}
+        if (element > None) :
+            elm_n = ParseTools.get_element_alias_by_key(element, kwd)
+            if ((element.getchildren() > None) & (len(element.getchildren()) > 0)) :
+                sub_dict ={}
+                for child in element.getchildren() :
+                    if (child.tag == 'order') :
+                        ord_value = ParseTools.get_element_value(child)
+                    else :
+                        sub_dict.update(ParseTools.__parse_ordered_tags_from_xml(child, kwd, p_elm_n, ikwd))
+                        
+                a_value = ParseTools.sort_ordered_dict_to_array(sub_dict)
+                if (ord_value > None) :
+                    order = ord_value if (len(ord_value) > 0) else '0'
+                else :
+                    order = '0'
+                a_dict[order]=a_value
+                
+            else :
+                if (ord_value > None) :
+                    order = ord_value if ((len(ord_value) > 0)) else '0'
+                else :
+                    order = '0'
+                a_val = elm_n if elm_n > None else element.tag
+                a_dict[order] = a_val
+                
+        return a_dict
+
+    @staticmethod
+    def __parse_tags_from_xml(element, flow_dict, kwd, p_elm_n=None, ikwd=None):
+        if element > None :
+            # find and translate element.tag in key_word_dictionary
+            elm_n = ParseTools.get_element_alias_by_key(element, kwd)
+            if ((element.getchildren() > None) & (len(element.getchildren()) > 0)) :
+                for child in element.getchildren() :
+                    new_p_elm_n = elm_n if elm_n > None else p_elm_n
+                    ParseTools.__parse_tags_from_xml(child, flow_dict, kwd, new_p_elm_n, ikwd)
+            else :
+                # prefer parent element_name before elment_name and element_name before element.tag
+                a_key = elm_n if elm_n > None else p_elm_n if (p_elm_n > None) else element.tag
+                a_value = ParseTools.get_element_value(element)
+                # Don't continue for ignore tags
+                if (ikwd > None) :
+                    if (ikwd.get(a_key, None) > None) :
+                        # TODO add check for cookie_mask (mask has to have same or more length as cookie if is more as 0)
+                        return
+                flow_dict[a_key] = ConvertorTools.base_tag_values_conversion(a_key, a_value)
+
+    @staticmethod
+    def get_switchflow_from_xml(xml_string, key_dict=None, action_key_dict=None, match_key_dict=None, ignore_key_dict=None):
+        if xml_string > None :
+            # remove namespace
+            xml_string = re.sub(' xmlns="[^"]+"', '', xml_string, count=1)
+            tree = ET.fromstring(xml_string)
+            
+        flow_dict = {}
+        
+        if (tree > None) :
+            if (tree.getchildren() > None) :
+                for child in tree.getchildren() :
+                    if (child.tag == 'match') :
+                        ParseTools.__parse_tags_from_xml(child, flow_dict, match_key_dict, ikwd=ignore_key_dict)
+                    elif (child.tag == 'instructions') : 
+                        x_dict = ParseTools.__parse_ordered_tags_from_xml(child, action_key_dict, ikwd=ignore_key_dict)
+                        flow_dict['actions'] = ParseTools.sort_ordered_dict_to_array(x_dict)
+                    else :
+                        ParseTools.__parse_tags_from_xml(child, flow_dict, key_dict, ikwd=ignore_key_dict) 
+
+        return flow_dict
+
+    @staticmethod
+    def get_switchflow_dict(switch_dict, ignore_key_dict=None):
+        x_dict={}
+        for sw_key in switch_dict.keys() :
+            if (ignore_key_dict.get(sw_key,None) is None):
+                x_dict[sw_key] = switch_dict.get(sw_key)
+            
+        return x_dict
+    
     @staticmethod
     def all_nodes(xml_root):
         """
@@ -95,8 +225,9 @@ class MininetTools():
         net = mininet.net.Mininet(topo=topo, switch=switch, controller=controller)
 
         return net
+    
     @staticmethod
-    def get_flows(net):
+    def get_flows(net, ikwd={}):
         """Get list of flows from network's first switch.
 
         Return list of all flows on switch, sorted by duration (newest first)
@@ -129,52 +260,58 @@ class MininetTools():
 
         log.debug('switch flow table: {}'.format(output))
 
-        flows = []
+        flows = {}
 
-        for line in output.splitlines()[1:]:
-            flows.append(ParseTools.dump_string_to_dict(line))
+        for line in output.splitlines()[1:] :
+            output = line;
+        
+        action = re.split('actions=',output,1)[1]
+        output= re.split('actions=',output,1)[0]
 
-         # sort by duration
-        return sorted(flows, key=lambda x: x['duration'].rstrip('s'))
+        for elem in output.split(',') :
+            elm_d = elem.split('=')
+            a_key = (elm_d[0]).strip()
+            if (ikwd.get(a_key, None) is None) :
+                a_value = elm_d[1] if (len(elm_d) > 1) else None
+                flows[a_key] = a_value.strip() if isinstance(a_value,str) else (str(a_value)).strip()
+        
+        flows['actions'] = action.split(',')
+        
+        return flows
+#         for line in output.splitlines()[1:]:
+#             flows.append(ParseTools.dump_string_to_dict(line))
+# 
+#          # sort by duration
+#         return sorted(flows, key=lambda x: x['duration'].rstrip('s'))
 
 
-class Loader():
+class FileLoaderTools():
 
-    log = logging.getLogger('Loader')
+    log = logging.getLogger('FileLoaderTools')
 
     @staticmethod
-    def get_xml_test_path(test_id, path='xmls'):
-        return os.path.join(path, 'f%d.xml' % test_id)
-
-    @staticmethod
-    def get_mn_test_path(test_id, path='ofctl'):
-        return os.path.join(path, 't%d' % test_id)
-
-    @staticmethod
-    def get_xml_test_path(path, test_id):
-        return os.path.join(path, 'f%d.xml' % test_id)
-
-    @staticmethod
-    def load_test_file_to_string(path_to_file):
+    def load_file_to_string(path_to_file):
         output_string = None
 
         try:
             with open(path_to_file) as f:
                 output_string = f.read()
         except IOError, e:
-            Loader.log.error('cannot find {}: {}'.format(path_to_file, e.strerror), exc_info=True)
+            FileLoaderTools.log.error('cannot find {}: {}'.format(path_to_file, e.strerror), exc_info=True)
 
         return output_string
 
     @staticmethod
-    def get_xml_dict(test_id):
-        xml_string = Loader.load_test_file_to_string(Loader.get_xml_test_path(test_id))
-        return xmltodict(xml_string)
-
-    @staticmethod
-    def get_mn_dict(test_id):
-        mn_string = Loader.load_test_file_to_string(Loader.get_mn_test_path(test_id))
-        return ParseTools.dump_string_to_dict(mn_string)
+    def load_file_to_dict(path_to_file):
+        dictionary = None
+        
+        try :
+            with open(path_to_file) as f:
+                dictionary = dict(line.strip().split(';') for line in f
+                            if not line.startswith('#'))
+        except IOError, e:
+            FileLoaderTools.log.error('cannot find {}: {}'.format(path_to_file, e.strerror), exc_info=True)
+        return dictionary
 
 
 class Comparator():
@@ -217,89 +354,95 @@ class Comparator():
             # VD - what should we do in this case
             pass
 
-class TestOpenFlowXMLs(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.net = MininetTools.create_network(cls.host, cls.mn_port)
-        cls.net.start()
-        time.sleep(15)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.net.stop()
-
-def generate_tests_from_xmls(path, xmls=None):
-    # generate test function from path to request xml
-    def generate_test(path_to_xml, path_to_md):
-        xml_string = Loader.load_test_file_to_string(path_to_xml)
-        mn_string = Loader.load_test_file_to_string(path_to_md)
-
+class TestOpenFlowXml(TestOpenFlowXml_Base):        
+        
+    def test_xml(self):
+        test_number = int(self.path_to_xml[6:-4])
+        log = logging.getLogger('test_xml_%04d' %test_number)
+        
+        xml_string = FileLoaderTools.load_file_to_string(self.path_to_xml)
         tree = md.parseString(xml_string)
+        
+        switch_etalon = ParseTools.get_switchflow_from_xml(xml_string, 
+                                                           key_dict = keywords, 
+                                                           action_key_dict = action_keywords, 
+                                                           match_key_dict = match_keywords,
+                                                           ignore_key_dict = ignore_keywords)
+        print (switch_etalon)
         ids = ParseTools.get_values(tree.documentElement, 'table_id', 'id')
-
-        def new_test(self):
-            log = logging.getLogger(__name__)
-            # send request throught RESTCONF
-            data = (self.host, self.port, ids['table_id'], ids['id'])
-            url = 'http://%s:%d/restconf/config/opendaylight-inventory:nodes' \
-                  '/node/openflow:1/table/%s/flow/%s' % data
-            headers = {
-                'Content-Type': 'application/xml',
-                'Accept': 'application/xml',
-            }
-            log.info('sending request to url: {}'.format(url))
-            rsp = requests.put(url, auth=('admin', 'admin'), data=xml_string,
-                               headers=headers)
-            log.info('received status code: {}'.format(rsp.status_code))
-            log.debug('received content: {}'.format(rsp.text))
-            assert rsp.status_code == 204 or rsp.status_code == 200, 'Status' \
-                    ' code returned %d' % rsp.status_code
-            try:
-                # check request content against restconf's datastore
-                response = requests.get(url, auth=('admin', 'admin'),
-                                        headers={'Accept': 'application/xml'})
-                assert response.status_code == 200
-                req = (xmltodict.parse(ET.tostring(ET.fromstring(xml_string))))
-                res = (xmltodict.parse(ET.tostring(ET.fromstring(response.text))))
-                assert req == res, 'uploaded and stored xml, are not the same\n' \
-                    'uploaded: %s\nstored:%s' % (req, res)
-
-                # collect flow table state on switch
-                switch_flows = MininetTools.get_flows(self.net)
-                assert len(switch_flows) > 0
-
-                # compare requested object and flow table state
-                if mn_string is not None:
-                    #log.info('running tests')
-                    Comparator.compare_results(switch_flows[0], ParseTools.dump_string_to_dict(mn_string))
-                else:
-                    log.error('cannot find test results - comparison skipped')
-            finally:    
-                response = requests.delete(url, auth=('admin', 'admin'),
+        
+        
+        data = (self.host, self.port, ids['table_id'], ids['id'])
+        url = 'http://%s:%d/restconf/config/opendaylight-inventory:nodes' \
+              '/node/openflow:1/table/%s/flow/%s' % data
+        # send request via RESTCONF
+        headers = {
+            'Content-Type': 'application/xml',
+            'Accept': 'application/xml',
+        }
+        log.info('sending request to url: {}'.format(url))
+        rsp = requests.put(url, auth=('admin', 'admin'), data=xml_string,
+                           headers=headers)
+        log.info('received status code: {}'.format(rsp.status_code))
+        log.debug('received content: {}'.format(rsp.text))
+        assert rsp.status_code == 204 or rsp.status_code == 200, 'Status' \
+                            ' code returned %d' % rsp.status_code
+        try:
+            # check request content against restconf's datastore
+            response = requests.get(url, auth=('admin', 'admin'),
                                     headers={'Accept': 'application/xml'})
-                assert response.status_code == 200
-                print '\n\n\n'    
+            assert response.status_code == 200
+            req = (xmltodict.parse(ET.tostring(ET.fromstring(xml_string))))
+            res = (xmltodict.parse(ET.tostring(ET.fromstring(response.text))))
+            assert req == res, 'uploaded and stored xml, are not the same\n' \
+                'uploaded: %s\nstored:%s' % (req, res)
+    
+            # collect flow table state on switch
+            switch_flows = MininetTools.get_flows(net, ignore_keywords)
+#             switch_flows_actions = re.sub('[\s]', '', switch_flows, count=1))
+            assert len(switch_flows) > 0
+            assert switch_etalon == switch_flows, 'expected and stored switch settings are not the same \n'\
+                'expected: %s\nstored: %s' %(switch_etalon,switch_flows)
 
-        return new_test
+            # compare requested object and flow table state
+            ## TODO look at action parsing separatly from a flow
+#             switch_flow_dict = ParseTools.get_switchflow_dict(switch_flows[0], ignore_keywords)
+#             assert switch_etalon == switch_flow_dict, 'expected and stored switch settings are not the same \n'\
+#                 'expected: %s\nstored: %s' %(switch_etalon,switch_flow_dict)
+#         if mn_string is not None:
+#             #log.info('running tests')
+#             Comparator.compare_results(switch_flows[0], ParseTools.dump_string_to_dict(mn_string))
+#         else:
+#             log.error('cannot find test results - comparison skipped')
+        finally:
+            response = requests.delete(url, auth=('admin', 'admin'),
+                                headers={'Accept': 'application/xml'})
+            assert response.status_code == 200
+            print '\n\n\n'    
 
-    # generate list of available xml requests
+def suite(path='xmls', test_class='TestOpenFlowXml_Base') :
+    suite = unittest.TestSuite()
+    if args.xmls is not None:
+        xmls = map(int, args.xmls.split(','))
+    
     xmlfiles = None
     if xmls is not None:
         xmlfiles = ('f%d.xml' % fid for fid in xmls)
     else:
         xmlfiles = (xml for xml in os.listdir(path) if xml.endswith('.xml'))
 
-    # define key getter for sorting
-    def get_test_number(test_name):
-        return int(test_name[1:-4])
+    #create xml validator
+    validator = XMLValidator(keywords, action_keywords, match_keywords, logging.ERROR)
+    validator.fill_fields()
 
     for xmlfile in xmlfiles:
-        test_number = get_test_number(xmlfile)
-        test_name = 'test_xml_%04d' % test_number
-        setattr(TestOpenFlowXMLs,
-                test_name,
-                generate_test(os.path.join(path, xmlfile), os.path.join('ofctl', 't{}'.format(test_number))))
+        #fill validator with data from xml and validate them - just logging to hint what can be wrong, test wont be stopped by invalid xml
+        validator.create_dictionaries(os.path.join(path, xmlfile))
+        validator.validate()
 
+        suite.addTest(TestOpenFlowXml_Base.load_file_name(test_class, path_to_xml=os.path.join(path, xmlfile)))
+    return suite
 
 if __name__ == '__main__':
     # set up logging
@@ -318,35 +461,28 @@ if __name__ == '__main__':
                         'from some xmls (i.e. 1,3,34) ')
     args = parser.parse_args()
 
-    # set host and port of ODL controller for test cases
-    TestOpenFlowXMLs.port = args.odlport
-    TestOpenFlowXMLs.host = args.odlhost
-    TestOpenFlowXMLs.mn_port = args.mnport
+    # set and start mininet
+    net = MininetTools.create_network(args.odlhost, args.mnport)
+    net.start()
+    time.sleep(15)
 
-    keywords = None
-    with open('keywords.csv') as f:
-        keywords = dict(line.strip().split(';') for line in f
-                        if not line.startswith('#'))
-
-    match_keywords = None
-    with open('match-keywords.csv') as f:
-        match_keywords = dict(line.strip().split(';') for line in f
-                              if not line.startswith('#'))
-
-    action_keywords = None
-    with open('action-keywords.csv') as f:
-        action_keywords = dict(line.strip().split(';') for line in f
-                                    if not line.startswith('#'))
-
-    # fix arguments for unittest
-    del sys.argv[1:]
-
-    # generate tests for TestOpenFlowXMLs
-    if args.xmls is not None:
-        xmls = map(int, args.xmls.split(','))
-        generate_tests_from_xmls('xmls', xmls)
-    else:
-        generate_tests_from_xmls('xmls')
-
-    # run all tests
-    unittest.main()
+    try:
+        # set host and port of ODL controller for test cases
+        TestOpenFlowXml.port = args.odlport
+        TestOpenFlowXml.host = args.odlhost
+        TestOpenFlowXml.mn_port = args.mnport
+        
+        # set keyword dictionaries
+        keywords = FileLoaderTools.load_file_to_dict('keywords.csv')
+        match_keywords = FileLoaderTools.load_file_to_dict('match-keywords.csv')
+        action_keywords = FileLoaderTools.load_file_to_dict('action-keywords.csv')
+        ignore_keywords = FileLoaderTools.load_file_to_dict('ignore-keywords.csv')
+    
+#         # fix arguments for unittest
+#         del sys.argv[1:]
+#     
+        odl_suite = suite(test_class=TestOpenFlowXml)
+        unittest.TextTestRunner().run(odl_suite)
+    finally:
+        # stop mininet
+        net.stop()
