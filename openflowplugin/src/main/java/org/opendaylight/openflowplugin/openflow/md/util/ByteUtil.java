8efa9b44efad62ebb5aa3426bf0f926a3034560b
Ed Warnicke <eaw@cisco.com>
2014-01-23 18:15:41 +0530

    Openflow plugin code changes due to match yang model change: https://git.opendaylight.org/gerrit/#/c/4635/1
    Change-Id: I193be03b8aea075939bada818ba4983de2a5a365
    Signed-off-by: Deepthi V V <deepthi.v.v@ericsson.com>
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/util/ByteUtil.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/util/ByteUtil.java
@@ -8,7 +8,13 @@
 package org.opendaylight.openflowplugin.openflow.md.util;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
+
+import com.google.common.base.Joiner;
 
 /**
  * @author mirehak
@@ -16,7 +22,7 @@ import java.util.Arrays;
  */
 public abstract class ByteUtil {
 
-    
+    private static final int MAC_ADDRESS_LENGTH = 6;
     /**
      * @param bytes
      * @param delimiter
@@ -32,16 +38,16 @@ public abstract class ByteUtil {
     }
 
     /**
-     * Utility method to convert BigInteger to 8 element byte array
+     * Utility method to convert BigInteger to n element byte array
      * @param bigInteger
-     * @return byte array containing 64 bits.
+     * @return byte array containing n * 8 bits.
      */
-    public static byte[] convertBigIntegerTo64Bit(BigInteger bigInteger) {
+    public static byte[] convertBigIntegerToNBytes(BigInteger bigInteger, int numBytes) {
         if (bigInteger == null) {
             return null;
         }
         byte[] inputArray = bigInteger.toByteArray();
-        byte[] outputArray = new byte[8];
+        byte[] outputArray = new byte[numBytes];
         if (bigInteger.compareTo(BigInteger.ZERO) < 0) {
             Arrays.fill(outputArray, (byte) -1);
         } else {
@@ -53,5 +59,97 @@ public abstract class ByteUtil {
                          Math.max(0, outputArray.length - inputArray.length),
                          Math.min(outputArray.length, inputArray.length));
         return outputArray;
+    }
+
+    /**
+     * Converts a 4 byte array of unsigned bytes to unsigned int
+     * @param bytes an array of 4 unsigned bytes
+     * @return a long representing the unsigned int
+     */
+    public static final long bytesToUnsignedInt(byte[] bytes)
+    {
+      long unsignedInt = 0;
+      unsignedInt |= bytes[0] & 0xFF;
+      unsignedInt <<= 8;
+      unsignedInt |= bytes[1] & 0xFF;
+      unsignedInt <<= 8;
+      unsignedInt |= bytes[2] & 0xFF;
+      unsignedInt <<= 8;
+      unsignedInt |= bytes[3] & 0xFF;
+      return unsignedInt;
+    }
+
+    /**
+     * Converts a 2 byte array of unsigned bytes to unsigned short
+     * @param bytes an array of 2 unsigned bytes
+     * @return an int representing the unsigned short
+     */
+    public static final int bytesToUnsignedShort(byte[] bytes)
+    {
+      int unsignedShort = 0;
+      unsignedShort |= bytes[0] & 0xFF;
+      unsignedShort <<= 8;
+      unsignedShort |= bytes[1] & 0xFF;
+      return unsignedShort;
+    }
+
+    /**
+     * Converts unsigned integer to a 4 byte array of unsigned bytes
+     * @param unsignedInt representing the unsigned integer
+     * @return bytes an array of 4 unsigned bytes
+     */
+    public static byte[] unsignedIntToBytes(Long unsignedInt)
+    {
+      byte[] bytes = new byte[4];
+      bytes[3] = (byte) (unsignedInt & 0xFF);
+      bytes[2] = (byte) ((unsignedInt >> 8) & 0xFF);
+      bytes[1] = (byte) ((unsignedInt >> 16) & 0xFF);
+      bytes[0] = (byte) ((unsignedInt >> 24) & 0xFF);
+      return bytes;
+    }
+
+    /**
+     * Converts unsigned short to a 2 byte array of unsigned bytes
+     * @param unsignedShort representing the unsigned short
+     * @return bytes an array of 2 unsigned bytes
+     */
+    public static byte[] unsignedShortToBytes(Integer unsignedShort)
+    {
+      byte[] bytes = new byte[2];
+      bytes[1] = (byte) (unsignedShort & 0xFF);
+      bytes[0] = (byte) ((unsignedShort >> 8) & 0xFF);
+      return bytes;
+    }
+
+    /**
+     * Converts mac address represented in bytes to String
+     * @param address
+     * @return String representation of mac address
+     * @see {@link MacAddress}
+     */
+    public static String macAddressToString(byte[] address) {
+        List<String> groups = new ArrayList<>();
+        for(int i=0; i < MAC_ADDRESS_LENGTH; i++){
+            groups.add(String.format("%02X", address[i]));
+        }
+        Joiner joiner = Joiner.on(":");
+        return joiner.join(groups);
+    }
+
+    /**
+     * Converts macAddress to byte array
+     * @param macAddress
+     * @return byte representation of mac address
+     * @see {@link MacAddress}
+     */
+    public static byte[] macAddressToBytes(MacAddress macAddress) {
+        String macAddressString = macAddress.getValue();
+        String[] sequences = macAddressString.split(":");
+        int MAC_ADDRESS_LENGTH = 6;
+        byte[] result = new byte[MAC_ADDRESS_LENGTH];
+        for (int i = 0; i < sequences.length; i++) {
+             result[i] = (byte) Short.parseShort(sequences[i], 16);
+        }
+        return result;
     }
 }
