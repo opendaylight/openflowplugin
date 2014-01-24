da8c742bf323cb192161b285a6df530468ba8101
Ed Warnicke <eaw@cisco.com>
2014-01-24 14:42:27 -0600

    Turned down logging in ErrorTranslator from ERROR to DEBUG
    Change-Id: I296451951e41320def91234b67671211948f5908
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorTranslator.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorTranslator.java
@@ -35,7 +35,7 @@ public class ErrorTranslator implements IMDMessageTranslator<OfHeader, List<Data
         if (msg instanceof ErrorMessage) {
             ErrorMessage message = (ErrorMessage) msg;
             List<DataObject> list = new CopyOnWriteArrayList<DataObject>();
-            LOG.error(" Error Message received: type={}[{}], code={}[{}], data={}[{}] ", message.getType(),
+            LOG.debug(" Error Message received: type={}[{}], code={}[{}], data={}[{}] ", message.getType(),
                     message.getTypeString(), message.getCode(), message.getCodeString(), new String(message.getData()),
                     ByteUtil.bytesToHexstring(message.getData(), " "));
 
@@ -61,7 +61,7 @@ public class ErrorTranslator implements IMDMessageTranslator<OfHeader, List<Data
             list.add(nodeErrorEvent);
             return list;
         } else {
-            LOG.error("Message is not of Error Message ");
+            LOG.debug("Message is not of Error Message ");
             return Collections.emptyList();
         }
     }
