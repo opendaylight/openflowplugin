1e19fe3dc29c15b29f29a7b1f8264de72495bf1c
Tony Tkacik <ttkacik@cisco.com>
2014-02-04 03:30:21 +0100

    initial: learning switch example
    example code for learning switch / hub
    added as module into parent pom
    added into distribution/base pom
    handler + listener divided to atomic methods
    initial flow written + sent to switch
    bridge flows written to switch
    storing covered mac-paths
    
    Change-Id: I016055ed0920fb08c1923fa337c3e5b8880aba56
    Signed-off-by: Michal Rehak <mirehak@cisco.com>
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ samples/learning-switch/src/main/java/org/opendaylight/openflowplugin/learningswitch/OFDataStoreAccessorImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.learningswitch;
+
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
+import org.opendaylight.controller.sal.binding.api.data.DataBrokerService;
+import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * 
+ */
+public class OFDataStoreAccessorImpl implements OFDataStoreAccessor {
+    
+    private DataBrokerService dataBrokerService;
+
+    /**
+     * @param dataBrokerService
+     */
+    public OFDataStoreAccessorImpl(DataBrokerService dataBrokerService) {
+        this.dataBrokerService = dataBrokerService;
+    }
+
+    @Override
+    public Future<RpcResult<TransactionStatus>> writeFlowToConfig(InstanceIdentifier<Flow> flowPath, 
+            Flow allToCtrlFlow) {
+        DataModificationTransaction addFlowTransaction = dataBrokerService.beginTransaction();
+        addFlowTransaction.putConfigurationData(flowPath, allToCtrlFlow);
+        return addFlowTransaction.commit();
+    }
+    
+
+}
