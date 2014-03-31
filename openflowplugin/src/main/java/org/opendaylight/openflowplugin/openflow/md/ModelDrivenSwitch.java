67870b7df54e674a54d69375950ee37e67d1efb3
Michal Rehak <mirehak@cisco.com>
2014-03-31 15:14:45 +0200

    fix BUG 596 (part2)
    - reenabled fixed SalPortService interface (providing portUpdate)
    
    Change-Id: I5adbf8e1868abf55f319b1d03d5d44deca88b378
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/ModelDrivenSwitch.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/ModelDrivenSwitch.java
@@ -7,49 +7,24 @@
  */
 package org.opendaylight.openflowplugin.openflow.md;
 
-import java.util.concurrent.Future;
-
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.OpendaylightFlowStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.table.statistics.rev131215.OpendaylightFlowTableStatisticsService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.AddGroupInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.AddGroupOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.RemoveGroupInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.RemoveGroupOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.SalGroupService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.UpdateGroupInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.UpdateGroupOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.OpendaylightGroupStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.SalMeterService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.statistics.rev131111.OpendaylightMeterStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.port.service.rev131107.UpdatePortInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.port.service.rev131107.UpdatePortOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.service.rev131107.SalPortService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.OpendaylightQueueStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableOutput;
 import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
 import org.opendaylight.yangtools.concepts.Identifiable;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 
 /**
  * interface concatenating all md-sal services provided by OF-switch
@@ -59,6 +34,7 @@ public interface ModelDrivenSwitch extends //
         SalFlowService, //
         SalMeterService, //
         SalTableService,//
+        SalPortService,
         PacketProcessingService, //
         OpendaylightGroupStatisticsService, //
         OpendaylightMeterStatisticsService, //
@@ -68,50 +44,14 @@ public interface ModelDrivenSwitch extends //
         OpendaylightQueueStatisticsService, //
         Identifiable<InstanceIdentifier<Node>> {
 
+    /**
+     * @param ctx
+     * @return wrapped list of {service provider + path} registration couples
+     */
     CompositeObjectRegistration<ModelDrivenSwitch> register(ProviderContext ctx);
 
-    @Override
-    public InstanceIdentifier<Node> getIdentifier();
-
-    public NodeId getNodeId();
-
-    @Override
-    public Future<RpcResult<AddFlowOutput>> addFlow(AddFlowInput input);
-
-    @Override
-    public Future<RpcResult<RemoveFlowOutput>> removeFlow(RemoveFlowInput input);
-
-    @Override
-    public Future<RpcResult<Void>> transmitPacket(TransmitPacketInput input);
-
-    @Override
-    public Future<RpcResult<UpdateFlowOutput>> updateFlow(UpdateFlowInput input);
-
-    @Override
-    public Future<RpcResult<AddGroupOutput>> addGroup(AddGroupInput input);
-
-    @Override
-    public Future<RpcResult<AddMeterOutput>> addMeter(AddMeterInput input);
-
-    @Override
-    public Future<RpcResult<RemoveGroupOutput>> removeGroup(RemoveGroupInput input);
-
-    @Override
-    public Future<RpcResult<RemoveMeterOutput>> removeMeter(RemoveMeterInput input);
-
-    @Override
-    public Future<RpcResult<UpdateGroupOutput>> updateGroup(UpdateGroupInput input);
-
-    @Override
-    public Future<RpcResult<UpdateMeterOutput>> updateMeter(UpdateMeterInput input);
-    
     /**
-     * FIXME: previously propagated from SalPortService, should get removed or used via service
-     * @param input
-     * @return result of port update action
+     * @return id of encapsulated node (served by this impl)
      */
-    public Future<RpcResult<UpdatePortOutput>> updatePort(UpdatePortInput input);
-    
-    @Override
-    public Future<RpcResult<UpdateTableOutput>> updateTable(UpdateTableInput input) ;
+    NodeId getNodeId();
 }
