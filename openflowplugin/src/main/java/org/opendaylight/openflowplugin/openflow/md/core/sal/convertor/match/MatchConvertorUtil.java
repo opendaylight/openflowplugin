5a8696d05e290c73f8b8fcdad7bad7008f6b37a3
Michal Rehak <mirehak@cisco.com>
2014-05-22 17:51:19 +0200

    BUG-1065: translation of Ipv6ExtHeader flags to int
    - moved translation to separate method + test
    - fixed the false part of ternary operator
    
    (cherry picked from commit 4b7d8d11de20dc6c0a22b7d0e305ae25ead39d1e)
    
    Change-Id: I498dbf6c37f2d7348743d33bd49fe2ff46870621
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/match/MatchConvertorUtil.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.Ipv6ExthdrFlags;
+
+/**
+ * match related tools
+ */
+public abstract class MatchConvertorUtil {
+
+    /**
+     * @param pField
+     * @return integer containing lower 9 bits filled with corresponding flags
+     */
+    public static Integer ipv6ExthdrFlagsToInt(Ipv6ExthdrFlags pField) {
+        Integer bitmap = 0;
+        bitmap |= pField.isNonext() ? (1 << 0) : 0;
+        bitmap |= pField.isEsp() ?    (1 << 1) : 0;
+        bitmap |= pField.isAuth() ?   (1 << 2) : 0;
+        bitmap |= pField.isDest() ?   (1 << 3) : 0;
+        bitmap |= pField.isFrag() ?   (1 << 4) : 0;
+        bitmap |= pField.isRouter() ? (1 << 5) : 0;
+        bitmap |= pField.isHop() ?    (1 << 6) : 0;
+        bitmap |= pField.isUnrep() ?  (1 << 7) : 0;
+        bitmap |= pField.isUnseq() ?  (1 << 8) : 0;
+        return bitmap;
+    }
+
+}
