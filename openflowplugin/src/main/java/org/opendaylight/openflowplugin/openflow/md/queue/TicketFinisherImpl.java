365c0e3c1b998ea8728502156cee99cdbf6013ba
Michal Rehak <mirehak@cisco.com>
2014-05-27 13:43:42 +0200

    BUG-1075: ingress back pressure
    - added fair competition queue keeper implementation
    - added queueProcessor (harvesting and processing messages)
    - per-connection-queues register for queueProcessor and are blocked if polling queue is full
    - removed netty thread blocking by publish
    - added dropping of messages
    - added meaningful names to threads
    - added harvester, queues zipper, messageSource registration stuff
    - changed ticketPool to blocking if poolQueue full
    - doubleCheckedLocking for harverster starving
    - handshake pool switched to logging implementation
    - onHello method is no more synchronized
    - for messages {hello, features} timeout = 8s
    - optimized synchronized singleton methods
    - used ThreadFactoryBuilder
    
    Change-Id: Ic0b4ebc2df4e19793fa1d1d125a0d31dd2f9c6c8
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/queue/TicketFinisherImpl.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.openflow.md.queue;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class TicketFinisherImpl implements TicketFinisher<DataObject> {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(TicketFinisherImpl.class);
+
+    private final Map<Class<? extends DataObject>, Collection<PopListener<DataObject>>> popListenersMapping;
+
+    private boolean finished;
+
+    private BlockingQueue<TicketResult<DataObject>> queue;
+
+    /**
+     * @param queue
+     * @param popListenersMapping
+     */
+    public TicketFinisherImpl(BlockingQueue<TicketResult<DataObject>> queue,
+            Map<Class<? extends DataObject>, Collection<PopListener<DataObject>>> popListenersMapping) {
+        this.queue = queue;
+        this.popListenersMapping = popListenersMapping;
+    }
+    
+    @Override
+    public void run() {
+        while (! finished ) {
+            try {
+                //TODO:: handle shutdown of queue
+                TicketResult<DataObject> result = queue.take();
+                List<DataObject> processedMessages = result.getResult().get();
+                firePopNotification(processedMessages);
+            } catch (Exception e) {
+                LOG.warn("processing (translate, publish) of ticket failed", e);
+            }
+        }
+    }
+    
+    @Override
+    public void firePopNotification(List<DataObject> processedMessages) {
+        for (DataObject msg : processedMessages) {
+            Class<? extends Object> registeredType =
+                    msg.getImplementedInterface();
+            Collection<PopListener<DataObject>> popListeners = popListenersMapping.get(registeredType);
+            if (popListeners == null) {
+                LOG.warn("no popListener registered for type {}", registeredType);
+            } else {
+                for (PopListener<DataObject> consumer : popListeners) {
+                    consumer.onPop(msg);
+                }
+            }
+        }
+    }
+    
+    /**
+     * initiate shutdown of this worker
+     */
+    @Override
+    public void finish() {
+        finished = true;
+    }
+}
--- openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/session/MessageDispatchServiceImplTest.java
+++ openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/session/MessageDispatchServiceImplTest.java
@@ -23,7 +23,7 @@ import org.opendaylight.openflowplugin.openflow.md.ModelDrivenSwitch;
 import org.opendaylight.openflowplugin.openflow.md.core.ConnectionConductor;
 import org.opendaylight.openflowplugin.openflow.md.core.ErrorHandler;
 import org.opendaylight.openflowplugin.openflow.md.core.SwitchConnectionDistinguisher;
-import org.opendaylight.openflowplugin.openflow.md.queue.QueueKeeper;
+import org.opendaylight.openflowplugin.openflow.md.queue.QueueProcessor;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierOutput;
@@ -397,13 +397,22 @@ class MockConnectionConductor implements ConnectionConductor {
     }
 
     @Override
-    public void setQueueKeeper(QueueKeeper<OfHeader, DataObject> queueKeeper) {
+    public void setQueueProcessor(QueueProcessor<OfHeader, DataObject> queueKeeper) {
         // NOOP
     }
 
     @Override
     public void setErrorHandler(ErrorHandler errorHandler) {
         // NOOP
+    }
+
+    /* (non-Javadoc)
+     * @see org.opendaylight.openflowplugin.openflow.md.core.ConnectionConductor#setId(int)
+     */
+    @Override
+    public void setId(int conductorId) {
+        // TODO Auto-generated method stub
+        
     }
 }
 
@@ -590,5 +599,4 @@ class MockConnectionAdapter implements ConnectionAdapter {
         // TODO Auto-generated method stub
         return null;
     }
-
 }
