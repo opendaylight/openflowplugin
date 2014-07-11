37cc6417293857fa519ce9d629c1512fdc568266
Michal Rehak <mirehak@cisco.com>
2014-07-11 11:04:39 +0200

    BUG-1255: extension-api proposal
    - added converter registration, registration key, converter
    - openflowplugin depends on this api
    - separated convertors
    - fixed poms (order to satisfy dependency to extension-api)
    
    Change-Id: Id0ebefdf4aa5a07ca238d614f148194bd8e76455
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ extension/openflowplugin-extension-api/src/main/java/org/opendaylight/openflowplugin/extension/api/ConvertorToOFJava.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.extension.api;
+
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+
+/**
+ * convert message from MD-SAL model into OFJava-API model
+ * @param <FROM> input message model - MD-SAL
+ * @param <TO> output message model - OFJava-API
+ * 
+ * TODO: consider creating of specialized subinterfaces
+ */
+public interface ConvertorToOFJava<FROM extends DataContainer, TO extends DataContainer> {
+    
+    /**
+     * @param input
+     * @param sessionContext TODO: fix type when moved to API
+     * @return message converted to OFJava-API
+     */
+    TO convert(FROM input, Object sessionContext);
+}
