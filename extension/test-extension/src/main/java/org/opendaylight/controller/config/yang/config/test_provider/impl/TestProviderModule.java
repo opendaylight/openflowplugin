6c117cc05aa33f3a443d407b3f672fb187d405c1
Martin Sunal <msunal@cisco.com>
2014-07-15 07:51:49 +0200

    Changes in API and created openflowplugin-extension-general project
    - openflowplugin-extension-api was changed
    - initial commit for project openflowplugin-extension-general
    - minor changes based on ps-6
    - added augmentation extension points for actions and match (+enum)
    - civilized GroupingResolvers
    - added md-sal -> ofjava for action
    - added md-sal -> ofjava for match
    - added ofjava -> md-sal for action
    - added ofjava -> md-sal for match
    - refactor of Autocloseable to ObjectRegistration
    - plenty of TODOs created
    - openflowplugin-nicira-extension
    	match convertors:
    		nxm_nx_reg
    		nxm_nx_tun_id
    		nxm-nx-arp-sha
    		nxm-nx-arp-tha
    		nxm-of-arp-op
    		nxm-of-arp-spa
    		nxm-of-arp-tpa
    		nxm-nx-tun-ipv4-dst
    		nxm-nx-tun-ipv4-src
    		nxm-of-eth-src
    		nxm-of-eth-dst
    		nxm-of-eth-type
    	action convertors:
    		reg_load
    		reg_move
    - completed action+match roundup
    - added apply-actions-case extensions
    - added NPE protections
    - added ofjava-codecs nicira bundle to distribution
    - fixed missing type for extended messages sended to MD-SAL
    - added test-extension project (adds flow direct via rpc)
      - md-sal bug workaround for nicira action
    - fixed pom, failed merge, test-extension classes and adapted model change
    - rebased
    
    Change-Id: I766c6959b9981844b72bd4f8ed77a2fdc8017b5d
    Signed-off-by: Martin Sunal <msunal@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ extension/test-extension/src/main/java/org/opendaylight/controller/config/yang/config/test_provider/impl/TestProviderModule.java
@@ -0,0 +1,39 @@
+package org.opendaylight.controller.config.yang.config.test_provider.impl;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.openflowplugin.extension.test.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.test.rev130819.TestService;
+
+public class TestProviderModule extends org.opendaylight.controller.config.yang.config.test_provider.impl.AbstractTestProviderModule {
+    public TestProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public TestProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.test_provider.impl.TestProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        Test extTest = new Test();
+        SalFlowService flowService = getRpcRegistryDependency().getRpcService(SalFlowService.class);
+        extTest.setFlowService(flowService);
+        
+        final RpcRegistration<TestService> registry = getRpcRegistryDependency().addRpcImplementation(TestService.class, extTest);
+        
+        return new AutoCloseable() {
+            
+            @Override
+            public void close() throws Exception {
+                registry.close();
+            }
+        };
+    }
+
+}
