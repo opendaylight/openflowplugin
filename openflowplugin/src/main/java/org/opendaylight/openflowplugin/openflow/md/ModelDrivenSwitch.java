19aa178d024072ef3c9a536ffd53527fb597d435
Martin Sunal <msunal@cisco.com>
2014-08-02 19:28:40 +0200

    Stores and gets datapath id, IP address of a flow capable node
    - implements RPCs get-node-datapath-id and get-node-ip-address from flow-node-inventory.yang
    	depends on https://git.opendaylight.org/gerrit/#/c/9612/
    - impelementation is done by using switch IP address and datapath id from OFJava
    	depends on https://git.opendaylight.org/gerrit/#/c/9615/
    - stores datapath id and IP address of OF capable switch to data store (depends on Bug 1585)
    
    Signed-off-by: Martin Sunal <msunal@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/ModelDrivenSwitch.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/ModelDrivenSwitch.java
@@ -9,6 +9,7 @@ package org.opendaylight.openflowplugin.openflow.md;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.openflowplugin.openflow.md.core.session.SessionContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowNodeInventoryService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.OpendaylightFlowStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.table.statistics.rev131215.OpendaylightFlowTableStatisticsService;
@@ -43,6 +44,7 @@ public interface ModelDrivenSwitch extends //
         OpendaylightPortStatisticsService, //
         OpendaylightFlowTableStatisticsService, //
         OpendaylightQueueStatisticsService, //
+        FlowNodeInventoryService,
         Identifiable<InstanceIdentifier<Node>> {
 
     /**
