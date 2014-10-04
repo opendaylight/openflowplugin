e647cb87a2c2c10a86d7ec8897637ce08a392415
Anton Frolov <anton.frolov@pacnet.com>
2014-10-04 19:02:12 +0400

    BUG-2242: LLDP speaker as separate application.
     * Remove LLDPSpeaker and all related classes and methods from openflowplugin
     * Create separate LLDPSpeaker application
     * Replace usage of Timer with ScheduledExecutorService which use monotonic
       timer for delays instead of system time.
     * Decomposed LLDPSpeaker into several classes to improve unit testabilty
     * added lldp app to base distro
    
    Change-Id: I997c74203af7461840c1c711355120b415995c06
    Signed-off-by: Anton Frolov <anton.frolov@pacnet.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/NodeConnectorEventsObserver.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Pacnet and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.lldpspeaker;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * NodeConnectorEventsObserver can be added to NodeConnectorInventoryEventTranslator to receive events
+ * when node connector added or removed.
+ */
+public interface NodeConnectorEventsObserver {
+    /**
+     * This method is called when new node connector is added to inventory or when existing
+     * node connector changed it's status to UP. This method can be called multiple times for
+     * the same creation event.
+     *
+     * @param nodeConnectorInstanceId Object that uniquely identify added node connector
+     * @param flowConnector object containing almost all of details about node connector
+     */
+    public void nodeConnectorAdded(InstanceIdentifier<NodeConnector> nodeConnectorInstanceId,
+                                   FlowCapableNodeConnector flowConnector);
+
+    /**
+     * This method is called when some node connector is removed from inventory or when existing
+     * node connector changed it's status to DOWN. This method can be called multiple times for
+     * the same removal event.
+     * @param nodeConnectorInstanceId Object that uniquely identify added node connector
+     */
+    public void nodeConnectorRemoved(InstanceIdentifier<NodeConnector> nodeConnectorInstanceId);
+}
---
+++ applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/NodeConnectorInventoryEventTranslator.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2014 Pacnet and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.lldpspeaker;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Map;
+import java.util.Set;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * NodeConnectorInventoryEventTranslator is listening for changes in inventory operational DOM tree
+ * and update LLDPSpeaker and topology.
+ */
+public class NodeConnectorInventoryEventTranslator implements DataChangeListener, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(NodeConnectorInventoryEventTranslator.class);
+
+    private final ListenerRegistration<DataChangeListener> dataChangeListenerRegistration;
+    private final Set<NodeConnectorEventsObserver> observers;
+
+    public NodeConnectorInventoryEventTranslator(DataBroker dataBroker, NodeConnectorEventsObserver... observers) {
+        this.observers = ImmutableSet.copyOf(observers);
+        dataChangeListenerRegistration = dataBroker.registerDataChangeListener(
+                LogicalDatastoreType.OPERATIONAL,
+                InstanceIdentifier.builder(Nodes.class)
+                        .child(Node.class)
+                        .child(NodeConnector.class)
+                        .augmentation(FlowCapableNodeConnector.class)
+                        .toInstance(),
+                this, AsyncDataBroker.DataChangeScope.BASE);
+    }
+
+    @Override
+    public void close() {
+        dataChangeListenerRegistration.close();
+    }
+
+    @Override
+    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+        LOG.trace("Node connectors in inventory changed: {} created, {} updated, {} removed",
+                change.getCreatedData().size(), change.getUpdatedData().size(), change.getRemovedPaths().size());
+
+        // Iterate over created node connectors
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : change.getCreatedData().entrySet()) {
+            InstanceIdentifier<NodeConnector> nodeConnectorInstanceId =
+                    entry.getKey().firstIdentifierOf(NodeConnector.class);
+
+            FlowCapableNodeConnector flowConnector = (FlowCapableNodeConnector) entry.getValue();
+            if (!isPortDown(flowConnector)) {
+                notifyNodeConnectorAppeared(nodeConnectorInstanceId, flowConnector);
+            }
+        }
+
+        // Iterate over updated node connectors (port down state may change)
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : change.getUpdatedData().entrySet()) {
+            InstanceIdentifier<NodeConnector> nodeConnectorInstanceId =
+                    entry.getKey().firstIdentifierOf(NodeConnector.class);
+            FlowCapableNodeConnector flowConnector = (FlowCapableNodeConnector) entry.getValue();
+            if (isPortDown(flowConnector)) {
+                notifyNodeConnectorDisappeared(nodeConnectorInstanceId);
+            } else {
+                notifyNodeConnectorAppeared(nodeConnectorInstanceId, flowConnector);
+            }
+        }
+
+        // Iterate over removed node connectors
+        for (InstanceIdentifier<?> removed : change.getRemovedPaths()) {
+            InstanceIdentifier<NodeConnector> nodeConnectorInstanceId = removed.firstIdentifierOf(NodeConnector.class);
+            notifyNodeConnectorDisappeared(nodeConnectorInstanceId);
+        }
+    }
+
+    private static boolean isPortDown(FlowCapableNodeConnector flowCapableNodeConnector) {
+        PortState portState = flowCapableNodeConnector.getState();
+        PortConfig portConfig = flowCapableNodeConnector.getConfiguration();
+        return portState != null && portState.isLinkDown() ||
+                portConfig != null && portConfig.isPORTDOWN();
+    }
+
+    private void notifyNodeConnectorAppeared(InstanceIdentifier<NodeConnector> nodeConnectorInstanceId,
+                                             FlowCapableNodeConnector flowConnector) {
+        for (NodeConnectorEventsObserver observer : observers) {
+            observer.nodeConnectorAdded(nodeConnectorInstanceId, flowConnector);
+        }
+    }
+
+    private void notifyNodeConnectorDisappeared(InstanceIdentifier<NodeConnector> nodeConnectorInstanceId) {
+        for (NodeConnectorEventsObserver observer : observers) {
+            observer.nodeConnectorRemoved(nodeConnectorInstanceId);
+        }
+    }
+}
