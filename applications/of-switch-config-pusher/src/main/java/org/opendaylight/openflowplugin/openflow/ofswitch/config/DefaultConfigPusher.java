e94d9d2778478e996451ec25044aa229a2e3f8e2
Martin Bobak <mbobak@cisco.com>
2014-10-15 10:32:36 +0200

    Bug 1764 - implementation of default config pusher
    Change-Id: I6f910c3b68f6e50fb3de2e12b01fc33bb28eb80c
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ applications/of-switch-config-pusher/src/main/java/org/opendaylight/openflowplugin/openflow/ofswitch/config/DefaultConfigPusher.java
@@ -0,0 +1,52 @@
+package org.opendaylight.openflowplugin.openflow.ofswitch.config;/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+import java.util.Set;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.module.config.rev141015.NodeConfigService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.module.config.rev141015.SetConfigInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.SwitchConfigFlag;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Created by Martin Bobak mbobak@cisco.com on 10/14/14.
+ */
+public class DefaultConfigPusher implements AutoCloseable, DataChangeListener {
+
+    private NodeConfigService nodeConfigService;
+
+    public DefaultConfigPusher(NodeConfigService nodeConfigService) {
+        this.nodeConfigService = nodeConfigService;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    @Override
+    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+        final Set<InstanceIdentifier<?>> changedDataKeys = change.getCreatedData().keySet();
+
+        if (changedDataKeys != null) {
+            for (InstanceIdentifier<?> key : changedDataKeys) {
+                SetConfigInputBuilder setConfigInputBuilder = new SetConfigInputBuilder();
+                setConfigInputBuilder.setFlag(SwitchConfigFlag.FRAGNORMAL.toString());
+                setConfigInputBuilder.setMissSearchLength(OFConstants.OFPCML_NO_BUFFER);
+                setConfigInputBuilder.setNode(new NodeRef(key.firstIdentifierOf(Node.class)));
+                nodeConfigService.setConfig(setConfigInputBuilder.build());
+            }
+        }
+
+    }
+}
