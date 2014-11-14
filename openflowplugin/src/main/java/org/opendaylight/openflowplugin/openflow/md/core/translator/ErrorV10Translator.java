4e52f4e80c82e46a39db667ef9237c8ea91eb189
Shigeru Yasuda <s-yasuda@da.jp.nec.com>
2014-11-14 16:48:49 +0900

    Bug 2388: Set node reference into OFPT_ERROR notification.
    (cherry picked from commit 4107a95584fb2cd2d9f067daa44fcd1594c35195)
    Change-Id: I5b7f23c4029e8cf2c6458086c6cadc5ff22912ed
    Signed-off-by: Shigeru Yasuda <s-yasuda@da.jp.nec.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorV10Translator.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorV10Translator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2013-2014 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,6 +9,7 @@ package org.opendaylight.openflowplugin.openflow.md.core.translator;
 
 import java.math.BigInteger;
 
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.NodeErrorNotificationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.transaction.rev131103.TransactionId;
@@ -35,18 +36,20 @@ public class ErrorV10Translator extends AbstractErrorTranslator {
         }
         return type;
     }
-    
+
     /**
      * @param message
      * @param errorType
+     * @param node
      * @return translated error message of general type (OF-1.0)
      */
     @Override
-    public org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorMessage getGranularNodeErrors(ErrorMessage message, ErrorType errorType){
+    public org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorMessage getGranularNodeErrors(ErrorMessage message, ErrorType errorType, NodeRef node){
         NodeErrorNotificationBuilder nodeErrBuilder = new NodeErrorNotificationBuilder();
         nodeErrBuilder.setTransactionId(new TransactionId(BigInteger.valueOf(message.getXid())));
         nodeErrBuilder.setType(errorType);
         nodeErrBuilder.setCode(message.getCode());
+        nodeErrBuilder.setNode(node);
 
         if (message.getData() != null) {
             nodeErrBuilder.setData(new String(message.getData()));
--- openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorTranslatorTest.java
+++ openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/translator/ErrorTranslatorTest.java
@@ -1,21 +1,34 @@
 /**
- * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
- * 
+ * Copyright (c) 2013-2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.openflowplugin.openflow.md.core.translator;
 
-import java.lang.reflect.Method;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.openflowplugin.api.openflow.md.core.SwitchConnectionDistinguisher;
+import org.opendaylight.openflowplugin.openflow.md.core.session.SessionContext;
+import org.opendaylight.openflowplugin.openflow.md.util.InventoryDataServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.BaseNodeErrorNotification;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.transaction.rev131103.TransactionId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ErrorMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesOutput;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,42 +36,77 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class ErrorTranslatorTest {
-    
-    private ErrorTranslator errorTranslator;
+
+    private final ErrorTranslator errorTranslator = new ErrorTranslator();
+    private final ErrorMessageBuilder builder = new ErrorMessageBuilder();;
+    private static final BigInteger  DATAPATH_ID = BigInteger.valueOf(0x7777L);
     private static Logger LOG = LoggerFactory
             .getLogger(ErrorTranslatorTest.class);
-    
+
+    @Mock
+    SwitchConnectionDistinguisher cookie;
+    @Mock
+    SessionContext sc;
+    @Mock
+    GetFeaturesOutput features;
+
     /**
      * startup method
      */
     @Before
     public void setUp() {
-        errorTranslator = new ErrorTranslator();
+        builder.setCode(21);
+        builder.setXid(42L);
+        builder.setData(new byte[]{42});
+
+        MockitoAnnotations.initMocks(this);
+        when(sc.getFeatures()).thenReturn(features);
+        when(features.getDatapathId()).thenReturn(DATAPATH_ID);
     }
 
     /**
-     * Test method for {@link org.opendaylight.openflowplugin.openflow.md.core.translator.ErrorTranslator#getGranularNodeErrors(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ErrorMessage, org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorType)}.
-     * @throws Exception 
+     * Test method for
+     * {@link AbstractErrorTranslator#translate(SwitchConnectionDistinguisher, SessionContext, org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader)}.
+     */
+    @Test
+    public void testTranslate() {
+        builder.setType(1);
+        List<DataObject> data = errorTranslator.translate(cookie, sc, builder.build());
+        assertNotNull(data);
+        Assert.assertEquals(1, data.size());
+        DataObject obj = data.get(0);
+        Assert.assertTrue(obj instanceof BaseNodeErrorNotification);
+        BaseNodeErrorNotification nodeError = (BaseNodeErrorNotification)obj;
+        NodeRef expectedNode = new NodeRef(
+            InventoryDataServiceUtil.identifierFromDatapathId(DATAPATH_ID));
+        Assert.assertEquals(expectedNode, nodeError.getNode());
+    }
+
+    /**
+     * Test method for {@link org.opendaylight.openflowplugin.openflow.md.core.translator.ErrorTranslator#getGranularNodeErrors(org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ErrorMessage, org.opendaylight.yang.gen.v1.urn.opendaylight.flow.errors.rev131116.ErrorType, NodeRef)}.
+     *
+     * @throws Exception
      */
     @Test
     public void testGetGranularNodeErrors() throws Exception {
+        BigInteger dpid = BigInteger.valueOf(0x1122334455667788L);
+        NodeRef node = new NodeRef(
+            InventoryDataServiceUtil.identifierFromDatapathId(dpid));
         for (ErrorType eType : ErrorType.values()) {
-            ErrorMessageBuilder builder = new ErrorMessageBuilder();
             builder.setType(eType.getIntValue());
-            builder.setCode(21);
-            builder.setXid(42L);
-            builder.setData(new byte[]{42});
-
-            ErrorMessage errorMessage = errorTranslator.getGranularNodeErrors(builder.build(), eType);
+            ErrorMessage errorMessage = errorTranslator.getGranularNodeErrors(builder.build(), eType, node);
             LOG.debug("translating errorMessage of type {}", eType);
-            Assert.assertNotNull("translated error is null", errorMessage);
+            assertNotNull("translated error is null", errorMessage);
             Assert.assertEquals(21, errorMessage.getCode().intValue());
             Assert.assertEquals(eType, errorMessage.getType());
+            Method getNode = errorMessage.getClass().getMethod("getNode");
+            getNode.setAccessible(true);
+            Assert.assertEquals(node, getNode.invoke(errorMessage));
             Method getXid = errorMessage.getClass().getMethod("getTransactionId", new Class[0]);
             getXid.setAccessible(true);
             TransactionId xid = (TransactionId) getXid.invoke(errorMessage, new Object[0]);
             Assert.assertEquals(42L, xid.getValue().longValue());
-            Assert.assertNotNull("data is null", errorMessage.getData());
+            assertNotNull("data is null", errorMessage.getData());
         }
     }
 
