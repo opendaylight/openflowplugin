b272951c6545d3b92fa6d3c26dc4d5e241eca207
Martin Bobak <mbobak@cisco.com>
2014-11-21 11:13:42 +0100

    Bug 2428 - add rpc method for LLDP speaker to configure its operational status
    Change-Id: I91e0246b47ad74e94b2bd3c14ed521b29607d43d
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- applications/lldp-speaker/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/openflow/applications/lldp/speaker/rev141023/LLDPSpeakerModule.java
+++ applications/lldp-speaker/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/openflow/applications/lldp/speaker/rev141023/LLDPSpeakerModule.java
@@ -4,6 +4,7 @@ import org.opendaylight.controller.config.api.DependencyResolver;
 import org.opendaylight.controller.config.api.ModuleIdentifier;
 import org.opendaylight.openflowplugin.applications.lldpspeaker.LLDPSpeaker;
 import org.opendaylight.openflowplugin.applications.lldpspeaker.NodeConnectorInventoryEventTranslator;
+import org.opendaylight.openflowplugin.applications.lldpspeaker.OperationalStatusChangeService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,7 +36,8 @@ public class LLDPSpeakerModule extends AbstractLLDPSpeakerModule {
         final LLDPSpeaker lldpSpeaker = new LLDPSpeaker(packetProcessingService);
         final NodeConnectorInventoryEventTranslator eventTranslator =
                 new NodeConnectorInventoryEventTranslator(getDataBrokerDependency(), lldpSpeaker);
-
+        OperationalStatusChangeService operationalStatusChangeService = new OperationalStatusChangeService(lldpSpeaker);
+        getRpcRegistryDependency().addRpcImplementation(LldpSpeakerService.class, operationalStatusChangeService);
         return new AutoCloseable() {
             @Override
             public void close() {
--- applications/lldp-speaker/src/test/java/org/opendaylight/openflowplugin/applications/lldpspeaker/LLDPSpeakerTest.java
+++ applications/lldp-speaker/src/test/java/org/opendaylight/openflowplugin/applications/lldpspeaker/LLDPSpeakerTest.java
@@ -29,6 +29,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.N
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.OperStatus;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
@@ -64,6 +65,24 @@ public class LLDPSpeakerTest {
                 any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class)))
                 .thenReturn(scheduledSpeakerTask);
         lldpSpeaker = new LLDPSpeaker(packetProcessingService, scheduledExecutorService);
+        lldpSpeaker.setOperationalStatus(OperStatus.RUN);
+    }
+
+    /**
+     * Test that speaker does nothing when in {@link OperStatus.STANDBY} mode.
+     */
+    @Test
+    public void testStandBy() {
+        lldpSpeaker.setOperationalStatus(OperStatus.STANDBY);
+        // Add node connector - LLDP packet should be transmitted through packetProcessingService
+        lldpSpeaker.nodeConnectorAdded(id, fcnc);
+
+        // Execute one iteration of periodic task - LLDP packet should be transmitted second time
+        lldpSpeaker.run();
+
+        // Check packet transmission
+        verify(packetProcessingService, times(1)).transmitPacket(packet);
+        verifyNoMoreInteractions(packetProcessingService);
     }
 
     /**
