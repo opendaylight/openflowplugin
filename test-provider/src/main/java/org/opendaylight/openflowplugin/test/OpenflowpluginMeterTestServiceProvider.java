8d93ad4e9894685ae2faeaf2e9f6e9c9ddd580d8
ary <jatoth@cisco.com>
2014-12-17 11:19:07 -0500

    bug 2493 - eliminate the use of xtend and remove dependecies
    Signed-off-by: ary <jatoth@cisco.com>

---
+++ test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginMeterTestServiceProvider.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.test;
+
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RoutedRpcRegistration;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.SalMeterService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterOutput;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration.CompositeObjectRegistrationBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenflowpluginMeterTestServiceProvider implements AutoCloseable,
+        SalMeterService {
+    private final static Logger LOG = LoggerFactory
+            .getLogger(OpenflowpluginMeterTestServiceProvider.class);
+    private DataBroker dataService;
+    private RoutedRpcRegistration<SalMeterService> meterRegistration;
+    private NotificationProviderService notificationService;
+
+    /**
+     * get data service
+     * 
+     * @return {@link #dataService}
+     */
+    public DataBroker getDataService() {
+        return this.dataService;
+    }
+
+    /**
+     * set {@link #dataService}
+     * 
+     * @param dataService
+     */
+    public void setDataService(final DataBroker dataService) {
+        this.dataService = dataService;
+    }
+
+    /**
+     * get meter registration
+     * 
+     * @return {@link #meterRegistration}
+     */
+    public RoutedRpcRegistration<SalMeterService> getMeterRegistration() {
+        return this.meterRegistration;
+    }
+
+    /**
+     * set {@link #meterRegistration}
+     * 
+     * @param meterRegistration
+     */
+    public void setMeterRegistration(
+            final RoutedRpcRegistration<SalMeterService> meterRegistration) {
+        this.meterRegistration = meterRegistration;
+    }
+
+    /**
+     * get notification service
+     * 
+     * @return {@link #notificationService}
+     */
+    public NotificationProviderService getNotificationService() {
+        return this.notificationService;
+    }
+
+    /**
+     * set {@link #notificationService}
+     * 
+     * @param notificationService
+     */
+    public void setNotificationService(
+            final NotificationProviderService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void start() {
+        OpenflowpluginMeterTestServiceProvider.LOG
+                .info("SalMeterServiceProvider Started.");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.AutoCloseable#close()
+     */
+    public void close() {
+        OpenflowpluginMeterTestServiceProvider.LOG
+                .info("SalMeterServiceProvide stopped.");
+        RoutedRpcRegistration<SalMeterService> meterRegistration = this
+                .getMeterRegistration();
+        meterRegistration.close();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918
+     * .SalMeterService
+     * #addMeter(org.opendaylight.yang.gen.v1.urn.opendaylight.meter
+     * .service.rev130918.AddMeterInput)
+     */
+    public Future<RpcResult<AddMeterOutput>> addMeter(final AddMeterInput input) {
+        String plus = ("addMeter - " + input);
+        OpenflowpluginMeterTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918
+     * .SalMeterService
+     * #removeMeter(org.opendaylight.yang.gen.v1.urn.opendaylight
+     * .meter.service.rev130918.RemoveMeterInput)
+     */
+    public Future<RpcResult<RemoveMeterOutput>> removeMeter(
+            final RemoveMeterInput input) {
+        String plus = ("removeMeter - " + input);
+        OpenflowpluginMeterTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918
+     * .SalMeterService
+     * #updateMeter(org.opendaylight.yang.gen.v1.urn.opendaylight
+     * .meter.service.rev130918.UpdateMeterInput)
+     */
+    public Future<RpcResult<UpdateMeterOutput>> updateMeter(
+            final UpdateMeterInput input) {
+        String plus = ("updateMeter - " + input);
+        OpenflowpluginMeterTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /**
+     * @param ctx
+     * @return {@link CompositeObjectRegistrationBuilder #toInstance()}
+     */
+    public CompositeObjectRegistration<OpenflowpluginMeterTestServiceProvider> register(
+            final ProviderContext ctx) {
+        CompositeObjectRegistrationBuilder<OpenflowpluginMeterTestServiceProvider> builder = CompositeObjectRegistration
+                .<OpenflowpluginMeterTestServiceProvider> builderFor(this);
+
+        RoutedRpcRegistration<SalMeterService> addRoutedRpcImplementation = ctx
+                .<SalMeterService> addRoutedRpcImplementation(
+                        SalMeterService.class, this);
+
+        setMeterRegistration(addRoutedRpcImplementation);
+
+        InstanceIdentifierBuilder<Nodes> builder1 = InstanceIdentifier
+                .<Nodes> builder(Nodes.class);
+
+        NodeId nodeId = new NodeId(OpenflowpluginTestActivator.NODE_ID);
+        NodeKey nodeKey = new NodeKey(nodeId);
+
+        InstanceIdentifierBuilder<Node> nodeIndentifier = builder1
+                .<Node, NodeKey> child(Node.class, nodeKey);
+
+        RoutedRpcRegistration<SalMeterService> meterRegistration = this
+                .getMeterRegistration();
+
+        InstanceIdentifier<Node> instance = nodeIndentifier.toInstance();
+
+        meterRegistration.registerPath(NodeContext.class, instance);
+
+        RoutedRpcRegistration<SalMeterService> meterRegistration1 = this
+                .getMeterRegistration();
+
+        builder.add(meterRegistration1);
+
+        return builder.toInstance();
+    }
+
+}
---
+++ test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginTestServiceProvider.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.test;
+
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RoutedRpcRegistration;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration.CompositeObjectRegistrationBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenflowpluginTestServiceProvider implements AutoCloseable,
+        SalFlowService {
+
+    private final static Logger LOG = LoggerFactory
+            .getLogger(OpenflowpluginTestServiceProvider.class);
+
+    private DataBroker dataService;
+    private RoutedRpcRegistration<SalFlowService> flowRegistration;
+    private NotificationProviderService notificationProviderService;
+
+    /**
+     * get data service
+     * 
+     * @return {@link #dataService}
+     */
+    public DataBroker getDataService() {
+        return dataService;
+    }
+
+    /**
+     * set {@link #dataService}
+     * 
+     * @param dataService
+     */
+    public void setDataService(final DataBroker dataService) {
+        this.dataService = dataService;
+    }
+
+    /**
+     * get flow registration
+     * 
+     * @return {@link #flowRegistration}
+     */
+    public RoutedRpcRegistration<SalFlowService> getFlowRegistration() {
+        return flowRegistration;
+    }
+
+    /**
+     * set {@link #flowRegistration}
+     * 
+     * @param flowRegistration
+     */
+    public void setFlowRegistration(
+            final RoutedRpcRegistration<SalFlowService> flowRegistration) {
+        this.flowRegistration = flowRegistration;
+    }
+
+    /**
+     * get notification service
+     * 
+     * @return {@link #notificationProviderService}
+     */
+    public NotificationProviderService getNotificationService() {
+        return notificationProviderService;
+    }
+
+    /**
+     * set {@link #notificationProviderService}
+     * 
+     * @param notificationProviderService
+     */
+    public void setNotificationService(
+            final NotificationProviderService notificationProviderService) {
+        this.notificationProviderService = notificationProviderService;
+    }
+
+    public void start() {
+        OpenflowpluginTestServiceProvider.LOG
+                .info("SalFlowServiceProvider Started.");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.AutoCloseable#close()
+     */
+    @Override
+    public void close() throws Exception {
+        OpenflowpluginTestServiceProvider.LOG
+                .info("SalFlowServiceProvide stopped.");
+        RoutedRpcRegistration<SalFlowService> flowRegistration = getFlowRegistration();
+        flowRegistration.close();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
+     * SalFlowService
+     * #addFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow.
+     * service.rev130819.AddFlowInput)
+     */
+    @Override
+    public Future<RpcResult<AddFlowOutput>> addFlow(AddFlowInput input) {
+        String plus = ("addFlow - " + input);
+        OpenflowpluginTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
+     * SalFlowService
+     * #removeFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow
+     * .service.rev130819.RemoveFlowInput)
+     */
+    @Override
+    public Future<RpcResult<RemoveFlowOutput>> removeFlow(RemoveFlowInput input) {
+        String plus = ("removeFlow - " + input);
+        OpenflowpluginTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
+     * SalFlowService
+     * #updateFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow
+     * .service.rev130819.UpdateFlowInput)
+     */
+    @Override
+    public Future<RpcResult<UpdateFlowOutput>> updateFlow(UpdateFlowInput input) {
+        String plus = ("updateFlow - " + input);
+        OpenflowpluginTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /**
+     * @param ctx
+     * @return {@link CompositeObjectRegistrationBuilder #toInstance()}
+     */
+    public CompositeObjectRegistration<OpenflowpluginTestServiceProvider> register(
+            final ProviderContext ctx) {
+        CompositeObjectRegistrationBuilder<OpenflowpluginTestServiceProvider> builder = CompositeObjectRegistration
+                .<OpenflowpluginTestServiceProvider> builderFor(this);
+
+        RoutedRpcRegistration<SalFlowService> addRoutedRpcImplementation = ctx
+                .<SalFlowService> addRoutedRpcImplementation(
+                        SalFlowService.class, this);
+
+        setFlowRegistration(addRoutedRpcImplementation);
+
+        InstanceIdentifierBuilder<Nodes> builderII = InstanceIdentifier
+                .<Nodes> builder(Nodes.class);
+
+        NodeId nodeId = new NodeId(OpenflowpluginTestActivator.NODE_ID);
+        NodeKey nodeKey = new NodeKey(nodeId);
+
+        InstanceIdentifierBuilder<Node> nodeIdentifier = builderII
+                .<Node, NodeKey> child(Node.class, nodeKey);
+
+        RoutedRpcRegistration<SalFlowService> flowRegistration = getFlowRegistration();
+        InstanceIdentifier<Node> instance = nodeIdentifier.toInstance();
+
+        flowRegistration.registerPath(NodeContext.class, instance);
+
+        RoutedRpcRegistration<SalFlowService> flowRegistration2 = getFlowRegistration();
+
+        builder.add(flowRegistration2);
+
+        return builder.toInstance();
+    }
+}
