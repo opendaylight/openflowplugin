f36d89a0a6931a1175bb4300276101d920e1fa8a
ary <jatoth@cisco.com>
2014-12-17 11:19:07 -0500

    bug 2493 - eliminate the use of xtend and remove dependecies
    Signed-off-by: ary <jatoth@cisco.com>
    Change-Id: I2b2c49d93daf90c90e024d3af727f87957a8427e

---
+++ test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginGroupTestServiceProvider.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.test;
+
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RoutedRpcRegistration;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.AddGroupInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.AddGroupOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.RemoveGroupInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.RemoveGroupOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.SalGroupService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.UpdateGroupInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.UpdateGroupOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration.CompositeObjectRegistrationBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenflowpluginGroupTestServiceProvider implements AutoCloseable,
+        SalGroupService {
+
+    private final static Logger LOG = LoggerFactory
+            .getLogger(OpenflowpluginGroupTestServiceProvider.class);
+    private RoutedRpcRegistration<SalGroupService> groupRegistration;
+    private NotificationProviderService notificationService;
+
+    /**
+     * get group registration
+     * 
+     * @return {@link #groupRegistration}
+     */
+    public RoutedRpcRegistration<SalGroupService> getGroupRegistration() {
+        return groupRegistration;
+    }
+
+    /**
+     * set {@link #groupRegistration}
+     * 
+     * @param groupRegistration
+     */
+    public void setGroupRegistration(
+            final RoutedRpcRegistration<SalGroupService> groupRegistration) {
+        this.groupRegistration = groupRegistration;
+    }
+
+    /**
+     * get notification service
+     * 
+     * @return {@link #notificationService}
+     */
+    public NotificationProviderService getNotificationService() {
+        return notificationService;
+    }
+
+    /**
+     * set {@link #notificationService}
+     * 
+     * @param notificationService
+     */
+    public void setNotificationService(
+            final NotificationProviderService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void start() {
+        OpenflowpluginGroupTestServiceProvider.LOG
+                .info("SalGroupServiceProvider Started.");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.AutoCloseable#close()
+     */
+    @Override
+    public void close() throws Exception {
+        OpenflowpluginGroupTestServiceProvider.LOG
+                .info("SalGroupServiceProvide stopped.");
+        groupRegistration.close();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918
+     * .SalGroupService
+     * #addGroup(org.opendaylight.yang.gen.v1.urn.opendaylight.group
+     * .service.rev130918.AddGroupInput)
+     */
+    @Override
+    public Future<RpcResult<AddGroupOutput>> addGroup(AddGroupInput input) {
+        String plus = ("addGroup - " + input);
+        OpenflowpluginGroupTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918
+     * .SalGroupService
+     * #removeGroup(org.opendaylight.yang.gen.v1.urn.opendaylight
+     * .group.service.rev130918.RemoveGroupInput)
+     */
+    @Override
+    public Future<RpcResult<RemoveGroupOutput>> removeGroup(
+            RemoveGroupInput input) {
+        String plus = ("removeGroup - " + input);
+        OpenflowpluginGroupTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918
+     * .SalGroupService
+     * #updateGroup(org.opendaylight.yang.gen.v1.urn.opendaylight
+     * .group.service.rev130918.UpdateGroupInput)
+     */
+    @Override
+    public Future<RpcResult<UpdateGroupOutput>> updateGroup(
+            UpdateGroupInput input) {
+        String plus = ("updateGroup - " + input);
+        OpenflowpluginGroupTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /**
+     * @param ctx
+     * @return {@link CompositeObjectRegistrationBuilder #toInstance()}
+     */
+    public CompositeObjectRegistration<OpenflowpluginGroupTestServiceProvider> register(
+            final ProviderContext ctx) {
+        CompositeObjectRegistrationBuilder<OpenflowpluginGroupTestServiceProvider> builder = CompositeObjectRegistration
+                .<OpenflowpluginGroupTestServiceProvider> builderFor(this);
+
+        RoutedRpcRegistration<SalGroupService> addRoutedRpcImplementation = ctx
+                .<SalGroupService> addRoutedRpcImplementation(
+                        SalGroupService.class, this);
+        setGroupRegistration(addRoutedRpcImplementation);
+
+        InstanceIdentifierBuilder<Nodes> builder1 = InstanceIdentifier
+                .<Nodes> builder(Nodes.class);
+
+        NodeId nodeId = new NodeId(OpenflowpluginTestActivator.NODE_ID);
+        NodeKey nodeKey = new NodeKey(nodeId);
+
+        InstanceIdentifierBuilder<Node> nodeIndentifier = builder1
+                .<Node, NodeKey> child(Node.class, nodeKey);
+        InstanceIdentifier<Node> instance = nodeIndentifier.build();
+        groupRegistration.registerPath(NodeContext.class, instance);
+        RoutedRpcRegistration<SalGroupService> groupRegistration1 = this
+                .getGroupRegistration();
+        builder.add(groupRegistration1);
+        return builder.build();
+    }
+
+}
---
+++ test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginTableFeaturesTestServiceProvider.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.test;
+
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RoutedRpcRegistration;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableOutput;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration;
+import org.opendaylight.yangtools.concepts.CompositeObjectRegistration.CompositeObjectRegistrationBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpenflowpluginTableFeaturesTestServiceProvider implements
+        AutoCloseable, SalTableService {
+
+    private final static Logger LOG = LoggerFactory
+            .getLogger(OpenflowpluginTableFeaturesTestServiceProvider.class);
+    private RoutedRpcRegistration<SalTableService> tableRegistration;
+    private NotificationProviderService notificationService;
+
+    /**
+     * get table registration
+     * 
+     * @return {@link #tableRegistration}
+     */
+    public RoutedRpcRegistration<SalTableService> getTableRegistration() {
+        return this.tableRegistration;
+    }
+
+    /**
+     * set {@link #tableRegistration}
+     * 
+     * @param tableRegistration
+     */
+    public void setTableRegistration(
+            final RoutedRpcRegistration<SalTableService> tableRegistration) {
+        this.tableRegistration = tableRegistration;
+    }
+
+    /**
+     * get notification service
+     * 
+     * @return {@link #notificationService}
+     */
+    public NotificationProviderService getNotificationService() {
+        return this.notificationService;
+    }
+
+    /**
+     * set {@link #notificationService}
+     * 
+     * @param notificationService
+     */
+    public void setNotificationService(
+            final NotificationProviderService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void start() {
+        OpenflowpluginTableFeaturesTestServiceProvider.LOG
+                .info("SalTableServiceProvider Started.");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.AutoCloseable#close()
+     */
+    @Override
+    public void close() throws Exception {
+        OpenflowpluginTableFeaturesTestServiceProvider.LOG
+                .info("SalTableServiceProvider stopped.");
+        tableRegistration.close();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026
+     * .SalTableService
+     * #updateTable(org.opendaylight.yang.gen.v1.urn.opendaylight
+     * .table.service.rev131026.UpdateTableInput)
+     */
+    @Override
+    public Future<RpcResult<UpdateTableOutput>> updateTable(
+            UpdateTableInput input) {
+        String plus = ("updateTable - " + input);
+        OpenflowpluginTableFeaturesTestServiceProvider.LOG.info(plus);
+        return null;
+    }
+
+    /**
+     * @param ctx
+     * @return {@link CompositeObjectRegistrationBuilder #toInstance()}
+     */
+    public CompositeObjectRegistration<OpenflowpluginTableFeaturesTestServiceProvider> register(
+            final ProviderContext ctx) {
+        CompositeObjectRegistrationBuilder<OpenflowpluginTableFeaturesTestServiceProvider> builder = CompositeObjectRegistration
+                .<OpenflowpluginTableFeaturesTestServiceProvider> builderFor(this);
+
+        RoutedRpcRegistration<SalTableService> addRoutedRpcImplementation = ctx
+                .<SalTableService> addRoutedRpcImplementation(
+                        SalTableService.class, this);
+
+        setTableRegistration(addRoutedRpcImplementation);
+
+        InstanceIdentifierBuilder<Nodes> builder1 = InstanceIdentifier
+                .<Nodes> builder(Nodes.class);
+
+        NodeId nodeId = new NodeId(OpenflowpluginTestActivator.NODE_ID);
+        NodeKey nodeKey = new NodeKey(nodeId);
+
+        InstanceIdentifierBuilder<Node> nodeIndentifier = builder1
+                .<Node, NodeKey> child(Node.class, nodeKey);
+
+        InstanceIdentifier<Node> instance = nodeIndentifier.build();
+
+        tableRegistration.registerPath(NodeContext.class, instance);
+
+        RoutedRpcRegistration<SalTableService> tableRegistration1 = this
+                .getTableRegistration();
+
+        builder.add(tableRegistration1);
+
+        return builder.build();
+    }
+
+}
