ce2084a97861d70279b9e166e8222e6925a8dfda
Martin Bobak <mbobak@cisco.com>
2015-02-25 18:26:36 +0100

    Bug 2753 - 4 managers api proposal
    - proposal for new API managers
    - HandshakeManager code cleanup
    - added handshakeContext
    - minor cleanup in connectionContext
    - added features to connectionContext
    
    Change-Id: Iba6b90247c54affa3be326f98f93b00d763b5402
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/connection/ConnectionContext.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.connection;
+
+import org.opendaylight.openflowjava.protocol.api.connection.ConnectionAdapter;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
+
+/**
+ * Each OpenFlow session is tracked by a Connection Context. These attach to a particular Device Context in such a way,
+ * that there is at most one primary session associated with a Device Context.
+ * <p/>
+ * Created by Martin Bobak <mbobak@cisco.com> on 25.2.2015.
+ */
+public interface ConnectionContext {
+
+    /**
+     * distinguished connection states
+     */
+    enum CONNECTION_STATE {
+        /**
+         * initial phase of talking to switch
+         */
+        HANDSHAKING,
+        /**
+         * standard phase - interacting with switch
+         */
+        WORKING,
+        /**
+         * connection is idle, waiting for echo reply from switch
+         */
+        TIMEOUTING,
+        /**
+         * talking to switch is over - resting in pieces
+         */
+        RIP
+    }
+
+    /**
+     * Method returns identifier of device whic connection represents this context.
+     *
+     * @return {@link org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId}
+     */
+    NodeId getNodeId();
+
+    /**
+     * @return the connectionAdapter
+     */
+    ConnectionAdapter getConnectionAdapter();
+
+
+    /**
+     * Method returns current connection state.
+     *
+     * @return {@link ConnectionContext.CONNECTION_STATE}
+     */
+    CONNECTION_STATE getConnectionState();
+
+    /**
+     * Method sets connection state of current context.
+     *
+     * @param connectionState
+     */
+    void setConnectionState(CONNECTION_STATE connectionState);
+    
+    /**
+     * @param featuresReply as received from device during handshake
+     */
+    void setFeatures(FeaturesReply featuresReply);
+    
+    /**
+     * @return featureReply as received from device during handshake
+     */
+    FeaturesReply getFeatures();
+
+}
---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/handlers/DeviceSynchronizedHandler.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.device.handlers;
+
+/**
+ * This handler does the same, but is not called in the same time as
+ * RequestContextReadyHandler.
+ *
+ * Created by Martin Bobak <mbobak@cisco.com> on 27.2.2015.
+ */
+
+public interface DeviceSynchronizedHandler extends DeviceContextReadyHandler {
+
+}
