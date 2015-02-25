427788daf90d3d9101b6fc9511d9f38c3c5350e4
Martin Bobak <mbobak@cisco.com>
2015-02-25 18:26:36 +0100

    Bug 2753 - 4 managers api proposal
    - proposal for new API managers
    - HandshakeManager code cleanup
    
    Change-Id: Iba6b90247c54affa3be326f98f93b00d763b5402
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/device/DeviceContext.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.md.core.device;
+
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
+import org.opendaylight.openflowplugin.api.openflow.md.core.connection.ConnectionContext;
+import org.opendaylight.openflowplugin.api.openflow.md.core.device.handlers.MessageHandler;
+import org.opendaylight.openflowplugin.api.openflow.md.core.device.handlers.DeviceContextReadyHandler;
+import org.opendaylight.openflowplugin.api.openflow.md.core.rpc.RpcContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.types.rev131026.TableFeatures;
+
+/**
+ * The central entity of OFP is the Device Context, which encapsulate the logical state of a switch
+ * as seen by the controller. Each OpenFlow session is tracked by a Connection Context.
+ * These attach to a particular Device Context in such a way, that there is at most one primary
+ * session associated with a Device Context. Whenever the controller needs to interact with a
+ * particular switch, it will do so in the context of the calling thread, obtaining a lock on
+ * the corresponding Device Context – thus the Device Context becomes the fine-grained point
+ * of synchronization. The only two entities allowed to send requests towards the switch are
+ * Statistics Manager and RPC Manager. Each of them allocates a Request Context for interacting
+ * with a particular Device Context. The Request Contexts are the basic units of fairness,
+ * which is enforced by keeping a cap on the number of outstanding requests a particular Request
+ * Context can have at any point in time. Should this quota be exceeded, any further attempt to make
+ * a request to the switch will fail immediately, with proper error indication.
+ * <p/>
+ * Created by Martin Bobak <mbobak@cisco.com> on 25.2.2015.
+ */
+public interface DeviceContext extends MessageHandler {
+
+
+    /**
+     * Method add auxiliary connection contexts to this context representing single device connection.
+     *
+     * @param connectionContext
+     */
+    void addAuxiliaryConenctionContext(ConnectionContext connectionContext);
+
+    /**
+     * Method removes auxiliary connection context from this context representing single device connection.
+     *
+     * @param connectionContext
+     */
+    void removeAuxiliaryConenctionContext(ConnectionContext connectionContext);
+
+
+    /**
+     * Method provides state of device represented by this device context.
+     *
+     * @return {@link org.opendaylight.openflowplugin.api.openflow.md.core.device.DeviceState}
+     */
+    DeviceState getDeviceState();
+
+    /**
+     * Method sets transaction chain used for all data store operations on device
+     * represented by this context. This transaction chain is write only.
+     *
+     * @param transactionChain
+     */
+    void setTransactionChain(TransactionChain transactionChain);
+
+    /**
+     * Method exposes transaction created for device
+     * represented by this context. This should be used as write only.
+     */
+    TransactionChain getTransactionChain();
+
+    /**
+     * Method provides capabilities of connected device.
+     *
+     * @return
+     */
+    TableFeatures getCapabilities();
+
+}
+
---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/rpc/RpcContext.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.api.openflow.md.core.rpc;
+
+import org.opendaylight.openflowplugin.api.openflow.md.core.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.md.core.device.RequestContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.SalGroupService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.SalMeterService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.service.rev131107.SalPortService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
+import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.RpcService;
+
+/**
+ * This context is registered with MD-SAL as a routed RPC provider for the inventory node backed by
+ * this switch and tracks the state of any user requests and how they map onto protocol requests. It
+ * uses {@link org.opendaylight.openflowplugin.api.openflow.md.core.device.RequestContext} to perform
+ * requests.
+ * <p/>
+ * Created by Martin Bobak <mbobak@cisco.com> on 25.2.2015.
+ */
+public interface RpcContext {
+
+    <S extends RpcService> void registerRpcServiceImplementation(Class<S> serviceClass, S serviceInstance);
+
+    void setRequestContext(RequestContext requestContext);
+
+    void setDeviceContext(DeviceContext deviceContext);
+
+}
