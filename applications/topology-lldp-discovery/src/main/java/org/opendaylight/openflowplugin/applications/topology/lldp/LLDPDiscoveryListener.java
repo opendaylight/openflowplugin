bb7fd6285d2457c4c0175a58f72751f4d869708e
Jozef Gloncak <jgloncak@cisco.com>
2015-03-17 14:35:57 +0100

    BUG 2723 - Topology spoofing via LLDP - hash check in topology-discovery
    Checking of CustomSec (TLV field in LLDP packet). Value of CustomSec from LLDP
    packet has to be equal to hash value which is computed in
    topology-lldp-discovery artifact. Hash value is obtained as MD5 value
    calculated from concatenation of strings:
     - node connector ID
     - pseudo PID of running JAVA karaf
    
    Method getValueForLLDPPacketIntegrityEnsuring() prepare array of bytes which
    will be after hashing used to check integrity of LLDP packets. Ensuring that
    LLDP packet wasn't modified. (extra authenticator; CVE-2015-1611 CVE-2015-1612)
    
    Change-Id: Ic8f50c88e7d8e3722d8d83a01ffa94a96bde313f
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    (cherry picked from commit 4951c6a8106fa404e3715a7d2addd0c838cf90bc)

--- applications/topology-lldp-discovery/src/main/java/org/opendaylight/openflowplugin/applications/topology/lldp/LLDPDiscoveryListener.java
+++ applications/topology-lldp-discovery/src/main/java/org/opendaylight/openflowplugin/applications/topology/lldp/LLDPDiscoveryListener.java
@@ -28,7 +28,7 @@ class LLDPDiscoveryListener implements PacketProcessingListener {
     }
 
     public void onPacketReceived(PacketReceived lldp) {
-        NodeConnectorRef src = LLDPDiscoveryUtils.lldpToNodeConnectorRef(lldp.getPayload());
+        NodeConnectorRef src = LLDPDiscoveryUtils.lldpToNodeConnectorRef(lldp.getPayload(), true);
         if(src != null) {
             LinkDiscoveredBuilder ldb = new LinkDiscoveredBuilder();
             ldb.setDestination(lldp.getIngress());
