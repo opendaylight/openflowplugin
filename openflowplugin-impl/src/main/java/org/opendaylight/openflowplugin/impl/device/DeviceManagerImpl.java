8359cdf99e3890a236a2cd61d86052c5e9898304
Michal Rehak <mirehak@cisco.com>
2015-03-21 18:59:25 +0100

    DeviceContext proposal
     - initial test
    
    Change-Id: I703b94d8dcc19c720175d02a86d747fca08af47f
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceManagerImpl.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.impl.device;
+
+import java.util.concurrent.ExecutionException;
+
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceManager;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
+import org.opendaylight.openflowplugin.api.openflow.device.Xid;
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceContextReadyHandler;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartRequestFlags;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartRequestInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.MultipartRequestDescCaseBuilder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * 
+ */
+public class DeviceManagerImpl implements DeviceManager {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(DeviceManagerImpl.class);
+    
+    private Xid xid;
+
+    @Override
+    public void deviceConnected(ConnectionContext connectionContext) {
+        xid = new Xid(0L);
+        DeviceContextImpl deviceContextImpl = new DeviceContextImpl();
+
+        try {
+            FlowCapableNode description = queryDescription(connectionContext, xid.getNextValue()).get();
+            
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            LOG.info("Failed to retrieve node static info: {}", e.getMessage());
+        }
+    }
+
+    /**
+     * @param connectionContext 
+     * @param nextXid 
+     */
+    private static ListenableFuture<FlowCapableNode> queryDescription(ConnectionContext connectionContext, long nextXid) {
+        MultipartRequestInputBuilder builder = new MultipartRequestInputBuilder();
+        builder.setType(MultipartType.OFPMPDESC);
+        builder.setVersion(connectionContext.getFeatures().getVersion());
+        builder.setFlags(new MultipartRequestFlags(false));
+        builder.setMultipartRequestBody(new MultipartRequestDescCaseBuilder()
+                .build());
+        builder.setXid(nextXid);
+        connectionContext.getConnectionAdapter().multipartRequest(builder.build());
+        
+        //TODO: involve general wait-for-answer mechanism and return future with complete value
+        //TODO: translate message
+        return Futures.immediateFuture(null); 
+    }
+
+    @Override
+    public void sendMessage(DataObject dataObject, RequestContext requestContext) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Xid sendRequest(DataObject dataObject, RequestContext requestContext) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addRequestContextReadyHandler(DeviceContextReadyHandler deviceContextReadyHandler) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
