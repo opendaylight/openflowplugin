030ecca1c14e7a986e8faa91258e50c4b2ca1445
Martin Bobak <mbobak@cisco.com>
2015-03-23 07:43:32 +0100

    Rpc
    adding RpcContextImpl and RpcManagerImpl
    
    Change-Id: I57ba3d71f3b3bddcf40b6527fe76b34d97eb376e
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/OpendaylightPortStatisticsServiceImpl.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.api.openflow.device;
+
+import java.util.concurrent.Future;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatisticsInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatisticsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatisticsInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatisticsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * @author joe
+ * 
+ */
+// TODO: implement this
+public class OpendaylightPortStatisticsServiceImpl extends CommonService implements OpendaylightPortStatisticsService {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService#
+     * getAllNodeConnectorsStatistics
+     * (org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatisticsInput)
+     */
+    @Override
+    public Future<RpcResult<GetAllNodeConnectorsStatisticsOutput>> getAllNodeConnectorsStatistics(
+            final GetAllNodeConnectorsStatisticsInput input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService#
+     * getNodeConnectorStatistics
+     * (org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatisticsInput)
+     */
+    @Override
+    public Future<RpcResult<GetNodeConnectorStatisticsOutput>> getNodeConnectorStatistics(
+            final GetNodeConnectorStatisticsInput input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
