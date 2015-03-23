130ca6e023a36055556271ccc8318e3b3680beb9
Jozef Gloncak <jgloncak@cisco.com>
2015-03-23 09:54:42 +0100

    Bug 2753 - 4 managers api proposal
    - proposal for new API managers
    - HandshakeManager code cleanup
    - added handshakeContext
    - minor cleanup in connectionContext
    - added features to connectionContext
    
    Change-Id: Iba6b90247c54affa3be326f98f93b00d763b5402
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/rpc/RpcManager.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http:www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.rpc;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceSynchronizedHandler;
+
+/**
+ * The RPC Manager will maintain an RPC Context for each online switch. RPC context for device is created when
+ * {@link org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceSynchronizedHandler#deviceConnected(org.opendaylight.openflowplugin.api.openflow.device.RequestContext)}
+ * is called.
+ * <p/>
+ * Created by Martin Bobak <mbobak@cisco.com> on 25.2.2015.
+ */
+public interface RpcManager extends DeviceSynchronizedHandler, BindingAwareProvider {
+
+}
