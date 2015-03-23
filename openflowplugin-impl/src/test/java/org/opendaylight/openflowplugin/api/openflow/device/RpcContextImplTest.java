a6f19fdd26644714a0b537b1b18d3736f724ccdf
Martin Bobak <mbobak@cisco.com>
2015-03-23 07:43:32 +0100

    Rpc
    adding RpcContextImpl and RpcManagerImpl
    
    Change-Id: I57ba3d71f3b3bddcf40b6527fe76b34d97eb376e
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/api/openflow/device/RpcContextImplTest.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.api.openflow.device;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+
+/**
+ * @author joe
+ * 
+ */
+public class RpcContextImplTest {
+
+    @Test
+    public void invokeRpcTest() {
+        final RpcProviderRegistry mockedRpcProviderRegistry = mock(RpcProviderRegistry.class);
+    }
+}
