674044f19ef8e0d8dbecf8ea30cc3f147b53d2f7
Jozef Gloncak <jgloncak@cisco.com>
2015-03-23 13:31:38 +0100

    Bug 2753 - 4 managers api proposal
    - proposal for new API manager
    - HandshakeManager code cleanup
    - added handshakeContext
    - minor cleanup in connectionContext
    - added features to connectionContext
    
    Change-Id: Iba6b90247c54affa3be326f98f93b00d763b5402
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/DeviceManager.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.device;
+
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceConnectedHandler;
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceContextReadyHandler;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+/**
+ * This interface is responsible for instantiating DeviceContext and
+ * registering transaction chain for each DeviceContext. Each device
+ * has its own device context managed by this manager.
+ * <p/>
+ * <p/>
+ * Created by Martin Bobak <mbobak@cisco.com> on 25.2.2015.
+ */
+public interface DeviceManager extends DeviceConnectedHandler {
+
+
+    /**
+     * Method allows to send message that will be using RequestContext
+     * wrapped by this context.
+     *
+     * @param dataObject
+     */
+    void sendMessage(DataObject dataObject, RequestContext requestContext);
+
+    /**
+     * Method allows to send rpc request that will be using RequestContext
+     * wrapped by this context.
+     *
+     * @param dataObject
+     */
+    Xid sendRequest(DataObject dataObject, RequestContext requestContext);
+
+    /**
+     * Method registers handler responsible for handling operations related to connected device after
+     * request context is created.
+     *
+     * @param deviceContextReadyHandler
+     */
+    public void addRequestContextReadyHandler(DeviceContextReadyHandler deviceContextReadyHandler);
+
+}
