6dbbf8b5827d25189130ad81d96dfc584a34d1a1
Martin Bobak <mbobak@cisco.com>
2015-03-24 13:25:06 +0100

    XID holds only long value doesn't generate new values
    Change-Id: Ia1ee8c8510840f1fdf33b7d52ec0c38147a70c40
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/CommonService.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/CommonService.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -19,7 +19,7 @@ public class CommonService {
     protected OFRpcTaskContext rpcTaskContext;
     protected RpcContext rpcContext;
     protected final static Future<RpcResult<Void>> errorRpcResult = Futures.immediateFuture(RpcResultBuilder
-            .<Void> failed().withError(ErrorType.APPLICATION, "", "Request quota exceeded.").build());
+            .<Void>failed().withError(ErrorType.APPLICATION, "", "Request quota exceeded.").build());
 
     /**
      * 
