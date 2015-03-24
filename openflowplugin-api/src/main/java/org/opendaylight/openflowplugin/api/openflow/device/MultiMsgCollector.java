e51e4efb67681879f90685e4fa7c588f2e2a2f4d
Martin Bobak <mbobak@cisco.com>
2015-03-24 17:18:39 +0100

    MultiMsgCollector for Multipart response messaging
    Note: OF device could return Multipart msg response for a requests.
    So we would like to have one-to-one contract for request-to-CollectionResponse
    and we will able to identify differences between actualState and
    a future state.
    
    * MultiMsgCollector interface - a definition
    * MultiMsgCollectorImpl class - implementation of definition with
    internal Cache for collecting MultipartResponseMessages
    * MultiMsgCollectorImplTest   - test suite for testing basic
    functionality
    
    Change-Id: I920e2ee69929ed874304116da6711ab3f2dfb159
    Signed-off-by: Timotej Kubas <tkubas@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/MultiMsgCollector.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.device;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.Collection;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReply;
+
+/**
+ * openflowplugin-api
+ * org.opendaylight.openflowplugin.api.openflow.device
+ *
+ * Collects multipart msgs from device by provided XID and returns them
+ * to the caller as request/collection response one-to-one contract.
+ *
+ * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
+ * @author <a href="mailto:tkubas@cisco.com">Timotej Kubas</a>
+ *
+ * Created: Mar 23, 2015
+ */
+public interface MultiMsgCollector {
+
+    /**
+     * Property used to know a max life time of Multipart collection in internal Cache
+     */
+    final int DEFAULT_TIME_OUT = 10;
+
+    /**
+     * Method registers a transaction id xid to the Multipart messages collector
+     * and returns Settable future with all MultipartReply. Method has to be called before
+     * send a request to the device, otherwise there is a small possibility to miss a first msg.
+     *
+     * @param xid
+     * @return
+     */
+    ListenableFuture<Collection<MultipartReply>> registerMultipartMsg(@Nonnull long xid);
+
+    /**
+     * Method adds a reply multipart message to the collection and if the message has marker
+     * "I'M A LAST" method set whole Collection to Future object and remove from cache.
+     *
+     * @param reply
+     */
+    void addMultipartMsg(@Nonnull MultipartReply reply);
+}
