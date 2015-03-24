a91d07e16eb2dc402366773019b0257cbf3f9a73
Martin Bobak <mbobak@cisco.com>
2015-03-24 13:03:50 +0100

    RequestContext implementation
    Change-Id: I566ff0168e061d15271d931fda61e0b193b74139
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/rpc/RequestContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/rpc/RequestContextImpl.java
@@ -1,27 +1,34 @@
 /**
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.openflowplugin.impl.rpc;
 
-import java.util.concurrent.Future;
+import com.google.common.util.concurrent.SettableFuture;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
+import org.opendaylight.openflowplugin.api.openflow.rpc.RpcContext;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import java.util.concurrent.Future;
 
 /**
  * @author joe
- * 
  */
-public class RequestContextImpl implements RequestContext {
+public class RequestContextImpl<T extends DataObject> implements RequestContext {
 
-    private final Future<RpcResult<? extends DataObject>> result;
+    private final RpcContext rpcContext;
+    private SettableFuture<RpcResult<T>> rpcResultFuture;
 
-    public RequestContextImpl(final Future<RpcResult<? extends DataObject>> result) {
-        this.result = result;
+    public RequestContextImpl(RpcContext rpcContext) {
+        this.rpcContext = rpcContext;
     }
 
+    @Override
+    public Future<RpcResult<T>> createRequestFuture(final DataObject dataObject) {
+        rpcResultFuture = SettableFuture.create();
+        return rpcResultFuture;
+    }
 }
