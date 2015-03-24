57ed5d528cc828b48a15f412042eabe7eef80271
Martin Bobak <mbobak@cisco.com>
2015-03-24 13:01:20 +0100

    Service SalFlowService - removing utilization of OFRpcTask
    - OFRpcTask utilization was removed from service SalFlowService to CommonService
    - implementation of addFlow, removeFlow, updateFlow methods from SalFlowServices
      was moved from OFRpcTaskFactory.
    - make openflowplugin-impl compilable
    - added tests for adding flow (not working)
    
    Change-Id: I8f1f7e7342e04f94f74f7900eeaede3515b37f73
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/rpc/RequestContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/rpc/RequestContextImpl.java
@@ -8,11 +8,11 @@
 package org.opendaylight.openflowplugin.impl.rpc;
 
 import com.google.common.util.concurrent.SettableFuture;
+import java.util.concurrent.Future;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
 import org.opendaylight.openflowplugin.api.openflow.rpc.RpcContext;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.common.RpcResult;
-import java.util.concurrent.Future;
 
 /**
  * @author joe
@@ -22,13 +22,42 @@ public class RequestContextImpl<T extends DataObject> implements RequestContext 
     private final RpcContext rpcContext;
     private SettableFuture<RpcResult<T>> rpcResultFuture;
 
-    public RequestContextImpl(RpcContext rpcContext) {
+    public RequestContextImpl(final RpcContext rpcContext) {
         this.rpcContext = rpcContext;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.opendaylight.openflowplugin.api.openflow.device.RequestContext#createRequestFuture(org.opendaylight.yangtools
+     * .yang.binding.DataObject)
+     */
     @Override
     public Future<RpcResult<T>> createRequestFuture(final DataObject dataObject) {
         rpcResultFuture = SettableFuture.create();
         return rpcResultFuture;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.opendaylight.openflowplugin.api.openflow.device.RequestContext#requestSucceeded()
+     */
+    @Override
+    public void requestSucceeded() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.opendaylight.openflowplugin.api.openflow.device.RequestContext#requestFailed(java.lang.String)
+     */
+    @Override
+    public void requestFailed(final String exception) {
+        // TODO Auto-generated method stub
+
     }
 }
