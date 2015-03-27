318e9137b5d839d45d392d671f7232e412ccc053
Jozef Gloncak <jgloncak@cisco.com>
2015-03-27 15:17:42 +0100

    Handle exceptional state while closing RequestContext.
    Current close() method is wrong one. Correct one if from AutoClosable
    interface and raises Exception.
    
    Change-Id: I7fe8d65c3ac5c7ab036cee1da642c8e3c7672162
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/RequestContextUtil.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.impl.services;
+
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
+
+import org.slf4j.Logger;
+
+public final class RequestContextUtil {
+
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(RequestContextUtil.class);
+
+    private RequestContextUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    static void closeRequstContext(final RequestContext<?> requestContext) {
+        try {
+            requestContext.close();
+        } catch (Exception e) {
+            LOG.debug("Request context wasn't closed. Exception message: {}",e.getMessage());
+        }
+    }
+}
