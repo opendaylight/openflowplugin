3237a17a946e95af341d269713e8ade864e074ed
Martin Bobak <mbobak@cisco.com>
2015-03-29 05:18:44 +0200

    DeviceState implementation
    Note: DeviceState is a basic object which we need to populate
    from ConnectionContext and it has to be a input object for
    DeviceContext. So this implementation has to be before
    DeviceContext implementation in the chain order.
    
    * DeviceState interface - fix getNextXid and getPortsBandwidth methods
    * impl DeviceStateImpl
    * impl test suite in DeviceStateImplTest
    
    Change-Id: Iadf19754c3373b25a6a320c676118d13338a0097
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/DeviceStateImplTest.java
@@ -0,0 +1,182 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.device;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortGrouping;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.features.reply.PhyPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.features.reply.PhyPortBuilder;
+
+/**
+ * openflowplugin-impl
+ * org.opendaylight.openflowplugin.impl.device
+ *
+ * test of {@link DeviceStateImpl} - lightweight version, using basic ways (TDD)
+ *
+ * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
+ *
+ * Created: Mar 29, 2015
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DeviceStateImplTest {
+
+    private NodeId nodeId;
+    @Mock
+    private FeaturesReply featuresReply;
+    private DeviceStateImpl deviceState;
+
+    private final short version = 13;
+    private final long portNr = 10L;
+    private final Long portBandwidth = 1024L;
+    private final List<PhyPort> pPort = Arrays.asList(new PhyPortBuilder()
+                    .setPortNo(portNr).setMaxSpeed(portBandwidth).build());
+
+    @Before
+    public void initialization() {
+        nodeId = new NodeId("test-node-id");
+        Mockito.when(featuresReply.getVersion()).thenReturn(version);
+        Mockito.when(featuresReply.getPhyPort()).thenReturn(pPort);
+        deviceState = new DeviceStateImpl(featuresReply, nodeId);
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#DeviceStateImpl(FeaturesReply, NodeId)}.
+     */
+    @Test(expected=NullPointerException.class)
+    public void testDeviceStateImplNullNodeId(){
+        new DeviceStateImpl(featuresReply, null);
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#DeviceStateImpl(FeaturesReply, NodeId)}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testDeviceStateImplNullFeaturesReply(){
+        new DeviceStateImpl(null, nodeId);
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#DeviceStateImpl(FeaturesReply, NodeId)}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testDeviceStateImplNullPhyPort(){
+        final FeaturesReply emptyFeaturesReply = Mockito.mock(FeaturesReply.class);
+        Mockito.when(emptyFeaturesReply.getPhyPort()).thenReturn(null);
+        new DeviceStateImpl(emptyFeaturesReply, nodeId);
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getNodeId()}.
+     */
+    @Test
+    public void testGetNodeId(){
+        final NodeId getNodeId = deviceState.getNodeId();
+        Assert.assertNotNull(getNodeId);
+        Assert.assertEquals(nodeId, getNodeId);
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getFeatures()}.
+     */
+    @Test
+    public void testGetFeatures(){
+        final GetFeaturesOutputBuilder expetedResult = new GetFeaturesOutputBuilder(featuresReply);
+        final GetFeaturesOutput getFeatures = deviceState.getFeatures();
+        Assert.assertNotNull(getFeatures);
+        Assert.assertEquals(expetedResult.getVersion(), getFeatures.getVersion());
+        Assert.assertEquals(expetedResult.getPhyPort(), getFeatures.getPhyPort());
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getPhysicalPorts()}.
+     */
+    @Test
+    public void testGetPhysicalPorts(){
+        final Map<Long, PortGrouping> getPhysPort = deviceState.getPhysicalPorts();
+        Assert.assertNotNull(getPhysPort);
+        Assert.assertTrue(getPhysPort.values().contains(pPort.get(0)));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getPortsBandwidth()}.
+     */
+    @Test
+    public void testGetPortsBandwidth(){
+        final Map<Long, Long> portBandwidth = deviceState.getPortsBandwidth();
+        Assert.assertNotNull(portBandwidth);
+        Assert.assertTrue(portBandwidth.containsKey(portNr));
+        Assert.assertEquals(this.portBandwidth, portBandwidth.get(portNr));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getPorts()}.
+     */
+    @Test
+    public void testGetPorts(){
+        final Set<Long> portNrs = deviceState.getPorts();
+        Assert.assertTrue(portNrs.contains(portNr));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getPhysicalPort(java.lang.Long)}.
+     */
+    @Test
+    public void testGetPhysicalPort(){
+        Assert.assertEquals(pPort.get(0), deviceState.getPhysicalPort(portNr));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getPortBandwidth(java.lang.Long)}.
+     */
+    @Test
+    public void testGetPortBandwidth(){
+        Assert.assertEquals(portBandwidth, deviceState.getPortBandwidth((portNr)));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#isPortEnabled(long)}.
+     */
+    @Test
+    public void testIsPortEnabledLong(){
+        Assert.assertTrue(deviceState.isPortEnabled(portNr));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#isPortEnabled(PortGrouping)}.
+     */
+    @Test
+    public void testIsPortEnabledPortGrouping(){
+        Assert.assertTrue(deviceState.isPortEnabled(pPort.get(0)));
+    }
+
+    /**
+     * Test method for {@link DeviceStateImpl#getEnabledPorts()}.
+     */
+    @Test
+    public void testGetEnabledPorts(){
+        final List<PortGrouping> getEnabledPort = deviceState.getEnabledPorts();
+        Assert.assertNotNull(getEnabledPort);
+        Assert.assertTrue(getEnabledPort.contains(pPort.get(0)));
+    }
+
+}
