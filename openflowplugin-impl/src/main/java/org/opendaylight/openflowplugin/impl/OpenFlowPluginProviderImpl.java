f5939dca970b9f7f94a0ce643a7cd3c75ea96b82
Vaclav Demcak <vdemcak@cisco.com>
2015-04-02 19:04:23 +0200

    added switch connection providers starting
     - involved netty startup upon sessionInitialized
     - small fixes
    
    Change-Id: I6b03d5fe4d956619f414d77227c163159b611611
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/OpenFlowPluginProviderImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/OpenFlowPluginProviderImpl.java
@@ -9,53 +9,106 @@
 package org.opendaylight.openflowplugin.impl;
 
 
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.openflowjava.protocol.spi.connection.SwitchConnectionProvider;
 import org.opendaylight.openflowplugin.api.openflow.OpenFlowPluginProvider;
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionManager;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceManager;
 import org.opendaylight.openflowplugin.api.openflow.rpc.RpcManager;
 import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsManager;
+import org.opendaylight.openflowplugin.impl.connection.ConnectionManagerImpl;
 import org.opendaylight.openflowplugin.impl.device.DeviceManagerImpl;
 import org.opendaylight.openflowplugin.impl.rpc.RpcManagerImpl;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.api.types.rev150327.OfpRole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 27.3.2015.
  */
 public class OpenFlowPluginProviderImpl implements OpenFlowPluginProvider {
 
+    protected static final Logger LOG = LoggerFactory.getLogger(OpenFlowPluginProviderImpl.class);
+
     private DeviceManager deviceManager;
     private RpcManager rpcManager;
-    private BindingAwareBroker.ProviderContext providerContext;
     private StatisticsManager statisticsManager;
+    private ConnectionManager connectionManager;
+    private BindingAwareBroker bindingAwareBroker;
+    private ProviderContext providerContext;
+    private OfpRole role;
+    private Collection<SwitchConnectionProvider> switchConnectionProviders;
 
     @Override
-    public void onSessionInitiated(final BindingAwareBroker.ProviderContext session) {
-        providerContext = session;
+    public void onSessionInitiated(final ProviderContext providerContextArg) {
+        providerContext = providerContextArg;
+        connectionManager = new ConnectionManagerImpl();
+        rpcManager = new RpcManagerImpl(providerContext);
+        deviceManager = new DeviceManagerImpl(rpcManager, providerContext.getSALService(DataBroker.class));
+        connectionManager.setDeviceConnectedHandler(deviceManager);
+        //TODO : initialize statistics manager
+        //TODO : initialize translatorLibrary + inject into deviceMngr
+        startSwitchConnections();
+    }
+
+    /**
+     * @param connectionManager2
+     */
+    private void startSwitchConnections() {
+        final List<ListenableFuture<Boolean>> starterChain = new ArrayList<>(switchConnectionProviders.size());
+        for (final SwitchConnectionProvider switchConnectionPrv : switchConnectionProviders) {
+            switchConnectionPrv.setSwitchConnectionHandler(connectionManager);
+            final ListenableFuture<Boolean> isOnlineFuture = switchConnectionPrv.startup();
+            starterChain.add(isOnlineFuture);
+        }
+
+        final ListenableFuture<List<Boolean>> srvStarted = Futures.allAsList(starterChain);
+        Futures.addCallback(srvStarted, new FutureCallback<List<Boolean>>() {
+            @Override
+            public void onSuccess(final List<Boolean> result) {
+                LOG.info("All switchConnectionProviders are up and running ({}).",
+                        result.size());
+            }
+            @Override
+            public void onFailure(final Throwable t) {
+                LOG.warn("Some switchConnectionProviders failed to start.", t);
+            }
+        });
     }
 
     @Override
-    public void setSwitchConnectionProviders(final Collection<SwitchConnectionProvider> switchConnectionProvider) {
-
+    public void setSwitchConnectionProviders(final Collection<SwitchConnectionProvider> switchConnectionProviders) {
+        this.switchConnectionProviders = switchConnectionProviders;
     }
 
     @Override
     public void setRole(final OfpRole role) {
+        this.role = role;
+    }
+
+    @Override
+    public void setBindingAwareBroker(final BindingAwareBroker bindingAwareBroker) {
+        this.bindingAwareBroker = bindingAwareBroker;
 
     }
 
     @Override
     public void initialize() {
-        rpcManager = new RpcManagerImpl(providerContext);
-        deviceManager = new DeviceManagerImpl(rpcManager, providerContext.getSALService(DataBroker.class));
-        //TODO : initialize statistics manager
-        //TODO : initialize translatorLibrary + inject into deviceMngr
+        Preconditions.checkNotNull(bindingAwareBroker, "missing bindingAwareBroker");
+        bindingAwareBroker.registerProvider(this);
     }
 
     @Override
     public void close() throws Exception {
-
+        //TODO: close all contexts, switchConnections (, managers)
     }
 }
