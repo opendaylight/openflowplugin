e3b7bf17e5b004fe644aa23328e2e572b7b212d5
Vaclav Demcak <vdemcak@cisco.com>
2015-04-02 19:40:00 +0200

    statistics manager initial commit
    Change-Id: Ie032cb86c271e80684053cf8e192902fdf245d9a
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.statistics;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
+import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsContext;
+import org.opendaylight.openflowplugin.impl.rpc.RequestContextImpl;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightFlowStatisticsServiceImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.GetAllFlowsStatisticsFromAllFlowTablesInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.GetAllFlowsStatisticsFromAllFlowTablesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.OpendaylightFlowStatisticsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 1.4.2015.
+ */
+public class StatisticsContextImpl implements StatisticsContext {
+
+    private final List<RequestContext> requestContexts = new ArrayList();
+    private final DeviceContext deviceContext;
+    private final RequestContext requestContext;
+
+    private final OpendaylightFlowStatisticsService flowStatisticsService;
+
+    /**
+     * FIXME : Why we need RequestContext
+     * @param deviceContext
+     * @param requestContext
+     */
+    public StatisticsContextImpl(final DeviceContext deviceContext, final RequestContext requestContext) {
+        this.deviceContext = deviceContext;
+        this.requestContext = requestContext;
+        flowStatisticsService = new OpendaylightFlowStatisticsServiceImpl(this, deviceContext);
+
+    }
+
+    private void pollFlowStatistics() {
+        final KeyedInstanceIdentifier<Node, NodeKey> nodeII = InstanceIdentifier.create(Nodes.class).child(Node.class, new NodeKey(deviceContext.getPrimaryConnectionContext().getNodeId()));
+        final NodeRef nodeRef = new NodeRef(nodeII);
+        final GetAllFlowsStatisticsFromAllFlowTablesInputBuilder builder =
+                new GetAllFlowsStatisticsFromAllFlowTablesInputBuilder();
+        builder.setNode(nodeRef);
+        final Future<RpcResult<GetAllFlowsStatisticsFromAllFlowTablesOutput>> flowStatisticsResult = flowStatisticsService.getAllFlowsStatisticsFromAllFlowTables(builder.build());
+        //TODO : process data from result
+    }
+
+    @Override
+    public ListenableFuture<Void> gatherDynamicData() {
+        //TODO : call all necessary statistics services to gather data and store them in Oper DS
+        return null;
+    }
+
+    @Override
+    public <T> void forgetRequestContext(final RequestContext<T> requestContext) {
+        requestContexts.remove(requestContexts);
+    }
+
+    @Override
+    public <T> SettableFuture<RpcResult<T>> storeOrFail(final RequestContext<T> data) {
+        requestContexts.add(data);
+        return data.getFuture();
+    }
+
+    @Override
+    public <T> RequestContext<T> createRequestContext() {
+        return new RequestContextImpl<>(this);
+    }
+}
