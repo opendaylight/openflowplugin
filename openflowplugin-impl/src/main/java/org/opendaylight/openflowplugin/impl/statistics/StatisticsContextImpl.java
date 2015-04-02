9cdfed0a24aed84f1afbeb8ed7ef7ee2aac8b4ca
Martin Bobak <mbobak@cisco.com>
2015-04-02 14:59:12 +0200

    statistics context implements statistic services calls
    Change-Id: Ifcda7a4e41369cbe0077e36f80354ceb339a6e13
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
@@ -15,19 +15,26 @@ import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
 import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsContext;
 import org.opendaylight.openflowplugin.impl.rpc.RequestContextImpl;
 import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightFlowStatisticsServiceImpl;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.GetAllFlowsStatisticsFromAllFlowTablesInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.GetAllFlowsStatisticsFromAllFlowTablesOutput;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightFlowTableStatisticsServiceImpl;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightGroupStatisticsServiceImpl;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightMeterStatisticsServiceImpl;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightPortStatisticsServiceImpl;
+import org.opendaylight.openflowplugin.impl.statistics.services.OpendaylightQueueStatisticsServiceImpl;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.OpendaylightFlowStatisticsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.table.statistics.rev131215.OpendaylightFlowTableStatisticsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.statistics.rev131111.OpendaylightGroupStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.statistics.rev131111.OpendaylightMeterStatisticsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.OpendaylightQueueStatisticsService;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
 
 /**
  * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 1.4.2015.
@@ -38,28 +45,37 @@ public class StatisticsContextImpl implements StatisticsContext {
     private final DeviceContext deviceContext;
     private final RequestContext requestContext;
 
+
     private final OpendaylightFlowStatisticsService flowStatisticsService;
+    private final OpendaylightFlowTableStatisticsService flowTableStatisticsService;
+    private final OpendaylightGroupStatisticsService groupStatisticsService;
+    private final OpendaylightMeterStatisticsService meterStatisticsService;
+    private final OpendaylightPortStatisticsService portStatisticsService;
+    private final OpendaylightQueueStatisticsService queueStatisticsService;
+
 
     public StatisticsContextImpl(DeviceContext deviceContext, RequestContext requestContext) {
         this.deviceContext = deviceContext;
         this.requestContext = requestContext;
+
         flowStatisticsService = new OpendaylightFlowStatisticsServiceImpl(this, deviceContext);
+        flowTableStatisticsService = new OpendaylightFlowTableStatisticsServiceImpl(this, deviceContext);
+        groupStatisticsService = new OpendaylightGroupStatisticsServiceImpl(this, deviceContext);
+        meterStatisticsService = new OpendaylightMeterStatisticsServiceImpl(this, deviceContext);
+        portStatisticsService = new OpendaylightPortStatisticsServiceImpl(this, deviceContext);
+        queueStatisticsService = new OpendaylightQueueStatisticsServiceImpl(this, deviceContext);
 
     }
 
-    private void pollFlowStatistics() {
-        KeyedInstanceIdentifier<Node, NodeKey> nodeII = InstanceIdentifier.create(Nodes.class).child(Node.class, new NodeKey(deviceContext.getPrimaryConnectionContext().getNodeId()));
-        NodeRef nodeRef = new NodeRef(nodeII);
-        final GetAllFlowsStatisticsFromAllFlowTablesInputBuilder builder =
-                new GetAllFlowsStatisticsFromAllFlowTablesInputBuilder();
-        builder.setNode(nodeRef);
-        Future<RpcResult<GetAllFlowsStatisticsFromAllFlowTablesOutput>> flowStatisticsResult = flowStatisticsService.getAllFlowsStatisticsFromAllFlowTables(builder.build());
-        //TODO : process data from result
-    }
 
     @Override
     public ListenableFuture<Void> gatherDynamicData() {
-        //TODO : call all necessary statistics services to gather data and store them in Oper DS
+        ListenableFuture<Boolean> flowStatistics = StatisticsGatheringUtils.gatherFlowStatistics(flowStatisticsService, deviceContext);
+        ListenableFuture<Boolean> tableStatistics = StatisticsGatheringUtils.gatherTableStatistics(flowTableStatisticsService, deviceContext);
+        ListenableFuture<Boolean> groupStatistics = StatisticsGatheringUtils.gatherGroupStatistics(groupStatisticsService, deviceContext);
+        ListenableFuture<Boolean> meterStatistics = StatisticsGatheringUtils.gatherMeterStatistics(meterStatisticsService, deviceContext);
+        ListenableFuture<Boolean> portStatistics = StatisticsGatheringUtils.gatherPortStatistics(portStatisticsService, deviceContext);
+        ListenableFuture<Boolean> queueStatistics = StatisticsGatheringUtils.gatherQueueStatistics(queueStatisticsService, deviceContext);
         return null;
     }
 
