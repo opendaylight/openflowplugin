c62ea10a0899dc03a032eb2715eda211478cacf3
Vaclav Demcak <vdemcak@cisco.com>
2015-04-02 17:25:16 +0200

    Time scheduler for transaction submit in DeviceContextImpl
    Note: TransactionManager could submit transaction after specified
    nr. of write action but we are missing a time scheduler for "submit"
    after some time scenario. So we add a HashedWheelTimer for fix this
    scenario.
    
    * HashedWheelTimer implementation
    
    Change-Id: If937e8700a8e83bc3d5ddac87975f14df46db122
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
@@ -10,10 +10,14 @@ package org.opendaylight.openflowplugin.impl.device;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.SettableFuture;
+import io.netty.util.HashedWheelTimer;
+import io.netty.util.Timeout;
+import io.netty.util.TimerTask;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import javax.annotation.Nonnull;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -61,6 +65,7 @@ public class DeviceContextImpl implements DeviceContext, DeviceReplyProcessor {
     private final DeviceState deviceState;
     private final DataBroker dataBroker;
     private final XidGenerator xidGenerator;
+    private final HashedWheelTimer hashedWheelTimer;
     private Map<Long, RequestContext> requests = new HashMap<Long, RequestContext>();
 
     private final Map<SwitchConnectionDistinguisher, ConnectionContext> auxiliaryConnectionContexts;
@@ -69,18 +74,30 @@ public class DeviceContextImpl implements DeviceContext, DeviceReplyProcessor {
 
     @VisibleForTesting
     DeviceContextImpl(@Nonnull final ConnectionContext primaryConnectionContext,
-                      @Nonnull final DeviceState deviceState, @Nonnull final DataBroker dataBroker) {
+                      @Nonnull final DeviceState deviceState, @Nonnull final DataBroker dataBroker,
+                      @Nonnull final HashedWheelTimer hashedWheelTimer) {
         this.primaryConnectionContext = Preconditions.checkNotNull(primaryConnectionContext);
         this.deviceState = Preconditions.checkNotNull(deviceState);
         this.dataBroker = Preconditions.checkNotNull(dataBroker);
+        this.hashedWheelTimer = Preconditions.checkNotNull(hashedWheelTimer);
         xidGenerator = new XidGenerator();
         txChainManager = new TransactionChainManager(dataBroker, 500L);
         auxiliaryConnectionContexts = new HashMap<>();
         requests = new HashMap<>();
     }
 
+    /**
+     * This method is called from {@link DeviceManagerImpl} only. So we could say "posthandshake process finish"
+     * and we are able to set a scheduler for an automatic transaction submitting by time (0,5sec).
+     */
     void submitTransaction() {
         txChainManager.submitTransaction();
+        hashedWheelTimer.newTimeout(new TimerTask() {
+            @Override
+            public void run(final Timeout timeout) throws Exception {
+                submitTransaction();
+            }
+        }, 0, TimeUnit.MILLISECONDS);
     }
 
     @Override
