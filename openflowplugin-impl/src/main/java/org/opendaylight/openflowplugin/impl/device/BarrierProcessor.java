818d846eee7cb0e867d93ccf9e43d4971372d251
Vaclav Demcak <vdemcak@cisco.com>
2015-04-03 16:57:18 +0200

    Barrier init commit
    * BarrierTaskBuilder - barrier msg self restarting builder.
    * BarrierProcessor - TODO
    
    Change-Id: I9fd41e488a2ced7bb44b2c43a6d1adbe7628fe14
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/BarrierProcessor.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.device;
+
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+
+/**
+ * openflowplugin-impl
+ * org.opendaylight.openflowplugin.impl.device
+ *
+ *
+ *
+ * @author <a href="mailto:vdemcak@cisco.com">Vaclav Demcak</a>
+ *
+ * Created: Apr 3, 2015
+ */
+public class BarrierProcessor {
+
+    public static void processOutstandingRequests(final long xid, final DeviceContext deviceCtx) {
+        // TODO
+    }
+}
