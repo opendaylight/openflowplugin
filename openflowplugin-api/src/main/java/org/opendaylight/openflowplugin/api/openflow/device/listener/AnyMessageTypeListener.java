df64be6975d7680279920b3d3484ebd543ea8854
Martin Bobak <mbobak@cisco.com>
2015-04-04 15:10:18 +0200

    unifiying interface for multipart message collector and ofp protocol listener
    Change-Id: Ida1263e2d191345fed3a2e6aeff30d04d67fcd33
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/listener/AnyMessageTypeListener.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.device.listener;
+
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.MultiMsgCollector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OpenflowProtocolListener;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 4.4.2015.
+ */
+public interface AnyMessageTypeListener extends OpenflowProtocolListener, MultiMsgCollector {
+}
