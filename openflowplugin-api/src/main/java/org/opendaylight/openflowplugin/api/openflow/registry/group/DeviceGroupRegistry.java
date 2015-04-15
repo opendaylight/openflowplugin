225fa7ae861f783eaf6625e17d7a5bdb8a7d70fe
Martin Bobak <mbobak@cisco.com>
2015-04-15 10:59:21 +0200

    added api for DeviceGroupRegistry
    Change-Id: I1604e0079edb2751f5d48424e74f6697701070c1
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/group/DeviceGroupRegistry.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.registry.group;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.GroupId;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 15.4.2015.
+ */
+public interface DeviceGroupRegistry {
+
+    void store(GroupId groupId);
+
+    void markToBeremoved(GroupId groupId);
+
+    void removeMarked();
+
+}
