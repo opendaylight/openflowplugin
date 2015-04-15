7d030ff6923bf116488aaf56d2df09501522cde7
Martin Bobak <mbobak@cisco.com>
2015-04-15 11:02:47 +0200

    device group registry implementation
    Change-Id: I02d97ec684be9ff901e22b4bf306aea5eb2772bd
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/registry/group/DeviceGroupRegistryImpl.java
@@ -0,0 +1,7 @@
+package org.opendaylight.openflowplugin.impl.registry.group;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 15.4.2015.
+ */
+public class DeviceGroupRegistryImpl {
+}
