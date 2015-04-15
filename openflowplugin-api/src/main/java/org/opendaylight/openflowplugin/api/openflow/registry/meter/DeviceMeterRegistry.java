40d1090a71e7ad551e1bc563d84035a10614be6d
Martin Bobak <mbobak@cisco.com>
2015-04-15 12:58:14 +0200

    added api and implementation of device meter registry
    Change-Id: Ie4f3bedc6e38f855938b9c3df3cb110087edbcc2
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/meter/DeviceMeterRegistry.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.registry.meter;
+
+import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterId;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 15.4.2015.
+ */
+public interface DeviceMeterRegistry {
+
+    void store(MeterId meterId);
+
+    void markToBeremoved(MeterId meterId);
+
+    void removeMarked();
+
+    List<MeterId> getAllMeterIds();
+
+}
