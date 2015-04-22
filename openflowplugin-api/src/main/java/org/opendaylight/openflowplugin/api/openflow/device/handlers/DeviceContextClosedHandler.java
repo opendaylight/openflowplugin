10596584241cf847d2f0ab98029abc9e8e9d2d3f
Martin Bobak <mbobak@cisco.com>
2015-04-22 14:37:51 +0200

    introduced handler for handling processes when device context is closed
    Change-Id: I10eeee1811ce8ca213de59a6a6053553e27e5ac4
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/handlers/DeviceContextClosedHandler.java
@@ -0,0 +1,7 @@
+package org.opendaylight.openflowplugin.api.openflow.device.handlers;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 22.4.2015.
+ */
+public interface DeviceContextClosedHandler {
+}
