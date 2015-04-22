ac19f53be7ba2bd5909969faafa5e18500a6e085
Martin Bobak <mbobak@cisco.com>
2015-04-22 13:21:25 +0200

    introduced DeviceDisconnectedHandler for handling closed connections
    Change-Id: Icf4da609fd185d69fafd66bf2dd2ad27c0bac59a
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/handlers/DeviceDisconnectedHandler.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.device.handlers;
+
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
+
+/**
+ * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 22.4.2015.
+ */
+
+public interface DeviceDisconnectedHandler {
+    /**
+     * Method is used to propagate information about closed connection with device.
+     * It propagates connected device's connection context.
+     */
+    void onDeviceDisconnected(ConnectionContext connectionContext);
+}
