4d26b7cf0711308a9fe60ee8e0a6433a180b294e
Martin Bobak <mbobak@cisco.com>
2015-04-28 16:32:27 +0200

    added initial waiting for mdsal in tableMissEnforcer
    Change-Id: Ifd315526e6263231ee41c7d36a9601e7a3bb6294
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- applications/table-miss-enforcer/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/openflow/applications/table/miss/enforcer/rev140326/LLDPPacketPuntEnforcerModule.java
+++ applications/table-miss-enforcer/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/openflow/applications/table/miss/enforcer/rev140326/LLDPPacketPuntEnforcerModule.java
@@ -1,15 +1,25 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.table.miss.enforcer.rev140326;
 
+import java.util.concurrent.Callable;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.openflowplugin.applications.tableMissEnforcer.LLDPPacketPuntEnforcer;
+import org.opendaylight.openflowplugin.common.wait.SimpleTaskRetryLooper;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LLDPPacketPuntEnforcerModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.table.miss.enforcer.rev140326.AbstractLLDPPacketPuntEnforcerModule {
+    private static final long STARTUP_LOOP_TICK = 500L;
+    private static final int STARTUP_LOOP_MAX_RETRIES = 8;
+    private static Logger LOG = LoggerFactory.getLogger(LLDPPacketPuntEnforcerModule.class);
+
     public LLDPPacketPuntEnforcerModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
@@ -25,13 +35,28 @@ public class LLDPPacketPuntEnforcerModule extends org.opendaylight.yang.gen.v1.u
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        InstanceIdentifier<FlowCapableNode> path = InstanceIdentifier.create(Nodes.class).child(Node.class).augmentation(FlowCapableNode.class);
-        SalFlowService salFlowService = getRpcRegistryDependency().getRpcService(SalFlowService.class);
-        return getDataBrokerDependency().registerDataChangeListener(
-                LogicalDatastoreType.OPERATIONAL,
-                path,
-                new LLDPPacketPuntEnforcer(salFlowService),
-                AsyncDataBroker.DataChangeScope.BASE);
+        final InstanceIdentifier<FlowCapableNode> path = InstanceIdentifier.create(Nodes.class).child(Node.class).augmentation(FlowCapableNode.class);
+        final SalFlowService salFlowService = getRpcRegistryDependency().getRpcService(SalFlowService.class);
+
+        ListenerRegistration<DataChangeListener> dataChangeListenerRegistration;
+        SimpleTaskRetryLooper looper = new SimpleTaskRetryLooper(STARTUP_LOOP_TICK, STARTUP_LOOP_MAX_RETRIES);
+        try {
+            dataChangeListenerRegistration = looper.loopUntilNoException(new Callable<ListenerRegistration<DataChangeListener>>() {
+                @Override
+                public ListenerRegistration<DataChangeListener> call() throws Exception {
+                    return getDataBrokerDependency().registerDataChangeListener(
+                            LogicalDatastoreType.OPERATIONAL,
+                            path,
+                            new LLDPPacketPuntEnforcer(salFlowService),
+                            AsyncDataBroker.DataChangeScope.BASE);
+                }
+            });
+        } catch (Exception e) {
+            LOG.warn("data listener registration failed: {}", e.getMessage());
+            LOG.debug("data listener registration failed.. ", e);
+            throw new IllegalStateException("LLDPPacketPuntEnforcer startup fail! System needs restart.", e);
+        }
+        return dataChangeListenerRegistration;
     }
 
 }
