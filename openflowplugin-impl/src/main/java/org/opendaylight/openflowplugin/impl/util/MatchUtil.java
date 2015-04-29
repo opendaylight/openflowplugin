bbe7d05210d5cbc8b0d79b45a7806d7a56ed78ef
Timotej Kubas <tkubas@cisco.com>
2015-04-29 10:32:31 +0200

    fixing of10 issue in MatchUtil
    Change-Id: I406cefad80f19be511b343c3e82c48d907a19aea
    Signed-off-by: Timotej Kubas <tkubas@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/util/MatchUtil.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/util/MatchUtil.java
@@ -43,7 +43,7 @@ public final class MatchUtil {
         matchV10Builder.setNwTos(zeroShort);
         matchV10Builder.setTpDst(zeroInteger);
         matchV10Builder.setTpSrc(zeroInteger);
-        FlowWildcardsV10 flowWildcardsV10 = FlowWildcardsV10.getDefaultInstance("");
+        FlowWildcardsV10 flowWildcardsV10 = new FlowWildcardsV10(false, false, false, false, false, false, false, false, false, false);
         matchV10Builder.setWildcards(flowWildcardsV10);
         return matchV10Builder;
     }
