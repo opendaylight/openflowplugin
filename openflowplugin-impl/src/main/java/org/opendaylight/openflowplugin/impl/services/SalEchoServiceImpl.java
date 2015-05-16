59a8b7279cc440bd9a53903e3c072481d3de8e59
Martin Bobak <mbobak@cisco.com>
2015-05-16 12:10:35 +0200

    FlowCapableTransactionServiceImpl  and SalEchoServiceImpl.java shifted to new OFJ codepath
    Change-Id: I3787c1c8ed16a2ebabdc26425ea8f4e823c72cc8
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/SalEchoServiceImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/SalEchoServiceImpl.java
@@ -7,22 +7,22 @@
  */
 package org.opendaylight.openflowplugin.impl.services;
 
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.JdkFutureAdapters;
-import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.SettableFuture;
 import java.util.concurrent.Future;
+import org.opendaylight.openflowjava.protocol.api.connection.OutboundQueue;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
 import org.opendaylight.openflowplugin.api.openflow.statistics.ofpspecific.MessageSpy;
-import org.opendaylight.openflowplugin.impl.callback.SuccessCallback;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SalEchoService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
+import org.opendaylight.yangtools.yang.common.RpcError;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
@@ -56,30 +56,28 @@ public class SalEchoServiceImpl extends CommonService implements SalEchoService 
         echoInputOFJavaBuilder.setData(sendEchoInput.getData());
         final EchoInput echoInputOFJava = echoInputOFJavaBuilder.build();
 
-        // FIXME: should be submitted via OutboundQueue
-        final Future<RpcResult<EchoOutput>> rpcEchoOutputOFJava = getPrimaryConnectionAdapter()
-                .echo(echoInputOFJava);
-        LOG.debug("Echo with xid {} was sent from controller", requestContext.getXid());
+        LOG.debug("Echo with xid {} was sent from controller", xid);
 
-        ListenableFuture<RpcResult<EchoOutput>> listenableRpcEchoOutputOFJava = JdkFutureAdapters
-                .listenInPoolThread(rpcEchoOutputOFJava);
+        final OutboundQueue outboundQueue = getDeviceContext().getPrimaryConnectionContext().getOutboundQueueProvider();
+        final SettableFuture<RpcResult<SendEchoOutput>> settableFuture = SettableFuture.create();
+        outboundQueue.commitEntry(xid.getValue(), echoInputOFJava, new FutureCallback<OfHeader>() {
+            @Override
+            public void onSuccess(final OfHeader ofHeader) {
+                RequestContextUtil.closeRequstContext(requestContext);
+                getDeviceContext().unhookRequestCtx(requestContext.getXid());
+                getMessageSpy().spyMessage(FlowModInput.class, MessageSpy.STATISTIC_GROUP.TO_SWITCH_SUBMIT_SUCCESS);
 
-        // callback on OF JAVA future
-        SuccessCallback<EchoOutput, SendEchoOutput> successCallback = new SuccessCallback<EchoOutput, SendEchoOutput>(
-                deviceContext, requestContext, listenableRpcEchoOutputOFJava) {
+                settableFuture.set(RpcResultBuilder.<SendEchoOutput>success().build());
+            }
 
             @Override
-            public RpcResult<SendEchoOutput> transform(final RpcResult<EchoOutput> rpcResult) {
-                EchoOutput echoOutputOFJava = rpcResult.getResult();
-                SendEchoOutputBuilder sendEchoOutputBuilder = new SendEchoOutputBuilder();
-                sendEchoOutputBuilder.setData(echoOutputOFJava.getData());
-
-                LOG.debug("Echo with xid {} was received by controller.", rpcResult.getResult().getXid());
-                return RpcResultBuilder.success(sendEchoOutputBuilder.build()).build();
+            public void onFailure(final Throwable throwable) {
+                RpcResultBuilder rpcResultBuilder = RpcResultBuilder.<Void>failed().withError(RpcError.ErrorType.APPLICATION, throwable.getMessage(), throwable);
+                RequestContextUtil.closeRequstContext(requestContext);
+                getDeviceContext().unhookRequestCtx(requestContext.getXid());
+                settableFuture.set(rpcResultBuilder.build());
             }
-        };
-        Futures.addCallback(listenableRpcEchoOutputOFJava, successCallback);
-
-        return requestContext.getFuture();
+        });
+        return settableFuture;
     }
 }
