4aa5dc39d3eb6c4f3302e75f9284683f1a709c5c
Martin Bobak <mbobak@cisco.com>
2015-05-20 08:36:07 +0000

    Revert "binding thread pools in drop test to target node"
    This change is suspect of significant decrease of throughput performance by He-codebase ofPlugin.
    
    This reverts commit 673a667dc331d5877f321e45396ad143e0c8055e.
    
    Change-Id: I34c23c280c0c1e6d2e31816af4da0ec2e9ae2725
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/AbstractDropTest.java
+++ test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/AbstractDropTest.java
@@ -52,8 +52,8 @@ abstract class AbstractDropTest implements PacketProcessingListener, AutoCloseab
     static final long STARTUP_LOOP_TICK = 500L;
     static final int STARTUP_LOOP_MAX_RETRIES = 8;
 
-    static private final int PROCESSING_THREAD_NUMBER = 8;
-    private final ExecutorService[] executorServiceLot;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(8);
+
 
     private static final AtomicIntegerFieldUpdater<AbstractDropTest> SENT_UPDATER = AtomicIntegerFieldUpdater.newUpdater(AbstractDropTest.class, "sent");
     private volatile int sent;
@@ -63,13 +63,6 @@ abstract class AbstractDropTest implements PacketProcessingListener, AutoCloseab
 
     private static final AtomicIntegerFieldUpdater<AbstractDropTest> EXCS_UPDATER = AtomicIntegerFieldUpdater.newUpdater(AbstractDropTest.class, "excs");
     private volatile int excs;
-
-    public AbstractDropTest() {
-        executorServiceLot = new ExecutorService[PROCESSING_THREAD_NUMBER];
-        for (int i = 0; i < PROCESSING_THREAD_NUMBER; i++) {
-            executorServiceLot[i] = Executors.newFixedThreadPool(1);
-        }
-    }
 
     public final DropTestStats getStats() {
         return new DropTestStats(this.sent, this.rcvd, this.excs);
@@ -87,10 +80,7 @@ abstract class AbstractDropTest implements PacketProcessingListener, AutoCloseab
 
         RCVD_UPDATER.incrementAndGet(this);
 
-        NodeKey nodeKey = notification.getIngress().getValue().firstKeyOf(Node.class, NodeKey.class);
-        int nodeIdHash = nodeKey.getId().getValue().hashCode();
-
-        executorServiceLot[nodeIdHash % PROCESSING_THREAD_NUMBER].submit(new Runnable() {
+        executorService.submit(new Runnable() {
 
             @Override
             public void run() {
@@ -164,8 +154,6 @@ abstract class AbstractDropTest implements PacketProcessingListener, AutoCloseab
 
     @Override
     public void close() {
-        for (ExecutorService service : executorServiceLot) {
-            service.shutdown();
-        }
+        executorService.shutdown();
     }
 }
