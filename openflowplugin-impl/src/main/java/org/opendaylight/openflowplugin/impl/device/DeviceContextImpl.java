f985e53ae49aff53f09a82f69bab70e293593a13
Martin Bobak <mbobak@cisco.com>
2015-05-20 17:56:23 +0200

    device context holds OQHandler registration
    Handler registration is closed when device context is closed.
    
    Change-Id: Ib4a58c943789c5505542858767e5b6311bea3f3b
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
@@ -28,6 +28,7 @@ import org.opendaylight.controller.md.sal.binding.api.ReadTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.openflowjava.protocol.api.connection.ConnectionAdapter;
 import org.opendaylight.openflowjava.protocol.api.connection.OutboundQueue;
+import org.opendaylight.openflowjava.protocol.api.connection.OutboundQueueHandlerRegistration;
 import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
@@ -107,6 +108,7 @@ public class DeviceContextImpl implements DeviceContext {
     private volatile boolean filteringPacketIn = false;
     private final Object throttlingLock = new Object();
     private int filteringHighWaterMark = 0;
+    private OutboundQueueHandlerRegistration outboundQueueHandlerRegistration;
 
     @Override
     public MultiMsgCollector getMultiMsgCollector() {
@@ -348,6 +350,8 @@ public class DeviceContextImpl implements DeviceContext {
     public void close() {
         deviceState.setValid(false);
 
+        outboundQueueHandlerRegistration.close();
+
         LOG.trace("Removing node {} from operational DS.", getDeviceState().getNodeId());
         addDeleteToTxChain(LogicalDatastoreType.OPERATIONAL, getDeviceState().getNodeInstanceIdentifier());
 
@@ -437,5 +441,10 @@ public class DeviceContextImpl implements DeviceContext {
         for (ConnectionContext switchAuxConnectionContext : auxiliaryConnectionContexts.values()) {
             switchAuxConnectionContext.getConnectionAdapter().setPacketInFiltering(false);
         }
+    }
+
+    @Override
+    public void registerOutboundQueueHandler(final OutboundQueueHandlerRegistration outboundQueueHandlerRegistration) {
+        this.outboundQueueHandlerRegistration = outboundQueueHandlerRegistration;
     }
 }
