0ca8b89dc05a0fdba513ea0c0479432aa5400540
Martin Bobak <mbobak@cisco.com>
2015-05-21 14:58:01 +0200

    more logging in drop test added
    Change-Id: Idce5769c4440e1192ebbd355d0ec154aa36e9a5a
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/DropTestStats.java
+++ test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/DropTestStats.java
@@ -6,13 +6,18 @@ public class DropTestStats {
     private final int excs;
     private volatile int ftrSuccess;
     protected volatile int ftrFailed;
+    private final int runablesExecuted;
+    private final int runablesRejected;
+
     private final String message;
 
     public DropTestStats(int sent, int rcvd) {
         this.sent = sent;
         this.rcvd = rcvd;
         this.excs = 0;
+        this.runablesExecuted = 0;
         this.message = null;
+        runablesRejected = 0;
     }
 
     public DropTestStats(int sent, int rcvd, int excs) {
@@ -20,42 +25,60 @@ public class DropTestStats {
         this.rcvd = rcvd;
         this.excs = excs;
         this.message = null;
+        this.runablesExecuted = 0;
+        runablesRejected = 0;
     }
-    public DropTestStats(int sent, int rcvd, int excs, int ftrFailed, int ftrSuccess) {
+
+    public DropTestStats(int sent, int rcvd, int excs, int ftrFailed, int ftrSuccess, int runablesExecuted, int runablesRejected) {
         this.sent = sent;
         this.rcvd = rcvd;
         this.excs = excs;
         this.ftrFailed = ftrFailed;
         this.ftrSuccess = ftrSuccess;
         this.message = null;
+        this.runablesExecuted = runablesExecuted;
+        this.runablesRejected = runablesRejected;
+        runablesRejected = 0;
     }
 
-        public DropTestStats(String message) {
+    public DropTestStats(String message) {
         this.sent = -1;
         this.rcvd = -1;
         this.excs = -1;
         this.message = message;
+        this.runablesExecuted = -1;
+        runablesRejected = 0;
     }
 
-    public int getSent() { return this.sent; }
-    public int getRcvd() { return this.rcvd; }
-    public String getMessage() { return this.message; }
+    public int getSent() {
+        return this.sent;
+    }
+
+    public int getRcvd() {
+        return this.rcvd;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
         if (this.message == null) {
-            result.append("Rcvd: ");
+            result.append("\n Rcvd:");
             result.append(this.rcvd);
-            result.append(", Sent: ");
+            result.append("\n Sent: ");
             result.append(this.sent);
-            result.append("; Exceptions: ");
+            result.append("\n Exceptions: ");
             result.append(this.excs);
 
-            result.append("future success :");
+            result.append("\n future success :");
             result.append(this.ftrSuccess);
-            result.append("future failed :");
+            result.append("\n future failed :");
             result.append(this.ftrFailed);
+            result.append("\n run() executions :");
+            result.append(this.runablesExecuted);
 
         } else {
             result.append(this.message);
