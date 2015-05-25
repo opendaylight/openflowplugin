8f5238390c79f7b36e52b43ecf6229ef3a8438dc
Robert Varga <rovarga@cisco.com>
2015-05-25 16:33:36 +0200

    Make sure RpcResultBuilder is local
    There is no need for it to be a field.
    
    Change-Id: Ia046491699f8c9af8b9b1a3e5e45734567d55d67
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/PacketProcessingServiceImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/PacketProcessingServiceImpl.java
@@ -46,7 +46,6 @@ public class PacketProcessingServiceImpl extends CommonService implements Packet
                 final OutboundQueue outboundQueue = getDeviceContext().getPrimaryConnectionContext().getOutboundQueueProvider();
 
                 outboundQueue.commitEntry(xid.getValue(), message, new FutureCallback<OfHeader>() {
-
                     @Override
                     public void onSuccess(final OfHeader ofHeader) {
                         getMessageSpy().spyMessage(message.getImplementedInterface(), MessageSpy.STATISTIC_GROUP.TO_SWITCH_SUBMIT_SUCCESS);
