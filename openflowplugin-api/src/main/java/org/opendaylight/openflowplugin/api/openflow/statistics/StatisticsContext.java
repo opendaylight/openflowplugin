49be1f45b5fabc81828c289f5237592ab652b90a
Vaclav Demcak <vdemcak@cisco.com>
2015-05-27 11:43:26 +0200

    Bug 3367 - StatManagerImpl scalable statPolling per stat type on device
    Note: Running all stats for all device statistics in one time is
    realy not good idea. So we'd like to bring a possibility for
    scalable statistics aproach (one stat in one device in one time).
    
    * remove gatherDynamicData(MultipartType multipartType) method
    
    Change-Id: I3078cfe871b28fd11c09b7001f783b8e2cfbaa0d
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/statistics/StatisticsContext.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/statistics/StatisticsContext.java
@@ -9,9 +9,7 @@
 package org.opendaylight.openflowplugin.api.openflow.statistics;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import javax.annotation.CheckForNull;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
 
 /**
  * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 27.2.2015.
@@ -19,7 +17,5 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev13
 public interface StatisticsContext extends RequestContextStack, AutoCloseable {
 
     ListenableFuture<Boolean> gatherDynamicData();
-
-    ListenableFuture<Boolean> gatherDynamicData(@CheckForNull MultipartType multipartType);
 
 }
