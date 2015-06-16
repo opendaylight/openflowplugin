c67c3f0f42a4f5ddccc58273adc19b592ac6c116
Martin Bobak <mbobak@cisco.com>
2015-06-16 11:24:03 +0200

    drop test balances load in single thread per one switch
    Change-Id: Ie6e0376641b652626a146470462621f08d379d56
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/DropTestRpcProvider.java
+++ test-common/src/main/java/org/opendaylight/openflowplugin/testcommon/DropTestRpcProvider.java
@@ -7,15 +7,23 @@
  */
 package org.opendaylight.openflowplugin.testcommon;
 
+import java.util.Set;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.NotificationService;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * provides activation and deactivation of drop responder service - responds on packetIn
  */
-public class DropTestRpcProvider implements AutoCloseable {
+public class DropTestRpcProvider implements AutoCloseable, DataChangeListener {
     private static final Logger LOG = LoggerFactory.getLogger(DropTestDsProvider.class);
 
     private SalFlowService flowService;
@@ -82,5 +90,34 @@ public class DropTestRpcProvider implements AutoCloseable {
      */
     public boolean isActive() {
         return active;
+    }
+
+    @Override
+    public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+        final Set<InstanceIdentifier<?>> createdNodes = change.getCreatedData().keySet();
+        final Set<InstanceIdentifier<?>> removedPaths = change.getRemovedPaths();
+
+        if (null != createdNodes) {
+            for (InstanceIdentifier<?> path : createdNodes) {
+                NodeId nodeId = getNodeId(path);
+                LOG.debug("Node {} was added to inventory.", nodeId);
+                commiter.createExecutorForNode(nodeId);
+            }
+        }
+
+        if (null != removedPaths) {
+            for (InstanceIdentifier<?> path : removedPaths) {
+                NodeId nodeId = getNodeId(path);
+                LOG.debug("Node {} was removed from inventory.", nodeId);
+                commiter.removeNodesExecutor(nodeId);
+            }
+        }
+
+
+    }
+
+    private NodeId getNodeId(final InstanceIdentifier<?> path) {
+        NodeKey nodeKey = path.firstKeyOf(Node.class, NodeKey.class);
+        return nodeKey.getId();
     }
 }
