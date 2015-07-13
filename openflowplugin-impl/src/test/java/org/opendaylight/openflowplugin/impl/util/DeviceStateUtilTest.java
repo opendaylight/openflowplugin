c58488a16464dac4a4ace1eb1add7c98597561aa
Jozef Gloncak <jgloncak@cisco.com>
2015-07-13 11:23:53 +0200

    Coverage - registrator and translator libraty util.
    Increasing code coverage for classes:
    - MdSalRegistratorUtils
    - TranslatorLibratyUtil
    - DeviceStateUtil
    - MatchUtil
    
    Cosmetics.
    
    Change-Id: I193862f8a2d9e96e719fc1b1185140230a7d31eb
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/DeviceStateUtilTest.java
@@ -0,0 +1,80 @@
+/*
+ *
+ *  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ */
+
+package org.opendaylight.openflowplugin.impl.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.Capabilities;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.CapabilitiesV10;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeviceStateUtilTest {
+
+    @Mock
+    private DeviceState mockedDeviceState;
+
+    @After
+    public void tearDown() {
+        Mockito.verifyNoMoreInteractions(mockedDeviceState);
+    }
+
+    @Test
+    public void setDeviceStateBasedOnV13CapabilitiesTest() {
+        final Capabilities dummyCapabilities =  new Capabilities(false,false,false,false,false,false,false);
+
+        DeviceStateUtil.setDeviceStateBasedOnV13Capabilities(mockedDeviceState, dummyCapabilities);
+
+        verify(mockedDeviceState).setFlowStatisticsAvailable(false);
+        verify(mockedDeviceState).setTableStatisticsAvailable(false);
+        verify(mockedDeviceState).setPortStatisticsAvailable(false);
+        verify(mockedDeviceState).setQueueStatisticsAvailable(false);
+        verify(mockedDeviceState).setGroupAvailable(false);
+    }
+
+    @Test
+    public void setDeviceStateBasedOnV10CapabilitiesTest() {
+        CapabilitiesV10 dummyCapabilitiesV10 = new CapabilitiesV10(false, false, false, false, false, false, false, false);
+
+        DeviceStateUtil.setDeviceStateBasedOnV10Capabilities(mockedDeviceState, dummyCapabilitiesV10);
+        verify(mockedDeviceState).setFlowStatisticsAvailable(false);
+        verify(mockedDeviceState).setTableStatisticsAvailable(false);
+        verify(mockedDeviceState).setPortStatisticsAvailable(false);
+        verify(mockedDeviceState).setQueueStatisticsAvailable(false);
+    }
+
+    @Test
+    public void createNodeInstanceIdentifierTest() {
+        final NodeId nodeId = new NodeId("dummyId");
+        final KeyedInstanceIdentifier<Node, NodeKey> expectedII = InstanceIdentifier.create(Nodes.class).child(Node
+                .class, new NodeKey(nodeId));
+
+        final KeyedInstanceIdentifier<Node, NodeKey> nodeInstanceIdentifier = DeviceStateUtil
+                .createNodeInstanceIdentifier(nodeId);
+        assertEquals(expectedII, nodeInstanceIdentifier);
+    }
+}
---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/TranslatorLibratyUtilTest.java
@@ -0,0 +1,85 @@
+/*
+ *
+ *  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ */
+
+package org.opendaylight.openflowplugin.impl.util;
+
+import static org.junit.Assert.assertNotNull;
+import static org.opendaylight.openflowplugin.api.OFConstants.OFP_VERSION_1_0;
+import static org.opendaylight.openflowplugin.api.OFConstants.OFP_VERSION_1_3;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.openflowplugin.api.openflow.device.MessageTranslator;
+import org.opendaylight.openflowplugin.api.openflow.device.TranslatorLibrary;
+import org.opendaylight.openflowplugin.api.openflow.translator.TranslatorLibrarian;
+import org.opendaylight.openflowplugin.impl.translator.TranslatorKeyFactory;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketIn;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortGrouping;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.MultipartReplyAggregateCase;
+
+public class TranslatorLibratyUtilTest {
+
+    private static class TranslatorLibrarianTestImpl implements TranslatorLibrarian {
+
+        private TranslatorLibrary translatorLibrary;
+
+        @Override
+        public TranslatorLibrary oook() {
+            return translatorLibrary;
+        }
+
+        @Override
+        public void setTranslatorLibrary(TranslatorLibrary translatorLibrary) {
+            this.translatorLibrary = translatorLibrary;
+        }
+    }
+
+    private TranslatorLibrarianTestImpl translatorLibrarian;
+
+    @Before
+    public void setUp() {
+        translatorLibrarian = new TranslatorLibrarianTestImpl();
+    }
+
+    @Test
+    public void setBasicTranslatorLibraryTest() {
+        TranslatorLibraryUtil.setBasicTranslatorLibrary(translatorLibrarian);
+        TranslatorLibrary translatorLibrary = translatorLibrarian.oook();
+
+        TranslatorKeyFactory of13TranslatorKeyFactory = new TranslatorKeyFactory(OFP_VERSION_1_3);
+        TranslatorKeyFactory of10TranslatorKeyFactory = new TranslatorKeyFactory(OFP_VERSION_1_0);
+
+        MessageTranslator<Object, Object> translator;
+        translator = translatorLibrary.lookupTranslator(of13TranslatorKeyFactory
+                .createTranslatorKey(PacketIn.class));
+        assertNotNull(translator);
+
+        translator = translatorLibrary.lookupTranslator(of13TranslatorKeyFactory.createTranslatorKey(PortGrouping
+                .class));
+        assertNotNull(translator);
+
+        translator = translatorLibrary.lookupTranslator(of13TranslatorKeyFactory.createTranslatorKey
+                (MultipartReplyAggregateCase
+                .class));
+        assertNotNull(translator);
+
+        translator = translatorLibrary.lookupTranslator(of10TranslatorKeyFactory.createTranslatorKey(PacketIn.class));
+        assertNotNull(translator);
+
+        translator = translatorLibrary.lookupTranslator(of10TranslatorKeyFactory.createTranslatorKey(PortGrouping.class));
+        assertNotNull(translator);
+
+        translator = translatorLibrary.lookupTranslator(of10TranslatorKeyFactory.createTranslatorKey(MultipartReplyAggregateCase
+                .class));
+        assertNotNull(translator);
+    }
+
+}
