931b6a20f80d9a52c5dace685bbf1eacdba4a86f
Jozef Gloncak <jgloncak@cisco.com>
2015-07-13 11:23:53 +0200

    Coverage - registrator and translator libraty util.
    Increasing code coverage for classes:
    - MdSalRegistratorUtils
    - TranslatorLibratyUtil
    - DeviceStateUtil
    - MatchUtil
    
    Change-Id: I193862f8a2d9e96e719fc1b1185140230a7d31eb
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/MatchUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ *
+ *  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ */
+
+package org.opendaylight.openflowplugin.impl.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.FlowWildcardsV10;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.v10.grouping.MatchV10Builder;
+
+public class MatchUtilTest {
+
+    private static final MacAddress ZERO_MAC_ADDRESS = new MacAddress("00:00:00:00:00:00");
+    private static final Ipv4Address ZERO_IPV4_ADDRESS = new Ipv4Address("0.0.0.0");
+
+    @Test
+    public void createEmptyV10MatchTest() {
+        MatchV10Builder expectedV10Match = expectedV10Match();
+        MatchV10Builder emptyV10Match = MatchUtil.createEmptyV10Match();
+        assertEquals(expectedV10Match.build(),emptyV10Match.build());
+    }
+
+    private MatchV10Builder expectedV10Match() {
+        Short zeroShort = Short.valueOf("0");
+        Integer zeroInteger = Integer.valueOf(0);
+        MatchV10Builder matchV10Builder = new MatchV10Builder();
+        matchV10Builder.setDlDst( ZERO_MAC_ADDRESS);
+        matchV10Builder.setDlSrc(ZERO_MAC_ADDRESS);
+        matchV10Builder.setDlType(zeroInteger);
+        matchV10Builder.setDlVlan(zeroInteger);
+        matchV10Builder.setDlVlanPcp(zeroShort);
+        matchV10Builder.setInPort(zeroInteger);
+        matchV10Builder.setNwDst(ZERO_IPV4_ADDRESS);
+        matchV10Builder.setNwDstMask(zeroShort);
+        matchV10Builder.setNwProto(zeroShort);
+        matchV10Builder.setNwSrc(ZERO_IPV4_ADDRESS);
+        matchV10Builder.setNwSrcMask(zeroShort);
+        matchV10Builder.setNwTos(zeroShort);
+        matchV10Builder.setTpDst(zeroInteger);
+        matchV10Builder.setTpSrc(zeroInteger);
+        FlowWildcardsV10 flowWildcardsV10 = new FlowWildcardsV10(true, true, true, true, true, true, true, true, true, true);
+        matchV10Builder.setWildcards(flowWildcardsV10);
+        return matchV10Builder;
+    }
+}
---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/MdSalRegistratorUtilsTest.java
@@ -0,0 +1,54 @@
+/*
+ *
+ *  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ */
+
+package org.opendaylight.openflowplugin.impl.util;
+
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+
+import java.math.BigInteger;
+import org.junit.Test;
+
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.rpc.RpcContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
+import org.opendaylight.yangtools.yang.binding.RpcService;
+
+public class MdSalRegistratorUtilsTest {
+
+    /**
+     * Number of currently registrated services (can be changed) in {@link MdSalRegistratorUtils#registerServices
+     * (RpcContext, DeviceContext)}
+     */
+    private static final int NUMBER_OF_RPC_SERVICE_REGISTRATION = 9;
+
+    @Test
+    public void registerServiceTest() {
+
+        final RpcContext mockedRpcContext = mock(RpcContext.class);
+        final DeviceContext mockedDeviceContext = mock(DeviceContext.class);
+        final ConnectionContext mockedConnectionContext = mock(ConnectionContext.class);
+
+        final FeaturesReply mockedFeatures = mock(FeaturesReply.class);
+        when(mockedConnectionContext.getFeatures()).thenReturn(mockedFeatures);
+
+        final BigInteger mockedDataPathId = mock(BigInteger.class);
+        when(mockedFeatures.getDatapathId()).thenReturn(mockedDataPathId);
+
+        when(mockedDeviceContext.getPrimaryConnectionContext()).thenReturn(mockedConnectionContext);
+        MdSalRegistratorUtils.registerServices(mockedRpcContext,mockedDeviceContext);
+        verify(mockedRpcContext, times(NUMBER_OF_RPC_SERVICE_REGISTRATION)).registerRpcServiceImplementation(any
+                        (RpcService.class.getClass()), any(RpcService.class));
+    }
+
+}
