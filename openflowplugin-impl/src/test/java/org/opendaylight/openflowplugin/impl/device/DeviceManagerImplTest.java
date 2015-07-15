b42921d394ec2a90e639ea446a200dea46075db4
Jozef Gloncak <jgloncak@cisco.com>
2015-07-15 15:50:39 +0200

    Coverage - DeviceManagerImpl.
    Increase of code coverage for clase DeviceManagerImpl.
    
    Change-Id: I0dbee99d7ba299ec2a6a7052a70a3cc28e979bd9
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/DeviceManagerImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/DeviceManagerImplTest.java
@@ -1,99 +1,147 @@
-/**
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+/*
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
  */
+
 package org.opendaylight.openflowplugin.impl.device;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.google.common.util.concurrent.CheckedFuture;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
+import java.util.concurrent.ExecutionException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.openflowjava.protocol.api.connection.ConnectionAdapter;
+import org.opendaylight.openflowjava.protocol.api.connection.OutboundQueueHandlerRegistration;
+import org.opendaylight.openflowplugin.api.OFConstants;
 import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
-import org.opendaylight.openflowplugin.api.openflow.rpc.RpcManager;
+import org.opendaylight.openflowplugin.api.openflow.connection.OutboundQueueProvider;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
+import org.opendaylight.openflowplugin.api.openflow.device.TranslatorLibrary;
+import org.opendaylight.openflowplugin.api.openflow.md.core.TranslatorKey;
 import org.opendaylight.openflowplugin.api.openflow.statistics.ofpspecific.MessageIntelligenceAgency;
+import org.opendaylight.openflowplugin.impl.device.DeviceManagerImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartRequestInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.MultipartRequestDescCase;
 
 
-/**
- * test of {@link DeviceManagerImpl} - lightweight version, using basic ways (TDD)
- */
+
 @RunWith(MockitoJUnitRunner.class)
 public class DeviceManagerImplTest {
 
-    private DeviceManagerImpl deviceManager;
-    @Mock
-    private ConnectionContext connectionContext;
-    @Mock
-    private ConnectionAdapter connectionAdapter;
-    @Mock
-    private FeaturesReply features;
-    @Mock
-    private DataBroker dataBroker;
-    @Mock
-    private RpcManager rpcManager;
-    @Mock
-    private WriteTransaction wTx;
-    @Mock
-    private MessageIntelligenceAgency messageIntelligenceAgency;
-    @Mock
-    private CheckedFuture<Void, TransactionCommitFailedException> checkedFuture;
+    private static final boolean TEST_VALUE_SWITCH_FEATURE_MANDATORY = true;
+    private static final long TEST_VALUE_GLOBAL_NOTIFICATION_QUOTA = 2000l;
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        Mockito.when(connectionContext.getConnectionAdapter()).thenReturn(connectionAdapter);
-        Mockito.when(connectionContext.getFeatures()).thenReturn(features);
-        Mockito.when(features.getVersion()).thenReturn((short) 42);
-        Mockito.when(dataBroker.newWriteOnlyTransaction()).thenReturn(wTx);
-        Mockito.when(wTx.submit()).thenReturn(checkedFuture);
-        Mockito.when(checkedFuture.get()).thenReturn((Void) null);
-
-    }
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     * Test method for {@link org.opendaylight.openflowplugin.impl.device.DeviceManagerImpl#deviceConnected(org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext)}.
-     */
-    @Test
-    @Ignore // FIXME : fix the test ASAP
-    public void testDeviceConnected() {
-        deviceManager.deviceConnected(connectionContext);
-
-        final ArgumentCaptor<MultipartRequestInput> mpInputCaptor = ArgumentCaptor.forClass(MultipartRequestInput.class);
-        Mockito.verify(connectionAdapter).multipartRequest(mpInputCaptor.capture());
-
-        Assert.assertTrue(mpInputCaptor.getAllValues().get(0).getMultipartRequestBody() instanceof MultipartRequestDescCase);
-        //Assert.assertTrue(mpInputCaptor.getAllValues().get(1).getMultipartRequestBody() instanceof MultipartRequestGroupDescCase);
-    }
-
+    @Mock
+    CheckedFuture<Void,TransactionCommitFailedException> mockedFuture;
 
     @Test
-    public void testHookRequest() {
-
+    public void onDeviceContextLevelUpFailTest() {
+        onDeviceContextLevelUp(true);
     }
+
+    @Test
+    public void onDeviceContextLevelUpSuccessTest() {
+        onDeviceContextLevelUp(false);
+    }
+
+    private DeviceManagerImpl prepareDeviceManager() {
+        return prepareDeviceManager(false);
+    }
+
+    private DeviceManagerImpl prepareDeviceManager(boolean withException) {
+        DataBroker mockedDataBroker = mock(DataBroker.class);
+        WriteTransaction mockedWriteTransaction = mock(WriteTransaction.class);
+
+        BindingTransactionChain mockedTxChain = mock(BindingTransactionChain.class);
+        WriteTransaction mockedWTx = mock(WriteTransaction.class);
+        when(mockedTxChain.newWriteOnlyTransaction()).thenReturn(mockedWTx);
+        when(mockedDataBroker.createTransactionChain(any(TransactionChainListener.class))).thenReturn
+                (mockedTxChain);
+        when(mockedDataBroker.newWriteOnlyTransaction()).thenReturn(mockedWriteTransaction);
+
+        when(mockedWriteTransaction.submit()).thenReturn(mockedFuture);
+
+        MessageIntelligenceAgency mockedMessageIntelligenceAgency = mock(MessageIntelligenceAgency.class);
+        DeviceManagerImpl deviceManager = new DeviceManagerImpl(mockedDataBroker, mockedMessageIntelligenceAgency, TEST_VALUE_SWITCH_FEATURE_MANDATORY,
+                TEST_VALUE_GLOBAL_NOTIFICATION_QUOTA);
+
+        return deviceManager;
+    }
+
+    public void onDeviceContextLevelUp(boolean withException) {
+        DeviceManagerImpl deviceManager = prepareDeviceManager(withException);
+
+        DeviceContextImpl mockedDeviceContext = mock(DeviceContextImpl.class);
+        if (withException) {
+            doThrow(new IllegalStateException("dummy")).when(mockedDeviceContext).initialSubmitTransaction();
+            DeviceState mockedDeviceState = mock(DeviceState.class);
+            when(mockedDeviceContext.getDeviceState()).thenReturn(mockedDeviceState);
+        }
+
+        deviceManager.onDeviceContextLevelUp(mockedDeviceContext);
+        if (withException) {
+            verify(mockedDeviceContext).close();
+        } else {
+            verify(mockedDeviceContext).initialSubmitTransaction();
+            verify(mockedDeviceContext).onPublished();
+        }
+    }
+
+    @Test
+    public void deviceConnectedTest() {
+    }
+
+    @Test
+    public void deviceConnectedNewConnectionTest() {
+        DeviceManagerImpl deviceManager = prepareDeviceManager();
+        TranslatorLibrary mockedTraslatorLibrary = mock(TranslatorLibrary.class);
+        mockedTraslatorLibrary.lookupTranslator(any(TranslatorKey.class));
+
+        deviceManager.setTranslatorLibrary(mockedTraslatorLibrary);
+
+        ConnectionContext mockConnectionContext = mock(ConnectionContext.class);
+        when(mockConnectionContext.getNodeId()).thenReturn(new NodeId("dummyNodeId"));
+
+
+        FeaturesReply mockFeatures = mock(FeaturesReply.class);
+//        when(mockFeatures.getVersion()).thenReturn(OFConstants.OFP_VERSION_1_0);
+        when(mockConnectionContext.getFeatures()).thenReturn(mockFeatures);
+
+
+        ConnectionAdapter mockedConnectionAdapter = mock(ConnectionAdapter.class);
+        when(mockConnectionContext.getConnectionAdapter()).thenReturn(mockedConnectionAdapter);
+
+        deviceManager.deviceConnected(mockConnectionContext);
+
+        InOrder order = inOrder(mockConnectionContext);
+        order.verify(mockConnectionContext).getFeatures();
+        order.verify(mockConnectionContext).setOutboundQueueProvider(any(OutboundQueueProvider.class));
+        order.verify(mockConnectionContext).setOutboundQueueHandleRegistration(any(OutboundQueueHandlerRegistration
+                .class));
+        order.verify(mockConnectionContext).getNodeId();
+        order.verify(mockConnectionContext).setDeviceDisconnectedHandler(any(DeviceContext.class));
+    }
+
 
 }
