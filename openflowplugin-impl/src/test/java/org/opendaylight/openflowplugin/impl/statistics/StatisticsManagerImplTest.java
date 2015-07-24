b8555a387a93113905ce635d72950149b066314e
Jozef Gloncak <jgloncak@cisco.com>
2015-07-24 14:47:14 +0200

    Coverage - StatisticsManagerImpl.
    Increase code coverage for StatisticsManagerImpl.
    
    Change-Id: I7c61449766c7400748e19ee8b3bdcb98ee7fc924
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    (cherry picked from commit aee437fdbdc903fba11b9c853722e41ae9ce15b9)

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsManagerImplTest.java
@@ -0,0 +1,77 @@
+package org.opendaylight.openflowplugin.impl.statistics;
+
+import io.netty.util.HashedWheelTimer;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.openflowjava.protocol.api.connection.ConnectionAdapter;
+import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceInitializationPhaseHandler;
+import org.opendaylight.openflowplugin.api.openflow.statistics.ofpspecific.MessageSpy;
+import org.opendaylight.openflowplugin.impl.registry.flow.DeviceFlowRegistryImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
+
+import java.math.BigInteger;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class StatisticsManagerImplTest extends TestCase {
+
+    private static final BigInteger DUMMY_DATAPATH_ID = new BigInteger("444");
+    private static final Short DUMMY_VERSION = OFConstants.OFP_VERSION_1_3;
+
+    @Mock
+    RequestContextStack mockedRequestContextStack;
+    @Mock
+    ConnectionContext mockedPrimConnectionContext;
+    @Mock
+    FeaturesReply mockedFeatures;
+    @Mock
+    ConnectionAdapter mockedConnectionAdapter;
+    @Mock
+    MessageSpy mockedMessagSpy;
+    @Mock
+    DeviceContext mockedDeviceContext;
+    @Mock
+    DeviceState mockedDeviceState;
+    @Mock
+    DeviceInitializationPhaseHandler mockedDevicePhaseHandler;
+
+    @Before
+    public void initialization() {
+        when(mockedFeatures.getDatapathId()).thenReturn(DUMMY_DATAPATH_ID);
+        when(mockedFeatures.getVersion()).thenReturn(DUMMY_VERSION);
+
+        when(mockedPrimConnectionContext.getFeatures()).thenReturn(mockedFeatures);
+        when(mockedPrimConnectionContext.getConnectionAdapter()).thenReturn(mockedConnectionAdapter);
+        when(mockedPrimConnectionContext.getConnectionState()).thenReturn(ConnectionContext.CONNECTION_STATE.WORKING);
+
+        when(mockedDeviceContext.getPrimaryConnectionContext()).thenReturn(mockedPrimConnectionContext);
+        when(mockedDeviceContext.getMessageSpy()).thenReturn(mockedMessagSpy);
+        when(mockedDeviceContext.getDeviceFlowRegistry()).thenReturn(new DeviceFlowRegistryImpl());
+        when(mockedDeviceContext.getDeviceState()).thenReturn(mockedDeviceState);
+        when(mockedDeviceContext.getTimer()).thenReturn(mock(HashedWheelTimer.class));
+    }
+
+    @Test
+    public void testOnDeviceContextLevelUp() throws Exception {
+        final StatisticsManagerImpl statisticsManager = new StatisticsManagerImpl();
+        statisticsManager.setDeviceInitializationPhaseHandler(mockedDevicePhaseHandler);
+        statisticsManager.onDeviceContextLevelUp(mockedDeviceContext);
+        verify(mockedDeviceState).setDeviceSynchronized(eq(true));
+
+        statisticsManager.onDeviceContextClosed(mockedDeviceContext);
+    }
+}
