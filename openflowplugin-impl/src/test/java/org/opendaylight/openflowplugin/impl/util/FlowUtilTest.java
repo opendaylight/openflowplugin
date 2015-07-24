c5c7f1b19b81e69e1b46a0c26f86113b42e9dc5b
Jozef Gloncak <jgloncak@cisco.com>
2015-07-24 11:43:56 +0200

    Coverage - fix error in FlowUtilTest.
    If method createAlienFlowId() was called before this test it caused that
    dummy alien ID didn't contain index 1 but index 2 and test failed.
    
    Currently test calls createAlienFlowId() method twice and then compares
    whether index suffix from first call is smaller then index from second call.
    
    Change-Id: Ie8e1e1ab7825e266e94045a30c9d63770ef33611
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/FlowUtilTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/util/FlowUtilTest.java
@@ -12,18 +12,38 @@
 package org.opendaylight.openflowplugin.impl.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FlowUtilTest {
 
     private static final short DUMMY_TABLE_ID = 1;
+    public static final Pattern INDEX_PATTERN = Pattern.compile("^#UF\\$TABLE\\*1-([0-9]+)$");
 
     @Test
     public void createAlienFlowIdTest() {
-        FlowId alienFlowId = FlowUtil.createAlienFlowId(DUMMY_TABLE_ID);
-        assertEquals("#UF$TABLE*1-1", alienFlowId.getValue());
+        final String alienFlowId1 = FlowUtil.createAlienFlowId(DUMMY_TABLE_ID).getValue();
+        final Integer index1 = parseIndex(alienFlowId1);
+        final String alienFlowId2 = FlowUtil.createAlienFlowId(DUMMY_TABLE_ID).getValue();
+        final Integer index2 = parseIndex(alienFlowId2);
+
+        assertNotNull("index1 parsing failed: "+alienFlowId1, index1);
+        assertNotNull("index2 parsing failed: "+alienFlowId2, index2);
+        assertTrue(index1 < index2);
+    }
+
+    private static Integer parseIndex(String alienFlowIdValue) {
+        final Matcher mach = INDEX_PATTERN.matcher(alienFlowIdValue);
+        if (mach.find()) {
+            return Integer.valueOf(mach.group(1));
+        }
+        return null;
     }
 
 }
