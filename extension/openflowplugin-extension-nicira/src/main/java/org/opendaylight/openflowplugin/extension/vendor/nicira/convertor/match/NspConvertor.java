7c6abf48a3c1d6b87711d31cc67e9a272fb839d1
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 18:22:01 -0400

    Fix license header violations in extensions/openflowplugin
    Change-Id: I207ce541e33b4686ffbc70b637684ac6c0f0b784
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    (cherry picked from commit 706746a3792f3cc66b328fdbe1cc887ac310c36e)

--- extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/NspConvertor.java
+++ extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/NspConvertor.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2014 Red Hat, Inc.
+ * Copyright (c) 2014, 2015 Red Hat, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Authors : Madhu Venugopal
  */
+
 package org.opendaylight.openflowplugin.extension.vendor.nicira.convertor.match;
 
 import com.google.common.base.Optional;
