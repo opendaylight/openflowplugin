58f2699d7372124cd59cb73ad133d21529d2c18d
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 18:32:00 -0400

    Fix license header violations in applications
    Change-Id: I3cdee24a89daaa84c8016d084ce5fd6820d8e1bf
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    (cherry picked from commit 0f443f45df5b16d901d8cdc0e54fa00e1ff72e6c)

--- applications/of-switch-config-pusher/src/main/java/org/opendaylight/openflowplugin/openflow/ofswitch/config/DefaultConfigPusher.java
+++ applications/of-switch-config-pusher/src/main/java/org/opendaylight/openflowplugin/openflow/ofswitch/config/DefaultConfigPusher.java
@@ -1,10 +1,12 @@
-package org.opendaylight.openflowplugin.openflow.ofswitch.config;/*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+/*
+ * Copyright (c) 2014, 2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
+package org.opendaylight.openflowplugin.openflow.ofswitch.config;
 
 import java.util.Set;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
