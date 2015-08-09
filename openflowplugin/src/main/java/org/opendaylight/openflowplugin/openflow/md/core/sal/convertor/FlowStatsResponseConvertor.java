572b2fb668726a7868c6ee962ea13f15c1aae2b4
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 18:03:27 -0400

    Fix license header violations in openflowplugin
    Change-Id: I875803a56f7d9841bffc7acc9a06c488e0bfa010
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/FlowStatsResponseConvertor.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/FlowStatsResponseConvertor.java
@@ -1,10 +1,11 @@
 /*
- * Copyright IBM Corporation, 2013.  All rights reserved.
+ * Copyright (c) 2013, 2015 IBM Corporation and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor;
 
 import org.opendaylight.openflowplugin.extension.api.AugmentTuple;
@@ -82,15 +83,15 @@ public class FlowStatsResponseConvertor {
         }
         if(flowStats.getMatch() != null){
             MatchBuilder matchBuilder = MatchConvertorImpl.fromOFMatchToSALMatch(flowStats.getMatch(),datapathid, ofVersion);
-            
-            AugmentTuple<Match> matchExtensionWrap = 
+
+            AugmentTuple<Match> matchExtensionWrap =
                     MatchExtensionHelper.processAllExtensions(
                             flowStats.getMatch().getMatchEntry(), ofVersion, MatchPath.FLOWSSTATISTICSUPDATE_FLOWANDSTATISTICSMAPLIST_MATCH);
             if (matchExtensionWrap != null) {
                 matchBuilder.addAugmentation(matchExtensionWrap.getAugmentationClass(), matchExtensionWrap.getAugmentationObject());
             }
-            
-            
+
+
             salFlowStatsBuilder.setMatch(matchBuilder.build());
             salFlowStatsBuilder.setFlags(
                     new FlowModFlags(flowStats.getFlags().isOFPFFCHECKOVERLAP(),
