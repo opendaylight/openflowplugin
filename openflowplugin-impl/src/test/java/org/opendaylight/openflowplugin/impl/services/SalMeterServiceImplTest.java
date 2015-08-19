8ff15d56c9b671371c954d2e0110917ae1dc020e
Jozef Gloncak <jgloncak@cisco.com>
2015-08-19 12:13:17 +0200

    BUG-4084: Li:Save groups in operational DS based on barrier success
    ItemLifeCycleListener also added to SalGroupServiceImpl class to be able
    persist information about add/delete/update of grouping to operational DS.
    
    Change-Id: Ia2347af3133d7c68f54f206683d5dc981e81f9dd
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/services/SalMeterServiceImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/services/SalMeterServiceImplTest.java
@@ -1,20 +1,23 @@
 package org.opendaylight.openflowplugin.impl.services;
 
-import junit.framework.TestCase;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
-import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
-import org.opendaylight.openflowplugin.api.openflow.registry.meter.DeviceMeterRegistry;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.*;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.meter.update.UpdatedMeter;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterId;
-
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.opendaylight.openflowplugin.api.openflow.registry.meter.DeviceMeterRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.AddMeterInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.RemoveMeterInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.SalMeterService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.UpdateMeterInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.meter.update.UpdatedMeter;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterId;
 
 public class SalMeterServiceImplTest extends ServiceMocking {
 
@@ -43,8 +46,11 @@ public class SalMeterServiceImplTest extends ServiceMocking {
 
     @Test
     public void testUpdateMeter() throws Exception {
-        UpdatedMeter mockedUptatedMeter = mock(UpdatedMeter.class);
-        final UpdateMeterInput updateMeterInput = new UpdateMeterInputBuilder().setUpdatedMeter(mockedUptatedMeter).build();
+        final UpdatedMeter mockedUpdatedMeter = mock(UpdatedMeter.class);
+        Mockito.when(mockedUpdatedMeter.getMeterId()).thenReturn(new MeterId(43L));
+        Mockito.doReturn(UpdatedMeter.class).when(mockedUpdatedMeter).getImplementedInterface();
+
+        final UpdateMeterInput updateMeterInput = new UpdateMeterInputBuilder().setUpdatedMeter(mockedUpdatedMeter).build();
         salMeterService.updateMeter(updateMeterInput);
         verify(mockedRequestContextStack).createRequestContext();
     }
