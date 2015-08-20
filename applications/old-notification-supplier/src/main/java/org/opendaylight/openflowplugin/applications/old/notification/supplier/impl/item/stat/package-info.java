2fc612f5a5cc63068e1d3792f90755046f7537ae
Martin Bobak <mbobak@cisco.com>
2015-08-20 23:45:07 +0200

    BUG-4117: add support of Old Notif. for Flow Statistics
     - keep backward old notification compatibility for FlowCapableNode flow statistics
    
    Note: Original commit https://git.opendaylight.org/gerrit/#/c/25965/
    has to be broken to a chain of smaller commits
    
    Change-Id: I2254374adc3462bd628721080929e449350f4176
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ applications/old-notification-supplier/src/main/java/org/opendaylight/openflowplugin/applications/old/notification/supplier/impl/item/stat/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contains OF Statistics Items (Flow, Group, Meter ...) notification listener/supplier implementations
+ */
+package org.opendaylight.openflowplugin.applications.old.notification.supplier.impl.item.stat;
