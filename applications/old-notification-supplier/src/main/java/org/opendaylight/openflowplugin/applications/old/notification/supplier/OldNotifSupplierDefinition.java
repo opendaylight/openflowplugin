9c94939ff57c2f816b2978c9b763dd971ba932d2
Martin Bobak <mbobak@cisco.com>
2015-08-20 23:45:07 +0200

    BUG-4117: initial commit for new App Old-Notification-Supplier
     - we wish to keep backward compatibilility for all old Openflow notification
    
    Note: Original commit https://git.opendaylight.org/gerrit/#/c/25965/
          has to be broken to a chain of smaller commits
    
    Change-Id: I6a63329611a04299d09ba265077de1e12e29b4e8
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ applications/old-notification-supplier/src/main/java/org/opendaylight/openflowplugin/applications/old/notification/supplier/OldNotifSupplierDefinition.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.old.notification.supplier;
+
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Default definition for every Old Notification Supplier. Migration from old notification
+ * to {@link org.opendaylight.controller.md.sal.binding.api.DataChangeListener} has one
+ * keyed component - WildCarded Path which represent a changes checker in DataStoreTreeNode
+ *
+ * @param <O> - {@link DataObject} represent Data Tree Item from DataStore
+ */
+public interface OldNotifSupplierDefinition<O extends DataObject> extends AutoCloseable, DataChangeListener {
+
+    /**
+     * Method return wildCardPath for Listener registration and for identify
+     * the correct KeyInstanceIdentifier from Data Tree Item in DataStore;
+     *
+     * @return {@link InstanceIdentifier}
+     */
+    InstanceIdentifier<O> getWildCardPath();
+}
+
