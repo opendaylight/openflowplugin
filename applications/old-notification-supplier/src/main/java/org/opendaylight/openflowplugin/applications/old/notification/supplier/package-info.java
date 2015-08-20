ab4c96cc9a709fe3e2515cf47bb34016b475b415
Martin Bobak <mbobak@cisco.com>
2015-08-20 23:45:07 +0200

    BUG-4117: initial commit for new App Old-Notification-Supplier
     - we wish to keep backward compatibilility for all old Openflow notification
    
    Note: Original commit https://git.opendaylight.org/gerrit/#/c/25965/
          has to be broken to a chain of smaller commits
    
    Change-Id: I6a63329611a04299d09ba265077de1e12e29b4e8
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

---
+++ applications/old-notification-supplier/src/main/java/org/opendaylight/openflowplugin/applications/old/notification/supplier/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Base project package for a module Provider and base notification listener/supplier interface
+ */
+package org.opendaylight.openflowplugin.applications.old.notification.supplier;
