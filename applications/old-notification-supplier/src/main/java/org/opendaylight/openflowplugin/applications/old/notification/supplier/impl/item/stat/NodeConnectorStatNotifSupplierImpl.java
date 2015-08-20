9400647b237780f635c56ff5d7b1231f9a23aa51
Martin Bobak <mbobak@cisco.com>
2015-08-20 23:45:07 +0200

    BUG-4117: add support of Old Notif. for Statistics
     - keep backward old notification compatibility
    for FlowCapableNode subnode's statistics
    
    Note: Original commit https://git.opendaylight.org/gerrit/#/c/25965/
    has to be broken to a chain of smaller commit
    
    Change-Id: I38233abd2514c22a3e8cd66cf2b03e7d2f311136
    Signed-off-by: Martin Bobak <mbobak@cisco.com>
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>
    (cherry picked from commit 678005a358323e9fc081760cc30593121303c9bb)
    (cherry picked from commit 2aba0b6d99d47fb44812686627cc8f8aba9cbf6c)

---
+++ applications/old-notification-supplier/src/main/java/org/opendaylight/openflowplugin/applications/old/notification/supplier/impl/item/stat/NodeConnectorStatNotifSupplierImpl.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.old.notification.supplier.impl.item.stat;
+
+import com.google.common.base.Preconditions;
+import java.util.Collections;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.FlowCapableNodeConnectorStatisticsData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.NodeConnectorStatisticsUpdate;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.NodeConnectorStatisticsUpdateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.flow.capable.node.connector.statistics.FlowCapableNodeConnectorStatistics;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.node.connector.statistics.and.port.number.map.NodeConnectorStatisticsAndPortNumberMapBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Implementation define a contract between {@link FlowCapableNodeConnectorStatistics} data object
+ * and {@link NodeConnectorStatisticsUpdate} notification.
+ */
+public class NodeConnectorStatNotifSupplierImpl extends
+        AbstractNotifSupplierForItemStat<FlowCapableNodeConnectorStatistics, NodeConnectorStatisticsUpdate> {
+
+    private static final InstanceIdentifier<FlowCapableNodeConnectorStatistics> wildCardedInstanceIdent =
+            getNodeWildII().child(NodeConnector.class)
+                    .augmentation(FlowCapableNodeConnectorStatisticsData.class)
+                    .child(FlowCapableNodeConnectorStatistics.class);
+
+    /**
+     * Constructor register supplier as DataChangeLister and create wildCarded InstanceIdentifier.
+     *
+     * @param notifProviderService - {@link NotificationProviderService}
+     * @param db                   - {@link DataBroker}
+     */
+    public NodeConnectorStatNotifSupplierImpl(final NotificationProviderService notifProviderService, final DataBroker db) {
+        super(notifProviderService, db, FlowCapableNodeConnectorStatistics.class);
+    }
+
+    @Override
+    public InstanceIdentifier<FlowCapableNodeConnectorStatistics> getWildCardPath() {
+        return wildCardedInstanceIdent;
+    }
+
+    @Override
+    public NodeConnectorStatisticsUpdate createNotification(final FlowCapableNodeConnectorStatistics o,
+                                                            final InstanceIdentifier<FlowCapableNodeConnectorStatistics> path) {
+        Preconditions.checkArgument(o != null);
+        Preconditions.checkArgument(path != null);
+
+        final NodeConnectorBuilder ncBuilder = new NodeConnectorBuilder();
+        final NodeConnectorKey ncKey = path.firstKeyOf(NodeConnector.class, NodeConnectorKey.class);
+        ncBuilder.setId(ncKey.getId());
+        ncBuilder.setKey(ncKey);
+
+        final NodeConnectorStatisticsUpdateBuilder builder = new NodeConnectorStatisticsUpdateBuilder();
+        builder.setId(getNodeId(path));
+        builder.setMoreReplies(Boolean.FALSE);
+        builder.setNodeConnector(Collections.singletonList(ncBuilder.build()));
+        builder.setNodeConnectorStatisticsAndPortNumberMap(Collections
+                .singletonList(new NodeConnectorStatisticsAndPortNumberMapBuilder(o).build()));
+        return builder.build();
+    }
+}
+
