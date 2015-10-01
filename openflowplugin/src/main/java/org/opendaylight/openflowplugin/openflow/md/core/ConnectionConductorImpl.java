16039489964d2098a72f93ca81204ab755287f11
Sania Zara <sania.zara@namal.edu.pk>
2015-10-01 15:02:47 +0500

    Fix for bug 1395.
    Change-Id: Ic05706d0c031eab72ca0819b551abdc3ae8031d7
    Signed-off-by: Sania Zara <sania.zara@namal.edu.pk>
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/ConnectionConductorImpl.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/ConnectionConductorImpl.java
@@ -399,6 +399,16 @@ public class ConnectionConductorImpl implements OpenflowProtocolListener,
     public void onDisconnectEvent(DisconnectEvent arg0) {
         SessionManager sessionManager = OFSessionUtil.getSessionManager();
         sessionManager.invalidateOnDisconnect(this);
+        /**
+         * When a switch disconnects, queueKeeper attached to the switch is
+         * closed. This is needed to avaid accumulation of unnecessary queueKeepers
+         * registered with the Harvester, after disconnection of switches.
+         */
+        try {
+             queue.close();
+        } catch (Exception e) {
+             LOG.warn("Failed to close queueKeeper", e);
+        }
         close();
     }
 
