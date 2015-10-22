38e5b14321192e7511d333b957974bbccc3468d5
Michal Rehak <mirehak@cisco.com>
2015-10-22 17:37:51 +0200

    BUG-4283 experimenter msg support - registration part
     - added registration point for experimenter message converters
     - depends on OFJava https://git.opendaylight.org/gerrit/#/c/28813/
     - checkstyle fix
    
    Change-Id: Ibb8294c522335a0f5df783fbf92b617e45f29930
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/extension/RegistrationCloser.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/extension/RegistrationCloser.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -12,10 +12,13 @@ import org.opendaylight.openflowplugin.extension.api.ConverterExtensionKey;
 import org.opendaylight.openflowplugin.extension.api.ConvertorActionFromOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorActionToOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorFromOFJava;
+import org.opendaylight.openflowplugin.extension.api.ConvertorMessageFromOFJava;
+import org.opendaylight.openflowplugin.extension.api.ConvertorMessageToOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorToOFJava;
 import org.opendaylight.openflowplugin.extension.api.TypeVersionKey;
 import org.opendaylight.openflowplugin.extension.api.path.AugmentationPath;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.experimenter.types.rev151020.experimenter.core.message.ExperimenterMessageOfChoice;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.ExtensionKey;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
 import org.opendaylight.yangtools.yang.binding.DataContainer;
@@ -25,11 +28,11 @@ import org.opendaylight.yangtools.yang.binding.DataContainer;
  * @param <CONVERTER> converter instance
  */
 public abstract class RegistrationCloser<KEY, CONVERTER> implements ObjectRegistration<CONVERTER> {
-    
+
     private ExtensionConverterManagerImpl registrator;
     private KEY key;
     private CONVERTER converter;
-    
+
     /**
      * @param registrator the registrator to set
      */
@@ -66,61 +69,93 @@ public abstract class RegistrationCloser<KEY, CONVERTER> implements ObjectRegist
     public CONVERTER getConverter() {
         return converter;
     }
-    
+
     @Override
     public CONVERTER getInstance() {
         return getConverter();
     }
-    
+
     /**
      * standalone deregistrator
      * @param <TO> target type of wrapped convertor
      */
-    public static class RegistrationCloserToOFJava<TO extends DataContainer> extends 
+    public static class RegistrationCloserToOFJava<TO extends DataContainer> extends
             RegistrationCloser<ConverterExtensionKey<? extends ExtensionKey>, ConvertorToOFJava<TO>> {
-        
+
         @Override
         public void close() throws Exception {
             getRegistrator().unregister(getKey(), getConverter());
         }
     }
-    
+
     /**
      * standalone deregistrator
      * @param <FROM> source type of wrapped convertor
      * @param <PATH> associated augmentation path
      */
     public static class RegistrationCloserFromOFJava<FROM extends DataContainer, PATH extends AugmentationPath> extends RegistrationCloser<MessageTypeKey<?>, ConvertorFromOFJava<FROM, PATH>> {
-        
+
         @Override
         public void close() throws Exception {
             getRegistrator().unregister(getKey(), getConverter());
         }
     }
-    
+
     /**
      * standalone deregistrator
      * @param <TO> target type of wrapped convertor
      */
-    public static class RegistrationCloserActionToOFJava<TO extends DataContainer> extends 
+    public static class RegistrationCloserActionToOFJava<TO extends DataContainer> extends
             RegistrationCloser<TypeVersionKey<? extends Action>, ConvertorActionToOFJava<Action, TO>> {
-        
+
         @Override
         public void close() throws Exception {
             getRegistrator().unregister(getKey(), getConverter());
         }
     }
-    
+
     /**
      * standalone deregistrator
      * @param <FROM> source type of wrapped convertor
      * @param <PATH> associated augmentation path
      */
-    public static class RegistrationCloserActionFromOFJava<FROM extends DataContainer, PATH extends AugmentationPath> extends RegistrationCloser<MessageTypeKey<?>, ConvertorActionFromOFJava<FROM, PATH>> {
-        
+    public static class RegistrationCloserActionFromOFJava<FROM extends DataContainer, PATH extends AugmentationPath> extends
+            RegistrationCloser<MessageTypeKey<?>, ConvertorActionFromOFJava<FROM, PATH>> {
+
         @Override
         public void close() throws Exception {
             getRegistrator().unregister(getKey(), getConverter());
         }
     }
+
+    /**
+     * standalone deregistrator
+     *
+     * @param <TO> target type of wrapped convertor
+     */
+    public static class RegistrationCloserMessageToOFJava<TO extends DataContainer> extends
+            RegistrationCloser<TypeVersionKey<? extends ExperimenterMessageOfChoice>, ConvertorMessageToOFJava<ExperimenterMessageOfChoice, TO>> {
+
+        @Override
+        public void close() throws Exception {
+            getRegistrator().unregister(getKey(), getConverter());
+        }
+    }
+
+    /**
+     * standalone deregistrator
+     *
+     * @param <FROM> source type of wrapped convertor
+     * @param <PATH> associated augmentation path
+     */
+    public static class RegistrationCloserMessageFromOFJava<FROM extends DataContainer, PATH extends AugmentationPath> extends
+            RegistrationCloser<MessageTypeKey<?>, ConvertorMessageFromOFJava<FROM, PATH>> {
+
+        @Override
+        public void close() throws Exception {
+            getRegistrator().unregister(getKey(), getConverter());
+        }
+    }
+
+
 }
