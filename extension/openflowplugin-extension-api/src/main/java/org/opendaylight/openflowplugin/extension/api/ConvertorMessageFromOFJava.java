ff20f8026ddfea8f7cec0ed357600b17169bf93a
Michal Rehak <mirehak@cisco.com>
2015-10-22 17:37:51 +0200

    BUG-4283 experimenter msg support - registration part
     - added registration point for experimenter message converters
     - depends on OFJava https://git.opendaylight.org/gerrit/#/c/28813/
    
    Change-Id: Ibb8294c522335a0f5df783fbf92b617e45f29930
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ extension/openflowplugin-extension-api/src/main/java/org/opendaylight/openflowplugin/extension/api/ConvertorMessageFromOFJava.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * <p/>
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.extension.api;
+
+import org.opendaylight.openflowplugin.extension.api.path.AugmentationPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.experimenter.types.rev151020.experimenter.core.message.ExperimenterMessageOfChoice;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+
+/**
+ * convert message from OFJava-API model into MD-SAL model
+ *
+ * @param <F> input message model - OFJava-API
+ * @param <P> represents possible paths in yang schema for augmentations
+ */
+public interface ConvertorMessageFromOFJava<F extends DataContainer, P extends AugmentationPath> {
+
+    /**
+     * @param input
+     * @param path  in yang schema where a converted value has to be augmented
+     * @return message converted to MD-SAL and its type
+     */
+    ExperimenterMessageOfChoice convert(F input, P path);
+}
