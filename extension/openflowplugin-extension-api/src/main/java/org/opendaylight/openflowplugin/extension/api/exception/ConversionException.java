84abba16bb82d5619720095ddcdf3093bde0f1de
Michal Rehak <mirehak@cisco.com>
2015-10-23 14:49:40 +0200

    BUG-4283 experimenter msg support - exceptions part
     - added conversion related exceptions for experimenter message converters
    
    Change-Id: I593f8896d138619b93de68f07be9b86b2ae899b3
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ extension/openflowplugin-extension-api/src/main/java/org/opendaylight/openflowplugin/extension/api/exception/ConversionException.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.extension.api.exception;
+
+/**
+ * General exception for conversion related operations
+ */
+public class ConversionException extends Exception {
+    public ConversionException(String message) {
+        super(message);
+    }
+
+    public ConversionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
