1c584c09cb973d0ccd1bf5a44975c463b850f3e6
Jozef Bacigal <jbacigal@cisco.com>
2015-12-07 17:46:45 +0100

    BUG-4715: SalEchoService return value fix
    Change-Id: I6e288652efcadfd0a21b47372039aed5a55cc207
    Signed-off-by: Jozef Bacigal <jbacigal@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/SalEchoServiceImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/SalEchoServiceImpl.java
@@ -7,33 +7,58 @@
  */
 package org.opendaylight.openflowplugin.impl.services;
 
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import java.util.concurrent.Future;
+import javax.annotation.Nullable;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
-import org.opendaylight.openflowplugin.api.openflow.device.Xid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SalEchoService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.echo.service.rev150305.SendEchoOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoOutput;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 
-public final class SalEchoServiceImpl extends AbstractSimpleService<SendEchoInput, SendEchoOutput> implements SalEchoService {
+public final class SalEchoServiceImpl implements SalEchoService {
+
+    private final EchoService echoService;
+
     public SalEchoServiceImpl(final RequestContextStack requestContextStack, final DeviceContext deviceContext) {
-        super(requestContextStack, deviceContext, SendEchoOutput.class);
+        echoService = new EchoService(requestContextStack, deviceContext);
     }
 
     @Override
     public Future<RpcResult<SendEchoOutput>> sendEcho(final SendEchoInput sendEchoInput) {
-        return handleServiceCall(sendEchoInput);
+        final EchoInputBuilder echoInputBld = new EchoInputBuilder()
+                .setData(sendEchoInput.getData());
+        return transform(echoService.handleServiceCall(echoInputBld));
     }
 
-    @Override
-    protected OfHeader buildRequest(final Xid xid, final SendEchoInput input) {
-        final EchoInputBuilder echoInputOFJavaBuilder = new EchoInputBuilder();
-        echoInputOFJavaBuilder.setVersion(getVersion());
-        echoInputOFJavaBuilder.setXid(xid.getValue());
-        echoInputOFJavaBuilder.setData(input.getData());
-        return echoInputOFJavaBuilder.build();
+    private Future<RpcResult<SendEchoOutput>> transform(final ListenableFuture<RpcResult<EchoOutput>> rpcResultListenableFuture) {
+        return Futures.transform(rpcResultListenableFuture, new Function<RpcResult<EchoOutput>, RpcResult<SendEchoOutput>>() {
+            @Nullable
+            @Override
+            public RpcResult<SendEchoOutput> apply(@Nullable final RpcResult<EchoOutput> input) {
+                Preconditions.checkNotNull(input, "echoOutput value is never expected to be NULL");
+                final RpcResult<SendEchoOutput> rpcOutput;
+                if (input.isSuccessful()) {
+                    final SendEchoOutput sendEchoOutput = new SendEchoOutputBuilder()
+                            .setData(input.getResult().getData())
+                            .build();
+                    rpcOutput = RpcResultBuilder.success(sendEchoOutput).build();
+                } else {
+                    rpcOutput = RpcResultBuilder.<SendEchoOutput>failed()
+                            .withRpcErrors(input.getErrors())
+                            .build();
+                }
+                return rpcOutput;
+            }
+        });
     }
+
 }
