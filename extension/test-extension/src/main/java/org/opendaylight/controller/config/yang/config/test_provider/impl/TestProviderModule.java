8da608ae783705f4c9a3c143736bde02505e7505
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-15 18:57:19 -0500

    Add missing license headers
    Change-Id: I75c9ba56119f99c3d1d5f75e4091a6fd27a900ff
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- extension/test-extension/src/main/java/org/opendaylight/controller/config/yang/config/test_provider/impl/TestProviderModule.java
+++ extension/test-extension/src/main/java/org/opendaylight/controller/config/yang/config/test_provider/impl/TestProviderModule.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.controller.config.yang.config.test_provider.impl;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
@@ -24,11 +32,11 @@ public class TestProviderModule extends org.opendaylight.controller.config.yang.
         Test extTest = new Test();
         SalFlowService flowService = getRpcRegistryDependency().getRpcService(SalFlowService.class);
         extTest.setFlowService(flowService);
-        
+
         final RpcRegistration<TestService> registry = getRpcRegistryDependency().addRpcImplementation(TestService.class, extTest);
-        
+
         return new AutoCloseable() {
-            
+
             @Override
             public void close() throws Exception {
                 registry.close();
