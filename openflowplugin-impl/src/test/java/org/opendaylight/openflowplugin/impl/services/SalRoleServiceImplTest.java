c14cb71d678b5ccb7d1972518fe22fd1bc1cb133
Jozef Bacigal <jbacigal@cisco.com>
2016-01-27 20:59:18 +0100

    Bug 5001 Test
     If we want to be able to run this test on different environment, we need to:
      - create maximum possible threads
      - stop the last two threads
      - a try to run tested method more times than two
      - if there is an Runtime exception we call at the end of catch clause, its ok and there was
      no java.lang.OutOfMemoryError: unable to create new native thread
    
     - this test is very unsafe and should be use only on local machine
      because is working with OS or VM threads and can cause fail in asynchronous testing environment
      for this reason is this test on ignore
    
    Change-Id: I47041aaa91da0b77dfc8a4fe0af44a302ad1558c
    Signed-off-by: Jozef Bacigal <jbacigal@cisco.com>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/services/SalRoleServiceImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/services/SalRoleServiceImplTest.java
@@ -10,11 +10,14 @@ package org.opendaylight.openflowplugin.impl.services;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import com.google.common.util.concurrent.ListenableFuture;
+
 import java.math.BigInteger;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ListenableFuture;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -44,11 +47,15 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.role.service.rev150727.SetR
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by kramesha on 8/27/15.
  */
 public class SalRoleServiceImplTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SalRoleServiceImplTest.class);
 
     @Mock
     private RequestContextStack mockRequestContextStack;
@@ -74,17 +81,18 @@ public class SalRoleServiceImplTest {
     @Mock
     private OutboundQueue mockOutboundQueue;
 
-    private NodeId testNodeId = new NodeId(Uri.getDefaultInstance("openflow:1"));
+    private final NodeId testNodeId = new NodeId(Uri.getDefaultInstance("openflow:1"));
 
-    private static short testVersion = 4;
-
-    private static long testXid = 100L;
+    private static final long testXid = 100L;
 
     private NodeRef nodeRef;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+
+        final short testVersion = 4;
+
         Mockito.when(mockDeviceContext.getPrimaryConnectionContext()).thenReturn(mockConnectionContext);
         Mockito.when(mockConnectionContext.getFeatures()).thenReturn(mockFeaturesReply);
         Mockito.when(mockConnectionContext.getNodeId()).thenReturn(testNodeId);
@@ -95,74 +103,160 @@ public class SalRoleServiceImplTest {
         Mockito.when(mockConnectionContext.getOutboundQueueProvider()).thenReturn(mockOutboundQueue);
         Mockito.when(mockDeviceContext.getPrimaryConnectionContext().getConnectionState()).thenReturn(ConnectionContext.CONNECTION_STATE.WORKING);
 
-        NodeKey key = new NodeKey(testNodeId);
-        InstanceIdentifier<Node> path = InstanceIdentifier.<Nodes>builder(Nodes.class)
-                .<Node, NodeKey>child(Node.class, key)
+        final NodeKey key = new NodeKey(testNodeId);
+        final InstanceIdentifier<Node> path = InstanceIdentifier.builder(Nodes.class)
+                .child(Node.class, key)
                 .build();
         nodeRef = new NodeRef(path);
-
     }
 
     @Test
     public void testSetRole() throws Exception {
-        RoleRequestOutput roleRequestOutput = (new RoleRequestOutputBuilder())
+        final RoleRequestOutput roleRequestOutput = (new RoleRequestOutputBuilder())
                 .setXid(testXid).setGenerationId(BigInteger.valueOf(1)).build();
-        ListenableFuture<RpcResult<RoleRequestOutput>> futureOutput =
+        final ListenableFuture<RpcResult<RoleRequestOutput>> futureOutput =
                 RpcResultBuilder.<RoleRequestOutput>success().withResult(roleRequestOutput).buildFuture();
 
         Mockito.when(mockRequestContext.getFuture()).thenReturn(futureOutput);
 
 
-        SalRoleService salRoleService = new SalRoleServiceImpl(mockRequestContextStack, mockDeviceContext);
+        final SalRoleService salRoleService = new SalRoleServiceImpl(mockRequestContextStack, mockDeviceContext);
 
-        SetRoleInput setRoleInput = new SetRoleInputBuilder()
+        final SetRoleInput setRoleInput = new SetRoleInputBuilder()
                 .setControllerRole(OfpRole.BECOMESLAVE)
                 .setNode(nodeRef)
                 .build();
 
-        Future<RpcResult<SetRoleOutput>> future = salRoleService.setRole(setRoleInput);
+        final Future<RpcResult<SetRoleOutput>> future = salRoleService.setRole(setRoleInput);
 
-        RpcResult<SetRoleOutput> roleOutputRpcResult = future.get(5, TimeUnit.SECONDS);
+        final RpcResult<SetRoleOutput> roleOutputRpcResult = future.get(5, TimeUnit.SECONDS);
         assertNotNull("RpcResult from future cannot be null.", roleOutputRpcResult);
         assertTrue("RpcResult from future is not successful.", roleOutputRpcResult.isSuccessful());
 
-        SetRoleOutput setRoleOutput = roleOutputRpcResult.getResult();
+        final SetRoleOutput setRoleOutput = roleOutputRpcResult.getResult();
         assertNotNull(setRoleOutput);
         assertEquals(BigInteger.valueOf(testXid), setRoleOutput.getTransactionId().getValue());
 
+    }
+
+    private static class SleeperThread implements Runnable
+    {
+        private final int i;
+
+        private SleeperThread(final int i)
+        {
+            this.i = i;
+        }
+
+        public void run()
+        {
+            try
+            {
+                if ((i % 1000) == 0) {
+                    LOG.debug("Thread {} about to sleep", this.i);
+                }
+                Thread.sleep(1000 * 60 * 60);
+            }
+            catch (final InterruptedException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * First if we want to be able to run this test on different environment, we need to:
+     * - create maximum possible threads
+     * - stop the last two threads
+     * - a try to run tested method more times than two
+     * - if there is an Runtime exception we call at the end of catch clause, its ok and there was no java.lang.OutOfMemoryError: unable to create new native thread
+     * -----------------------------------
+     * - this test is very unsafe and should be use only on local machine
+     *      because is working with OS or VM threads and cause fail in asynchronous testing environment
+     *      for this reason is this test on ignore
+     * @throws Exception
+     */
+    @Ignore
+    @Test(expected = RuntimeException.class)
+    public synchronized void howManyThreads() throws Exception {
+        Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            public void uncaughtException(final Thread t, final Throwable e)
+            {
+                LOG.debug(e.getMessage());
+                System.exit(1);
+            }
+        });
+        //It is possible make more than 70k threads?
+        final int numThreads = 70000;
+        final Thread [] thread = new Thread[70000];
+        for (int i = 0; i < numThreads; i++)
+        {
+            thread[i] = null;
+            try
+            {
+                thread[i] = new Thread(new SleeperThread(i));
+                thread[i].start();
+            }
+            //Maximum thread reached ....
+            catch (final OutOfMemoryError e)
+            {
+                //Fancy output
+                LOG.debug("Threads are out of memory on thread {}", i);
+                thread[i].interrupt();
+                thread[i-1].interrupt();
+                LOG.debug("Stopped last 2 threads");
+                LOG.debug("============================================");
+                LOG.debug("Running first time setRole");
+                testSetRole();
+                LOG.debug("FIRST RUN OK ....");
+                LOG.debug("============================================");
+                LOG.debug("Running second time setRole");
+                testSetRole();
+                LOG.debug("SECOND RUN OK ....");
+                LOG.debug("============================================");
+                LOG.debug("Running third time setRole");
+                testSetRole();
+                LOG.debug("THIRD RUN OK ....");
+                LOG.debug("============================================");
+                LOG.debug("Running fourth time setRole");
+                testSetRole();
+                LOG.debug("FOURTH RUN OK ...");
+                throw new RuntimeException(String.format("Out of Memory Error on Thread %d", i), e);
+            }
+        }
     }
 
     @Test
     public void testDuplicateRoles() throws Exception {
         // set role to slave
 
-        RoleRequestOutput roleRequestOutput = (new RoleRequestOutputBuilder())
+        final RoleRequestOutput roleRequestOutput = (new RoleRequestOutputBuilder())
                 .setXid(testXid).setGenerationId(BigInteger.valueOf(1)).build();
-        ListenableFuture<RpcResult<RoleRequestOutput>> futureOutput =
+        final ListenableFuture<RpcResult<RoleRequestOutput>> futureOutput =
                 RpcResultBuilder.<RoleRequestOutput>success().withResult(roleRequestOutput).buildFuture();
 
         Mockito.when(mockRequestContext.getFuture()).thenReturn(futureOutput);
 
-        SalRoleService salRoleService = new SalRoleServiceImpl(mockRequestContextStack, mockDeviceContext);
+        final SalRoleService salRoleService = new SalRoleServiceImpl(mockRequestContextStack, mockDeviceContext);
 
-        SetRoleInput setRoleInput = new SetRoleInputBuilder()
+        final SetRoleInput setRoleInput = new SetRoleInputBuilder()
                 .setControllerRole(OfpRole.BECOMESLAVE)
                 .setNode(nodeRef)
                 .build();
 
-        Future<RpcResult<SetRoleOutput>> future = salRoleService.setRole(setRoleInput);
+        final Future<RpcResult<SetRoleOutput>> future = salRoleService.setRole(setRoleInput);
 
-        RpcResult<SetRoleOutput> roleOutputRpcResult = future.get(5, TimeUnit.SECONDS);
+        final RpcResult<SetRoleOutput> roleOutputRpcResult = future.get(5, TimeUnit.SECONDS);
         assertNotNull("RpcResult from future cannot be null.", roleOutputRpcResult);
         assertTrue("RpcResult from future is not successful.", roleOutputRpcResult.isSuccessful());
 
-        SetRoleOutput setRoleOutput = roleOutputRpcResult.getResult();
+        final SetRoleOutput setRoleOutput = roleOutputRpcResult.getResult();
         assertNotNull(setRoleOutput);
         assertEquals(BigInteger.valueOf(testXid), setRoleOutput.getTransactionId().getValue());
 
         // make another role change with the same role - slave
-        Future<RpcResult<SetRoleOutput>> future2 = salRoleService.setRole(setRoleInput);
-        RpcResult<SetRoleOutput> roleOutputRpcResult2 = future2.get(5, TimeUnit.SECONDS);
+        final Future<RpcResult<SetRoleOutput>> future2 = salRoleService.setRole(setRoleInput);
+        final RpcResult<SetRoleOutput> roleOutputRpcResult2 = future2.get(5, TimeUnit.SECONDS);
         assertNotNull("RpcResult from future cannot be null.", roleOutputRpcResult2);
         assertTrue("RpcResult from future for duplicate role is not successful.", roleOutputRpcResult2.isSuccessful());
 
