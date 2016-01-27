05e089d9e121ed4a2ef81fac04d88fb139d95415
Jozef Bacigal <jbacigal@cisco.com>
2016-01-27 21:21:36 +0100

    Bug 4957 OpenflowPluginProviderImpl.close() fixing
     - added close invocation of all underlying managers
     - added TODOs for later reconsideration
     - OFPlugin-Li closing process has to wait for cleaning OperDS
       so we could have unclosed (MASTER) instances in context of
       RoleManager. Fist step was merge OpenflowOwnershipListener
       with RoleManager because Manager is here for managing
       all not finished RoleCtx.
       Second step was unregistred new OwnershipListener (RoleManager)
       as OwnershipListener and for every undstanding RoleCtx ask
       OwnershipService for actual state. If the Entity is still
       registred, NOOP otherwise clean OperDS.
    
    Change-Id: Ie041d6e0fb02b153850d8c9645740cf1164b6ac5
    Signed-off-by: Jozef Bacigal <jbacigal@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/role/RoleManagerImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/role/RoleManagerImpl.java
@@ -8,8 +8,10 @@
 package org.opendaylight.openflowplugin.impl.role;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -17,19 +19,30 @@ import java.util.concurrent.TimeoutException;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Verify;
+import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.clustering.CandidateAlreadyRegisteredException;
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipChange;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipListener;
+import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipListenerRegistration;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipState;
-import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.openflowplugin.api.OFConstants;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
 import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceInitializationPhaseHandler;
+import org.opendaylight.openflowplugin.api.openflow.role.RoleChangeListener;
 import org.opendaylight.openflowplugin.api.openflow.role.RoleContext;
 import org.opendaylight.openflowplugin.api.openflow.role.RoleManager;
 import org.opendaylight.openflowplugin.impl.util.DeviceInitializationUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.role.service.rev150727.OfpRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,24 +53,22 @@ import org.slf4j.LoggerFactory;
  *
  * Hands over to StatisticsManager at the end.
  */
-public class RoleManagerImpl implements RoleManager {
+public class RoleManagerImpl implements RoleManager, EntityOwnershipListener {
     private static final Logger LOG = LoggerFactory.getLogger(RoleManagerImpl.class);
 
     private DeviceInitializationPhaseHandler deviceInitializationPhaseHandler;
+    private final DataBroker dataBroker;
     private final EntityOwnershipService entityOwnershipService;
-    private final RpcProviderRegistry rpcProviderRegistry;
-    private final ConcurrentHashMap<DeviceContext, RoleContext> contexts = new ConcurrentHashMap<>();
-    private final OpenflowOwnershipListener openflowOwnershipListener;
+    private final ConcurrentMap<Entity, RoleContext> contexts = new ConcurrentHashMap<>();
+    private final EntityOwnershipListenerRegistration entityOwnershipListenerRegistration;
     private final boolean switchFeaturesMandatory;
 
-    public RoleManagerImpl(final RpcProviderRegistry rpcProviderRegistry,
-            final EntityOwnershipService entityOwnershipService, final boolean switchFeaturesMandatory) {
+    public RoleManagerImpl(final EntityOwnershipService entityOwnershipService, final DataBroker dataBroker, final boolean switchFeaturesMandatory) {
         this.entityOwnershipService = Preconditions.checkNotNull(entityOwnershipService);
-        this.rpcProviderRegistry = Preconditions.checkNotNull(rpcProviderRegistry);
+        this.dataBroker = Preconditions.checkNotNull(dataBroker);
         this.switchFeaturesMandatory = switchFeaturesMandatory;
-        this.openflowOwnershipListener = new OpenflowOwnershipListener(entityOwnershipService);
+        this.entityOwnershipListenerRegistration = Preconditions.checkNotNull(entityOwnershipService.registerListener(RoleManager.ENTITY_TYPE, this));
         LOG.debug("Registering OpenflowOwnershipListener listening to all entity ownership changes");
-        openflowOwnershipListener.init();
     }
 
     @Override
@@ -74,10 +85,11 @@ public class RoleManagerImpl implements RoleManager {
             return;
         }
 
-        final RoleContext roleContext = new RoleContextImpl(deviceContext, rpcProviderRegistry, entityOwnershipService, openflowOwnershipListener);
+        final RoleContext roleContext = new RoleContextImpl(deviceContext, entityOwnershipService,
+                makeEntity(deviceContext.getDeviceState().getNodeId()));
         // if the device context gets closed (mostly on connection close), we would need to cleanup
         deviceContext.addDeviceContextClosedHandler(this);
-        Verify.verify(contexts.putIfAbsent(deviceContext, roleContext) == null,
+        Verify.verify(contexts.putIfAbsent(roleContext.getEntity(), roleContext) == null,
                 "RoleCtx for master Node {} is still not close.", deviceContext.getDeviceState().getNodeId());
         OfpRole role = null;
         try {
@@ -130,29 +142,107 @@ public class RoleManagerImpl implements RoleManager {
 
     @Override
     public void close() throws Exception {
-        for (final Map.Entry<DeviceContext, RoleContext> roleContextEntry : contexts.entrySet()) {
-            if (roleContextEntry.getValue() != null) {
-                roleContextEntry.getValue().close();
+        entityOwnershipListenerRegistration.close();
+        for (final Map.Entry<Entity, RoleContext> roleContextEntry : contexts.entrySet()) {
+            // got here because last known role is LEADER and DS might need clearing up
+            final Entity entity = roleContextEntry.getKey();
+            final Optional<EntityOwnershipState> ownershipState = entityOwnershipService.getOwnershipState(entity);
+            final NodeId nodeId = roleContextEntry.getValue().getDeviceState().getNodeId();
+            if (ownershipState.isPresent())
+                if ((!ownershipState.get().hasOwner())) {
+                    LOG.trace("Last role is LEADER and ownershipService returned hasOwner=false for node: {}; " +
+                            "cleaning DS as being probably the last owner", nodeId);
+                    removeDeviceFromOperDS(roleContextEntry.getValue());
+                } else {
+                    // NOOP - there is another owner
+                    LOG.trace("Last role is LEADER and ownershipService returned hasOwner=true for node: {}; " +
+                            "leaving DS untouched", nodeId);
+                }
+            else {
+                // TODO: is this safe? When could this happen?
+                LOG.warn("Last role is LEADER but ownershipService returned empty ownership info for node: {}; " +
+                        "cleaning DS ANYWAY!", nodeId);
+                removeDeviceFromOperDS(roleContextEntry.getValue());
             }
         }
-        this.openflowOwnershipListener.close();
+        contexts.clear();
     }
 
     @Override
     public void onDeviceContextClosed(final DeviceContext deviceContext) {
-        LOG.debug("onDeviceContextClosed for node {}", deviceContext.getDeviceState().getNodeId());
-        final RoleContext removedContext = contexts.remove(deviceContext.getDeviceState().getNodeId());
-        if (removedContext != null) {
-            try {
-                LOG.info("Unregistering rpcs for device context closure");
-                removedContext.close();
-            } catch (final Exception e) {
-                LOG.error(
-                        "Exception while unregistering rpcs onDeviceContextClosed handler for node:{}. But continuing.",
-                        deviceContext.getDeviceState().getNodeId(), e);
+        final NodeId nodeId = deviceContext.getDeviceState().getNodeId();
+        final OfpRole role = deviceContext.getDeviceState().getRole();
+        LOG.debug("onDeviceContextClosed for node {}", nodeId);
+
+        final Entity entity = makeEntity(nodeId);
+        final RoleContext roleContext = contexts.get(entity);
+        if (roleContext != null) {
+            LOG.debug("Found roleContext associated to deviceContext: {}, now closing the roleContext", nodeId);
+            roleContext.close();
+            if (role == null || OfpRole.BECOMESLAVE.equals(role)) {
+                LOG.debug("No DS commitment for device {} - LEADER is somewhere else", nodeId);
+                contexts.remove(entity, roleContext);
             }
-        } else {
-            LOG.info("No RpcContext to close , for device:{}", deviceContext.getDeviceState().getNodeId());
+        }
+    }
+
+    private static Entity makeEntity(final NodeId nodeId) {
+        return new Entity(RoleManager.ENTITY_TYPE, nodeId.getValue());
+    }
+
+    private CheckedFuture<Void, TransactionCommitFailedException> removeDeviceFromOperDS(final RoleChangeListener roleChangeListener) {
+        Preconditions.checkArgument(roleChangeListener != null);
+        final DeviceState deviceState = roleChangeListener.getDeviceState();
+        final WriteTransaction delWtx = dataBroker.newWriteOnlyTransaction();
+        delWtx.delete(LogicalDatastoreType.OPERATIONAL, deviceState.getNodeInstanceIdentifier());
+        final CheckedFuture<Void, TransactionCommitFailedException> delFuture = delWtx.submit();
+        Futures.addCallback(delFuture, new FutureCallback<Void>() {
+
+            @Override
+            public void onSuccess(final Void result) {
+                LOG.debug("Delete Node {} was successful", deviceState.getNodeId());
+            }
+
+            @Override
+            public void onFailure(final Throwable t) {
+                LOG.warn("Delete Node {} fail.", deviceState.getNodeId(), t);
+            }
+        });
+        return delFuture;
+    }
+
+    @Override
+    public void ownershipChanged(final EntityOwnershipChange ownershipChange) {
+        Preconditions.checkArgument(ownershipChange != null);
+        final RoleChangeListener roleChangeListener = contexts.get(ownershipChange.getEntity());
+
+        LOG.info("Received EntityOwnershipChange:{}, roleChangeListener-present={}", ownershipChange, (roleChangeListener != null));
+
+        if (roleChangeListener != null) {
+            LOG.debug("Found roleChangeListener for local entity:{}", ownershipChange.getEntity());
+            // if this was the master and entity does not have a master
+            if (ownershipChange.wasOwner() && !ownershipChange.isOwner() && !ownershipChange.hasOwner()) {
+                LOG.info("Initiate removal from operational. Possibly the last node to be disconnected for :{}. ", ownershipChange);
+                Futures.addCallback(removeDeviceFromOperDS(roleChangeListener),
+                        new FutureCallback<Void>() {
+                            @Override
+                            public void onSuccess(@Nullable final Void aVoid) {
+                                LOG.debug("Freeing roleContext slot for device: {}", roleChangeListener.getDeviceState().getNodeId());
+                                contexts.remove(ownershipChange.getEntity(), roleChangeListener);
+                            }
+
+                            @Override
+                            public void onFailure(final Throwable throwable) {
+                                LOG.warn("NOT freeing roleContext slot for device: {}, {}",
+                                        roleChangeListener.getDeviceState().getNodeId(), throwable.getMessage());
+                            }
+                        });
+            } else {
+                final OfpRole newRole = ownershipChange.isOwner() ? OfpRole.BECOMEMASTER : OfpRole.BECOMESLAVE;
+                final OfpRole oldRole = ownershipChange.wasOwner() ? OfpRole.BECOMEMASTER : OfpRole.BECOMESLAVE;
+                // send even if they are same. we do the check for duplicates in SalRoleService and maintain a lastKnownRole
+                roleChangeListener.onRoleChanged(oldRole, newRole);
+            }
         }
     }
 }
