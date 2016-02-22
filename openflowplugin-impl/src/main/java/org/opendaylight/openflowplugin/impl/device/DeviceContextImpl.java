920efadf5d7227b1f79b321bcca1ba7b7e66826d
Vaclav Demcak <vdemcak@cisco.com>
2016-02-22 11:10:32 +0100

    Bug 4957: OF Role processing changes
    * change init order to CM -> DM -> SM -> RPC -> Role -> DM
    * set default Role setup to Slave by init phase
    * fix tests
    
    Change-Id: I9492a96d0725823d7901e98af81e0e3961e9f414
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
@@ -246,12 +246,13 @@ public class DeviceContextImpl implements DeviceContext, ExtensionConverterProvi
         }
         if (OfpRole.BECOMEMASTER.equals(role)) {
             if (!deviceState.deviceSynchronized()) {
-                LOG.debug("Setup Device Ctx {} for Master Role", getDeviceState().getNodeId());
+                LOG.debug("Setup Empty TxManager {} for initialization phase", getDeviceState().getNodeId());
                 transactionChainManager.activateTransactionManager();
                 return Futures.immediateCheckedFuture(null);
             }
             /* Relevant for no initial Slave-to-Master scenario in cluster */
             return asyncClusterRoleChange(role);
+
         } else if (OfpRole.BECOMESLAVE.equals(role)) {
             if (rpcContext != null) {
                 MdSalRegistratorUtils.registerSlaveServices(rpcContext, DeviceContextImpl.this, role);
@@ -321,12 +322,13 @@ public class DeviceContextImpl implements DeviceContext, ExtensionConverterProvi
                     return null;
                 }
                 LOG.debug("Get Initial Device {} information is successful", getDeviceState().getNodeId());
+                getDeviceState().setDeviceSynchronized(true);
+                transactionChainManager.activateTransactionManager();
+                initialSubmitTransaction();
                 if (null != getRpcContext()) {
                     MdSalRegistratorUtils.registerMasterServices(getRpcContext(), DeviceContextImpl.this, role);
                 }
-                getDeviceState().setDeviceSynchronized(true);
                 getDeviceState().setStatisticsPollingEnabledProp(true);
-                transactionChainManager.activateTransactionManager();
                 return null;
             }
         });
@@ -555,20 +557,20 @@ public class DeviceContextImpl implements DeviceContext, ExtensionConverterProvi
 
                 @Override
                 public void onSuccess(final Void result) {
-                    LOG.info("TxChain {} was shutdown successfull.", deviceState.getNodeId());
+                    LOG.info("TxChain {} was shutdown successfull.", getDeviceState().getNodeId());
                     tearDownClean();
                 }
 
                 @Override
                 public void onFailure(final Throwable t) {
-                    LOG.warn("Shutdown TxChain {} fail.", deviceState.getNodeId(), t);
+                    LOG.warn("Shutdown TxChain {} fail.", getDeviceState().getNodeId(), t);
                     tearDownClean();
                 }
             });
         }
     }
 
-    synchronized void tearDownClean() {
+    protected void tearDownClean() {
         LOG.info("Closing transaction chain manager without cleaning inventory operational");
         Preconditions.checkState(!deviceState.isValid());
         transactionChainManager.close();
