b01f566674db7159f225c40865f27a58595c5a83
Kamal Rameshan <kramesha@cisco.com>
2016-02-26 15:39:50 -0800

    Bug:3026 - Echo response timeout needs be exported to configuration
    Takes care of Bug-4677 : Li: Knob to configure Barrier duration
    
    Also incorporated Jozef bacigal's hot fix in stable
    
    Change-Id: I42697dbbb93cc40c1547b92e9665851e7f5f0b98
    Signed-off-by: Kamal Rameshan <kramesha@cisco.com>
    (cherry picked from commit e22904723888cfe73fff63e280e4c4c5af51b910)

--- openflowplugin-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/config/openflow/plugin/impl/rev150327/OpenFlowProviderModule.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/config/openflow/plugin/impl/rev150327/OpenFlowProviderModule.java
@@ -10,11 +10,11 @@ public class OpenFlowProviderModule extends org.opendaylight.yang.gen.v1.urn.ope
 
     private static final Logger LOG = LoggerFactory.getLogger(OpenFlowProviderModule.class);
 
-    public OpenFlowProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+    public OpenFlowProviderModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public OpenFlowProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.config.openflow.plugin.impl.rev150327.OpenFlowProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+    public OpenFlowProviderModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.config.openflow.plugin.impl.rev150327.OpenFlowProviderModule oldModule, final java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -27,7 +27,7 @@ public class OpenFlowProviderModule extends org.opendaylight.yang.gen.v1.urn.ope
     public java.lang.AutoCloseable createInstance() {
         LOG.info("Initializing new OFP southbound.");
         OpenflowPortsUtil.init();
-        OpenFlowPluginProvider openflowPluginProvider = new OpenFlowPluginProviderImpl(getRpcRequestsQuota(), getGlobalNotificationQuota());
+        final OpenFlowPluginProvider openflowPluginProvider = new OpenFlowPluginProviderImpl(getRpcRequestsQuota(), getGlobalNotificationQuota());
 
         openflowPluginProvider.setSwitchConnectionProviders(getOpenflowSwitchConnectionProviderDependency());
         openflowPluginProvider.setDataBroker(getDataBrokerDependency());
@@ -38,8 +38,16 @@ public class OpenFlowProviderModule extends org.opendaylight.yang.gen.v1.urn.ope
         openflowPluginProvider.setIsStatisticsPollingOff(getIsStatisticsPollingOff());
         openflowPluginProvider.setEntityOwnershipService(getEntityOwnershipServiceDependency());
         openflowPluginProvider.setIsStatisticsRpcEnabled(getIsStatisticsRpcEnabled());
+        openflowPluginProvider.setBarrierCountLimit(getBarrierCountLimit().getValue());
+        openflowPluginProvider.setBarrierInterval(getBarrierIntervalTimeoutLimit().getValue());
+        openflowPluginProvider.setEchoReplyTimeout(getEchoReplyTimeout().getValue());
 
         openflowPluginProvider.initialize();
+
+        LOG.info("Configured values, StatisticsPollingOff:{}, SwitchFeaturesMandatory:{}, BarrierCountLimit:{}, BarrierTimeoutLimit:{}, EchoReplyTimeout:{}",
+                getIsStatisticsPollingOff(), getSwitchFeaturesMandatory(), getBarrierCountLimit().getValue(),
+                getBarrierIntervalTimeoutLimit().getValue(), getEchoReplyTimeout().getValue());
+
 
         return openflowPluginProvider;
     }
