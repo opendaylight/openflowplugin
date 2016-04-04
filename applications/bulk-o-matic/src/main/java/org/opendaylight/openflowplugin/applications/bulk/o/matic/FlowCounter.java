9f33c0715e95117156c1f57e9349c295b2952b72
Faiz Ilahi Kothari <faiz.ilahi.k.kothari@ericsson.com>
2016-04-04 11:33:52 +0530

    BUG-5841: Enhances bulk-o-matic to stress test Data Store and Openflowplugin RPC
    	- Takes JSON input from REST to provision flows
    	- Different options to push to DS or Openflowplugin
    	- Results are printed to karaf.log
    
    Change-Id: I231e9278a7292ac2424751a0689f632d3382c497
    Signed-off-by: Faiz Ilahi Kothari <faiz.ilahi.k.kothari@ericsson.com>
    Signed-off-by: Shuva Kar <shuva.jyoti.kar@ericsson.com>

---
+++ applications/bulk-o-matic/src/main/java/org/opendaylight/openflowplugin/applications/bulk/o/matic/FlowCounter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Ericsson Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.applications.bulk.o.matic;
+
+public class FlowCounter implements FlowCounterMBean {
+    private FlowCounterMBean reader;
+    private FlowCounterMBean writer;
+
+    public enum OperationStatus {INIT, SUCCESS, FAILURE, INPROGRESS}
+
+    public void setReader(FlowCounterMBean reader) {
+        this.reader = reader;
+    }
+
+    public void setWriter(FlowCounterMBean writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public long getFlowCount() {
+        if(reader != null) {
+            return reader.getFlowCount();
+        }
+        return 0;
+    }
+
+    @Override
+    public int getReadOpStatus() {
+        if(reader != null) {
+            return reader.getReadOpStatus();
+        }
+        return OperationStatus.INIT.ordinal();
+    }
+
+    @Override
+    public int getWriteOpStatus() {
+        if(writer != null) {
+            return writer.getWriteOpStatus();
+        }
+        return OperationStatus.INIT.ordinal();
+    }
+}
