b48c012d7dc97e1bdb8e6bb8e4b522fa984499ef
Andrej Leitner <anleitne@cisco.com>
2016-04-26 15:50:04 +0200

    Bug 5575 main SyncReactor + utils
    Change-Id: I4a0ef8043e3e9b66a92c49deb86a83b083782ccc
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

---
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/util/SyncCrudCounters.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frsync.util;
+
+/**
+ * Purpose: oneshot (per sync) place holder for amounts of added/updated/removed flows/groups/meters
+ */
+public class SyncCrudCounters {
+
+    private final CrudCounts flowCrudCounts;
+    private final CrudCounts groupCrudCounts;
+    private final CrudCounts meterCrudCounts;
+
+    public SyncCrudCounters() {
+        flowCrudCounts = new CrudCounts();
+        groupCrudCounts = new CrudCounts();
+        meterCrudCounts = new CrudCounts();
+    }
+
+    public CrudCounts getFlowCrudCounts() {
+        return flowCrudCounts;
+    }
+
+    public CrudCounts getGroupCrudCounts() {
+        return groupCrudCounts;
+    }
+
+    public CrudCounts getMeterCrudCounts() {
+        return meterCrudCounts;
+    }
+
+}
