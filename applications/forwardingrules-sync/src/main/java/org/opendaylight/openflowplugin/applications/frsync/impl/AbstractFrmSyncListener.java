501dfcc94ed6fa215892af67b151f02a28a77a28
Andrej Leitner <anleitne@cisco.com>
2016-04-26 17:02:40 +0200

    Bug 5575 added listeners config/operational
    Change-Id: I568620061892e3fb997771f272f2beca4c160f81
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

---
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/impl/AbstractFrmSyncListener.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frsync.impl;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.openflowplugin.applications.frsync.NodeListener;
+import org.opendaylight.openflowplugin.applications.frsync.util.PathUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * Abstract Listener for node changes
+ * 
+ * @author joslezak
+ */
+public abstract class AbstractFrmSyncListener<T extends DataObject> implements NodeListener<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractFrmSyncListener.class);
+
+    @Override
+    public void onDataTreeChanged(@Nonnull final Collection<DataTreeModification<T>> modifications) {
+        for (DataTreeModification<T> modification : modifications) {
+            final NodeId nodeId = PathUtil.digNodeId(modification.getRootPath().getRootIdentifier());
+
+            try {
+                final Optional<ListenableFuture<Boolean>> optFuture = processNodeModification(modification);
+                if (optFuture.isPresent()) {
+                    final ListenableFuture<Boolean> future = optFuture.get();
+                    final Boolean ret = future.get(15000, TimeUnit.MILLISECONDS);
+                    LOG.debug("syncup ret {} {} {} thread:{}", dsType(), ret, nodeId, threadName());
+                }
+            } catch (InterruptedException e) {
+                LOG.warn("permit for forwarding rules sync not acquired: {}", nodeId);
+            } catch (Exception e) {
+                LOG.error("error processing inventory node modification: {}", nodeId, e);
+            }
+        }
+    }
+
+    protected abstract Optional<ListenableFuture<Boolean>> processNodeModification(
+            DataTreeModification<T> modification) throws ReadFailedException, InterruptedException;
+
+    public abstract LogicalDatastoreType dsType();
+    
+    static String threadName() {
+        final Thread currentThread = Thread.currentThread();
+        return currentThread.getName();
+    }
+}
