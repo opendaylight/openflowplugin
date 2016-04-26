97b25a0aa1c08f64c6b1bb7608f6dc2c58a7d530
Andrej Leitner <anleitne@cisco.com>
2016-04-26 15:50:04 +0200

    Bug 5575 main SyncReactor + utils
    Change-Id: I4a0ef8043e3e9b66a92c49deb86a83b083782ccc
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

---
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/util/CrudCounts.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.applications.frsync.util;
+
+/**
+ * Purpose: general place holder for add/update/remove counts
+ */
+public class CrudCounts {
+    private int added;
+    private int updated;
+    private int removed;
+
+    public int getAdded() {
+        return added;
+    }
+
+    public void setAdded(final int added) {
+        this.added = added;
+    }
+
+    public int getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(final int updated) {
+        this.updated = updated;
+    }
+
+    public int getRemoved() {
+        return removed;
+    }
+
+    public void setRemoved(final int removed) {
+        this.removed = removed;
+    }
+
+    public void incAdded() {
+        added++;
+    }
+
+    public void incUpdated() {
+        updated++;
+    }
+
+    public void incRemoved() {
+        removed++;
+    }
+}
