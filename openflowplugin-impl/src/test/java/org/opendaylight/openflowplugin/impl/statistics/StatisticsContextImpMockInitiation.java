26ca3998af906d31217f51686d4be446254dbea7
Jozef Bacigal <jbacigal@cisco.com>
2016-04-26 14:49:03 +0200

    Bug 5523 - CloseConnection changes
    - dependency of deviceContext removed from starting
    - lifecycle conductor thread safe now
    - role manager was not removing context from map
    - test update
    
    Change-Id: Iadafdfb8f377326bfb76e9163ad66656b16c15f3
    Signed-off-by: Jozef Bacigal <jbacigal@cisco.com>
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpMockInitiation.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpMockInitiation.java
@@ -15,11 +15,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
+import org.mockito.Mockito;
 import org.opendaylight.openflowjava.protocol.api.connection.OutboundQueue;
 import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceManager;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
 import org.opendaylight.openflowplugin.api.openflow.statistics.ofpspecific.MessageSpy;
+import org.opendaylight.openflowplugin.impl.LifecycleConductor;
 import org.opendaylight.openflowplugin.impl.statistics.services.dedicated.StatisticsGatheringOnTheFlyService;
 import org.opendaylight.openflowplugin.impl.statistics.services.dedicated.StatisticsGatheringService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
@@ -47,6 +50,7 @@ public class StatisticsContextImpMockInitiation {
     protected DeviceState mockedDeviceState;
     protected MessageSpy mockedMessageSpy;
     protected OutboundQueue mockedOutboundQueue;
+    protected DeviceManager mockedDeviceManager;
 
     protected static final KeyedInstanceIdentifier<Node, NodeKey> dummyNodeII = InstanceIdentifier.create(Nodes.class)
             .child(Node.class, new NodeKey(new NodeId("dummyNodeId")));
@@ -61,7 +65,11 @@ public class StatisticsContextImpMockInitiation {
         mockedDeviceState = mock(DeviceState.class);
         mockedMessageSpy = mock(MessageSpy.class);
         mockedOutboundQueue = mock(OutboundQueue.class);
+        mockedDeviceManager = mock(DeviceManager.class);
 
+        when(mockedDeviceContext.getDeviceState()).thenReturn(mockedDeviceState);
+        when(mockedDeviceContext.getPrimaryConnectionContext()).thenReturn(mockedConnectionContext);
+        when(mockedDeviceContext.getMessageSpy()).thenReturn(mockedMessageSpy);
         when(mockedDeviceState.isTableStatisticsAvailable()).thenReturn(isTable);
         when(mockedDeviceState.isFlowStatisticsAvailable()).thenReturn(isFlow);
         when(mockedDeviceState.isGroupAvailable()).thenReturn(isGroup);
@@ -77,6 +85,9 @@ public class StatisticsContextImpMockInitiation {
         when(mockedConnectionContext.getFeatures()).thenReturn(mockedFeatures);
         when(mockedConnectionContext.getConnectionState()).thenReturn(ConnectionContext.CONNECTION_STATE.WORKING);
         when(mockedConnectionContext.getOutboundQueueProvider()).thenReturn(mockedOutboundQueue);
+
+        when(mockedDeviceManager.getDeviceContextFromNodeId(Mockito.<NodeId>any())).thenReturn(mockedDeviceContext);
+        LifecycleConductor.getInstance().setDeviceManager(mockedDeviceManager);
 
     }
 }
