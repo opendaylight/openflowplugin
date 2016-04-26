0a1e23886d0a20bc781b8ad6ffa456f1c7520fa8
Andrej Leitner <anleitne@cisco.com>
2016-04-26 14:28:19 +0200

    Bug 5575 added config/operational DAOs
    Change-Id: Ic68e48d4e9a9b651c1db19dcc2f279fb5159a0ca
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

---
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/dao/FlowCapableNodeSnapshotDao.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.applications.frsync.dao;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.Nonnull;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+
+import com.google.common.base.Optional;
+
+/**
+ * @author joslezak
+ */
+public class FlowCapableNodeSnapshotDao implements FlowCapableNodeDao {
+
+    private final ConcurrentHashMap<String, FlowCapableNode> cache = new ConcurrentHashMap<>();
+
+    public void updateCache(@Nonnull NodeId nodeId, Optional<FlowCapableNode> dataAfter) {
+        if (dataAfter.isPresent()) {
+            cache.put(nodeId.getValue(), dataAfter.get());
+        } else {
+            cache.remove(nodeId.getValue());
+        }
+    }
+
+    public Optional<FlowCapableNode> loadByNodeId(@Nonnull NodeId nodeId) {
+        final FlowCapableNode node = cache.get(nodeId.getValue());
+        return Optional.fromNullable(node);
+    }
+}
