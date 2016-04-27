c628cbd7bd51ca11b6d32f93e5526d9a2de068e1
Andrej Leitner <anleitne@cisco.com>
2016-04-27 10:00:41 +0200

    Bug 5575 added JUnit tests
    Change-Id: I11fb3c9ca4bd049a170ce6e04ff754399cdeb7ef
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

---
+++ applications/forwardingrules-sync/src/test/java/org/opendaylight/openflowplugin/applications/frsync/impl/ForwardingRulesSyncProviderTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frsync.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.RpcConsumerRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.transaction.rev150304.FlowCapableTransactionService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.service.rev130918.SalGroupService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.service.rev130918.SalMeterService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
+import org.opendaylight.yangtools.yang.binding.RpcService;
+
+/**
+ * Test for {@link ForwardingRulesSyncProvider}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ForwardingRulesSyncProviderTest {
+
+    private ForwardingRulesSyncProvider provider;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
+    private RpcConsumerRegistry rpcRegistry;
+    @Mock
+    private BindingAwareBroker broker;
+    @Mock
+    private BindingAwareBroker.ProviderContext providerContext;
+
+    @Before
+    public void setUp() throws Exception {
+        Mockito.when(rpcRegistry.getRpcService(Matchers.<Class<? extends RpcService>>any()))
+                .thenAnswer(new Answer<RpcService>() {
+                    @Override
+                    public RpcService answer(final InvocationOnMock invocation) throws Throwable {
+                        Class<? extends RpcService> serviceType = (Class<? extends RpcService>) invocation.getArguments()[0];
+                        return Mockito.mock(serviceType);
+                    }
+                });
+
+        provider = new ForwardingRulesSyncProvider(broker, dataBroker, rpcRegistry);
+
+        Mockito.verify(rpcRegistry).getRpcService(SalFlowService.class);
+        Mockito.verify(rpcRegistry).getRpcService(SalGroupService.class);
+        Mockito.verify(rpcRegistry).getRpcService(SalMeterService.class);
+        Mockito.verify(rpcRegistry).getRpcService(SalTableService.class);
+        Mockito.verify(rpcRegistry).getRpcService(FlowCapableTransactionService.class);
+
+        Mockito.verify(broker).registerProvider(provider);
+    }
+
+    @Test
+    public void testOnSessionInitiated() throws Exception {
+        provider.onSessionInitiated(providerContext);
+
+        Mockito.verify(dataBroker, Mockito.times(2)).registerDataTreeChangeListener(
+                Matchers.<DataTreeIdentifier<FlowCapableNode>>any(),
+                Matchers.<DataTreeChangeListener<FlowCapableNode>>any());
+    }
+}
---
+++ applications/forwardingrules-sync/src/test/java/org/opendaylight/openflowplugin/applications/frsync/util/SemaphoreKeeperTest.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frsync.util;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.openflowplugin.applications.frsync.SemaphoreKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test for {@link SemaphoreKeeperGuavaImpl}.
+ */
+public class SemaphoreKeeperTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SemaphoreKeeperTest.class);
+    private SemaphoreKeeperGuavaImpl<String> semaphoreKeeper;
+    final String key = "11";
+
+    @Before
+    public void setUp() throws Exception {
+        semaphoreKeeper = new SemaphoreKeeperGuavaImpl(1, true);
+    }
+
+    @Test
+    public void testSummonGuard() throws Exception {
+        Semaphore semaphore1 = semaphoreKeeper.summonGuard(key);
+        final int g1FingerPrint = semaphore1.hashCode();
+        Semaphore semaphore2 = semaphoreKeeper.summonGuard(key);
+        final int g2FingerPrint = semaphore2.hashCode();
+
+        Assert.assertSame(semaphore1, semaphore2);
+        Assert.assertEquals(1, semaphore1.availablePermits());
+
+        semaphore1.acquire();
+        semaphore1.release();
+        Assert.assertEquals(1, semaphore1.availablePermits());
+        semaphore1 = null;
+        System.gc();
+
+        semaphore2.acquire();
+        semaphore2.release();
+        Assert.assertEquals(1, semaphore2.availablePermits());
+        semaphore2 = null;
+        Assert.assertEquals(g1FingerPrint, g2FingerPrint);
+
+        System.gc();
+        final Semaphore semaphore3 = semaphoreKeeper.summonGuard(key);
+        Assert.assertNotEquals(g1FingerPrint, semaphore3.hashCode());
+    }
+
+    @Test
+    public void testReleaseGuard() throws Exception {
+        for (int total = 1; total <= 10; total++) {
+            LOG.info("test run: {}", total);
+            final Worker task = new Worker(semaphoreKeeper, key);
+
+            final ExecutorService executorService = new ThreadPoolExecutor(5, 5,
+                    0L, TimeUnit.MILLISECONDS,
+                    new LinkedBlockingQueue<Runnable>()) {
+                @Override
+                protected void afterExecute(final Runnable r, final Throwable t) {
+                    super.afterExecute(r, t);
+                    if (t != null) {
+                        LOG.error("pool thread crashed", t);
+                    }
+                }
+            };
+
+            final int steps = 10;
+            for (int i = 0; i < steps; i++) {
+                executorService.submit(task);
+            }
+            Thread.sleep(50L);
+            LOG.info("STARTING new serie");
+            System.gc();
+
+            for (int i = 0; i < steps; i++) {
+                executorService.submit(task);
+            }
+            Thread.sleep(1000L);
+            System.gc();
+
+            executorService.shutdown();
+            final boolean terminated = executorService.awaitTermination(10, TimeUnit.SECONDS);
+            if (!terminated) {
+                LOG.warn("pool stuck, forcing termination");
+                executorService.shutdownNow();
+                Assert.fail("pool failed to finish gracefully");
+            }
+
+            final int counterSize = task.getCounterSize();
+            LOG.info("final counter = {}", counterSize);
+            Assert.assertEquals(20, counterSize);
+        }
+    }
+
+    private static class Worker implements Runnable {
+        private final SemaphoreKeeper<String> keeper;
+        private final String key;
+        private ConcurrentMap<Integer, Integer> counter = new ConcurrentHashMap<>();
+        private volatile int index = 0;
+
+        public Worker(SemaphoreKeeper<String> keeper, final String key) {
+            this.keeper = keeper;
+            this.key = key;
+        }
+
+        @Override
+        public void run() {
+            try {
+                final Semaphore guard = keeper.summonGuard(key);
+                Thread.sleep(2L);
+                guard.acquire();
+                counter.putIfAbsent(index, 0);
+                counter.put(index, counter.get(index) + 1);
+                LOG.debug("queue: {} [{}] - {}", guard.getQueueLength(), guard.hashCode(), counter.size());
+                index++;
+                guard.release();
+            } catch (Exception e) {
+                LOG.warn("acquiring failed.. ", e);
+            }
+        }
+
+        public int getCounterSize() {
+            return counter.size();
+        }
+    }
+}
