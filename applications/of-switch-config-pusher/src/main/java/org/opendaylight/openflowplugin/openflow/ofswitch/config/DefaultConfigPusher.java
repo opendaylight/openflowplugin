f54993bf2605290e43de084300772d4d6c3e9ebf
Tom Pantelis <tpanteli@brocade.com>
2016-05-10 18:12:49 -0400

    Add blueprint wiring for the of-switch-config-pusher app
    Change-Id: I38c0cbac5882468853a73882d717bb57b93d9c63
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

--- applications/of-switch-config-pusher/src/main/java/org/opendaylight/openflowplugin/openflow/ofswitch/config/DefaultConfigPusher.java
+++ applications/of-switch-config-pusher/src/main/java/org/opendaylight/openflowplugin/openflow/ofswitch/config/DefaultConfigPusher.java
@@ -9,14 +9,20 @@
 package org.opendaylight.openflowplugin.openflow.ofswitch.config;
 
 import java.util.Set;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.module.config.rev141015.NodeConfigService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.module.config.rev141015.SetConfigInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.SwitchConfigFlag;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
@@ -25,15 +31,28 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
  */
 public class DefaultConfigPusher implements AutoCloseable, DataChangeListener {
 
-    private NodeConfigService nodeConfigService;
+    private final NodeConfigService nodeConfigService;
+    private final DataBroker dataBroker;
+    private ListenerRegistration<DataChangeListener> dataChangeListenerRegistration;
 
-    public DefaultConfigPusher(NodeConfigService nodeConfigService) {
+    public DefaultConfigPusher(NodeConfigService nodeConfigService, DataBroker dataBroker) {
         this.nodeConfigService = nodeConfigService;
+        this.dataBroker = dataBroker;
+    }
+
+    public void start() {
+        InstanceIdentifier<FlowCapableNode> path = InstanceIdentifier.create(Nodes.class).child(Node.class).
+                augmentation(FlowCapableNode.class);
+        dataChangeListenerRegistration = dataBroker.registerDataChangeListener(
+                LogicalDatastoreType.OPERATIONAL,
+                path, this, AsyncDataBroker.DataChangeScope.BASE);
     }
 
     @Override
-    public void close() throws Exception {
-
+    public void close() {
+        if(dataChangeListenerRegistration != null) {
+            dataChangeListenerRegistration.close();
+        }
     }
 
     @Override
