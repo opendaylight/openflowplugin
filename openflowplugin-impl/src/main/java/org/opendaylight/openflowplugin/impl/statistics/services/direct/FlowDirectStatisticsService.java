d426c3a0f24ad6ceb1265b73173fda5eadc9a5d5
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-05-11 19:03:01 +0200

    Bug 5692 - Direct statistics RPC
    - Added new RPCs for direct gathering of statistics
      with optional flag of storing the statistics to DS/operational
    
    Change-Id: I9d94595f9fb4f5e93f22958c64dd6cf3d4c19bcc
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/direct/FlowDirectStatisticsService.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.statistics.services.direct;
+
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.openflowplugin.api.openflow.registry.flow.FlowRegistryKey;
+import org.opendaylight.openflowplugin.impl.registry.flow.FlowRegistryKeyFactory;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.FlowStatsResponseConvertor;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match.MatchReactor;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetFlowStatisticsInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetFlowStatisticsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetFlowStatisticsOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.FlowStatisticsData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.FlowStatisticsDataBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.flow.and.statistics.map.list.FlowAndStatisticsMapList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.flow.and.statistics.map.list.FlowAndStatisticsMapListBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.flow.and.statistics.map.list.FlowAndStatisticsMapListKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.flow.statistics.FlowStatisticsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.MultipartReplyFlowCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply.flow._case.MultipartReplyFlow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.MultipartRequestBody;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.MultipartRequestFlowCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.multipart.request.flow._case.MultipartRequestFlowBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Flow direct statistics service.
+ *
+ * @author Tomas Slusny
+ */
+public class FlowDirectStatisticsService extends AbstractDirectStatisticsService<GetFlowStatisticsInput, GetFlowStatisticsOutput> {
+    private final FlowStatsResponseConvertor flowStatsConvertor = new FlowStatsResponseConvertor();
+
+    /**
+     * Instantiates a new Flow direct statistics service.
+     *
+     * @param requestContextStack the request context stack
+     * @param deviceContext       the device context
+     */
+    public FlowDirectStatisticsService(RequestContextStack requestContextStack, DeviceContext deviceContext) {
+        super(MultipartType.OFPMPFLOW, requestContextStack, deviceContext);
+    }
+
+    @Override
+    protected MultipartRequestBody buildRequestBody(GetFlowStatisticsInput input) {
+        final MultipartRequestFlowBuilder mprFlowRequestBuilder = new MultipartRequestFlowBuilder();
+
+        if (input.getTableId() != null) {
+            mprFlowRequestBuilder.setTableId(input.getTableId());
+        } else {
+            mprFlowRequestBuilder.setTableId(OFConstants.OFPTT_ALL);
+        }
+
+        if (input.getOutPort() != null) {
+            mprFlowRequestBuilder.setOutPort(input.getOutPort().longValue());
+        } else {
+            mprFlowRequestBuilder.setOutPort(OFConstants.OFPP_ANY);
+        }
+
+        if (input.getOutGroup() != null) {
+            mprFlowRequestBuilder.setOutGroup(input.getOutGroup());
+        } else {
+            mprFlowRequestBuilder.setOutGroup(OFConstants.OFPG_ANY);
+        }
+
+        if (input.getCookie() != null) {
+            mprFlowRequestBuilder.setCookie(input.getCookie().getValue());
+        } else {
+            mprFlowRequestBuilder.setCookie(OFConstants.DEFAULT_COOKIE);
+        }
+
+        if (input.getCookieMask() != null) {
+            mprFlowRequestBuilder.setCookieMask(input.getCookieMask().getValue());
+        } else {
+            mprFlowRequestBuilder.setCookieMask(OFConstants.DEFAULT_COOKIE_MASK);
+        }
+
+        MatchReactor.getInstance().convert(input.getMatch(), getVersion(), mprFlowRequestBuilder, getDatapathId());
+
+        return new MultipartRequestFlowCaseBuilder()
+                .setMultipartRequestFlow(mprFlowRequestBuilder.build())
+                .build();
+    }
+
+    @Override
+    protected GetFlowStatisticsOutput buildReply(List<MultipartReply> input) {
+        final List<FlowAndStatisticsMapList> statsList = new ArrayList<>();
+
+        for (final MultipartReply mpReply : input) {
+            final MultipartReplyFlowCase caseBody = (MultipartReplyFlowCase) mpReply.getMultipartReplyBody();
+            final MultipartReplyFlow replyBody = caseBody.getMultipartReplyFlow();
+
+            final List<FlowAndStatisticsMapList> statsListPart = flowStatsConvertor.toSALFlowStatsList(replyBody.getFlowStats(), getDatapathId(), getOfVersion());
+
+            for (final FlowAndStatisticsMapList part : statsListPart) {
+                final org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.FlowId flowId =
+                        new org.opendaylight.yang.gen.v1.urn.opendaylight.flow.statistics.rev130819.FlowId(generateFlowId(part).getValue());
+
+                statsList.add(new FlowAndStatisticsMapListBuilder(part)
+                        .setKey(new FlowAndStatisticsMapListKey(flowId))
+                        .setFlowId(flowId)
+                        .build());
+            }
+        }
+
+        return new GetFlowStatisticsOutputBuilder()
+                .setFlowAndStatisticsMapList(statsList)
+                .build();
+    }
+
+    @Override
+    protected void storeStatistics(GetFlowStatisticsOutput output) throws Exception {
+        final InstanceIdentifier<FlowCapableNode> nodePath = getDeviceContext()
+                .getDeviceState().getNodeInstanceIdentifier().augmentation(FlowCapableNode.class);
+
+        for (final FlowAndStatisticsMapList flowStatistics : output.getFlowAndStatisticsMapList()) {
+            final FlowId flowId = generateFlowId(flowStatistics);
+            final FlowKey flowKey = new FlowKey(flowId);
+
+            final FlowStatisticsDataBuilder flowStatisticsDataBld = new FlowStatisticsDataBuilder()
+                    .setFlowStatistics(new FlowStatisticsBuilder(flowStatistics).build());
+
+            final FlowBuilder flowBuilder = new FlowBuilder(flowStatistics)
+                    .addAugmentation(FlowStatisticsData.class, flowStatisticsDataBld.build())
+                    .setKey(flowKey);
+
+            final InstanceIdentifier<Flow> flowStatisticsPath = nodePath
+                    .child(Table.class, new TableKey(flowStatistics.getTableId()))
+                    .child(Flow.class, flowKey);
+
+            getDeviceContext().writeToTransactionWithParentsSlow(LogicalDatastoreType.OPERATIONAL, flowStatisticsPath, flowBuilder.build());
+        }
+    }
+
+    private FlowId generateFlowId(FlowAndStatisticsMapList flowStatistics) {
+        final FlowStatisticsDataBuilder flowStatisticsDataBld = new FlowStatisticsDataBuilder()
+                .setFlowStatistics(new FlowStatisticsBuilder(flowStatistics).build());
+
+        final FlowBuilder flowBuilder = new FlowBuilder(flowStatistics)
+                .addAugmentation(FlowStatisticsData.class, flowStatisticsDataBld.build());
+
+        final short tableId = flowStatistics.getTableId();
+        final FlowRegistryKey flowRegistryKey = FlowRegistryKeyFactory.create(flowBuilder.build());
+        return getDeviceContext().getDeviceFlowRegistry().storeIfNecessary(flowRegistryKey, tableId);
+    }
+}
---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/statistics/services/direct/NodeConnectorDirectStatisticsServiceTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.statistics.services.direct;
+
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetNodeConnectorStatisticsInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetNodeConnectorStatisticsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.MultipartReplyPortStatsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply.port.stats._case.MultipartReplyPortStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply.port.stats._case.multipart.reply.port.stats.PortStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.MultipartRequestPortStatsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.multipart.request.port.stats._case.MultipartRequestPortStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.node.connector.statistics.and.port.number.map.NodeConnectorStatisticsAndPortNumberMap;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class NodeConnectorDirectStatisticsServiceTest extends AbstractDirectStatisticsServiceTest {
+    private NodeConnectorDirectStatisticsService service;
+
+    @Override
+    public void setUp() throws Exception {
+        service = new NodeConnectorDirectStatisticsService(requestContextStack, deviceContext);
+    }
+
+    @Override
+    public void testBuildRequestBody() throws Exception {
+        final GetNodeConnectorStatisticsInput input = mock(GetNodeConnectorStatisticsInput.class);
+
+        when(input.getNode()).thenReturn(createNodeRef(NODE_ID));
+        when(input.getNodeConnectorId()).thenReturn(nodeConnectorId);
+
+        final MultipartRequestPortStatsCase body = (MultipartRequestPortStatsCase) service.buildRequestBody(input);
+        final MultipartRequestPortStats nodeConnector = body.getMultipartRequestPortStats();
+
+        assertEquals(PORT_NO, nodeConnector.getPortNo());
+    }
+
+    @Override
+    public void testBuildReply() throws Exception {
+        final MultipartReply reply = mock(MultipartReply.class);
+        final MultipartReplyPortStatsCase nodeConnectorCase = mock(MultipartReplyPortStatsCase.class);
+        final MultipartReplyPortStats nodeConnector = mock(MultipartReplyPortStats.class);
+        final PortStats nodeConnectorStat = mock(PortStats.class);
+        final List<PortStats> nodeConnectorStats = Arrays.asList(nodeConnectorStat);
+        final List<MultipartReply> input = Arrays.asList(reply);
+
+        when(nodeConnector.getPortStats()).thenReturn(nodeConnectorStats);
+        when(nodeConnectorCase.getMultipartReplyPortStats()).thenReturn(nodeConnector);
+        when(reply.getMultipartReplyBody()).thenReturn(nodeConnectorCase);
+
+        when(nodeConnectorStat.getPortNo()).thenReturn(PORT_NO);
+        when(nodeConnectorStat.getTxBytes()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getCollisions()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxBytes()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxCrcErr()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxDropped()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxErrors()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxFrameErr()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxOverErr()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getRxPackets()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getTxDropped()).thenReturn(BigInteger.ONE);
+        when(nodeConnectorStat.getTxErrors()).thenReturn(BigInteger.ONE);
+
+        final GetNodeConnectorStatisticsOutput output = service.buildReply(input);
+        assertTrue(output.getNodeConnectorStatisticsAndPortNumberMap().size() > 0);
+
+        final NodeConnectorStatisticsAndPortNumberMap stats =
+                output.getNodeConnectorStatisticsAndPortNumberMap().get(0);
+
+        assertEquals(stats.getNodeConnectorId(), nodeConnectorId);
+    }
+
+    @Override
+    public void testStoreStatistics() throws Exception {
+        final NodeConnectorStatisticsAndPortNumberMap stat = mock(NodeConnectorStatisticsAndPortNumberMap.class);
+        when(stat.getNodeConnectorId()).thenReturn(nodeConnectorId);
+
+        final List<NodeConnectorStatisticsAndPortNumberMap> stats = Arrays.asList(stat);
+        final GetNodeConnectorStatisticsOutput output = mock(GetNodeConnectorStatisticsOutput.class);
+        when(output.getNodeConnectorStatisticsAndPortNumberMap()).thenReturn(stats);
+
+        service.storeStatistics(output);
+        verify(deviceContext).writeToTransactionWithParentsSlow(eq(LogicalDatastoreType.OPERATIONAL), any(), any());
+    }
+}
