e08d876dbb763fd8615e1bb1b6027fc8cac0709a
Shuva Kar <shuva.jyoti.kar@ericsson.com>
2016-05-21 15:33:38 +0530

    BUG-4117: notification supplier - rename
     - renamed from old-notification-supplier to notification-supplier
     - reason: prefix 'old' usually gets strongly misleading after
         a couple of releases
    
    Bringing Michal's draft to master
    
    Change-Id: I0fd387571714cda943042671f9aa454003fe5bed
    Signed-off-by: Shuva Kar <shuva.jyoti.kar@ericsson.com>

---
+++ applications/notification-supplier/src/main/java/org/opendaylight/openflowplugin/applications/notification/supplier/NotificationSupplierForItemStat.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.notification.supplier;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+/**
+ * Notifications for Statistics have little bit different case,
+ * because it looks like they have response for create and update.
+ * But follow the statistics internal implementation processing
+ * is talks only about create event.
+ *
+ * @param <O> - data tree item Object
+ * @param <N> - Statistics Notification
+ */
+public interface NotificationSupplierForItemStat<O extends DataObject, N extends Notification>
+        extends NotificationSupplierDefinition<O> {
+
+
+    /**
+     * Method produces relevant Statistics kind of {@link Notification} from statistics
+     * data tree item identified by {@link InstanceIdentifier} path.
+     * 
+     * @param o - Statistics Data Tree Item
+     * @param path - Identifier of Data Tree Item
+     * @return {@link Notification} - relevant API contract Notification
+     */
+    N createNotification(O o, InstanceIdentifier<O> path);
+}
+
