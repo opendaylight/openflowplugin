ba60a893f931f8edfab191e94bdccb37a2dde013
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-05-26 16:15:45 +0200

    Bug 5936 - DeviceFlowRegistry flowId bug
    - Added match to FlowRegistryKey
    - Added new method to DeviceFlowRegistry "fill(DataBroker, KeyedInstanceIdentifier)"
    - New method will fill flow registry with flow IDs from DS/config
    - If flow registry do not contains key, now retrieveIdForFlow will try to generate alien flow id
    - Changed storeIfNecessary method to use retrieveIdForFlow and removed tableId
      parameter from it
    
    Change-Id: I5a12f13bac0071efbfb78f672a6a5d0161d0f45b
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/registry/flow/DeviceFlowRegistryImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/registry/flow/DeviceFlowRegistryImpl.java
@@ -7,18 +7,33 @@
  */
 package org.opendaylight.openflowplugin.impl.registry.flow;
 
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
 import com.romix.scala.collection.concurrent.TrieMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
 import javax.annotation.concurrent.GuardedBy;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.openflowplugin.api.openflow.registry.flow.DeviceFlowRegistry;
 import org.opendaylight.openflowplugin.api.openflow.registry.flow.FlowDescriptor;
 import org.opendaylight.openflowplugin.api.openflow.registry.flow.FlowRegistryKey;
 import org.opendaylight.openflowplugin.impl.util.FlowUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,8 +48,66 @@ public class DeviceFlowRegistryImpl implements DeviceFlowRegistry {
     private final Collection<FlowRegistryKey> marks = new HashSet<>();
 
     @Override
+    public void fill(final DataBroker dataBroker, final KeyedInstanceIdentifier<Node, NodeKey> instanceIdentifier) {
+        // Create new read-only transaction
+        final ReadTransaction transaction = dataBroker.newReadOnlyTransaction();
+
+        // Now, we will try to populate fowRegistry with all flows we can find in DS/Config
+        if (transaction != null) {
+            // Prepare read operation from DS/Config for FlowCapableNode
+            // TODO: Read only Tables, and not entire FlowCapableNode (fix Yang model)
+            final InstanceIdentifier<FlowCapableNode> path = instanceIdentifier.augmentation(FlowCapableNode.class);
+            final CheckedFuture<Optional<FlowCapableNode>, ReadFailedException> future = transaction.read(LogicalDatastoreType.CONFIGURATION, path);
+
+            if (future != null) {
+                try {
+                    // Synchronously read all data in FlowCapableNode
+                    final Optional<FlowCapableNode> data = future.get();
+
+                    if (data.isPresent()) {
+                        final List<Table> tables = data.get().getTable();
+
+                        if (tables != null) {
+                            for (Table table : tables) {
+                                final List<Flow> flows = table.getFlow();
+
+                                if (flows != null) {
+                                    // If we finally got some flows, store each of them in registry if needed
+                                    for (Flow flow : table.getFlow()) {
+                                        LOG.trace("Reading and storing flowDescriptor with table ID : {} and flow ID : {}", flow.getTableId(), flow.getId().getValue());
+                                        final FlowRegistryKey key = FlowRegistryKeyFactory.create(flow);
+                                        final FlowDescriptor descriptor = FlowDescriptorFactory.create(flow.getTableId(), flow.getId());
+                                        flowRegistry.putIfAbsent(key, descriptor);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (InterruptedException | ExecutionException e) {
+                    LOG.error("Read transaction for identifier {} failed with exception: {}", instanceIdentifier, e);
+                }
+            }
+        }
+    }
+
+    @Override
     public FlowDescriptor retrieveIdForFlow(final FlowRegistryKey flowRegistryKey) {
+        LOG.trace("Trying to retrieve flowDescriptor for key: {}", flowRegistryKey);
+
+        // First, try to get FlowDescriptor from flow registry
         FlowDescriptor flowDescriptor = flowRegistry.get(flowRegistryKey);
+
+        // We was not able to retrieve FlowDescriptor, so we will at least try to generate it
+        if (flowDescriptor == null) {
+            final short tableId = flowRegistryKey.getTableId();
+            final FlowId alienFlowId = FlowUtil.createAlienFlowId(tableId);
+            flowDescriptor = FlowDescriptorFactory.create(tableId, alienFlowId);
+
+            // Finally we got flowDescriptor, so now we will store it to registry,
+            // so next time we won't need to generate it again
+            store(flowRegistryKey, flowDescriptor);
+        }
+
         return flowDescriptor;
     }
 
@@ -45,18 +118,10 @@ public class DeviceFlowRegistryImpl implements DeviceFlowRegistry {
     }
 
     @Override
-    public FlowId storeIfNecessary(final FlowRegistryKey flowRegistryKey, @Deprecated final short tableId) {
-        //TODO: remove tableId parameter - it is contained in the first one
-        final FlowId alienFlowId = FlowUtil.createAlienFlowId(tableId);
-        final FlowDescriptor alienFlowDescriptor = FlowDescriptorFactory.create(tableId, alienFlowId);
-
-        final FlowDescriptor previous = flowRegistry.putIfAbsent(flowRegistryKey, alienFlowDescriptor);
-        if (previous == null) {
-            LOG.trace("Flow descriptor for flow hash {} wasn't found.", flowRegistryKey.hashCode());
-            return alienFlowId;
-        } else {
-            return previous.getFlowId();
-        }
+    public FlowId storeIfNecessary(final FlowRegistryKey flowRegistryKey) {
+        // We will simply reuse retrieveIdForFlow to get or generate FlowDescriptor and store it if needed
+        final FlowDescriptor flowDescriptor = retrieveIdForFlow(flowRegistryKey);
+        return flowDescriptor.getFlowId();
     }
 
     @Override
