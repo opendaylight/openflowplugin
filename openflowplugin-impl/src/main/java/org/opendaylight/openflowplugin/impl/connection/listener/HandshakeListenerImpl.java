4df939734a3e2d8357919a22a63767ad1c399935
Jozef Bacigal <jbacigal@cisco.com>
2016-06-20 15:10:04 +0200

    After review updates
    Change-Id: I11f49d9469bd55ba8922506ebb9692d832290f3a
    Signed-off-by: Jozef Bacigal <jbacigal@cisco.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/connection/listener/HandshakeListenerImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/connection/listener/HandshakeListenerImpl.java
@@ -54,7 +54,7 @@ public class HandshakeListenerImpl implements HandshakeListener {
         connectionContext.changeStateToWorking();
         connectionContext.setFeatures(featureOutput);
         connectionContext.setNodeId(InventoryDataServiceUtil.nodeIdFromDatapathId(featureOutput.getDatapathId()));
-        connectionContext.createDeviceInfo();
+        connectionContext.handshakeSuccessful();
 
         // fire barrier in order to sweep all handshake and posthandshake messages before continue
         final ListenableFuture<RpcResult<BarrierOutput>> barrier = fireBarrier(version, 0L);
