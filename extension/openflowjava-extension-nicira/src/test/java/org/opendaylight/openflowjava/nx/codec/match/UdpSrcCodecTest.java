a015e50ddd3ab49397c455da652686d3bfcf2c88
miroslav.macko <miroslav.macko@pantheon.tech>
2016-06-22 15:31:41 +0200

    Bug 5543 - Bo: Update JUnit tests part_11
    	- Added tests for openflowjava-extension-nicira
    
    Change-Id: I6fb6f864655c69db933c4d1ebe2adde9189221ab
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

---
+++ extension/openflowjava-extension-nicira/src/test/java/org/opendaylight/openflowjava/nx/codec/match/UdpSrcCodecTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.nx.codec.match;
+
+import static org.junit.Assert.assertEquals;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm0Class;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmOfUdpSrc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.ofj.nxm.of.match.udp.src.grouping.UdpSrcValuesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.UdpSrcCaseValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.UdpSrcCaseValueBuilder;
+
+public class UdpSrcCodecTest {
+
+    UdpSrcCodec udpSrcCodec;
+    ByteBuf buffer;
+    MatchEntry input;
+
+    private static final int VALUE_LENGTH = 4;
+    private static final int NXM_FIELD_CODE = 11;
+
+    @Before
+    public void setUp() {
+        udpSrcCodec = new UdpSrcCodec();
+        buffer = ByteBufAllocator.DEFAULT.buffer();
+    }
+
+    @Test
+    public void serializeTest() {
+        input = createMatchEntry();
+        udpSrcCodec.serialize(input, buffer);
+
+        assertEquals(OxmMatchConstants.NXM_0_CLASS, buffer.readUnsignedShort());
+        short fieldMask = buffer.readUnsignedByte();
+        assertEquals(NXM_FIELD_CODE, fieldMask >> 1);
+        assertEquals(0, fieldMask & 1);
+        assertEquals(VALUE_LENGTH, buffer.readUnsignedByte());
+        assertEquals(1, buffer.readUnsignedShort());
+    }
+
+    @Test
+    public void deserializeTest() {
+        createBuffer(buffer);
+
+        input = udpSrcCodec.deserialize(buffer);
+
+        UdpSrcCaseValue result = ((UdpSrcCaseValue) input.getMatchEntryValue());
+
+        assertEquals(Nxm0Class.class, input.getOxmClass());
+        assertEquals(NxmOfUdpSrc.class, input.getOxmMatchField());
+        assertEquals(false, input.isHasMask());
+        assertEquals(1, result.getUdpSrcValues().getPort().getValue().shortValue());
+    }
+
+    private MatchEntry createMatchEntry() {
+        MatchEntryBuilder matchEntryBuilder = new MatchEntryBuilder();
+        UdpSrcCaseValueBuilder caseBuilder = new UdpSrcCaseValueBuilder();
+        UdpSrcValuesBuilder valuesBuilder = new UdpSrcValuesBuilder();
+
+        matchEntryBuilder.setOxmClass(Nxm0Class.class);
+        matchEntryBuilder.setOxmMatchField(NxmOfUdpSrc.class);
+        matchEntryBuilder.setHasMask(false);
+
+        valuesBuilder.setPort(new PortNumber(1));
+
+        caseBuilder.setUdpSrcValues(valuesBuilder.build());
+        matchEntryBuilder.setMatchEntryValue(caseBuilder.build());
+        return matchEntryBuilder.build();
+    }
+
+    private void createBuffer(ByteBuf message) {
+        message.writeShort(OxmMatchConstants.NXM_0_CLASS);
+
+        int fieldMask = (NXM_FIELD_CODE << 1);
+        message.writeByte(fieldMask);
+        message.writeByte(VALUE_LENGTH);
+        byte[] value = new byte[VALUE_LENGTH];
+        //Port num = 1
+        message.writeShort(1);
+    }
+
+}
