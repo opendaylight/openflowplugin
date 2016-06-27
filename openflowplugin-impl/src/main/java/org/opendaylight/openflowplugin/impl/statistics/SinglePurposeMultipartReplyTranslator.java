6ff08cf87c60ee8182100cd0f56f64820a814024
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-06-27 15:49:27 +0200

    Bug 5540 - MeterStatsResponseConvertor, MeterConfigStatsResponseConvertor
    - Reworked MeterStatsResponseConvertor to use new ConvertorManager desing
    - Moved part of logic of MeterStatsResponseConvertor to new
      MeterConfigStatsResponseConvertor
    - Updated tests and usage of MeterStatsResponseConvertor and
      MeterConfigStatsResponseConvertor accordingly
    
    Change-Id: I5555fae748fd8d3c2e0baa8d2b56ee450fe4697b
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/SinglePurposeMultipartReplyTranslator.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/SinglePurposeMultipartReplyTranslator.java
@@ -9,11 +9,13 @@ package org.opendaylight.openflowplugin.impl.statistics;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.opendaylight.openflowplugin.api.openflow.md.util.OpenflowVersion;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.ConvertorManager;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.FlowStatsResponseConvertor;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.GroupStatsResponseConvertor;
-import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.MeterStatsResponseConvertor;
 import org.opendaylight.openflowplugin.openflow.md.util.InventoryDataServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Counter32;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Counter64;
@@ -49,6 +51,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.Meter
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterKbps;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterPktps;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.meter.config.stats.reply.MeterConfigStats;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.statistics.types.rev130925.duration.DurationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.statistics.types.rev130925.node.connector.statistics.BytesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.statistics.types.rev130925.node.connector.statistics.PacketsBuilder;
@@ -106,7 +109,6 @@ public class SinglePurposeMultipartReplyTranslator {
 
     private static FlowStatsResponseConvertor flowStatsConvertor = new FlowStatsResponseConvertor();
     private static GroupStatsResponseConvertor groupStatsConvertor = new GroupStatsResponseConvertor();
-    private static MeterStatsResponseConvertor meterStatsConvertor = new MeterStatsResponseConvertor();
 
 
     public List<DataObject> translate(final BigInteger datapathId, final short version, final OfHeader msg) {
@@ -285,8 +287,11 @@ public class SinglePurposeMultipartReplyTranslator {
 
                     MultipartReplyMeterCase caseBody = (MultipartReplyMeterCase) mpReply.getMultipartReplyBody();
                     MultipartReplyMeter replyBody = caseBody.getMultipartReplyMeter();
-                    message.setMeterStats(meterStatsConvertor.toSALMeterStatsList(replyBody.getMeterStats()));
 
+                    final Optional<List<org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.meter.statistics.reply.MeterStats>> meterStatsList =
+                            ConvertorManager.getInstance().convert(replyBody.getMeterStats());
+
+                    message.setMeterStats(meterStatsList.orElse(Collections.emptyList()));
                     listDataObject.add(message.build());
                     return listDataObject;
                 }
@@ -299,8 +304,10 @@ public class SinglePurposeMultipartReplyTranslator {
 
                     MultipartReplyMeterConfigCase caseBody = (MultipartReplyMeterConfigCase) mpReply.getMultipartReplyBody();
                     MultipartReplyMeterConfig replyBody = caseBody.getMultipartReplyMeterConfig();
-                    message.setMeterConfigStats(meterStatsConvertor.toSALMeterConfigList(replyBody.getMeterConfig()));
 
+                    final Optional<List<MeterConfigStats>> meterConfigStatsList = ConvertorManager.getInstance().convert(replyBody.getMeterConfig());
+
+                    message.setMeterConfigStats(meterConfigStatsList.orElse(Collections.emptyList()));
                     listDataObject.add(message.build());
                     return listDataObject;
                 }
