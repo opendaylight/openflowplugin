6d3c9f46ce02d96852d1300764142d002ba4c528
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-06-27 16:10:40 +0200

    Bug 5540 - PortConvertor
    - Reworked PortConvertor to use new ConvertorManager design
    - Updated tests and usage of PortConvertor accordingly
    
    Change-Id: If53d7c521bf4a718a466ec7cd11d28e57d8cf4bd
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/PortConvertor.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/PortConvertor.java
@@ -1,15 +1,20 @@
-/**
+/*
  * Copyright (c) 2014 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor;
 
-import org.opendaylight.openflowplugin.openflow.md.util.OpenflowPortsUtil;
+import com.google.common.annotations.VisibleForTesting;
 import org.opendaylight.openflowplugin.api.openflow.md.util.OpenflowVersion;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.ParametrizedConvertor;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.data.VersionConvertorData;
+import org.opendaylight.openflowplugin.openflow.md.util.OpenflowPortsUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.port.mod.port.Port;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortConfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortConfigV10;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.PortFeatures;
@@ -20,55 +25,19 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortModInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply.port.desc._case.multipart.reply.port.desc.Ports;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply.port.desc._case.multipart.reply.port.desc.PortsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * @author esssuuu This convertor class is used for Port Mod,port status and
- *         port description messages,decodes SAL and encodes to OF Data
+ * Converts port mod, port status and port description MD-SAL messages to OF library data
  *
+ * Example usage:
+ * <pre>
+ * {@code
+ * VersionConvertorData data = new VersionConvertorData(version);
+ * Optional<PortModInput> ofPort = ConvertorManager.getInstance().convert(salPort, data);
+ * }
+ * </pre>
  */
-public final class PortConvertor {
-    private static final Logger LOG = LoggerFactory.getLogger(PortConvertor.class);
-
-    private PortConvertor() {
-
-    }
-
-    /**
-     * This method is used by PORT_MOD_MESSAGE
-     *
-     * @param source source port
-     * @param version openflow version
-     * @return port mod input
-     */
-
-    public static PortModInput toPortModInput(
-            org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.port.mod.port.Port source,
-            short version) {
-
-
-        PortConfig config = maskPortConfigFields(source.getConfiguration());
-        PortConfigV10 configV10 = maskPortConfigV10Fields(source.getConfiguration());
-
-        PortModInputBuilder portModInputBuilder = new PortModInputBuilder();
-        portModInputBuilder.setAdvertise(getPortFeatures(source.getAdvertisedFeatures()));
-        portModInputBuilder.setPortNo(new PortNumber(
-                OpenflowPortsUtil.getProtocolPortNumber(OpenflowVersion.get(version), source.getPortNumber())));
-
-        portModInputBuilder.setConfig(config);
-        portModInputBuilder.setMask(config);
-
-        portModInputBuilder.setHwAddress(new MacAddress(source.getHardwareAddress()));
-
-        portModInputBuilder.setVersion(version);
-
-        portModInputBuilder.setConfigV10(configV10);
-        portModInputBuilder.setMaskV10(configV10);
-        portModInputBuilder.setAdvertiseV10(getPortFeaturesV10(source.getAdvertisedFeatures()));
-        return portModInputBuilder.build();
-
-    }
+public class PortConvertor implements ParametrizedConvertor<Port, PortModInput, VersionConvertorData> {
 
     private static PortConfig maskPortConfigFields(
             org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortConfig configData) {
@@ -91,6 +60,7 @@ public final class PortConvertor {
         return new PortConfigV10(false, noFwd, noPacketIn, noRecv, true, true, portDown);
 
     }
+
     private static PortFeatures getPortFeatures(
             org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortFeatures salPortFeatures) {
 
@@ -110,22 +80,18 @@ public final class PortConvertor {
                 salPortFeatures.isPause(), salPortFeatures.isPauseAsym());
     }
 
-
-    /*
+    /**
      * This method is called as a reply to OFPMP_PORT_DESCRIPTION
      * message(OF1.3.1)
-     */
-    /**
-     * @param source FlowCapablePort
+     *
+     * @param source  FlowCapablePort
      * @param version openflow version
      * @return OF:Ports
      */
-    public static Ports toPortDesc(
+    @VisibleForTesting
+    static Ports toPortDesc(
             org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.FlowCapablePort source,
             short version) {
-
-        PortConfig config = null;
-        PortState portState = null;
 
         PortsBuilder oFPortDescDataBuilder = new PortsBuilder();
 
@@ -135,11 +101,11 @@ public final class PortConvertor {
         oFPortDescDataBuilder.setHwAddr(source.getHardwareAddress());
         oFPortDescDataBuilder.setName(source.getName());
 
-        config = maskPortConfigFields(source.getConfiguration());
+        PortConfig config = maskPortConfigFields(source.getConfiguration());
 
         oFPortDescDataBuilder.setConfig(config);
 
-        portState = getPortState(source.getState());
+        PortState portState = getPortState(source.getState());
 
         oFPortDescDataBuilder.setState(portState);
         oFPortDescDataBuilder.setCurrentFeatures(getPortFeatures(source.getCurrentFeature()));
@@ -164,6 +130,31 @@ public final class PortConvertor {
 
     }
 
+    @Override
+    public Class<?> getType() {
+        return Port.class;
+    }
 
+    @Override
+    public PortModInput convert(Port source, VersionConvertorData data) {
+        PortConfig config = maskPortConfigFields(source.getConfiguration());
+        PortConfigV10 configV10 = maskPortConfigV10Fields(source.getConfiguration());
 
+        PortModInputBuilder portModInputBuilder = new PortModInputBuilder();
+        portModInputBuilder.setAdvertise(getPortFeatures(source.getAdvertisedFeatures()));
+        portModInputBuilder.setPortNo(new PortNumber(
+                OpenflowPortsUtil.getProtocolPortNumber(OpenflowVersion.get(data.getVersion()), source.getPortNumber())));
+
+        portModInputBuilder.setConfig(config);
+        portModInputBuilder.setMask(config);
+
+        portModInputBuilder.setHwAddress(new MacAddress(source.getHardwareAddress()));
+
+        portModInputBuilder.setVersion(data.getVersion());
+
+        portModInputBuilder.setConfigV10(configV10);
+        portModInputBuilder.setMaskV10(configV10);
+        portModInputBuilder.setAdvertiseV10(getPortFeaturesV10(source.getAdvertisedFeatures()));
+        return portModInputBuilder.build();
+    }
 }
