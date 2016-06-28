1b83dd56654e1196c4a2bf234e5907f64fb91d6f
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-06-28 12:37:13 +0200

    Bug 5540 - MatchConvertor
    - Reworked MatchConvertorImpl and MatchConvertorV10Impl (extracted some logic from it)
    - Added MatchResponseConvertor and MatchV10ResponseConvertor
    - Updated and added new tests, and updated usage
    
    Change-Id: I4eede9de96b5b0882e2b79c739c2c80aa0ddf94e
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/match/MatchConvertorUtil.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/match/MatchConvertorUtil.java
@@ -1,28 +1,53 @@
-/**
- * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match;
 
+import java.util.Iterator;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Dscp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.Ipv6ExthdrFlags;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.IpDscp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.OpenflowBasicClass;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.VlanPcp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.IpDscpCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.VlanPcpCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.ip.dscp._case.IpDscpBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.vlan.pcp._case.VlanPcpBuilder;
 
 /**
  * match related tools
  */
 public abstract class MatchConvertorUtil {
+    // Pre-calculated masks for the 33 possible values. Do not give them out, but clone() them as they may
+    // end up being leaked and vulnerable.
+    private static final byte[][] IPV4_MASKS;
 
-    private static final String PREFIX_SEPARATOR = "/";
+    static {
+        final byte[][] tmp = new byte[33][];
+        for (int i = 0; i <= 32; ++i) {
+            final int mask = 0xffffffff << (32 - i);
+            tmp[i] = new byte[]{(byte) (mask >>> 24), (byte) (mask >>> 16), (byte) (mask >>> 8), (byte) mask};
+        }
+
+        IPV4_MASKS = tmp;
+    }
 
     /**
+     * Ipv 6 exthdr flags to int integer.
+     *
      * @param pField ipv6 external header flag
      * @return integer containing lower 9 bits filled with corresponding flags
      */
     public static Integer ipv6ExthdrFlagsToInt(final Ipv6ExthdrFlags pField) {
         Integer bitmap = 0;
-        bitmap |= pField.isNonext() ? (1 << 0) : 0;
+        bitmap |= pField.isNonext() ? 1 : 0;
         bitmap |= pField.isEsp() ? (1 << 1) : 0;
         bitmap |= pField.isAuth() ? (1 << 2) : 0;
         bitmap |= pField.isDest() ? (1 << 3) : 0;
@@ -34,4 +59,66 @@ public abstract class MatchConvertorUtil {
         return bitmap;
     }
 
+    /**
+     * Extract ipv 4 mask byte [ ].
+     *
+     * @param addressParts the address parts
+     * @return the byte [ ]
+     */
+    public static byte[] extractIpv4Mask(final Iterator<String> addressParts) {
+        final int prefix;
+        if (addressParts.hasNext()) {
+            int potentionalPrefix = Integer.parseInt(addressParts.next());
+            prefix = potentionalPrefix < 32 ? potentionalPrefix : 0;
+        } else {
+            prefix = 0;
+        }
+
+        if (prefix != 0) {
+            // clone() is necessary to protect our constants
+            return IPV4_MASKS[prefix].clone();
+        }
+
+        return null;
+    }
+
+    /**
+     * To of ip dscp match entry.
+     *
+     * @param ipDscp the ip dscp
+     * @return the match entry
+     */
+    public static MatchEntry toOfIpDscp(final Dscp ipDscp) {
+        MatchEntryBuilder matchEntryBuilder = new MatchEntryBuilder();
+        matchEntryBuilder.setOxmClass(OpenflowBasicClass.class);
+        matchEntryBuilder.setHasMask(false);
+        matchEntryBuilder.setOxmMatchField(IpDscp.class);
+
+        IpDscpCaseBuilder ipDscpCaseBuilder = new IpDscpCaseBuilder();
+        IpDscpBuilder ipDscpBuilder = new IpDscpBuilder();
+        ipDscpBuilder.setDscp(ipDscp);
+        ipDscpCaseBuilder.setIpDscp(ipDscpBuilder.build());
+        matchEntryBuilder.setMatchEntryValue(ipDscpCaseBuilder.build());
+        return matchEntryBuilder.build();
+    }
+
+    /**
+     * To of vlan pcp match entry.
+     *
+     * @param vlanPcp the vlan pcp
+     * @return the match entry
+     */
+    public static MatchEntry toOfVlanPcp(
+            final org.opendaylight.yang.gen.v1.urn.opendaylight.l2.types.rev130827.VlanPcp vlanPcp) {
+        MatchEntryBuilder matchEntryBuilder = new MatchEntryBuilder();
+        matchEntryBuilder.setOxmClass(OpenflowBasicClass.class);
+        matchEntryBuilder.setHasMask(false);
+        matchEntryBuilder.setOxmMatchField(VlanPcp.class);
+        VlanPcpCaseBuilder vlanPcpCaseBuilder = new VlanPcpCaseBuilder();
+        VlanPcpBuilder vlanPcpBuilder = new VlanPcpBuilder();
+        vlanPcpBuilder.setVlanPcp(vlanPcp.getValue());
+        vlanPcpCaseBuilder.setVlanPcp(vlanPcpBuilder.build());
+        matchEntryBuilder.setMatchEntryValue(vlanPcpCaseBuilder.build());
+        return matchEntryBuilder.build();
+    }
 }
---
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/match/cases/OfToSalIpv6NdTllCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match.cases;
+
+import java.util.Optional;
+import javax.annotation.Nonnull;
+import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.ConvertorCase;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.match.data.MatchResponseConvertorData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6MatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.Ipv6NdTllCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.ipv6.nd.tll._case.Ipv6NdTll;
+
+public class OfToSalIpv6NdTllCase extends ConvertorCase<Ipv6NdTllCase, MatchBuilder, MatchResponseConvertorData> {
+    public OfToSalIpv6NdTllCase() {
+        super(Ipv6NdTllCase.class, true, OFConstants.OFP_VERSION_1_0, OFConstants.OFP_VERSION_1_3);
+    }
+
+    @Override
+    public Optional<MatchBuilder> process(@Nonnull Ipv6NdTllCase source, MatchResponseConvertorData data) {
+        final MatchBuilder matchBuilder = data.getMatchBuilder();
+        final Ipv6MatchBuilder ipv6MatchBuilder = data.getIpv6MatchBuilder();
+
+        Ipv6NdTll ipv6NdTll = source.getIpv6NdTll();
+
+        if (ipv6NdTll != null) {
+            ipv6MatchBuilder.setIpv6NdTll(ipv6NdTll.getMacAddress());
+            matchBuilder.setLayer3Match(ipv6MatchBuilder.build());
+        }
+
+        return Optional.of(matchBuilder);
+    }
+}
