e76cd074e56695717f09b6a582a4d072ac4f7aa5
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-06-28 14:55:02 +0200

    Bug 5540 - ActionConvertor, ActionResponseConvertor
    - Reworked ActionConvertor to use new ConvertorManager desing
    - Added new ActionResponseConvertor (moved a bit of logic from ActionConvertor)
    - Updated tests and usages accordingly
    
    Change-Id: I76a9b345942272dc555e4d75766968931ee4ecdb
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

---
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/action/cases/SalToOfPushMplsActionCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.action.cases;
+
+import java.util.Optional;
+import javax.annotation.Nonnull;
+import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.action.data.ActionConvertorData;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.ConvertorCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushMplsActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.PushMplsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.push.mpls._case.PushMplsActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.EtherType;
+
+public class SalToOfPushMplsActionCase extends ConvertorCase<PushMplsActionCase, Action, ActionConvertorData> {
+    public SalToOfPushMplsActionCase() {
+        super(PushMplsActionCase.class, true, OFConstants.OFP_VERSION_1_0, OFConstants.OFP_VERSION_1_3);
+    }
+
+    @Nonnull
+    @Override
+    public Optional<Action> process(@Nonnull final PushMplsActionCase source, final ActionConvertorData data) {
+        PushMplsCaseBuilder pushMplsCaseBuilder = new PushMplsCaseBuilder();
+        PushMplsActionBuilder pushMplsBuilder = new PushMplsActionBuilder();
+        pushMplsBuilder.setEthertype(new EtherType(source.getPushMplsAction().getEthernetType()));
+        pushMplsCaseBuilder.setPushMplsAction(pushMplsBuilder.build());
+
+        return Optional.of(new ActionBuilder()
+                .setActionChoice(pushMplsCaseBuilder.build())
+                .build());
+    }
+}
