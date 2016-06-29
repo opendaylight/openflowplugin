ac177b9409d7351673bca61184c46f33e705ff2a
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-06-29 09:08:38 +0200

    Bug 5540 - FlowConvertor, FlowStatsResponseConvertor, FlowInstructionResponseConvertor
    - Reworked FlowConvertor, FlowStatsResponseConvertor to use new ConvertorManager design
    - Added FlowInstructionResponseConvertor (moved some loic from FlowStatsResponseConvertor)
    - Updated tests and usages accordingly
    
    Change-Id: Iff2dc752aab73473adb955394cf852d7524fddd9
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

--- openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/flow/flowflag/FlowFlagReactorTest.java
+++ openflowplugin/src/test/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/flow/flowflag/FlowFlagReactorTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.flowflag;
+package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.flow.flowflag;
 
 import org.junit.Assert;
 import org.junit.Before;
