535d359be46776aeeb04eadc4fedf7756e3c2e3c
Tomas Slusny <tomas.slusny@pantheon.sk>
2016-07-08 09:03:02 +0200

    Bug 5540 - ConvertorManager optimization, concurrency
    - Split ConvertorManager functionality to interfaces ConvertorRegistrator and
      ConvertorExecutor
    - Changed convertor maps to concurrent hash maps
    - registerConvertor now returns old convertor if already registered instead of boolean
    - Better type extraction from collections
    - Removed duplicate map lookups
    - Better log messages
    
    Change-Id: I38c6bc119861fef844535c50740c65fa7ae43192
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.sk>

---
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/sal/convertor/ConvertorExecutor.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.openflow.md.core.sal.convertor;
+
+import java.util.Optional;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.ConvertorData;
+
+public interface ConvertorExecutor {
+    /**
+     * Lookup and use convertor by specified type, then converts source and returns converted result
+     *
+     * @param <FROM> the source type
+     * @param <TO>   the result type
+     * @param source the source
+     * @return the result (can be empty, if no convertor was found)
+     */
+    <FROM, TO> Optional<TO> convert(final FROM source);
+
+    /**
+     * Lookup and use convertor by specified type, then converts source and returns converted result
+     *
+     * @param <FROM> the source type
+     * @param <TO>   the result type
+     * @param <DATA> the data type
+     * @param source the source
+     * @param data   convertor data
+     * @return the result (can be empty, if no convertor was found)
+     */
+    <FROM, TO, DATA extends ConvertorData> Optional<TO> convert(final FROM source, final DATA data);
+}
