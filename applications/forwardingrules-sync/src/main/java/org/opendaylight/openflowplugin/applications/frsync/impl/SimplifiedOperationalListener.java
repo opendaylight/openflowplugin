0eb6ba3a25cb4f7e7354bd8560f2449bab3631e1
Andrej Leitner <anleitne@cisco.com>
2016-07-13 09:47:43 +0200

    Bug 6170 + Bug 5919 - made FRS cluster-aware
     - NodeListener changed to clustered DTCL
     - added ClusterRegistration per connected device
       with corresponding actions (on mastership change)
     - added ClusterServiceManager to register services
       at the provider and keep these registrations
     - if device is not currently mastered skip all modifications
     - added and updated tests
    
    Change-Id: I4fd9459e67cc5127f7eacca93adbd456a34fab0f
    Signed-off-by: Andrej Leitner <anleitne@cisco.com>

--- applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/impl/SimplifiedOperationalListener.java
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/impl/SimplifiedOperationalListener.java
@@ -42,26 +42,29 @@ import org.slf4j.LoggerFactory;
  */
 public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node> {
     private static final Logger LOG = LoggerFactory.getLogger(SimplifiedOperationalListener.class);
+    public static final String DATE_AND_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
     private final SyncReactor reactor;
     private final FlowCapableNodeSnapshotDao operationalSnapshot;
     private final FlowCapableNodeDao configDao;
     private final ReconciliationRegistry reconciliationRegistry;
+    private final ClusterServiceManager clusterServiceManager;
 
     public SimplifiedOperationalListener(final SyncReactor reactor,
                                          final FlowCapableNodeSnapshotDao operationalSnapshot,
                                          final FlowCapableNodeDao configDao,
-                                         final ReconciliationRegistry reconciliationRegistry) {
+                                         final ReconciliationRegistry reconciliationRegistry,
+                                         final ClusterServiceManager clusterServiceManager) {
         this.reactor = reactor;
         this.operationalSnapshot = operationalSnapshot;
         this.configDao = configDao;
         this.reconciliationRegistry = reconciliationRegistry;
+        this.clusterServiceManager = clusterServiceManager;
     }
 
     @Override
     public void onDataTreeChanged(Collection<DataTreeModification<Node>> modifications) {
-        // TODO return for clustered listener if not master for device
-        LOG.trace("Inventory Operational changes {}", modifications.size());
+        LOG.trace("Operational changes: {}", modifications.size());
         super.onDataTreeChanged(modifications);
     }
 
@@ -77,12 +80,22 @@ public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node>
      */
     protected Optional<ListenableFuture<Boolean>> processNodeModification(
             DataTreeModification<Node> modification) throws InterruptedException {
-
+        final NodeId nodeId = ModificationUtil.nodeId(modification);
         updateCache(modification);
-        // TODO register cluster service if node added
-        if (isReconciliationNeeded(modification)) {
+
+        if (isAdd(modification) || isAddLogical(modification)) {
+            clusterServiceManager.onDeviceConnected(nodeId);
+        }
+
+        if (!clusterServiceManager.isDeviceMastered(nodeId)) {
+            LOG.trace("Not master for: {}", nodeId.getValue());
+            return skipModification(modification);
+        }
+
+        if (isRegisteredAndConsistentForReconcile(modification)) {
             return reconciliation(modification);
         }
+        LOG.trace("Nothing to do on: {}", nodeId);
         return skipModification(modification);
     }
 
@@ -95,15 +108,14 @@ public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node>
         NodeId nodeId = ModificationUtil.nodeId(modification);
         if (isDelete(modification) || isDeleteLogical(modification)) {
             operationalSnapshot.updateCache(nodeId, Optional.absent());
-            // TODO unregister/close cluster service if node deleted
-            reconciliationRegistry.unregisterIfRegistered(nodeId);
+            clusterServiceManager.onDeviceDisconnected(nodeId);
             return;
         }
         operationalSnapshot.updateCache(nodeId, Optional.fromNullable(ModificationUtil.flowCapableNodeAfter(modification)));
     }
 
     private Optional<ListenableFuture<Boolean>> skipModification(DataTreeModification<Node> modification) {
-        LOG.trace("Skipping Inventory Operational modification {}, before {}, after {}",
+        LOG.trace("Skipping operational modification: {}, before {}, after {}",
                 ModificationUtil.nodeIdValue(modification),
                 modification.getRootNode().getDataBefore() == null ? "null" : "nonnull",
                 modification.getRootNode().getDataAfter() == null ? "null" : "nonnull");
@@ -160,10 +172,6 @@ public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node>
         return false;
     }
 
-    private boolean isReconciliationNeeded(DataTreeModification<Node> modification) {
-        return isAdd(modification) || isAddLogical(modification) || isRegisteredAndConsistentForReconcile(modification);
-    }
-
     private Optional<ListenableFuture<Boolean>> reconciliation(DataTreeModification<Node> modification) throws InterruptedException {
         final NodeId nodeId = ModificationUtil.nodeId(modification);
         final Optional<FlowCapableNode> nodeConfiguration = configDao.loadByNodeId(nodeId);
@@ -176,6 +184,7 @@ public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node>
             final FlowCapableNode fcNode = ModificationUtil.flowCapableNodeAfter(modification);
             return Optional.of(reactor.syncup(nodePath, nodeConfiguration.get(), fcNode, dsType()));
         } else {
+            LOG.debug("Config not present for reconciliation: {}", nodeId.getValue());
             return skipModification(modification);
         }
     }
@@ -191,34 +200,34 @@ public class SimplifiedOperationalListener extends AbstractFrmSyncListener<Node>
                 .getAugmentation(FlowCapableStatisticsGatheringStatus.class);
 
         if (gatheringStatus == null) {
-            LOG.trace("Statistics gathering never started for: {}", nodeId.getValue());
+            LOG.trace("Statistics gathering never started: {}", nodeId.getValue());
             return false;
         }
 
         final SnapshotGatheringStatusEnd gatheringStatusEnd = gatheringStatus.getSnapshotGatheringStatusEnd();
 
         if (gatheringStatusEnd == null) {
-            LOG.trace("Statistics gathering is not over yet for: {}", nodeId.getValue());
+            LOG.trace("Statistics gathering is not over yet: {}", nodeId.getValue());
             return false;
         }
 
         if (!gatheringStatusEnd.isSucceeded()) {
-            LOG.debug("Statistics gathering was not successful for: {}", nodeId.getValue());
+            LOG.trace("Statistics gathering was not successful: {}", nodeId.getValue());
             return false;
         }
 
         try {
-            Date timestampOfRegistration = reconciliationRegistry.getRegistration(nodeId);
-            final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(ReconciliationRegistry.DATE_AND_TIME_FORMAT);
+            Date timestampOfRegistration = reconciliationRegistry.getRegistrationTimestamp(nodeId);
+            final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_AND_TIME_FORMAT);
             Date timestampOfStatistics = simpleDateFormat.parse(gatheringStatusEnd.getEnd().getValue());
             if (timestampOfStatistics.after(timestampOfRegistration)) {
-                LOG.debug("Fresh operational present for: {} -> going retry!", nodeId.getValue());
+                LOG.debug("Fresh operational present: {}", nodeId.getValue());
                 return true;
             }
         } catch (ParseException e) {
             LOG.error("Timestamp parsing error {}", e);
         }
-        LOG.debug("Fresh operational not present for: {}", nodeId.getValue());
+        LOG.debug("Fresh operational not present: {}", nodeId.getValue());
         return false;
     }
 
