be8b20337b483a9c6bad269161214d0170e56606
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2016-08-10 10:25:08 +0200

    Bug 5596 Initial commit failed
    - added closing connection after unsuccessful initial submit
    - added enum with possible connection state after handshake
    
    Change-Id: I62e06f8189dc593bc40267c96d253fdfe53b3a20
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/connection/ConnectionStatus.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.connection;
+
+/**
+ * Define all possible states of connection after handshake
+ */
+public enum ConnectionStatus {
+
+    // this flag should be return if no connection on this node exists
+    MAY_CONTINUE,
+
+    // this flag should be return if node still connected but not in termination state
+    ALREADY_CONNECTED,
+
+    // this flag should be return if node still connected but already in termination state
+    CLOSING
+}
