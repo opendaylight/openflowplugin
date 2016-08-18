bd815317386f8551430d75ad266b850ffeb053eb
Shuva Kar <shuva.jyoti.kar@ericsson.com>
2016-08-18 16:30:27 +0530

    BUG-6458:
    The issue occurred since during update the older
    flowregistry key wasnot getting removed when the
    newer one was getting stored. This was because the
    match/priority/cookie all forms part of the flow-registry
    key.
    
    Change-Id: Ibed34e79b6310723f7c8078bf54b8a2b4f9ab2b2
    Signed-off-by: Shuva Kar <shuva.jyoti.kar@ericsson.com>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/flow/DeviceFlowRegistry.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/flow/DeviceFlowRegistry.java
@@ -34,6 +34,8 @@ public interface DeviceFlowRegistry extends AutoCloseable {
 
     void markToBeremoved(FlowRegistryKey flowRegistryKey);
 
+    void update(FlowRegistryKey oldFlowRegistryKey,FlowRegistryKey newFlowRegistryKey,FlowDescriptor flowDescriptor);
+
     void removeMarked();
 
     Map<FlowRegistryKey, FlowDescriptor> getAllFlowDescriptors();
