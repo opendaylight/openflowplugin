9faa39bb121672370117fa3d30250d04c65babd2
Andrej Leitner <andrej.leitner@pantheon.tech>
2016-08-23 15:15:14 +0200

    Bug 6513 Remove FD from registry immediately
     - remove FlowDescriptor from DeviceFlowRegistry immeadiately
       instead of marking it as to-be-removed
    
    Change-Id: I269a510fa67b6d04181039515aaefda90b53b827
    Signed-off-by: Andrej Leitner <andrej.leitner@pantheon.tech>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/registry/flow/DeviceFlowRegistryImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/registry/flow/DeviceFlowRegistryImplTest.java
@@ -112,8 +112,7 @@ public class DeviceFlowRegistryImplTest {
         order.verify(readOnlyTransaction).read(LogicalDatastoreType.OPERATIONAL, path);
         assertTrue(allFlowDescriptors.containsKey(key));
 
-        deviceFlowRegistry.markToBeremoved(key);
-        deviceFlowRegistry.removeMarked();
+        deviceFlowRegistry.removeDescriptor(key);
     }
 
     @Test
@@ -208,8 +207,7 @@ public class DeviceFlowRegistryImplTest {
 
     @Test
     public void testRemoveMarked() throws Exception {
-        deviceFlowRegistry.markToBeremoved(key);
-        deviceFlowRegistry.removeMarked();
+        deviceFlowRegistry.removeDescriptor(key);
         Assert.assertEquals(0, deviceFlowRegistry.getAllFlowDescriptors().size());
     }
 
@@ -217,20 +215,18 @@ public class DeviceFlowRegistryImplTest {
     public void testRemoveMarkedNegative() throws Exception {
         final FlowAndStatisticsMapList flowStats = TestFlowHelper.createFlowAndStatisticsMapListBuilder(2).build();
         FlowRegistryKey key2 = FlowRegistryKeyFactory.create(flowStats);
-        deviceFlowRegistry.markToBeremoved(key2);
-        deviceFlowRegistry.removeMarked();
+        deviceFlowRegistry.removeDescriptor(key2);
         Assert.assertEquals(1, deviceFlowRegistry.getAllFlowDescriptors().size());
     }
 
     @Test
     public void testClose() throws Exception {
-        deviceFlowRegistry.markToBeremoved(key);
+        deviceFlowRegistry.removeDescriptor(key);
         deviceFlowRegistry.close();
         Assert.assertEquals(0, deviceFlowRegistry.getAllFlowDescriptors().size());
 
         deviceFlowRegistry.store(key, descriptor);
         Assert.assertEquals(1, deviceFlowRegistry.getAllFlowDescriptors().size());
-        deviceFlowRegistry.removeMarked();
         Assert.assertEquals(1, deviceFlowRegistry.getAllFlowDescriptors().size());
     }
 
