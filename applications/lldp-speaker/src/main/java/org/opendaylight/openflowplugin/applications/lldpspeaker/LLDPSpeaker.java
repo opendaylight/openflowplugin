cccfcce1f1c998357a64ab8d104db60b0ca8556f
senthil <senthil.b@tataelxsi.co.in>
2016-10-18 10:34:53 +0530

    Add new API's for LLDP and Statistics
      *Right now LLDP and statistics time period is hard coded
      *This patch enables to set the time period dynamically,
       which increases the performance of the CPU
      *Increase in CPU performance is observed clearly when we do scaling
    
    Change-Id: Iadc3836ff80efcd8cdc9ffcfe23ba1d05707b7e1
    Signed-off-by: senthil <senthil.b@tataelxsi.co.in>

--- applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/LLDPSpeaker.java
+++ applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/LLDPSpeaker.java
@@ -37,12 +37,13 @@ import org.slf4j.LoggerFactory;
 public class LLDPSpeaker implements AutoCloseable, NodeConnectorEventsObserver, Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(LLDPSpeaker.class);
     private static final long LLDP_FLOOD_PERIOD = 5;
+    private static long FLOOD_INTERVAL = LLDP_FLOOD_PERIOD;
 
     private final PacketProcessingService packetProcessingService;
     private final ScheduledExecutorService scheduledExecutorService;
     private final Map<InstanceIdentifier<NodeConnector>, TransmitPacketInput> nodeConnectorMap =
             new ConcurrentHashMap<>();
-    private final ScheduledFuture<?> scheduledSpeakerTask;
+    private ScheduledFuture<?> scheduledSpeakerTask;
     private final MacAddress addressDestionation;
     private volatile OperStatus operationalStatus = OperStatus.RUN;
 
@@ -60,6 +61,18 @@ public class LLDPSpeaker implements AutoCloseable, NodeConnectorEventsObserver, 
 
     public OperStatus getOperationalStatus() {
         return operationalStatus;
+    }
+
+    public void setLldpInterval(long time) {
+        this.FLOOD_INTERVAL = time;
+        scheduledSpeakerTask.cancel(false);
+        scheduledSpeakerTask = this.scheduledExecutorService
+                     .scheduleAtFixedRate(this, time, time, TimeUnit.SECONDS);
+        LOG.info("LLDPSpeaker restarted, it will send LLDP frames each {} seconds", time);
+   }
+
+    public long getLldpInterval() {
+        return FLOOD_INTERVAL;
     }
 
     public LLDPSpeaker(final PacketProcessingService packetProcessingService,
