d24260189065b6620af4f6829b91313c0a681c6d
epgoraj <p.govinda.rajulu@ericsson.com>
2016-10-27 16:52:18 +0530

    BUG:6997 supporting OXM_OF_MPLS_LABEL in nicira extensiona
            Description : currently under nicira-extension, MPLS support is not
            present in nicira Move action. Mainly in ECMP use-case we need to use
            mpls_label under nicira_move_action type. The mpls_label will be using as
            example ( move:NXM_NX_REG1[0..19]->OXM_OF_MPLS_LABEL[] )
    
    Change-Id: Ia497b963f30bf9ffcd09430db8ed85661a64859f
    Signed-off-by: epgoraj <p.govinda.rajulu@ericsson.com>
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

---
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/MplsLabelCodec.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2016 Ericsson, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowjava.nx.codec.match;
+
+import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.MatchEntrySerializerKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.MatchField;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm0Class;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.OxmClassBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmOfMplsLabel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.ofj.oxm.of.mpls.label.grouping.MplsLabelValuesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.OfMplsLabelCaseValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.OfMplsLabelCaseValueBuilder;
+
+import io.netty.buffer.ByteBuf;
+
+public class MplsLabelCodec extends AbstractMatchCodec {
+
+  private static final int VALUE_LENGTH = 4;
+  private static final int NXM_FIELD_CODE = 34;
+
+  public static final MatchEntrySerializerKey<Nxm0Class, NxmOfMplsLabel> SERIALIZER_KEY = new MatchEntrySerializerKey<>(
+      EncodeConstants.OF13_VERSION_ID, Nxm0Class.class,
+      NxmOfMplsLabel.class);
+
+  public static final MatchEntryDeserializerKey DESERIALIZER_KEY = new MatchEntryDeserializerKey(
+      EncodeConstants.OF13_VERSION_ID, OxmMatchConstants.NXM_0_CLASS,
+      NXM_FIELD_CODE);
+
+  @Override
+  public void serialize(MatchEntry input, ByteBuf outBuffer) {
+    serializeHeader(input, outBuffer);
+    OfMplsLabelCaseValue value = (OfMplsLabelCaseValue) input.getMatchEntryValue();
+    outBuffer.writeInt(value.getMplsLabelValues().getMplsLabel().intValue());
+  }
+
+  @Override
+  public MatchEntry deserialize(ByteBuf message) {
+    MatchEntryBuilder matchEntryBuilder = deserializeHeader(message);
+    OfMplsLabelCaseValueBuilder caseBuilder = new OfMplsLabelCaseValueBuilder();
+    MplsLabelValuesBuilder valuesBuilder = new MplsLabelValuesBuilder();
+    valuesBuilder.setMplsLabel(message.readLong()).build();
+    caseBuilder.setMplsLabelValues(valuesBuilder.build());
+    matchEntryBuilder.setMatchEntryValue(caseBuilder.build());
+    return matchEntryBuilder.build();
+  }
+
+  @Override
+  public int getNxmFieldCode() {
+    return NXM_FIELD_CODE;
+  }
+
+  @Override
+  public int getOxmClassCode() {
+    return OxmMatchConstants.NXM_0_CLASS;
+  }
+
+  @Override
+  public int getValueLength() {
+    return VALUE_LENGTH;
+  }
+
+  @Override
+  public Class<? extends MatchField> getNxmField() {
+    return NxmOfMplsLabel.class;
+  }
+
+  @Override
+  public Class<? extends OxmClassBase> getOxmClass() {
+    return Nxm0Class.class;
+  }
+}
