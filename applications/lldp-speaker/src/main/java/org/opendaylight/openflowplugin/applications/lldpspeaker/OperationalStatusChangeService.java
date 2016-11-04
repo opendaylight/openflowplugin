a8e8ba56cd86cf6d9c954fa07e734882116e07f4
senthil <senthil.b@tataelxsi.co.in>
2016-11-04 22:56:49 +0530

    Add new API's for LLDP
      *Right now LLDP time period is hard coded
      *This patch enables to set the time period dynamically,
       which increases the performance of the CPU
      *Increase in CPU performance is observed clearly when we do scaling
    
    Change-Id: I959e151c1e0f11799617bc3ae679cfb7539e764a
    Signed-off-by: senthil <senthil.b@tataelxsi.co.in>

--- applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/OperationalStatusChangeService.java
+++ applications/lldp-speaker/src/main/java/org/opendaylight/openflowplugin/applications/lldpspeaker/OperationalStatusChangeService.java
@@ -13,6 +13,9 @@ import java.util.concurrent.Future;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.ChangeOperationalStatusInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.GetOperationalStatusOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.GetOperationalStatusOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.SetLldpFloodIntervalInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.GetLldpFloodIntervalOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.GetLldpFloodIntervalOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.applications.lldp.speaker.rev141023.LldpSpeakerService;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
@@ -38,6 +41,22 @@ public class OperationalStatusChangeService implements LldpSpeakerService {
         GetOperationalStatusOutputBuilder getOperationalStatusOutputBuilder = new GetOperationalStatusOutputBuilder();
         getOperationalStatusOutputBuilder.setOperationalStatus(speakerInstance.getOperationalStatus());
         rpcResultBuilder.withResult(getOperationalStatusOutputBuilder.build());
+        return Futures.immediateFuture(rpcResultBuilder.build());
+    }
+
+    @Override
+    public Future<RpcResult<Void>> setLldpFloodInterval(final SetLldpFloodIntervalInput input) {
+        speakerInstance.setLldpFloodInterval(input.getInterval());
+        RpcResultBuilder<Void> rpcResultBuilder = RpcResultBuilder.success();
+        return Futures.immediateFuture(rpcResultBuilder.build());
+    }
+
+    @Override
+    public Future<RpcResult<GetLldpFloodIntervalOutput>> getLldpFloodInterval() {
+        RpcResultBuilder<GetLldpFloodIntervalOutput> rpcResultBuilder = RpcResultBuilder.success();
+        GetLldpFloodIntervalOutputBuilder getLldpFloodIntervalOutputBuilder = new GetLldpFloodIntervalOutputBuilder();
+        getLldpFloodIntervalOutputBuilder.setInterval(speakerInstance.getLldpFloodInterval());
+        rpcResultBuilder.withResult(getLldpFloodIntervalOutputBuilder.build());
         return Futures.immediateFuture(rpcResultBuilder.build());
     }
 }
