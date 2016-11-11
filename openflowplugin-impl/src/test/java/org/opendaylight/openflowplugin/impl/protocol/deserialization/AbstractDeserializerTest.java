098a271f22ad55455a13883788e5e74da3593955
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-11 12:51:51 +0100

    Add Serialization and Deserialization injectors
    Add SerializerInjector and DeserializerInjector that will
    allow us to register new serializers and deserializers in
    OpenflowPlugin easily.
    Add abstract classes for unit tests to speed up unit test
    development.
    
    See also: bug 7136
    
    Change-Id: I3135dd3d0ca49585f274f5c63f4ab6b35b525b6d
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/AbstractDeserializerTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization;
+
+import org.junit.Before;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerRegistry;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFGeneralDeserializer;
+import org.opendaylight.openflowjava.protocol.api.keys.ExperimenterActionDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.ExperimenterDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.ExperimenterIdDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.ExperimenterInstructionDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
+import org.opendaylight.openflowjava.protocol.impl.deserialization.DeserializerRegistryImpl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ErrorMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.experimenter.core.ExperimenterDataOfChoice;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.meter.band.header.meter.band.MeterBandExperimenterCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.queue.property.header.QueueProperty;
+
+public abstract class AbstractDeserializerTest {
+    private DeserializerExtensionProvider provider;
+    private DeserializerRegistry registry;
+
+    @Before
+    public void setUp() throws Exception {
+        registry = new DeserializerRegistryImpl();
+        registry.init();
+        provider = new DeserializerExtensionProviderImpl(registry);
+        DeserializerInjector.injectDeserializers(provider);
+        init();
+    }
+
+    protected DeserializerRegistry getRegistry() {
+        return registry;
+    }
+
+    protected DeserializerExtensionProvider getProvider() {
+        return provider;
+    }
+
+    protected abstract void init();
+
+    static class DeserializerExtensionProviderImpl implements DeserializerExtensionProvider {
+
+        private final DeserializerRegistry registry;
+
+        public DeserializerExtensionProviderImpl(final DeserializerRegistry registry) {
+            this.registry = registry;
+        }
+
+        @Override
+        public void registerDeserializer(MessageCodeKey messageCodeKey, OFGeneralDeserializer ofGeneralDeserializer) {
+            registry.registerDeserializer(messageCodeKey, ofGeneralDeserializer);
+        }
+
+        @Override
+        public boolean unregisterDeserializer(ExperimenterDeserializerKey key) {
+            return false;
+        }
+
+        @Override
+        public void registerActionDeserializer(ExperimenterActionDeserializerKey key, OFGeneralDeserializer deserializer) {
+
+        }
+
+        @Override
+        public void registerInstructionDeserializer(ExperimenterInstructionDeserializerKey key, OFGeneralDeserializer deserializer) {
+
+        }
+
+        @Override
+        public void registerMatchEntryDeserializer(MatchEntryDeserializerKey key, OFGeneralDeserializer deserializer) {
+
+        }
+
+        @Override
+        public void registerErrorDeserializer(ExperimenterIdDeserializerKey key, OFDeserializer<ErrorMessage> deserializer) {
+
+        }
+
+        @Override
+        public void registerExperimenterMessageDeserializer(ExperimenterIdDeserializerKey key, OFDeserializer<? extends ExperimenterDataOfChoice> deserializer) {
+
+        }
+
+        @Override
+        public void registerMultipartReplyMessageDeserializer(ExperimenterIdDeserializerKey key, OFDeserializer<? extends ExperimenterDataOfChoice> deserializer) {
+
+        }
+
+        @Override
+        public void registerMultipartReplyTFDeserializer(ExperimenterIdDeserializerKey key, OFGeneralDeserializer deserializer) {
+
+        }
+
+        @Override
+        public void registerMeterBandDeserializer(ExperimenterIdDeserializerKey key, OFDeserializer<MeterBandExperimenterCase> deserializer) {
+
+        }
+
+        @Override
+        public void registerQueuePropertyDeserializer(ExperimenterIdDeserializerKey key, OFDeserializer<QueueProperty> deserializer) {
+
+        }
+    }
+
+}
