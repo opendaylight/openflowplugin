19df41723c0886ea8aaaca64b08a08fac0f7aa00
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-11 12:51:51 +0100

    Add Serialization and Deserialization injectors
    Add SerializerInjector and DeserializerInjector that will
    allow us to register new serializers and deserializers in
    OpenflowPlugin easily.
    Add abstract classes for unit tests to speed up unit test
    development.
    
    See also: bug 7136
    
    Change-Id: I3135dd3d0ca49585f274f5c63f4ab6b35b525b6d
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/DeserializerInjector.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider;
+
+/**
+ * Util class for injecting new deserializers into OpenflowJava
+ */
+public class DeserializerInjector {
+
+    /**
+     * Injects deserializers into provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     */
+    public static void injectDeserializers(final DeserializerExtensionProvider provider) {
+        // Inject new deserializers here
+    }
+}
---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/SerializerInjector.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerExtensionProvider;
+
+/**
+ * Util class for injecting new serializers into OpenflowJava
+ */
+public class SerializerInjector {
+
+    /**
+     * Injects serializers into provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.SerializerExtensionProvider}
+     * @param provider OpenflowJava serializer extension provider
+     */
+    public static void injectSerializers(final SerializerExtensionProvider provider) {
+        // Inject new serializers here
+    }
+}
