60f8b7f40164f32a63672f77783a93cffdbbf885
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-21 13:59:54 +0100

    Add Action serializers
    Inject action serializers into OFJ. These serializers will serialize
    these actions to raw bytes:
    
      - CopyTtlIn
      - CopyTtlOut
      - DecMplsTtl
      - DecNwTtl
      - GroupAction
      - OutputAction
      - PopMplsAction
      - PopPbbAction
      - PopVlanAction
      - PushMplsAction
      - PushPbbAction
      - PushVlanAction
      - SetMplsTtlAction
      - SetNwTtlAction
      - SetQueueAction
    
    See also: bug 7138
    
    Change-Id: I5b42b0b2d2afd3468768e28bd11bfdcfcbbbc142
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/ActionSerializerInjector.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/ActionSerializerInjector.java
@@ -14,7 +14,37 @@ import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerExtens
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.AbstractActionSerializer;
 import org.opendaylight.openflowplugin.impl.protocol.serialization.keys.ActionSerializerKey;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.CopyTtlInActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.CopyTtlOutActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.DecMplsTtlActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.DecNwTtlActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.GroupActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.OutputActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PopMplsActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PopPbbActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PopVlanActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PushMplsActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PushPbbActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.PushVlanActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.SetMplsTtlActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.SetNwTtlActionSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.actions.SetQueueActionSerializer;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.CopyTtlInCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.CopyTtlOutCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.DecMplsTtlCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.DecNwTtlCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.GroupActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PopMplsActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PopPbbActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PopVlanActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushMplsActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushPbbActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushVlanActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetMplsTtlActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetNwTtlActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetQueueActionCase;
 
 /**
  * Util class for injecting new action serializers into OpenflowJava
@@ -29,6 +59,22 @@ class ActionSerializerInjector {
         // Inject new action serializers here using injector created by createInjector method
         final Function<Class<? extends Action>, Consumer<AbstractActionSerializer>> injector =
                 createInjector(provider, EncodeConstants.OF13_VERSION_ID);
+        
+        injector.apply(CopyTtlInCase.class).accept(new CopyTtlInActionSerializer());
+        injector.apply(CopyTtlOutCase.class).accept(new CopyTtlOutActionSerializer());
+        injector.apply(DecMplsTtlCase.class).accept(new DecMplsTtlActionSerializer());
+        injector.apply(DecNwTtlCase.class).accept(new DecNwTtlActionSerializer());
+        injector.apply(GroupActionCase.class).accept(new GroupActionSerializer());
+        injector.apply(OutputActionCase.class).accept(new OutputActionSerializer());
+        injector.apply(PopMplsActionCase.class).accept(new PopMplsActionSerializer());
+        injector.apply(PopPbbActionCase.class).accept(new PopPbbActionSerializer());
+        injector.apply(PopVlanActionCase.class).accept(new PopVlanActionSerializer());
+        injector.apply(PushMplsActionCase.class).accept(new PushMplsActionSerializer());
+        injector.apply(PushPbbActionCase.class).accept(new PushPbbActionSerializer());
+        injector.apply(PushVlanActionCase.class).accept(new PushVlanActionSerializer());
+        injector.apply(SetMplsTtlActionCase.class).accept(new SetMplsTtlActionSerializer());
+        injector.apply(SetNwTtlActionCase.class).accept(new SetNwTtlActionSerializer());
+        injector.apply(SetQueueActionCase.class).accept(new SetQueueActionSerializer());
     }
 
     /**
