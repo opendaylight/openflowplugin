438ab7dc364424f86744aae2916088aaced80583
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-21 13:59:54 +0100

    Add Action serializers
    Inject action serializers into OFJ. These serializers will serialize
    these actions to raw bytes:
    
      - CopyTtlIn
      - CopyTtlOut
      - DecMplsTtl
      - DecNwTtl
      - GroupAction
      - OutputAction
      - PopMplsAction
      - PopPbbAction
      - PopVlanAction
      - PushMplsAction
      - PushPbbAction
      - PushVlanAction
      - SetMplsTtlAction
      - SetNwTtlAction
      - SetQueueAction
      - SetFieldAction
    
    See also: bug 7138
    
    Change-Id: I5b42b0b2d2afd3468768e28bd11bfdcfcbbbc142
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/actions/OutputActionSerializer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization.actions;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
+import org.opendaylight.openflowplugin.api.openflow.md.util.OpenflowVersion;
+import org.opendaylight.openflowplugin.openflow.md.util.InventoryDataServiceUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.output.action._case.OutputAction;
+
+public class OutputActionSerializer extends AbstractActionSerializer {
+    @Override
+    public void serialize(Action action, ByteBuf outBuffer) {
+        super.serialize(action, outBuffer);
+        final OutputAction outputAction = OutputActionCase.class.cast(action).getOutputAction();
+        outBuffer.writeInt(InventoryDataServiceUtil.portNumberfromNodeConnectorId(
+                OpenflowVersion.OF13,
+                outputAction.getOutputNodeConnector().getValue()).intValue());
+        outBuffer.writeShort(outputAction.getMaxLength());
+        outBuffer.writeZero(ActionConstants.OUTPUT_PADDING);
+    }
+
+    @Override
+    protected int getLength() {
+        return ActionConstants.LARGER_ACTION_LENGTH;
+    }
+
+    @Override
+    protected int getType() {
+        return ActionConstants.OUTPUT_CODE;
+    }
+}
