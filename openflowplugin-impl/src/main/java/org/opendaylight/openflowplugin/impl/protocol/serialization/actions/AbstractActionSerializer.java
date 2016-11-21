211d66b84b395b1cb9f01378c2af6758bb35cbb5
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-21 13:59:54 +0100

    Add Action serializers
    Inject action serializers into OFJ. These serializers will serialize
    these actions to raw bytes:
    
      - CopyTtlIn
      - CopyTtlOut
      - DecMplsTtl
      - DecNwTtl
      - GroupAction
      - OutputAction
      - PopMplsAction
      - PopPbbAction
      - PopVlanAction
      - PushMplsAction
      - PushPbbAction
      - PushVlanAction
      - SetMplsTtlAction
      - SetNwTtlAction
      - SetQueueAction
    
    See also: bug 7138
    
    Change-Id: I5b42b0b2d2afd3468768e28bd11bfdcfcbbbc142
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/actions/AbstractActionSerializer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization.actions;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+
+public abstract class AbstractActionSerializer implements OFSerializer<Action> {
+    @Override
+    public void serialize(Action input, ByteBuf outBuffer) {
+        outBuffer.writeShort(getType());
+        outBuffer.writeShort(getLength());
+    }
+
+    /**
+     * @return numeric representation of action type
+     */
+    protected abstract int getType();
+
+    /**
+     * @return action length
+     */
+    protected abstract int getLength();
+}
