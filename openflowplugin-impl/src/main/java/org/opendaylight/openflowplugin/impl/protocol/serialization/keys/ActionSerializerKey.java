f7c5884bb0b7a8cd5df7b80a667410802f36ad24
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-22 11:35:34 +0100

    Add ActionSerializerInjector
    Add injector where all action serializers will be registered and
    call it from general SerializerInjector.
    Add AbstractActionSerializer and ActionSerializerKey to be able
    to actually create action serializers.
    Add unit tests for ActionSerializerInjector and ActionSerializerKey
    
    See also: bug 7138
    
    Change-Id: I25549b453a0a36d38f9b42de7b8087a41d438f7b
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/keys/ActionSerializerKey.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization.keys;
+
+
+import org.opendaylight.openflowjava.protocol.api.keys.ExperimenterSerializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.MessageTypeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+
+public class ActionSerializerKey<T extends Action> extends MessageTypeKey<Action>
+        implements ExperimenterSerializerKey {
+
+    private Class<T> actionType;
+    private Long experimenterId;
+
+    /**
+     * @param msgVersion protocol wire version
+     * @param actionType type of action
+     * @param experimenterId experimenter / vendor ID
+     */
+    public ActionSerializerKey(short msgVersion, Class<T> actionType,
+            Long experimenterId) {
+        super(msgVersion, Action.class);
+        this.actionType = actionType;
+        this.experimenterId = experimenterId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((actionType == null) ? 0 : actionType.hashCode());
+        result = prime * result + ((experimenterId == null) ? 0 : experimenterId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ActionSerializerKey<?> other = (ActionSerializerKey<?>) obj;
+        if (actionType == null) {
+            if (other.actionType != null) {
+                return false;
+            }
+        } else if (!actionType.equals(other.actionType)) {
+            return false;
+        }
+        if (experimenterId == null) {
+            if (other.experimenterId != null) {
+                return false;
+            }
+        } else if (!experimenterId.equals(other.experimenterId)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " action type: " + actionType.getName() + " experimenterID: " + experimenterId;
+    }
+}
