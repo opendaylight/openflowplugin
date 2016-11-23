5ef90986e35b835633808068610b14779ae1a0d1
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-23 14:25:47 +0100

    Add Match entry serializers
    Inject match entry serializers into MatchSerializer.
    These serializers will serialize these entries to raw bytes:
    
      - EthernetDestination
      - EthernetSource
      - EthernetType
      - InPhyPort
      - InPort
      - Metadata
      - VlanPcp
      - VlanVid
      - IpDscp
      - IpEcn
      - IpProto
      - TcpSource
      - TcpDestination
      - UdpSource
      - UdpDestination
      - SctpSource
      - SctpDestination
      - Icmpv4Type
      - Icmpv4Code
      - Icmpv6Type
      - Icmpv6Code
      - Ipv4ArbitraryBitMaskSource
      - Ipv4ArbitraryBitMaskDestination
      - Ipv4Source
      - Ipv4Destination
      - TunnelIpv4Source
      - TunnelIpv4Destination
      - ArpOp
      - ArpSourceTransportAddress
      - ArpTargetTransportAddress
      - ArpSourceHardwareAddress
      - ArpTargetHardwareAddress
      - Ipv6ArbitraryBitMaskSource
      - Ipv6ArbitraryBitMaskDestination
      - Ipv6Source
      - Ipv6Destination
      - Ipv6Label
      - Ipv6NdTarget
      - Ipv6NdSll
      - Ipv6NdTll
      - Ipv6ExtHeader
      - MplsLabel
      - MplsBos
      - MplsTc
      - Pbb
      - TunnelId
      - TcpFlags
    
    Change-Id: I886e990aebc4ce02e5c7b571d06078aaf8033e07
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/match/Ipv6ArbitraryBitMaskDestinationEntrySerializer.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization.match;
+
+import io.netty.buffer.ByteBuf;
+import java.util.Objects;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.IpConversionUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6MatchArbitraryBitMask;
+
+public class Ipv6ArbitraryBitMaskDestinationEntrySerializer extends AbstractMatchEntrySerializer {
+
+    @Override
+    public void serialize(Match match, ByteBuf outBuffer) {
+        super.serialize(match, outBuffer);
+        final Ipv6MatchArbitraryBitMask ipv6 = Ipv6MatchArbitraryBitMask.class.cast(match.getLayer3Match());
+        writeIpv6Address(ipv6.getIpv6DestinationAddressNoMask(), outBuffer);
+
+        if (getHasMask(match)) {
+            writeMask(IpConversionUtil.convertIpv6ArbitraryMaskToByteArray(ipv6.getIpv6DestinationArbitraryBitmask()),
+                    outBuffer,
+                    getValueLength());
+        }
+    }
+
+    @Override
+    boolean matchTypeCheck(Match match) {
+        return Objects.nonNull(match.getLayer3Match()) &&
+                Ipv6MatchArbitraryBitMask.class.isInstance(match.getLayer3Match()) &&
+                Objects.nonNull(Ipv6MatchArbitraryBitMask.class.cast(match.getLayer3Match()).getIpv6DestinationAddressNoMask());
+    }
+
+    @Override
+    protected boolean getHasMask(Match match) {
+        return Objects.nonNull(Ipv6MatchArbitraryBitMask.class.cast(match.getLayer3Match()).getIpv6DestinationArbitraryBitmask());
+    }
+
+    @Override
+    protected int getOxmFieldCode() {
+        return OxmMatchConstants.IPV6_DST;
+    }
+
+    @Override
+    protected int getOxmClassCode() {
+        return OxmMatchConstants.OPENFLOW_BASIC_CLASS;
+    }
+
+    @Override
+    protected int getValueLength() {
+        return EncodeConstants.SIZE_OF_IPV6_ADDRESS_IN_BYTES;
+    }
+
+}
