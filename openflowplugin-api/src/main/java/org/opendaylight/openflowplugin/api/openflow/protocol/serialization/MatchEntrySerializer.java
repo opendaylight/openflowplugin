6b86ae7b51a1dd570d2c5464f7a742c428c3eb2b
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-23 13:16:03 +0100

    Add MatchSerializerInjector
    Add injector where all match and match entry serializers will be
    registered and call it from general SerializerInjector.
    Add AbstractMatchEntrySerializer and MatchSerializer to be able to
    actually create match entry serializers and serialize match body.
    Add AbstractExperimenterMatchEntrySerializer to serialize match
    entries with experimenter id.
    Add match extension serialization to MatchSerializer, using match
    extension converters. This needs to be changed later, to also remove
    use of match extension converters.
    
    See also: bug 7138
    
    Change-Id: Ifa3f0d52b384b173cf639f7d00fd5a4a3e0d30f4
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/protocol/serialization/MatchEntrySerializer.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.protocol.serialization;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match;
+
+public interface MatchEntrySerializer extends OFSerializer<Match> {
+
+    /**
+     * Checks if current match is this match type
+     * @param match Openflow match
+     * @return true if matched
+     */
+    boolean matchTypeCheck(final Match match);
+
+}
