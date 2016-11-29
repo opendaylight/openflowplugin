f73a44cb1dbaa7967d6e33770a5f2de598287cd1
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-11-29 10:49:45 +0100

    Add Instruction serializers
    Inject instruction serializers into InstructionSerializerInjector.
    These serializers will serialize these intrusctions to raw bytes:
    
      - ApplyActions
      - ClearActions
      - GoToTable
      - Meter
      - WriteActions
      - WriteMetadata
    
    Change-Id: I72a254b897234b3e7f29c0ea2d1496f0bc517e43
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/InstructionSerializerInjector.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/InstructionSerializerInjector.java
@@ -14,8 +14,20 @@ import java.util.function.Function;
 import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
 import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerExtensionProvider;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.ApplyActionsSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.ClearActionsSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.GoToTableSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.MeterSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.WriteActionsSerializer;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.instructions.WriteMetadataSerializer;
 import org.opendaylight.openflowplugin.impl.protocol.serialization.keys.InstructionSerializerKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.Instruction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ClearActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.GoToTableCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.MeterCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.WriteActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.WriteMetadataCase;
 
 /**
  * Util class for injecting new instruction serializers into OpenflowJava
@@ -30,6 +42,12 @@ class InstructionSerializerInjector {
         // Inject new instruction serializers here using injector created by createInjector method
         final Function<Class<? extends Instruction>, Consumer<OFSerializer<? extends Instruction>>> injector =
                 createInjector(provider, EncodeConstants.OF13_VERSION_ID);
+        injector.apply(ApplyActionsCase.class).accept(new ApplyActionsSerializer());
+        injector.apply(ClearActionsCase.class).accept(new ClearActionsSerializer());
+        injector.apply(GoToTableCase.class).accept(new GoToTableSerializer());
+        injector.apply(MeterCase.class).accept(new MeterSerializer());
+        injector.apply(WriteActionsCase.class).accept(new WriteActionsSerializer());
+        injector.apply(WriteMetadataCase.class).accept(new WriteMetadataSerializer());
     }
 
     /**
