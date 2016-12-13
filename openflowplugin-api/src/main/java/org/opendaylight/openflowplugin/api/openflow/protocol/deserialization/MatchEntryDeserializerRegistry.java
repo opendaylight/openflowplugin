42754e36122f4716d556a39cf81c39a6b6fbb96c
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-12-13 11:00:48 +0100

    Add MatchDeserializerInjector
    Add injector where all match and match entry deserializers will be
    registered and call it from general DeserializerInjector.
    Add MatchEntryDeserializer and MatchDeserializer to be able to actually
    create match entry deserializers and deserialize match body.
    Add new method to MatchExtensionHelper that can inject extension
    augmentations one by one.
    
    See also: bug 7140
    
    Change-Id: Id15419539792cb4a21a5862be2dd423be34ab117
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/protocol/deserialization/MatchEntryDeserializerRegistry.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.protocol.deserialization;
+
+import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
+
+public interface MatchEntryDeserializerRegistry {
+
+    /**
+     * Registers match entry deserializer.
+     *
+     * @param key          used for deserializer lookup
+     * @param deserializer deserializer instance
+     */
+    void registerEntryDeserializer(MatchEntryDeserializerKey key, MatchEntryDeserializer deserializer);
+
+    /**
+     * Unregisters match entry deserializer
+     *
+     * @param key used for deserializer lookup
+     * @return true if deserializer was removed, false if no deserializer was found under specified key
+     */
+    boolean unregisterEntryDeserializer(MatchEntryDeserializerKey key);
+
+}
