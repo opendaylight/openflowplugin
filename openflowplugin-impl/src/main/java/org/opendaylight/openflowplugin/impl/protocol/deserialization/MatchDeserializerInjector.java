19a2c3fbb962a9c3d4d9787186dcab68d5254554
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-12-13 11:00:48 +0100

    Add MatchDeserializerInjector
    Add injector where all match and match entry deserializers will be
    registered and call it from general DeserializerInjector.
    Add MatchEntryDeserializer and MatchDeserializer to be able to actually
    create match entry deserializers and deserialize match body.
    Add new method to MatchExtensionHelper that can inject extension
    augmentations one by one.
    
    See also: bug 7140
    
    Change-Id: Id15419539792cb4a21a5862be2dd423be34ab117
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/MatchDeserializerInjector.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider;
+import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
+import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.impl.protocol.deserialization.key.MessageCodeMatchKey;
+import org.opendaylight.openflowplugin.impl.protocol.deserialization.match.MatchDeserializer;
+import org.opendaylight.openflowplugin.api.openflow.protocol.deserialization.MatchEntryDeserializer;
+import org.opendaylight.openflowplugin.api.openflow.protocol.deserialization.MatchEntryDeserializerRegistry;
+import org.opendaylight.openflowplugin.extension.api.path.MatchPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match;
+
+/**
+ * Util class for injecting new match entry deserializers into OpenflowJava
+ */
+public class MatchDeserializerInjector {
+
+    /**
+     * Injects deserializers into provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     */
+    public static void injectDeserializers(final DeserializerExtensionProvider provider) {
+        for (MatchPath path : MatchPath.values()) {
+            final MatchDeserializer deserializer = new MatchDeserializer(path);
+            provider.registerDeserializer(
+                    new MessageCodeMatchKey(
+                        EncodeConstants.OF13_VERSION_ID,
+                        EncodeConstants.EMPTY_LENGTH,
+                        Match.class,
+                        path),
+                    deserializer);
+
+            // Inject new match entry serializers here using injector created by createInjector method
+            final Function<Integer, Function<Long, Function<Integer, Consumer<MatchEntryDeserializer>>>> injector =
+                createInjector(deserializer, EncodeConstants.OF13_VERSION_ID);
+        }
+    }
+
+    /**
+     * Create injector that will inject new serializers into {@link org.opendaylight.openflowplugin.api.openflow.protocol.deserialization.MatchEntryDeserializerRegistry}
+     * @param registry Match entry deserializer registry
+     * @param version Openflow version
+     * @return injector
+     */
+    @VisibleForTesting
+    static Function<Integer, Function<Long, Function<Integer, Consumer<MatchEntryDeserializer>>>> createInjector(
+            final MatchEntryDeserializerRegistry registry,
+            final short version
+            ) {
+        return oxmClass -> expId -> oxmField -> deserializer -> {
+            final MatchEntryDeserializerKey key = new MatchEntryDeserializerKey(version, oxmClass, oxmField);
+            key.setExperimenterId(expId);
+            registry.registerEntryDeserializer(key, deserializer);
+        };
+    }
+
+}
