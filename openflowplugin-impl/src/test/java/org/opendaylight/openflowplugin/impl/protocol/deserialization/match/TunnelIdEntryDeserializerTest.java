fd6f84ded021635a4f8617ba6099cb5f087ef53d
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-12-14 09:23:19 +0100

    Add Match entry deserializers
    Inject match entry deserializers into MatchDeserializer.
    These deserializers will deserialize these match entries:
    
     - ArpOp
     - ArpSha
     - ArpSpa
     - ArpTha
     - ArpTpa
     - InPort
     - InPhyPort
     - EthSrc
     - EthDst
     - EthType
     - Metadata
     - VlanPcp
     - VlanVid
     - IpDscp
     - IpEcn
     - IpProto
     - TcpSrc
     - TcpDst
     - UdpSrc
     - UdpDst
     - SctpSrc
     - SctpDst
     - Icmpv4Code
     - Icmpv4Type
     - Icmpv6Code
     - Icmpv6Type
     - Ipv4Source
     - Ipv4Destination
     - Ipv6Source
     - Ipv6Destination
     - Ipv6ExtHeader
     - Ipv6Flabel
     - Ipv6NdSll
     - Ipv6NdTll
     - Ipv6NdTarget
     - MplsLabel
     - MplsBos
     - MplsTc
     - PbbIsid
     - TunnelId
     - TcpFlags
    
    Change-Id: Ic584d4610f2b2230542e449d3fae3f4affcc2756
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/match/TunnelIdEntryDeserializerTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.match;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.Tunnel;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+
+public class TunnelIdEntryDeserializerTest extends AbstractMatchEntryDeserializerTest {
+
+    @Test
+    public void deserializeEntry() throws Exception {
+        final ByteBuf in = UnpooledByteBufAllocator.DEFAULT.buffer();
+        final long tunnelId = 6;
+        final long tunnelIdMask = 5;
+
+        writeHeader(in, false);
+        in.writeLong(tunnelId);
+        
+        Tunnel match = deserialize(in).getTunnel();
+        assertEquals(tunnelId, match.getTunnelId().longValue());
+        assertEquals(0, in.readableBytes());
+
+        writeHeader(in, true);
+        in.writeLong(tunnelId);
+        in.writeLong(tunnelIdMask);
+        
+        match = deserialize(in).getTunnel();
+        assertEquals(tunnelId, match.getTunnelId().longValue());
+        assertEquals(tunnelIdMask, match.getTunnelMask().longValue());
+        assertEquals(0, in.readableBytes());
+    }        
+
+    @Override
+    protected int getOxmClassCode() {
+        return OxmMatchConstants.OPENFLOW_BASIC_CLASS;
+    }
+
+    @Override
+    protected int getOxmFieldCode() {
+        return OxmMatchConstants.TUNNEL_ID;
+    }
+
+    @Override
+    protected int getValueLength() {
+        return EncodeConstants.SIZE_OF_LONG_IN_BYTES;
+    }
+
+}
