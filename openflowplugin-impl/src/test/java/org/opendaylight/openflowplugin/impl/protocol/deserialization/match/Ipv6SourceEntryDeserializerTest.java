6bbda3332f56b24141d9ac11db7a789afad35ed0
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-12-14 09:23:19 +0100

    Add Match entry deserializers
    Inject match entry deserializers into MatchDeserializer.
    These deserializers will deserialize these match entries:
    
     - ArpOp
     - ArpSha
     - ArpSpa
     - ArpTha
     - ArpTpa
     - InPort
     - InPhyPort
     - EthSrc
     - EthDst
     - EthType
     - Metadata
     - VlanPcp
     - VlanVid
     - IpDscp
     - IpEcn
     - IpProto
     - TcpSrc
     - TcpDst
     - UdpSrc
     - UdpDst
     - SctpSrc
     - SctpDst
     - Icmpv4Code
     - Icmpv4Type
     - Icmpv6Code
     - Icmpv6Type
     - Ipv4Source
     - Ipv4Destination
     - Ipv6Source
     - Ipv6Destination
     - Ipv6ExtHeader
     - Ipv6Flabel
     - Ipv6NdSll
     - Ipv6NdTll
     - Ipv6NdTarget
     - MplsLabel
     - MplsBos
     - MplsTc
     - PbbIsid
     - TunnelId
     - TcpFlags
    
    Change-Id: Ic584d4610f2b2230542e449d3fae3f4affcc2756
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/match/Ipv6SourceEntryDeserializerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.match;
+
+import static org.junit.Assert.assertEquals;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.IpConversionUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6Match;
+
+public class Ipv6SourceEntryDeserializerTest extends AbstractMatchEntryDeserializerTest {
+
+    @Test
+    public void deserializeEntry() throws Exception {
+        final ByteBuf in = UnpooledByteBufAllocator.DEFAULT.buffer();
+        final Ipv6Prefix address = new Ipv6Prefix("fe80::200:f8ff:fe21:67cf/30");
+
+        writeHeader(in, true);
+        in.writeBytes(IetfInetUtil.INSTANCE.ipv6AddressBytes(IpConversionUtil.extractIpv6Address(address)));
+        in.writeBytes(IpConversionUtil.convertIpv6PrefixToByteArray(IpConversionUtil.extractIpv6Prefix(address)));
+
+        final Ipv6Match match = Ipv6Match.class.cast(deserialize(in).getLayer3Match());
+
+        assertEquals(address.getValue(), match.getIpv6Source().getValue());
+        assertEquals(0, in.readableBytes());
+    }
+
+    @Override
+    protected int getOxmClassCode() {
+        return OxmMatchConstants.OPENFLOW_BASIC_CLASS;
+    }
+
+    @Override
+    protected int getOxmFieldCode() {
+        return OxmMatchConstants.IPV6_SRC;
+    }
+
+    @Override
+    protected int getValueLength() {
+        return EncodeConstants.SIZE_OF_IPV6_ADDRESS_IN_BYTES;
+    }
+
+}
