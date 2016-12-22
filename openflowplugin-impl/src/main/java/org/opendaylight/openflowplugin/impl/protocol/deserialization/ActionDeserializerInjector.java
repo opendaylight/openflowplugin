19d38a1ef8badf2267aed04a144de4994b9fcdc9
Tomas Slusny <tomas.slusny@pantheon.tech>
2016-12-22 13:35:26 +0100

    Add ActionDeserializerInjector
    Add injector where all action deserializers will be registered and call it
    from general DeserializerInjector.
    Add AbstractActionDeserializer that will define structure for all action
    deserializers.
    Add ActionUtil#readAction that will try to deserialize action or use
    extensions.
    Add MessageCodeExperimenterKey for registering action deserializers with
    experimenters.
    
    See also: bug 7140
    
    Change-Id: I1a1eaec5c9aa128f2c2d5e2a31b37086f2a56734
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/ActionDeserializerInjector.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.api.openflow.protocol.deserialization.MessageCodeExperimenterKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class ActionDeserializerInjector {
+
+    /**
+     * Injects action deserializers into provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     */
+    static void injectDeserializers(final DeserializerExtensionProvider provider) {
+        // Inject new message deserializers here using injector created by createInjector method
+        final Function<Byte, Consumer<OFDeserializer<Action>>> injector =
+                createInjector(provider, EncodeConstants.OF13_VERSION_ID);
+    }
+
+    /**
+     * Create injector that will inject new deserializers into #{@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     * @param version Openflow version
+     * @return injector
+     */
+    @VisibleForTesting
+    static Function<Byte, Consumer<OFDeserializer<Action>>> createInjector(
+            final DeserializerExtensionProvider provider,
+            final short version) {
+        return code -> deserializer -> {
+            provider.registerDeserializer(
+                    new MessageCodeExperimenterKey(version, code, Action.class, null),
+                    deserializer);
+        };
+    }
+
+}
