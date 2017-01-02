4cae27c5b22a1a4ec1d8ab7e6a5387fed227fd26
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-01-02 10:01:30 +0100

    Add Action deserializers
    Inject action deserializers into DeserializerRegistry.
    These deserializers will deserialize these actions:
    
     - CopyTtlIn
     - CopyTtlOut
     - DecMplsTtl
     - DecNwTtl
     - Group
     - Output
     - PopMpls
     - PopPbb
     - PopVlan
     - PushMpls
     - PushPbb
     - PushVlan
     - SetField
     - SetMplsTtl
     - SetNwTtl
     - SetQueue
    
    See also: bug 7140
    
    Change-Id: I0f36092ca99de0ca51719c3c2099ebe1d904a72f
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/action/PopMplsActionDeserializerTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.action;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PopMplsActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.pop.mpls.action._case.PopMplsAction;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+
+public class PopMplsActionDeserializerTest extends AbstractActionDeserializerTest {
+
+    @Test
+    public void testDeserialize() throws Exception {
+        final ByteBuf in = UnpooledByteBufAllocator.DEFAULT.buffer();
+        final short ethType = 10;
+        writeHeader(in);
+        in.writeShort(ethType);
+        in.writeZero(ActionConstants.ETHERTYPE_ACTION_PADDING);
+
+        final Action action = deserializeAction(in);
+        assertTrue(PopMplsActionCase.class.isInstance(action));
+
+        final PopMplsAction popAction = PopMplsActionCase.class.cast(action).getPopMplsAction();
+        assertEquals(ethType, popAction.getEthernetType().shortValue());
+        assertEquals(0, in.readableBytes());
+    }
+
+    @Override
+    protected short getType() {
+        return ActionConstants.POP_MPLS_CODE;
+    }
+
+    @Override
+    protected short getLength() {
+        return ActionConstants.GENERAL_ACTION_LENGTH;
+    }
+
+}
---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/action/PushVlanActionDeserializerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.action;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushVlanActionCase;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+
+public class PushVlanActionDeserializerTest extends AbstractActionDeserializerTest {
+
+    @Test
+    public void testDeserialize() throws Exception {
+        final ByteBuf in = UnpooledByteBufAllocator.DEFAULT.buffer();
+        final short ethType = 10;
+        writeHeader(in);
+        in.writeShort(ethType);
+        in.writeZero(ActionConstants.ETHERTYPE_ACTION_PADDING);
+
+        final Action action = deserializeAction(in);
+        assertTrue(PushVlanActionCase.class.isInstance(action));
+        assertEquals(ethType, PushVlanActionCase.class.cast(action).getPushVlanAction().getEthernetType().shortValue());
+        assertEquals(0, in.readableBytes());
+    }
+
+    @Override
+    protected short getType() {
+        return ActionConstants.PUSH_VLAN_CODE;
+    }
+
+    @Override
+    protected short getLength() {
+        return ActionConstants.GENERAL_ACTION_LENGTH;
+    }
+
+}
