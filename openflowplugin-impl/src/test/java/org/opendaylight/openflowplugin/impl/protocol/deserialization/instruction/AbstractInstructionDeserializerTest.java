5c0d47f5bad56468c00830b8f499a5268e012bf1
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-01-05 10:02:30 +0100

    Add InstructionDeserializerInjector
    Add injector where all instruction deserializers will be
    registered and call it from general DeserializerInjector.
    Create AbstractInstructionDeserializer and
    AbstractActionInstructionDeserializer that will define
    structure for all instruction deserializers.
    
    See also: bug 7140
    
    Change-Id: Icf76321ed0e51c004d003be585f40cfe4710b1b3
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/instruction/AbstractInstructionDeserializerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.instruction;
+
+import io.netty.buffer.ByteBuf;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.api.openflow.protocol.deserialization.MessageCodeExperimenterKey;
+import org.opendaylight.openflowplugin.impl.protocol.deserialization.AbstractDeserializerTest;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.Instruction;
+
+public abstract class AbstractInstructionDeserializerTest extends AbstractDeserializerTest {
+
+    private OFDeserializer<Instruction> deserializer;
+
+    @Override
+    protected void init() {
+        deserializer = getRegistry().getDeserializer(
+                new MessageCodeExperimenterKey(EncodeConstants.OF13_VERSION_ID, getType(), Instruction.class, null));
+    }
+
+    protected void writeHeader(ByteBuf message) {
+        message.writeShort(getType());
+        message.writeShort(getLength());
+    }
+
+    protected Instruction deserializeInstruction(ByteBuf message) {
+        return deserializer.deserialize(message);
+    }
+
+    protected abstract short getType();
+    protected abstract short getLength();
+
+}
