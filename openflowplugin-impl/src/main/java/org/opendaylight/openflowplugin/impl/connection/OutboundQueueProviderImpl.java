dbb777ffccc10bdb2a71723a1ec72724cd2ea541
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-01-09 15:43:53 +0100

    Add disconect login after time to live is up.
    Change-Id: Icf90cd0154b2f1afbec8c5956f30595d4a948d29
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/connection/OutboundQueueProviderImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/connection/OutboundQueueProviderImpl.java
@@ -49,9 +49,6 @@ public class OutboundQueueProviderImpl implements OutboundQueueProvider {
 
     @Override
     public Long reserveEntry() {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Reserve entry with queue: {} in this {} implementation", outboundQueue, this);
-        }
         for (;;) {
             OutboundQueue queue = outboundQueue;
             if (queue == null) {
