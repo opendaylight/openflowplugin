0848759803762bfd9bc91cbf74a60cb46a495d47
miroslav.macko <miroslav.macko@pantheon.tech>
2017-01-20 08:51:38 +0100

    Add AbstractMessageService
    See also: bug 7139
    
    Change-Id: I0bcfe8c0a5ba8ad572427240b29d7522ce0c5cf5
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/AbstractMessageService.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.services;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+abstract class AbstractMessageService<R extends DataObject, I extends Builder<? extends R>, O extends DataObject>
+        extends AbstractSimpleService<I, O>{
+
+    protected AbstractMessageService(RequestContextStack requestContextStack, DeviceContext deviceContext, Class<O> clazz) {
+        super(requestContextStack, deviceContext, clazz);
+    }
+
+    /**
+     * Check if this service is supported in current OpenFlowPlugin configuration
+     * @return true if supported
+     */
+    public abstract boolean isSupported();
+
+    /**
+     * Converts input to {@link I}
+     * and calls #{@link org.opendaylight.openflowplugin.impl.services.AbstractService#handleServiceCall(Object)} on it.
+     * @param input rpc input
+     * @return future containing result of service call
+     */
+    public abstract ListenableFuture<RpcResult<O>> processInput(R input);
+}
