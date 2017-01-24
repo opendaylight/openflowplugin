79066cda70c6f8fedf5dd315e236c803758674f6
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-01-24 15:09:45 +0100

    Update bundle extension tests
    Reference: ONF approved extension #230
    Resolves: Bug 6806
    
    Change-Id: If4b197b8ecf334286d4bb01748b8b622c2112182
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/serializer/BundleControlFactoryTest.java
+++ extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/serializer/BundleControlFactoryTest.java
@@ -10,22 +10,22 @@ package org.opendaylight.openflowplugin.extension.onf.serializer;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.UnpooledByteBufAllocator;
+import java.util.ArrayList;
+import java.util.Collections;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
-import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerRegistry;
 import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerRegistryInjector;
 import org.opendaylight.openflowjava.protocol.api.keys.MessageTypeKey;
+import org.opendaylight.openflowplugin.extension.onf.BundleTestUtils;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundleControlType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundleFlags;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundleId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundlePropertyType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.bundle.property.experimenter.BundlePropertyExperimenterData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlBuilder;
 
@@ -33,20 +33,32 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.on
  * Test for {@link org.opendaylight.openflowplugin.extension.onf.serializer.BundleControlFactory}.
  */
 @RunWith(MockitoJUnitRunner.class)
-public class BundleControlFactoryTest {
+public class BundleControlFactoryTest extends AbstractBundleMessageFactoryTest {
 
     private final OFSerializer<BundleControl> factory = new BundleControlFactory();
-    @Mock
-    SerializerRegistry registry;
-    @Mock
-    OFSerializer<BundlePropertyExperimenterData> serializer;
 
     @Test
     public void testSerializeWithoutProperties() {
-        BundleControlBuilder builder = new BundleControlBuilder();
+        testSerialize(false);
+    }
+
+    @Test
+    public void testSerializeWithExperimenterProperty() {
+        testSerialize(true);
+    }
+
+    private void testSerialize(final boolean withProperty) {
+        final BundleControlBuilder builder = new BundleControlBuilder();
         builder.setBundleId(new BundleId(1L));
         builder.setType(BundleControlType.ONFBCTOPENREQUEST);
         builder.setFlags(new BundleFlags(true, true));
+
+        if (withProperty) {
+            builder.setBundleProperty((new ArrayList<>(Collections.singleton(
+                    BundleTestUtils.createExperimenterProperty(propertyExperimenterData)))));
+            Mockito.when(registry.getSerializer(Matchers.any(MessageTypeKey.class))).thenReturn(propertySerializer);
+            ((SerializerRegistryInjector) factory).injectSerializerRegistry(registry);
+        }
 
         ByteBuf out = UnpooledByteBufAllocator.DEFAULT.buffer();
         factory.serialize(builder.build(), out);
@@ -54,32 +66,16 @@ public class BundleControlFactoryTest {
         Assert.assertEquals("Wrong bundle ID", 1L, out.readUnsignedInt());
         Assert.assertEquals("Wrong type", BundleControlType.ONFBCTOPENREQUEST.getIntValue(), out.readUnsignedShort());
         Assert.assertEquals("Wrong flags", 3, out.readUnsignedShort());
-        Assert.assertTrue("Unexpected data", out.readableBytes() == 0);
-    }
+        if (withProperty) {
+            Assert.assertEquals("Wrong property type", BundlePropertyType.ONFETBPTEXPERIMENTER.getIntValue(), out.readUnsignedShort());
+            int length = out.readUnsignedShort();
+            Assert.assertEquals("Wrong experimenter ID", 1, out.readUnsignedInt());
+            Assert.assertEquals("Wrong experimenter type", 2, out.readUnsignedInt());
+            Mockito.verify(propertySerializer, Mockito.times(1)).serialize(propertyExperimenterData, out);
 
-    @Test
-    public void testSerializeWithExperimenterProperty() {
-        BundleControlBuilder builder = new BundleControlBuilder();
-        builder.setBundleId(new BundleId(3L));
-        builder.setType(BundleControlType.ONFBCTCOMMITREQUEST);
-        builder.setFlags(new BundleFlags(false, true));
-
-        BundlePropertyExperimenterData data = AbstractBundleMessageFactoryTest.createBundleExperimenterPropertyData();
-        builder.setBundleProperty(AbstractBundleMessageFactoryTest.createListWithBundleExperimenterProperty(data));
-
-        ByteBuf out = UnpooledByteBufAllocator.DEFAULT.buffer();
-        Mockito.when(registry.getSerializer(Matchers.any(MessageTypeKey.class))).thenReturn(serializer);
-        ((SerializerRegistryInjector) factory).injectSerializerRegistry(registry);
-        factory.serialize(builder.build(), out);
-
-        Assert.assertEquals("Wrong bundle ID", 3L, out.readUnsignedInt());
-        Assert.assertEquals("Wrong type", BundleControlType.ONFBCTCOMMITREQUEST.getIntValue(), out.readUnsignedShort());
-        Assert.assertEquals("Wrong flags", 2, out.readUnsignedShort());
-        Assert.assertEquals("Wrong property type", BundlePropertyType.ONFETBPTEXPERIMENTER.getIntValue(), out.readUnsignedShort());
-        int length = out.readUnsignedShort();
-        Assert.assertEquals("Wrong experimenter ID", 1, out.readUnsignedInt());
-        Assert.assertEquals("Wrong experimenter type", 2, out.readUnsignedInt());
-        Mockito.verify(serializer, Mockito.times(1)).serialize(data, out);
+        } else {
+            Assert.assertTrue("Unexpected data", out.readableBytes() == 0);
+        }
     }
 
 }
