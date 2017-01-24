83e086322123971ce323317ff327ee15c809a2df
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-01-24 15:32:41 +0100

    Add BundleControl converter from OFJ
    Reference: ONF approved extension #230
    Resolves: Bug 6806
    
    Change-Id: I57709f7d089e651525ddc65a4a51a68f39f38772
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/converter/BundleControlConverterTest.java
+++ extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/converter/BundleControlConverterTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.opendaylight.openflowplugin.extension.api.path.MessagePath;
 import org.opendaylight.openflowplugin.extension.onf.BundleTestUtils;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.ExperimenterId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundleControlType;
@@ -22,6 +23,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.on
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.BundlePropertyExperimenter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.bundle.property.experimenter.BundlePropertyExperimenterData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlBuilder;
 
 /**
  * Test for {@link org.opendaylight.openflowplugin.extension.onf.converter.BundleControlConverter}.
@@ -41,45 +43,76 @@ public class BundleControlConverterTest {
     }
 
     @Test
-    public void testConvertWithProperty() {
-        testConvert(true);
+    public void testConvertDownWithProperty() {
+        testConvertDown(true);
     }
 
     @Test
-    public void testConvertWithoutProperty() {
-        testConvert(false);
+    public void testConvertDownWithoutProperty() {
+        testConvertDown(false);
     }
 
-    private void testConvert(final boolean withProperty) {
+    @Test
+    public void testConvertUpWithProperty() {
+        testConvertUp(true);
+    }
+
+    @Test
+    public void testConvertUpWithoutProperty() {
+        testConvertUp(true);
+    }
+
+    private void testConvertDown(final boolean withProperty) {
         final org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl original
-                = createMessage(withProperty);
+                = createOFPMessage(withProperty);
         final BundleControl converted = converter.convert(original);
         testConvert(original, converted, withProperty);
     }
 
-    private static void testConvert(final org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl original,
-                                    final BundleControl converted, final boolean withProperty) {
-        Assert.assertEquals("Wrong BundleId", new BundleId(original.getBundleId().getValue()), converted.getBundleId());
-        Assert.assertEquals("Wrong type", BundleControlType.forValue(original.getType().getIntValue()), converted.getType());
-        Assert.assertEquals("Wrong flags", new BundleFlags(original.getFlags().isAtomic(), original.getFlags().isOrdered()), converted.getFlags());
+    private void testConvertUp(final boolean withProperty) {
+        final BundleControl original = createOFJMessage(withProperty);
+        final org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl converted
+                = converter.convert(original, MessagePath.MESSAGE_NOTIFICATION);
+        testConvert(converted, original, withProperty);
+    }
+
+    private static void testConvert(final org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl ofpMessage,
+                                 final BundleControl ofjMessage,
+                                 final boolean withProperty) {
+        Assert.assertEquals("Wrong BundleId", new BundleId(ofpMessage.getBundleId().getValue()), ofjMessage.getBundleId());
+        Assert.assertEquals("Wrong type", BundleControlType.forValue(ofpMessage.getType().getIntValue()), ofjMessage.getType());
+        Assert.assertEquals("Wrong flags", new BundleFlags(ofpMessage.getFlags().isAtomic(), ofpMessage.getFlags().isOrdered()), ofjMessage.getFlags());
         if (withProperty) {
-            final BundlePropertyExperimenter originalProperty = (BundlePropertyExperimenter) original.getBundleProperty().get(0).getBundlePropertyEntry();
-            final BundlePropertyExperimenter convertedProperty = ((BundlePropertyExperimenter) converted.getBundleProperty().get(0).getBundlePropertyEntry());
+            final BundlePropertyExperimenter originalProperty = (BundlePropertyExperimenter) ofpMessage.getBundleProperty().get(0).getBundlePropertyEntry();
+            final BundlePropertyExperimenter convertedProperty = ((BundlePropertyExperimenter) ofjMessage.getBundleProperty().get(0).getBundlePropertyEntry());
             Assert.assertEquals("Wrong property ExperimenterId", new ExperimenterId(originalProperty.getExperimenter()), convertedProperty.getExperimenter());
             Assert.assertEquals("Wrong property experimenter type", originalProperty.getExpType(), convertedProperty.getExpType());
             Assert.assertEquals("Wrong property data", originalProperty.getBundlePropertyExperimenterData(), convertedProperty.getBundlePropertyExperimenterData());
         } else {
-            Assert.assertTrue("Properties not empty", converted.getBundleProperty().isEmpty());
+            Assert.assertTrue("Properties not empty", ofjMessage.getBundleProperty().isEmpty());
         }
     }
 
     private static org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl
-                    createMessage(final boolean withProperty) {
+                    createOFPMessage(final boolean withProperty) {
         final org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControlBuilder builder
                 = new org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControlBuilder();
         builder.setBundleId(new BundleId(1L));
         builder.setType(BundleControlType.ONFBCTOPENREQUEST);
         builder.setFlags(new BundleFlags(true, false));
+        List<BundleProperty> properties = new ArrayList<>();
+        if (withProperty) {
+            properties.add(BundleTestUtils.createExperimenterProperty(Mockito.mock(BundlePropertyExperimenterData.class)));
+        }
+        builder.setBundleProperty(properties);
+        return builder.build();
+    }
+
+    private static BundleControl createOFJMessage(final boolean withProperty) {
+        final BundleControlBuilder builder = new BundleControlBuilder();
+        builder.setBundleId(new BundleId(1L));
+        builder.setType(BundleControlType.ONFBCTOPENREPLY);
+        builder.setFlags(new BundleFlags(false, false));
         List<BundleProperty> properties = new ArrayList<>();
         if (withProperty) {
             properties.add(BundleTestUtils.createExperimenterProperty(Mockito.mock(BundlePropertyExperimenterData.class)));
