c0814ce5ff87781e5ef16a0dd8a1796f1728dadd
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-01-24 13:47:09 +0100

    Add BundleAddMessageConvertor
    Reference: ONF approved extension #230
    Resolves: Bug 6806
    
    Change-Id: If6bc0a657c04e95454a3576a5707ddd00a16b38e
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

---
+++ extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/ConverterTestUtils.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.extension.onf;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.ExperimenterId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundlePropertyType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.common.grouping.BundleProperty;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.common.grouping.BundlePropertyBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.BundlePropertyExperimenterBuilder;
+
+/**
+ * Abstract class for common Converter util methods.
+ */
+public abstract class ConverterTestUtils {
+
+    public static BundleProperty createExperimenterProperty() {
+        final BundlePropertyBuilder propertyBuilder = new BundlePropertyBuilder();
+        propertyBuilder.setType(BundlePropertyType.ONFETBPTEXPERIMENTER);
+        propertyBuilder.setBundlePropertyEntry(new BundlePropertyExperimenterBuilder()
+                .setExperimenter(new ExperimenterId(1L))
+                .setExpType(1L)
+                .setBundlePropertyExperimenterData(null)
+                .build());
+        return propertyBuilder.build();
+    }
+
+}
