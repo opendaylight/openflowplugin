2b9974eda8b8d0c35612e96d190b96ad098b2c17
miroslav.macko <miroslav.macko@pantheon.tech>
2017-01-24 16:00:29 +0100

    Add multipart reply deserializers unit tests
    Add unit tests
    
    See also: bug 7140
    
    Change-Id: Ice9189919efca286c06ea1cced0265dd7e43429d
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/multipart/MultipartReplyFlowStatsDeserializerTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.deserialization.multipart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MultipartReplyFlowStatsDeserializerTest extends AbstractMultipartDeserializerTest{
+
+    @Test
+    public void testDeserialize() throws Exception {
+
+    }
+
+    @Override
+    protected int getType() {
+        return MultipartType.OFPMPFLOW.getIntValue();
+    }
+}
