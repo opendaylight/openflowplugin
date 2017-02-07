b1b49f0c0d62175f5e126d205dbd1ce1154c6371
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-02-07 09:21:20 +0100

    Change yang models due to errors
    - Added container for cases where was duplicate declaration of fields.
    - Renamed cases to not to have duplicate name of interfaces.
    - Renamed wrong spelled "Convertor" to "Converter".
    - Fixed tests. (Ignored, fixed in following patches)
    
    Change-Id: Ie268f9d8a697097e3892196e26442c917026b993
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/deserializer/BundleControlFactoryTest.java
+++ extension/openflowplugin-extension-onf/src/test/java/org/opendaylight/openflowplugin/extension/onf/deserializer/BundleControlFactoryTest.java
@@ -27,7 +27,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.on
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.common.grouping.BundleProperty;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.BundlePropertyExperimenter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.bundle.property.grouping.bundle.property.entry.bundle.property.experimenter.BundlePropertyExperimenterData;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlOnf;
 
 /**
  * Tests for {@link org.opendaylight.openflowplugin.extension.onf.deserializer.BundleControlFactory}.
@@ -35,7 +35,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.on
 @RunWith(MockitoJUnitRunner.class)
 public class BundleControlFactoryTest {
 
-    private final OFDeserializer<BundleControl> factory = new BundleControlFactory();
+    private final OFDeserializer<BundleControlOnf> factory = new BundleControlFactory();
     @Mock
     DeserializerRegistry registry;
     @Mock
@@ -46,13 +46,17 @@ public class BundleControlFactoryTest {
         ByteBuf buffer = ByteBufUtils.hexStringToByteBuf("00 00 00 01 " // bundle ID
                                                        + "00 01 " // type
                                                        + "00 03"); // flags
-        BundleControl builtByFactory = factory.deserialize(buffer);
-        Assert.assertEquals(1, builtByFactory.getBundleId().getValue().intValue());
+        BundleControlOnf builtByFactory = factory.deserialize(buffer);
+        Assert.assertEquals(1, builtByFactory.getOnfControlGroupingData().getBundleId().getValue().intValue());
         BundleFlags flags = new BundleFlags(true, true);
-        Assert.assertEquals("Wrong atomic flag", flags.isAtomic(), builtByFactory.getFlags().isAtomic());
-        Assert.assertEquals("Wrong ordered flag", flags.isOrdered(), builtByFactory.getFlags().isOrdered());
-        Assert.assertEquals("Wrong type", BundleControlType.ONFBCTOPENREPLY, builtByFactory.getType());
-        Assert.assertTrue("Properties not empty", builtByFactory.getBundleProperty().isEmpty());
+        Assert.assertEquals("Wrong atomic flag",
+                flags.isAtomic(), builtByFactory.getOnfControlGroupingData().getFlags().isAtomic());
+        Assert.assertEquals("Wrong ordered flag",
+                flags.isOrdered(), builtByFactory.getOnfControlGroupingData().getFlags().isOrdered());
+        Assert.assertEquals("Wrong type",
+                BundleControlType.ONFBCTOPENREPLY, builtByFactory.getOnfControlGroupingData().getType());
+        Assert.assertTrue("Properties not empty",
+                builtByFactory.getOnfControlGroupingData().getBundleProperty().isEmpty());
     }
 
     @Test
@@ -68,19 +72,28 @@ public class BundleControlFactoryTest {
                                                        + "00 00 " // type 2
                                                        + "00 04 " // length 2
                                                        + "00 00 00 00"); // data 2
-        Mockito.when(registry.getDeserializer(Matchers.any(MessageCodeKey.class))).thenReturn(experimenterPropertyDeserializer);
+        Mockito.when(registry.getDeserializer(Matchers.any(MessageCodeKey.class)))
+                .thenReturn(experimenterPropertyDeserializer);
         ((DeserializerRegistryInjector)factory).injectDeserializerRegistry(registry);
-        BundleControl builtByFactory = factory.deserialize(buffer);
-        Assert.assertEquals(1, builtByFactory.getBundleId().getValue().intValue());
+        BundleControlOnf builtByFactory = factory.deserialize(buffer);
+        Assert.assertEquals(1, builtByFactory.getOnfControlGroupingData().getBundleId().getValue().intValue());
         BundleFlags flags = new BundleFlags(false, true);
-        Assert.assertEquals("Wrong atomic flag", flags.isAtomic(), builtByFactory.getFlags().isAtomic());
-        Assert.assertEquals("Wrong ordered flag", flags.isOrdered(), builtByFactory.getFlags().isOrdered());
-        Assert.assertEquals("Wrong type", BundleControlType.ONFBCTCOMMITREPLY, builtByFactory.getType());
-        BundleProperty property = builtByFactory.getBundleProperty().get(0);
-        Assert.assertEquals("Wrong bundle property type", BundlePropertyType.ONFETBPTEXPERIMENTER, property.getType());
-        BundlePropertyExperimenter experimenterProperty = (BundlePropertyExperimenter) property.getBundlePropertyEntry();
-        Assert.assertEquals("Wrong experimenter ID", 1, experimenterProperty.getExperimenter().getValue().intValue());
-        Assert.assertEquals("Wrong experimenter type", 2, experimenterProperty.getExpType().longValue());
+        Assert.assertEquals("Wrong atomic flag",
+                flags.isAtomic(), builtByFactory.getOnfControlGroupingData().getFlags().isAtomic());
+        Assert.assertEquals("Wrong ordered flag",
+                flags.isOrdered(),
+                builtByFactory.getOnfControlGroupingData().getFlags().isOrdered());
+        Assert.assertEquals("Wrong type",
+                BundleControlType.ONFBCTCOMMITREPLY, builtByFactory.getOnfControlGroupingData().getType());
+        BundleProperty property = builtByFactory.getOnfControlGroupingData().getBundleProperty().get(0);
+        Assert.assertEquals("Wrong bundle property type",
+                BundlePropertyType.ONFETBPTEXPERIMENTER, property.getType());
+        BundlePropertyExperimenter experimenterProperty
+                = (BundlePropertyExperimenter) property.getBundlePropertyEntry();
+        Assert.assertEquals("Wrong experimenter ID",
+                1, experimenterProperty.getExperimenter().getValue().intValue());
+        Assert.assertEquals("Wrong experimenter type",
+                2, experimenterProperty.getExpType().longValue());
         Mockito.verify(experimenterPropertyDeserializer, Mockito.times(1)).deserialize(buffer);
     }
 
