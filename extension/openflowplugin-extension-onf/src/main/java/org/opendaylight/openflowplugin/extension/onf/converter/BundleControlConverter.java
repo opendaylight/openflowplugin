ccc58970062943dfe5c98a71d24cf0408c05b697
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-02-07 09:21:20 +0100

    Change yang models due to errors
    - Added container for cases where was duplicate declaration of fields.
    - Renamed cases to not to have duplicate name of interfaces.
    - Renamed wrong spelled "Convertor" to "Converter".
    - Fixed tests. (Ignored, fixed in following patches)
    - Added nbactions.xml to gitignore (netbeans help file)
    
    Change-Id: Ie268f9d8a697097e3892196e26442c917026b993
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- extension/openflowplugin-extension-onf/src/main/java/org/opendaylight/openflowplugin/extension/onf/converter/BundleControlConverter.java
+++ extension/openflowplugin-extension-onf/src/main/java/org/opendaylight/openflowplugin/extension/onf/converter/BundleControlConverter.java
@@ -9,30 +9,39 @@
 package org.opendaylight.openflowplugin.extension.onf.converter;
 
 import org.opendaylight.openflowplugin.extension.api.ConvertorMessageFromOFJava;
-import org.opendaylight.openflowplugin.extension.api.ConvertorMessageToOFJava;
+import org.opendaylight.openflowplugin.extension.api.ConverterMessageToOFJava;
 import org.opendaylight.openflowplugin.extension.api.path.MessagePath;
 import org.opendaylight.openflowplugin.extension.onf.OnfConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.ExperimenterId;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControl;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControlSal;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.BundleControlSalBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice.bundle.control.sal.SalControlDataBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlOnf;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControlOnfBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.bundle.control.onf.OnfControlGroupingDataBuilder;
 
 /**
  * Converter for BundleControl messages (ONF approved extension #230).
  */
 public class BundleControlConverter implements
-        ConvertorMessageToOFJava<BundleControl, org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl>,
-        ConvertorMessageFromOFJava<org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl, MessagePath> {
+        ConverterMessageToOFJava<BundleControlSal, BundleControlOnf>,
+        ConvertorMessageFromOFJava<BundleControlOnf, MessagePath> {
 
     @Override
-    public org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl
-            convert(final BundleControl experimenterMessageCase) {
-        return new BundleControlBuilder(experimenterMessageCase).build();
+    public BundleControlOnf
+            convert(final BundleControlSal experimenterMessageCase) {
+        return new BundleControlOnfBuilder()
+                .setOnfControlGroupingData(new OnfControlGroupingDataBuilder(experimenterMessageCase.getSalControlData()).build())
+                .build();
     }
 
     @Override
-    public BundleControl convert(org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.experimenter.input.experimenter.data.of.choice.BundleControl input, MessagePath path) {
-        return new org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.send.experimenter.input.experimenter.message.of.choice
-                .BundleControlBuilder(input).build();
+    public BundleControlSal convert(BundleControlOnf input, MessagePath path) {
+        return new BundleControlSalBuilder()
+                .setSalControlData(
+                        new SalControlDataBuilder(input.getOnfControlGroupingData()).build()
+                )
+                .build();
     }
 
     @Override
