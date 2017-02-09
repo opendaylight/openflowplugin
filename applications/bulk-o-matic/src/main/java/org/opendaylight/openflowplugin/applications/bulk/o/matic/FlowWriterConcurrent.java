615d8c458904cf15ad8259f5ecc1e679742c8734
Faiz Ilahi Kothari <faiz.ilahi.k.kothari@ericsson.com>
2017-02-09 11:27:43 +0530

    Add table writer to bulk-o-matic
    	* Adds table writer and corresponding rpc
    	* Modify flow-test rpc to accomodate a boolean: create-parents
    
    Change-Id: Ic3fe4764631d9cbc7550ad3d7d926debb3ca6977
    Signed-off-by: Faiz Ilahi Kothari <faiz.ilahi.k.kothari@ericsson.com>

--- applications/bulk-o-matic/src/main/java/org/opendaylight/openflowplugin/applications/bulk/o/matic/FlowWriterConcurrent.java
+++ applications/bulk-o-matic/src/main/java/org/opendaylight/openflowplugin/applications/bulk/o/matic/FlowWriterConcurrent.java
@@ -40,13 +40,14 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
     }
 
     public void addFlows(Integer dpnCount, Integer flowsPerDPN, int batchSize,
-                         int sleepMillis, int sleepAfter, short startTableId, short endTableId) {
+                         int sleepMillis, int sleepAfter, short startTableId, short endTableId,
+                         boolean isCreateParents) {
         LOG.info(USING_CONCURRENT_IMPLEMENTATION_OF_FLOW_WRITER);
         countDpnWriteCompletion.set(dpnCount);
         startTime = System.nanoTime();
         for (int i = 1; i <= dpnCount; i++) {
             FlowHandlerTask task = new FlowHandlerTask(Integer.toString(i),
-                    flowsPerDPN, true, batchSize, sleepMillis, sleepAfter, startTableId, endTableId);
+                    flowsPerDPN, true, batchSize, sleepMillis, sleepAfter, startTableId, endTableId, isCreateParents);
             flowPusher.execute(task);
         }
     }
@@ -57,7 +58,7 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
         countDpnWriteCompletion.set(dpnCount);
         for (int i = 1; i <= dpnCount; i++) {
             FlowHandlerTask task = new FlowHandlerTask(Integer.toString(i), flowsPerDPN, false, batchSize,
-                    0, 1, startTableId, endTableId);
+                    0, 1, startTableId, endTableId, false);
             flowPusher.execute(task);
         }
     }
@@ -87,6 +88,11 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
         return UNITS;
     }
 
+    @Override
+    public long getTableCount() {
+        return 0;
+    }
+
     private class FlowHandlerTask implements Runnable {
         private final String dpId;
         private final boolean add;
@@ -97,6 +103,7 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
         private final short startTableId;
         private final short endTableId;
         private AtomicInteger remainingTxReturn = new AtomicInteger(0);
+        private final boolean isCreateParents;
 
         public FlowHandlerTask(final String dpId,
                                final int flowsPerDpn,
@@ -105,7 +112,8 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
                                final int sleepMillis,
                                final int sleepAfter,
                                final short startTableId,
-                               final short endTableId){
+                               final short endTableId,
+                               final boolean isCreateParents){
             this.dpId = BulkOMaticUtils.DEVICE_TYPE_PREFIX + dpId;
             this.add = add;
             this.flowsPerDpn = flowsPerDpn;
@@ -114,6 +122,7 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
             this.sleepAfter = sleepAfter;
             this.startTableId = startTableId;
             this.endTableId = endTableId;
+            this.isCreateParents = isCreateParents;
             remainingTxReturn.set(flowsPerDpn/batchSize);
         }
 
@@ -163,7 +172,7 @@ public class FlowWriterConcurrent implements FlowCounterMBean {
         private void addFlowToTx(WriteTransaction writeTransaction, String flowId, InstanceIdentifier<Flow> flowIid, Flow flow, Integer sourceIp, Short tableId){
             if (add) {
                 LOG.trace("Adding flow for flowId: {}, flowIid: {}", flowId, flowIid);
-                writeTransaction.put(LogicalDatastoreType.CONFIGURATION, flowIid, flow, true);
+                writeTransaction.put(LogicalDatastoreType.CONFIGURATION, flowIid, flow, isCreateParents);
             } else {
                 LOG.trace("Deleting flow for flowId: {}, flowIid: {}", flowId, flowIid);
                 writeTransaction.delete(LogicalDatastoreType.CONFIGURATION, flowIid);
