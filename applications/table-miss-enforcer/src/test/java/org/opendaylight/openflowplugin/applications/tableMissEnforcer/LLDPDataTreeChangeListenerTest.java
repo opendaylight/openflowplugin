17a9bdbc0374b0c4067540a07813dc9780b404d3
miroslav.macko <miroslav.macko@pantheon.tech>
2017-02-21 16:01:45 +0100

    Fix table miss flow push
        - Use ClusterSingletonService
    
    Resolves: Bug 7770
    
    Change-Id: Idcc8e27d1efa9a9816fa757314275fc8dd5cc6dd
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

--- applications/table-miss-enforcer/src/test/java/org/opendaylight/openflowplugin/applications/tableMissEnforcer/LLDPDataTreeChangeListenerTest.java
+++ applications/table-miss-enforcer/src/test/java/org/opendaylight/openflowplugin/applications/tableMissEnforcer/LLDPDataTreeChangeListenerTest.java
@@ -17,22 +17,26 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
 import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.ModificationType;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Instructions;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ApplyActionsCase;
@@ -41,6 +45,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
@@ -51,38 +56,97 @@ public class LLDPDataTreeChangeListenerTest {
     private LLDPPacketPuntEnforcer lldpPacketPuntEnforcer;
     private final static InstanceIdentifier<Node> nodeIID = InstanceIdentifier.create(Nodes.class)
             .child(Node.class, new NodeKey(new NodeId("testnode:1")));
+
+    private final static InstanceIdentifier<FlowCapableNode> nodeII =
+            InstanceIdentifier.create(Nodes.class)
+            .child(Node.class)
+                    .augmentation(FlowCapableNode.class);
     @Mock
     private SalFlowService flowService;
     @Mock
     private DataTreeModification<FlowCapableNode> dataTreeModification;
-    @Captor
-    private ArgumentCaptor<AddFlowInput> addFlowInputCaptor;
+    @Mock
+    private DataObjectModification<FlowCapableNode> operationalModification;
+    @Mock
+    private ClusterSingletonServiceProvider clusterSingletonService;
+    @Mock
+    private BindingAwareBroker bindingAwareBroker;
+    @Mock
+    private DataBroker dataBroker;
+    @Mock
+    private FlowCapableNode operationalNode;
+    @Mock
+    private ProviderContext providerContext;
+    @Mock
+    private ClusterSingletonServiceRegistration registration;
+    @Mock
+    private ListenerRegistration<LLDPPacketPuntEnforcer> dataTreeChangeRegistration;
+    @Mock
+    private TableMissEnforcerManager tableMissEnforcerManager;
 
     @Before
     public void setUp() {
-        lldpPacketPuntEnforcer = new LLDPPacketPuntEnforcer(flowService, Mockito.mock(DataBroker.class));
+        lldpPacketPuntEnforcer = new LLDPPacketPuntEnforcer(
+                bindingAwareBroker,
+                dataBroker,
+                clusterSingletonService,
+                flowService);
         final DataTreeIdentifier<FlowCapableNode> identifier = new DataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, nodeIID);
         Mockito.when(dataTreeModification.getRootPath()).thenReturn(identifier);
-        Mockito.when(dataTreeModification.getRootNode()).thenReturn(Mockito.mock(DataObjectModification.class));
-        Mockito.when(dataTreeModification.getRootNode().getModificationType()).thenReturn(ModificationType.WRITE);
+        Mockito.when(dataTreeModification.getRootNode()).thenReturn(operationalModification);
+
+
+        final DataTreeIdentifier<FlowCapableNode> nodeIdentifier = new DataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, nodeII);
+        Mockito.when(dataBroker.registerDataTreeChangeListener(nodeIdentifier, lldpPacketPuntEnforcer)).thenReturn(dataTreeChangeRegistration);
+
+        lldpPacketPuntEnforcer.onSessionInitiated(providerContext);
+
+        tableMissEnforcerManager = new TableMissEnforcerManager(clusterSingletonService, flowService);
+
+        Mockito.when(clusterSingletonService.registerClusterSingletonService(Matchers.<ClusterSingletonService>any()))
+                .thenReturn(registration);
+    }
+
+    @Test
+    public void testDataTreeChangeListenerRegistration() {
+        lldpPacketPuntEnforcer.onDataTreeChanged(Collections.singleton(dataTreeModification));
+
+        Mockito.verify(dataBroker).registerDataTreeChangeListener(
+                Matchers.<DataTreeIdentifier<FlowCapableNode>>any(),
+                Matchers.<DataTreeChangeListener<FlowCapableNode>>any());
+        Mockito.verify(bindingAwareBroker).registerProvider(lldpPacketPuntEnforcer);
     }
 
     @Test
     public void testOnDataTreeChanged() {
+        // add
+        Mockito.when(dataTreeModification.getRootNode().getModificationType()).thenReturn(ModificationType.WRITE);
+        Mockito.when(operationalModification.getDataBefore()).thenReturn(null);
+        Mockito.when(operationalModification.getDataAfter()).thenReturn(operationalNode);
+
         lldpPacketPuntEnforcer.onDataTreeChanged(Collections.singleton(dataTreeModification));
-        Mockito.verify(flowService).addFlow(addFlowInputCaptor.capture());
-        AddFlowInput captured = addFlowInputCaptor.getValue();
-        Assert.assertEquals(nodeIID, captured.getNode().getValue());
+
+        Mockito.verify(clusterSingletonService).registerClusterSingletonService(Matchers.any());
+        Assert.assertEquals(1, lldpPacketPuntEnforcer.getTableMissEnforcerManager().getTableMissEnforcers().size());
+
+        // delete
+        Mockito.when(dataTreeModification.getRootNode().getModificationType()).thenReturn(ModificationType.DELETE);
+        Mockito.when(operationalModification.getDataBefore()).thenReturn(operationalNode);
+        Mockito.when(operationalModification.getDataAfter()).thenReturn(null);
+
+        lldpPacketPuntEnforcer.onDataTreeChanged(Collections.singleton(dataTreeModification));
+
+        Assert.assertEquals(0, lldpPacketPuntEnforcer.getTableMissEnforcerManager().getTableMissEnforcers().size());
     }
 
     @Test
     public void testCreateFlow() {
-        final Flow flow = lldpPacketPuntEnforcer.createFlow();
+        final Flow flow = TableMissUtils.createFlow();
         evaluateInstructions(flow.getInstructions());
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
         lldpPacketPuntEnforcer.close();
     }
 
