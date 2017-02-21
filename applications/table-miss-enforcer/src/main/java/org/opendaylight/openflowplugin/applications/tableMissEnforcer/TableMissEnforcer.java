abe85ec80d4d6af176ffade790122af33058f918
miroslav.macko <miroslav.macko@pantheon.tech>
2017-02-21 16:01:45 +0100

    Fix table miss flow push
        - Use ClusterSingletonService
    
    Resolves: Bug 7770
    
    Change-Id: Idcc8e27d1efa9a9816fa757314275fc8dd5cc6dd
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

---
+++ applications/table-miss-enforcer/src/main/java/org/opendaylight/openflowplugin/applications/tableMissEnforcer/TableMissEnforcer.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.tableMissEnforcer;
+
+import static org.opendaylight.openflowplugin.applications.tableMissEnforcer.TableMissUtils.DEFAULT_FLOW_ID;
+import static org.opendaylight.openflowplugin.applications.tableMissEnforcer.TableMissUtils.TABLE_ID;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
+import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService} clusterSingletonServiceRegistration per connected device.
+ */
+public class TableMissEnforcer implements ClusterSingletonService, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(TableMissEnforcer.class);
+
+    private final NodeId nodeId;
+    private final ServiceGroupIdentifier identifier;
+    private final ClusterSingletonServiceRegistration clusterSingletonServiceRegistration;
+    private final InstanceIdentifier<FlowCapableNode> instanceIdentifier;
+    private final SalFlowService flowService;
+
+    public TableMissEnforcer(final InstanceIdentifier<FlowCapableNode> instanceIdentifier,
+                             final ClusterSingletonServiceProvider clusterSingletonService,
+                             final SalFlowService flowService) {
+        this.nodeId = TableMissUtils.retreiveNodeId(instanceIdentifier);
+        this.identifier = ServiceGroupIdentifier.create(nodeId.getValue());
+        this.instanceIdentifier = instanceIdentifier;
+        this.flowService = flowService;
+        LOG.debug("Table miss flow registerClusterSingletonService: {}", nodeId.getValue());
+        clusterSingletonServiceRegistration = clusterSingletonService.registerClusterSingletonService(this);
+    }
+
+    @Override
+    public void instantiateServiceInstance() {
+        LOG.debug("Table miss flow instantiateServiceInstance: {}", nodeId.getValue());
+        final AddFlowInput addFlowInput = new AddFlowInputBuilder(TableMissUtils.createFlow())
+                .setNode(new NodeRef(instanceIdentifier.firstIdentifierOf(Node.class)))
+                .setFlowRef(new FlowRef(
+                        instanceIdentifier
+                                .child(Table.class, new TableKey(TABLE_ID))
+                                .child(Flow.class, new FlowKey(new FlowId(DEFAULT_FLOW_ID)))))
+                .build();
+
+        Futures.lazyTransform(flowService.addFlow(addFlowInput), result -> {
+            LOG.debug("Table miss flow added for node: {}", nodeId.getValue());
+            return result;
+        });
+    }
+
+    @Override
+    public ListenableFuture<Void> closeServiceInstance() {
+        return Futures.immediateFuture(null);
+    }
+
+    @Override
+    public ServiceGroupIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public void close() {
+        if (clusterSingletonServiceRegistration != null) {
+            try {
+                clusterSingletonServiceRegistration.close();
+            } catch (Exception e) {
+                LOG.error("Table miss enforcer cluster service close fail: {} {}", nodeId.getValue(), e);
+            }
+        }
+    }
+
+}
---
+++ applications/table-miss-enforcer/src/main/java/org/opendaylight/openflowplugin/applications/tableMissEnforcer/TableMissEnforcerManager.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.tableMissEnforcer;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Manager for clustering service registrations of {@link TableMissEnforcer}.
+ */
+public class TableMissEnforcerManager implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(TableMissEnforcerManager.class);
+    private final ClusterSingletonServiceProvider clusterSingletonService;
+    private final SalFlowService salFlowService;
+    private final ConcurrentHashMap<InstanceIdentifier<FlowCapableNode>, TableMissEnforcer> tableMissEnforcers = new ConcurrentHashMap();
+
+    public TableMissEnforcerManager(final ClusterSingletonServiceProvider clusterSingletonService,
+                                    final SalFlowService salFlowService) {
+        this.clusterSingletonService = clusterSingletonService;
+        this.salFlowService = salFlowService;
+    }
+
+    public void onDeviceConnected(final InstanceIdentifier<FlowCapableNode> nodeIdent) {
+        final NodeId nodeId = TableMissUtils.retreiveNodeId(nodeIdent);
+        LOG.debug("Table miss enforcer connected node: {}", nodeId.getValue());
+        if (!tableMissEnforcers.contains(nodeId)) {
+            final TableMissEnforcer tableMissEnforcer = new TableMissEnforcer(
+                    nodeIdent,
+                    clusterSingletonService,
+                    salFlowService);
+
+            tableMissEnforcers.put(nodeIdent, tableMissEnforcer);
+            LOG.debug("Table miss enforcer service registered for node: {}", nodeId.getValue());
+        }
+    }
+
+    public void onDeviceDisconnected(final InstanceIdentifier<FlowCapableNode> nodeIdent) {
+        final NodeId nodeId = TableMissUtils.retreiveNodeId(nodeIdent);
+        LOG.debug("Table miss enforcer disconnected node: {}", nodeId.getValue());
+        Optional.ofNullable(tableMissEnforcers.remove(nodeIdent)).ifPresent(TableMissEnforcer::close);
+    }
+
+    @Override
+    public void close() throws Exception {
+        tableMissEnforcers.values().forEach(TableMissEnforcer::close);
+        tableMissEnforcers.clear();
+    }
+
+    @VisibleForTesting
+    ConcurrentHashMap<InstanceIdentifier<FlowCapableNode>, TableMissEnforcer> getTableMissEnforcers() {
+        return tableMissEnforcers;
+    }
+}
