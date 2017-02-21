af90bd678fbdcb0b9030cb688db76b4b9c2bfabc
miroslav.macko <miroslav.macko@pantheon.tech>
2017-02-21 16:01:45 +0100

    Use Mastership change listener in table miss enforcer
    - Fix table miss flow push
    - Send flow only from the master instance
    - For he plugin it is not possible to use this
      new approach, so the not optimal old one with
      ClusteredDataTreeChange listener is used.
    
    Resolves: Bug 7770
    
    Change-Id: I095b731dec07e7977379eb07c920da3988972f73
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

---
+++ applications/table-miss-enforcer-he/src/test/java/org/opendaylight/openflowplugin/applications/tableMissEnforcer/LLDPDataTreeChangeListenerTest.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2014 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.tableMissEnforcer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collections;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.ModificationType;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Instructions;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ApplyActionsCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Test for {@link LLDPPacketPuntEnforcer}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LLDPDataTreeChangeListenerTest {
+    private LLDPPacketPuntEnforcer lldpPacketPuntEnforcer;
+    private final static InstanceIdentifier<Node> nodeIID = InstanceIdentifier.create(Nodes.class)
+            .child(Node.class, new NodeKey(new NodeId("testnode:1")));
+    @Mock
+    private SalFlowService flowService;
+    @Mock
+    private DataTreeModification<FlowCapableNode> dataTreeModification;
+    @Captor
+    private ArgumentCaptor<AddFlowInput> addFlowInputCaptor;
+
+    @Before
+    public void setUp() {
+        lldpPacketPuntEnforcer = new LLDPPacketPuntEnforcer(flowService, Mockito.mock(DataBroker.class));
+        final DataTreeIdentifier<FlowCapableNode> identifier = new DataTreeIdentifier(LogicalDatastoreType.OPERATIONAL, nodeIID);
+        Mockito.when(dataTreeModification.getRootPath()).thenReturn(identifier);
+        Mockito.when(dataTreeModification.getRootNode()).thenReturn(Mockito.mock(DataObjectModification.class));
+        Mockito.when(dataTreeModification.getRootNode().getModificationType()).thenReturn(ModificationType.WRITE);
+    }
+
+    @Test
+    public void testOnDataTreeChanged() {
+        lldpPacketPuntEnforcer.onDataTreeChanged(Collections.singleton(dataTreeModification));
+        Mockito.verify(flowService).addFlow(addFlowInputCaptor.capture());
+        AddFlowInput captured = addFlowInputCaptor.getValue();
+        Assert.assertEquals(nodeIID, captured.getNode().getValue());
+    }
+
+    @Test
+    public void testCreateFlow() {
+        final Flow flow = lldpPacketPuntEnforcer.createFlow();
+        evaluateInstructions(flow.getInstructions());
+    }
+
+    @After
+    public void tearDown() {
+        lldpPacketPuntEnforcer.close();
+    }
+
+    private static void evaluateInstructions(final Instructions instructions) {
+        assertNotNull(instructions.getInstruction());
+        assertEquals(1, instructions.getInstruction().size());
+        Instruction instruction = instructions.getInstruction().get(0);
+        evaluateInstruction(instruction);
+    }
+
+    private static void evaluateInstruction(final Instruction instruction) {
+        if (instruction.getInstruction() instanceof ApplyActionsCase) {
+            ApplyActionsCase applyActionsCase = (ApplyActionsCase) instruction.getInstruction();
+            assertNotNull(applyActionsCase.getApplyActions().getAction());
+            assertEquals(1, applyActionsCase.getApplyActions().getAction().size());
+            Action action = applyActionsCase.getApplyActions().getAction().get(0);
+            evaluateAction(action);
+        }
+    }
+
+    private static void evaluateAction(final Action action) {
+        if (action.getAction() instanceof OutputActionCase) {
+            OutputActionCase outputActionCase = (OutputActionCase) action.getAction();
+            assertEquals("CONTROLLER", outputActionCase.getOutputAction().getOutputNodeConnector().getValue());
+            assertEquals(new Integer(0xffff).intValue(), outputActionCase.getOutputAction().getMaxLength().intValue());
+        }
+    }
+}
---
+++ applications/table-miss-enforcer/src/main/java/org/opendaylight/openflowplugin/applications/table/miss/enforcer/TableMissEnforcerMastershipChangeService.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.table.miss.enforcer;
+
+import com.google.common.util.concurrent.Futures;
+import java.util.concurrent.Future;
+import javax.annotation.Nonnull;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
+import org.opendaylight.openflowplugin.api.openflow.mastership.MastershipChangeService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Mastership change service.
+ */
+public class TableMissEnforcerMastershipChangeService implements MastershipChangeService {
+    private static final Logger LOG = LoggerFactory.getLogger(TableMissEnforcerMastershipChangeService.class);
+    private final SalFlowService salFlowService;
+
+    public TableMissEnforcerMastershipChangeService(final SalFlowService salFlowService) {
+        this.salFlowService = salFlowService;
+    }
+
+    @Override
+    public Future<Void> onBecomeOwner(@Nonnull DeviceInfo deviceInfo) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Table miss enforcer connected node: {}", deviceInfo.getLOGValue());
+        }
+
+        final AddFlowInput addFlowInput = new AddFlowInputBuilder(TableMissUtils.createFlow())
+                .setNode(new NodeRef(deviceInfo.getNodeInstanceIdentifier()))
+                .setFlowRef(new FlowRef(
+                        deviceInfo
+                                .getNodeInstanceIdentifier()
+                                .augmentation(FlowCapableNode.class)
+                                .child(Table.class, new TableKey(TableMissUtils.TABLE_ID))
+                                .child(Flow.class, new FlowKey(new FlowId(TableMissUtils.DEFAULT_FLOW_ID)))))
+                .build();
+
+        return Futures.lazyTransform(salFlowService.addFlow(addFlowInput), result -> {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Table miss flow added for node: {}", deviceInfo.getLOGValue());
+            }
+
+            return null;
+        });
+    }
+
+    @Override
+    public Future<Void> onLoseOwnership(@Nonnull DeviceInfo deviceInfo) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Table miss enforcer disconnected node: {}", deviceInfo.getLOGValue());
+        }
+
+        return Futures.immediateFuture(null);
+    }
+
+    @Override
+    public void close() throws Exception {
+        // NOOP
+    }
+}
