050f0dceefb75f5c74e65630e80007e8d4bf5904
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-02-24 09:40:45 +0100

    Fix statistics race condition on big flows
    Fix race condition that happens when we receive statistics faster
    than flow is deleted from device, but we already sent request for
    deletion and removed this flow from device registy.
    
    Resolves: bug 6917
    
    Change-Id: I95510fbc5d507eaffc34d58b3b7743e60911b541
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/CommonDeviceRegistry.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.registry;
+
+import java.util.function.Consumer;
+
+public interface CommonDeviceRegistry<KEY> extends AutoCloseable {
+
+    /**
+     * Store KEY in device registry.
+     * @param key device registry key
+     */
+    void store(KEY key);
+
+    /**
+     * Add mark for specified KEY.
+     * @param key device registry key
+     */
+    void addMark(KEY key);
+
+    /**
+     * Checks if registry has mark for KEY.
+     * @param key device registry key
+     * @return true if device registry has mark for KEY
+     */
+    boolean hasMark(KEY key);
+
+    /**
+     * Process marked keys.
+     */
+    void processMarks();
+
+    /**
+     * Iterate over all keys in device registry.
+     * @param consumer key consumer
+     */
+    void forEach(Consumer<KEY> consumer);
+
+    /**
+     * Get device registry size.
+     * @return device registry size
+     */
+    int size();
+
+    @Override
+    void close();
+
+}
