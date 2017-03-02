d958bf3ac06cbc557d18ec7d7fa307fe2c3ea993
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-03-02 14:50:17 +0100

    Fix checkstyle warnings
    - org.opendaylight.openflowplugin.api.openflow.md.core
    
    Change-Id: I98490f3a4054bcfdb1339ac490d432ece0539097
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/IMDMessageTranslator.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/IMDMessageTranslator.java
@@ -10,7 +10,7 @@ package org.opendaylight.openflowplugin.api.openflow.md.core;
 import org.opendaylight.openflowplugin.api.openflow.md.core.session.SessionContext;
 
 /**
- * translates between messages
+ * translates between messages.
  * @param <I> source message type (IN)
  * @param <O> result message type (OUT)
  */
@@ -18,14 +18,9 @@ public interface IMDMessageTranslator<I, O> {
 
     /**
      * This method is called in order to translate message to MD-SAL or from MD-SAL.
-     *
-     * @param cookie
-     *            auxiliary connection identifier
-     * @param sc
-     *            The SessionContext which sent the OF message
-     * @param msg
-     *            The OF message
-     *
+     * @param cookie auxiliary connection identifier
+     * @param sc The SessionContext which sent the OF message
+     * @param msg The OF message
      * @return translated message
      */
     O translate(SwitchConnectionDistinguisher cookie, SessionContext sc, I msg);
