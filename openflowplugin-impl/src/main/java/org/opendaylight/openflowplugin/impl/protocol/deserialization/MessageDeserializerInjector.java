7afd33097a01d9b9ebebbd48e4e5279ff52ab6b5
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-03-02 17:50:50 +0100

    Add configuration knob for Single Layer
    Add configuration knob to openflowplugin.cfg that will enable/disable
    use-single-layer-serialization.
    
    Change-Id: I9473a80495060aaa252757ba4d0b1450d252b08e
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/MessageDeserializerInjector.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/deserialization/MessageDeserializerInjector.java
@@ -17,14 +17,21 @@ import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
 import org.opendaylight.openflowjava.protocol.api.keys.TypeToClassKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowplugin.impl.protocol.deserialization.messages.FlowMessageDeserializer;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowMessage;
 import org.opendaylight.openflowplugin.impl.protocol.deserialization.messages.GroupMessageDeserializer;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.GroupMessage;
 import org.opendaylight.openflowplugin.impl.protocol.deserialization.messages.MeterMessageDeserializer;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterMessage;
 import org.opendaylight.openflowplugin.impl.protocol.deserialization.messages.PortMessageDeserializer;
+import org.opendaylight.openflowplugin.impl.protocol.deserialization.multipart.MultipartReplyMessageDeserializer;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.FlowMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.group.types.rev131018.GroupMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.meter.types.rev130918.MeterMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.multipart.types.rev170112.MultipartReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GroupModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MeterModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReplyMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortModInput;
 
 public class MessageDeserializerInjector {
 
@@ -37,10 +44,24 @@ public class MessageDeserializerInjector {
         final Function<Integer, Function<Class<? extends OfHeader>, Consumer<OFDeserializer<? extends OfHeader>>>> injector =
                 createInjector(provider, EncodeConstants.OF13_VERSION_ID);
 
-        injector.apply(14).apply(FlowMessage.class).accept(new FlowMessageDeserializer());
-        injector.apply(15).apply(GroupMessage.class).accept(new GroupMessageDeserializer());
-        injector.apply(29).apply(MeterMessage.class).accept(new MeterMessageDeserializer());
-        injector.apply(16).apply(PortMessage.class).accept(new PortMessageDeserializer());
+        injector.apply(19).apply(MultipartReply.class).accept(new MultipartReplyMessageDeserializer());
+    }
+
+    /**
+     * Reverts original message deserializers in provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     */
+    static void revertDeserializers(final DeserializerExtensionProvider provider) {
+        provider.unregisterDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 14));
+        provider.registerDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 14), FlowModInput.class);
+        provider.unregisterDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 15));
+        provider.registerDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 15), GroupModInput.class);
+        provider.unregisterDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 29));
+        provider.registerDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 29), MeterModInput.class);
+        provider.unregisterDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 16));
+        provider.registerDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 16), PortModInput.class);
+        provider.unregisterDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 19));
+        provider.registerDeserializerMapping(new TypeToClassKey(EncodeConstants.OF13_VERSION_ID, 19), MultipartReplyMessage.class);
     }
 
     /**
@@ -60,6 +81,17 @@ public class MessageDeserializerInjector {
                     new MessageCodeKey(version, code, retType),
                     deserializer);
         };
+    }
+
+    @VisibleForTesting
+    static void injectLegacyDeserializers(final DeserializerExtensionProvider provider) {
+        final Function<Integer, Function<Class<? extends OfHeader>, Consumer<OFDeserializer<? extends OfHeader>>>> injector =
+            createInjector(provider, EncodeConstants.OF13_VERSION_ID);
+
+         injector.apply(14).apply(FlowMessage.class).accept(new FlowMessageDeserializer());
+         injector.apply(15).apply(GroupMessage.class).accept(new GroupMessageDeserializer());
+         injector.apply(29).apply(MeterMessage.class).accept(new MeterMessageDeserializer());
+         injector.apply(16).apply(PortMessage.class).accept(new PortMessageDeserializer());
     }
 
 }
