1474458757bb94b3039df86ba1503fa98f031f28
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-03-03 12:46:20 +0100

    Fix checkstyle warnings.
    - org.opendaylight.openflowplugin.api.openflow.md.core
    
    Change-Id: Ida0bf4149aaa70657a22fb527708d61b0d7d43b3
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/sal/NotificationComposer.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/sal/NotificationComposer.java
@@ -1,6 +1,5 @@
 /**
  * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
- * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -11,11 +10,13 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.transaction.rev150304.
 import org.opendaylight.yangtools.yang.binding.Notification;
 
 /**
+ * Notification composer.
  * @param <N> type of notification
  */
 public interface NotificationComposer<N extends Notification> {
-    
+
     /**
+     * Compose.
      * @param xid corresponding OF transaction id
      * @return notification instance
      */
--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/session/SessionListener.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/md/core/session/SessionListener.java
@@ -1,6 +1,5 @@
 /**
  * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
- *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -10,22 +9,23 @@ package org.opendaylight.openflowplugin.api.openflow.md.core.session;
 import java.util.EventListener;
 
 /**
- * listens on session changes
+ * listens on session changes.
  */
 public interface SessionListener extends EventListener {
 
     /**
-     * fired upon session added
-     * @param sessionKey
-     * @param context
+     * fired upon session added.
+     * @param sessionKey session key
+     * @param context context
      */
     void onSessionAdded(SwitchSessionKeyOF sessionKey, SessionContext context);
 
     /**
-     * fired upon session removed
-     * @param context
+     * fired upon session removed.
+     * @param context context
      */
     void onSessionRemoved(SessionContext context);
+
     void setRole(SessionContext context);
 
 }
