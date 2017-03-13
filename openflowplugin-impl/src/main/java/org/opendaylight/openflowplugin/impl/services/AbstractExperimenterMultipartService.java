ed6fc9dd2e82df8a2e8ba15d72028829cbb07c1c
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-03-13 14:27:55 +0100

    Update multipart request services for single layer
    Update all single layer services that was sending OpenFlowJava
    multipart requests to send OpenFlowPlugin multipart requests
    instead to avoid translations.
    Remove failing tests from NodeConnectorRefToPortTranslatorTest. These
    tests are not needed anymore, since now JUnit auto-handles @NonNull
    annotations.
    
    See also: bug 6915
    
    Change-Id: I427a6148e67d61080a85de5c304f478df0e26682
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/AbstractExperimenterMultipartService.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/AbstractExperimenterMultipartService.java
@@ -8,26 +8,13 @@
 
 package org.opendaylight.openflowplugin.impl.services;
 
-import java.util.Objects;
 import java.util.concurrent.Future;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
-import org.opendaylight.openflowplugin.api.openflow.device.Xid;
-import org.opendaylight.openflowplugin.extension.api.ConvertorMessageToOFJava;
-import org.opendaylight.openflowplugin.extension.api.TypeVersionKey;
 import org.opendaylight.openflowplugin.extension.api.core.extension.ExtensionConverterProvider;
-import org.opendaylight.openflowplugin.extension.api.exception.ConversionException;
-import org.opendaylight.openflowplugin.extension.api.exception.ConverterNotFoundException;
-import org.opendaylight.openflowplugin.impl.services.util.RequestInputUtils;
-import org.opendaylight.openflowplugin.impl.services.util.ServiceException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.experimenter.mp.message.service.rev151020.SendExperimenterMpRequestInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.experimenter.mp.message.service.rev151020.SendExperimenterMpRequestOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.experimenter.core.ExperimenterDataOfChoice;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.MultipartRequestExperimenterCaseBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.request.multipart.request.body.multipart.request.experimenter._case.MultipartRequestExperimenterBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.experimenter.types.rev151020.experimenter.core.message.ExperimenterMessageOfChoice;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
 public abstract class AbstractExperimenterMultipartService<T extends OfHeader> extends AbstractMultipartService<SendExperimenterMpRequestInput, T> {
@@ -38,37 +25,6 @@ public abstract class AbstractExperimenterMultipartService<T extends OfHeader> e
                                                    ExtensionConverterProvider extensionConverterProvider) {
         super(requestContextStack, deviceContext);
         this.extensionConverterProvider = extensionConverterProvider;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    protected OfHeader buildRequest(Xid xid, SendExperimenterMpRequestInput input) throws ServiceException {
-        final TypeVersionKey key = new TypeVersionKey<>(
-                input.getExperimenterMessageOfChoice().getImplementedInterface(),
-                getVersion());
-
-        final ConvertorMessageToOFJava<ExperimenterMessageOfChoice, ExperimenterDataOfChoice> messageConverter =
-                getExtensionConverterProvider().getMessageConverter(key);
-
-        if (Objects.isNull(messageConverter)) {
-            throw new ServiceException(new ConverterNotFoundException(key.toString()));
-        }
-
-        try {
-            return RequestInputUtils
-                .createMultipartHeader(MultipartType.OFPMPEXPERIMENTER, xid.getValue(), getVersion())
-                .setMultipartRequestBody(new MultipartRequestExperimenterCaseBuilder()
-                        .setMultipartRequestExperimenter(new MultipartRequestExperimenterBuilder()
-                                .setExperimenter(messageConverter.getExperimenterId())
-                                .setExpType(messageConverter.getType())
-                                .setExperimenterDataOfChoice(messageConverter
-                                        .convert(input.getExperimenterMessageOfChoice()))
-                                .build())
-                        .build())
-                .build();
-        } catch (final ConversionException e) {
-            throw new ServiceException(e);
-        }
     }
 
     /**
--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/direct/singlelayer/PortDirectStatisticsService.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/direct/singlelayer/PortDirectStatisticsService.java
@@ -12,13 +12,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.openflowplugin.api.openflow.device.Xid;
 import org.opendaylight.openflowplugin.impl.datastore.MultipartWriterProvider;
+import org.opendaylight.openflowplugin.impl.services.util.ServiceException;
 import org.opendaylight.openflowplugin.impl.statistics.services.direct.AbstractPortDirectStatisticsService;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.ConvertorExecutor;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetNodeConnectorStatisticsInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetNodeConnectorStatisticsOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.GetNodeConnectorStatisticsOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.multipart.types.rev170112.MultipartReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.multipart.types.rev170112.MultipartRequestBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.multipart.reply.multipart.reply.body.MultipartReplyPortStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.multipart.request.multipart.request.body.MultipartRequestPortStatsBuilder;
 
 public class PortDirectStatisticsService extends AbstractPortDirectStatisticsService<MultipartReply> {
 
@@ -39,6 +45,18 @@ public class PortDirectStatisticsService extends AbstractPortDirectStatisticsSer
                     .getNodeConnectorStatisticsAndPortNumberMap()
                     .stream())
                 .collect(Collectors.toList()))
+            .build();
+    }
+
+    @Override
+    protected OfHeader buildRequest(final Xid xid, final GetNodeConnectorStatisticsInput input) throws ServiceException {
+        return new MultipartRequestBuilder()
+            .setXid(xid.getValue())
+            .setVersion(getVersion())
+            .setRequestMore(false)
+            .setMultipartRequestBody(new MultipartRequestPortStatsBuilder()
+                .setNodeConnectorId(input.getNodeConnectorId())
+                .build())
             .build();
     }
 
