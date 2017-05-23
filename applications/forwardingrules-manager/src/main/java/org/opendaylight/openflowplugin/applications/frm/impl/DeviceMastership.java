d5d9c6afdf9a294e1de67208e10e56e5dd33bd48
Anil Vishnoi <vishnoianil@gmail.com>
2017-05-23 15:27:20 -0700

    Split connection implementation
    Change-Id: Ia05c1a3ae891a5bbaeae1bff0e417185ab59473a
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/impl/DeviceMastership.java
+++ applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/impl/DeviceMastership.java
@@ -12,8 +12,6 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
-import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
-import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
 import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
 import org.opendaylight.openflowplugin.applications.frm.FlowNodeReconciliation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
@@ -32,19 +30,15 @@ public class DeviceMastership implements ClusterSingletonService, AutoCloseable 
     private static final Logger LOG = LoggerFactory.getLogger(DeviceMastership.class);
     private final NodeId nodeId;
     private final ServiceGroupIdentifier identifier;
-    private final ClusterSingletonServiceProvider clusterSingletonServiceProvider;
     private final FlowNodeReconciliation reconcliationAgent;
     private final AtomicBoolean deviceMastered = new AtomicBoolean(false);
     private final AtomicBoolean isDeviceInOperDS = new AtomicBoolean(false);
     private final InstanceIdentifier<FlowCapableNode> fcnIID;
-    private ClusterSingletonServiceRegistration clusterSingletonServiceRegistration;
 
     public DeviceMastership(final NodeId nodeId,
-                            final ClusterSingletonServiceProvider clusterSingletonService,
                             final FlowNodeReconciliation reconcliationAgent) {
         this.nodeId = nodeId;
         this.identifier = ServiceGroupIdentifier.create(nodeId.getValue());
-        this.clusterSingletonServiceProvider = clusterSingletonService;
         this.reconcliationAgent = reconcliationAgent;
         fcnIID = InstanceIdentifier.create(Nodes.class).child(Node.class, new NodeKey(nodeId)).augmentation
                 (FlowCapableNode.class);
@@ -55,6 +49,7 @@ public class DeviceMastership implements ClusterSingletonService, AutoCloseable 
         LOG.info("FRM started for: {}", nodeId.getValue());
         deviceMastered.set(true);
         if(canReconcile()) {
+            LOG.info("Triggering reconciliation for device {}", nodeId.getValue());
             reconcliationAgent.reconcileConfiguration(fcnIID);
         }
     }
@@ -73,13 +68,6 @@ public class DeviceMastership implements ClusterSingletonService, AutoCloseable 
 
     @Override
     public void close() {
-        if (clusterSingletonServiceRegistration != null) {
-            try {
-                clusterSingletonServiceRegistration.close();
-            } catch (Exception e) {
-                LOG.error("FRM cluster service close fail: {} {}", nodeId.getValue(), e);
-            }
-        }
     }
 
     public boolean isDeviceMastered() {
@@ -89,17 +77,17 @@ public class DeviceMastership implements ClusterSingletonService, AutoCloseable 
     public void setDeviceOperationalStatus(boolean inOperDS) {
         isDeviceInOperDS.set(inOperDS);
         if(canReconcile()) {
-            LOG.info("Triggering reconciliation for device {}", nodeId.getValue());
             reconcliationAgent.reconcileConfiguration(fcnIID);
         }
     }
 
-    public void registerClusterSingletonService() {
-        LOG.info("Registering FRM as a cluster singleton service listner for service id : {}",getIdentifier());
-        clusterSingletonServiceRegistration = clusterSingletonServiceProvider.registerClusterSingletonService(this);
-    }
-
     private boolean canReconcile() {
         return (deviceMastered.get() && isDeviceInOperDS.get());
+    }
+
+    public void reconcile() {
+        deviceMastered.set(true);
+        LOG.info("Triggering reconciliation for device {}", nodeId.getValue());
+        reconcliationAgent.reconcileConfiguration(fcnIID);
     }
 }
--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/LifecycleServiceImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/LifecycleServiceImpl.java
@@ -7,240 +7,150 @@
  */
 package org.opendaylight.openflowplugin.impl.lifecycle;
 
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
 import com.google.common.base.Verify;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
 import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
-import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
-import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext.CONNECTION_STATE;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
 import org.opendaylight.openflowplugin.api.openflow.device.handlers.ClusterInitializationPhaseHandler;
 import org.opendaylight.openflowplugin.api.openflow.device.handlers.DeviceRemovedHandler;
 import org.opendaylight.openflowplugin.api.openflow.lifecycle.LifecycleService;
-import org.opendaylight.openflowplugin.api.openflow.rpc.RpcContext;
-import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsContext;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.openflowplugin.api.openflow.lifecycle.MastershipChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.role.service.rev150727.SetRoleOutput;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LifecycleServiceImpl implements LifecycleService {
     private static final Logger LOG = LoggerFactory.getLogger(LifecycleServiceImpl.class);
 
-    private final List<DeviceRemovedHandler> deviceRemovedHandlers = new ArrayList<>();
-    private volatile CONTEXT_STATE state = CONTEXT_STATE.INITIALIZATION;
-    private DeviceContext deviceContext;
-    private RpcContext rpcContext;
-    private StatisticsContext statContext;
     private ClusterSingletonServiceRegistration registration;
     private ClusterInitializationPhaseHandler clusterInitializationPhaseHandler;
+    private final List<DeviceRemovedHandler> deviceRemovedHandlers = new ArrayList<>();
+    private ServiceGroupIdentifier serviceGroupIdentifier;
+    private DeviceInfo deviceInfo;
+    private boolean terminationState = false;
+    private final MastershipChangeListener mastershipChangeListener;
 
+
+    public LifecycleServiceImpl(@Nonnull final MastershipChangeListener mastershipChangeListener) {
+        this.mastershipChangeListener = mastershipChangeListener;
+    }
+
+    @Override
+    public void makeDeviceSlave(final DeviceContext deviceContext) {
+
+        final DeviceInfo deviceInf = Objects.isNull(deviceInfo) ? deviceContext.getDeviceInfo() : deviceInfo;
+
+        Futures.addCallback(deviceContext.makeDeviceSlave(), new FutureCallback<RpcResult<SetRoleOutput>>() {
+            @Override
+            public void onSuccess(@Nullable RpcResult<SetRoleOutput> setRoleOutputRpcResult) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Role SLAVE was successfully propagated on device, node {}",
+                            deviceContext.getDeviceInfo().getLOGValue());
+                }
+                mastershipChangeListener.onSlaveRoleAcquired(deviceInf);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                LOG.warn("Was not able to set role SLAVE to device on node {} ",
+                        deviceContext.getDeviceInfo().getLOGValue());
+                mastershipChangeListener.onSlaveRoleNotAcquired(deviceInf);
+            }
+        });
+
+    }
 
     @Override
     public void instantiateServiceInstance() {
-        LOG.info("Starting clustering MASTER services for node {}", getDeviceInfo().getLOGValue());
 
-        if (!clusterInitializationPhaseHandler.onContextInstantiateService(null)) {
-            closeConnection();
+        LOG.info("Starting clustering MASTER services for node {}", deviceInfo.getLOGValue());
+        if (!clusterInitializationPhaseHandler.onContextInstantiateService(mastershipChangeListener)) {
+            mastershipChangeListener.onNotAbleToStartMastershipMandatory(deviceInfo, "Cannot initialize device.");
         }
     }
 
     @Override
     public ListenableFuture<Void> closeServiceInstance() {
-
-        // Chain all jobs that will stop our services
-        final List<ListenableFuture<Void>> futureList = new ArrayList<>();
-        futureList.add(statContext.stopClusterServices());
-        futureList.add(rpcContext.stopClusterServices());
-        futureList.add(deviceContext.stopClusterServices());
-
-        return Futures.transform(Futures.successfulAsList(futureList), new Function<List<Void>, Void>() {
-            @Nullable
-            @Override
-            public Void apply(@Nullable List<Void> input) {
-                LOG.debug("Closed clustering MASTER services for node {}", getDeviceInfo().getLOGValue());
-                return null;
-            }
-        });
+        return Futures.immediateFuture(null);
     }
 
     @Override
+    @Nonnull
     public ServiceGroupIdentifier getIdentifier() {
-        return getServiceIdentifier();
-    }
-
-    @Override
-    public CONTEXT_STATE getState() {
-        return this.state;
-    }
-
-    @Override
-    public ServiceGroupIdentifier getServiceIdentifier() {
-        return deviceContext.getServiceIdentifier();
-    }
-
-    @Override
-    public DeviceInfo getDeviceInfo() {
-        return deviceContext.getDeviceInfo();
+        return this.serviceGroupIdentifier;
     }
 
     @Override
     public void close() {
-        if (CONTEXT_STATE.TERMINATION.equals(getState())){
+        if (terminationState) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("LifecycleService is already in TERMINATION state.");
             }
         } else {
-            this.state = CONTEXT_STATE.TERMINATION;
+            this.terminationState = true;
 
             // We are closing, so cleanup all managers now
-            deviceRemovedHandlers.forEach(h -> h.onDeviceRemoved(getDeviceInfo()));
+            deviceRemovedHandlers.forEach(h -> h.onDeviceRemoved(deviceInfo));
 
             // If we are still registered and we are not already closing, then close the registration
             if (Objects.nonNull(registration)) {
                 try {
-                    LOG.debug("Closing clustering singleton services for node {}", getDeviceInfo().getLOGValue());
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Closing clustering services for node {}", deviceInfo.getLOGValue());
+                    }
                     registration.close();
-                } catch (Exception e) {
-                    LOG.debug("Failed to close clustering singleton services for node {} with exception: ",
-                            getDeviceInfo().getLOGValue(), e);
+                } catch (final Exception e) {
+                    LOG.warn("Failed to close clustering services for node {} with exception: ",
+                            deviceInfo.getLOGValue(), e);
                 }
             }
         }
     }
 
     @Override
-    public void registerService(final ClusterSingletonServiceProvider singletonServiceProvider) {
-        LOG.debug("Registered clustering singleton services for node {}", getDeviceInfo().getLOGValue());
+    public void registerService(@Nonnull final ClusterSingletonServiceProvider singletonServiceProvider,
+                                @Nonnull final DeviceContext deviceContext) {
 
-        // lifecycle service -> device context -> statistics context -> rpc context -> role context -> lifecycle service
         this.clusterInitializationPhaseHandler = deviceContext;
-        this.deviceContext.setLifecycleInitializationPhaseHandler(this.statContext);
-        this.statContext.setLifecycleInitializationPhaseHandler(this.rpcContext);
-        this.rpcContext.setLifecycleInitializationPhaseHandler(this);
-        //Set initial submit handler
-        this.statContext.setInitialSubmitHandler(this.deviceContext);
+        this.serviceGroupIdentifier = deviceContext.getServiceIdentifier();
+        this.deviceInfo = deviceContext.getDeviceInfo();
+        this.registration = Verify.verifyNotNull(retrySingletonServiceRegistration(singletonServiceProvider,2));
 
-        // Register cluster singleton service
-        try {
-            this.registration = Verify.verifyNotNull(singletonServiceProvider.registerClusterSingletonService(this));
-            LOG.info("Registered clustering singleton services for node {}", getDeviceInfo().getLOGValue());
-        } catch (Exception e) {
-            LOG.warn("Failed to register cluster singleton service for node {}, with exception: {}", getDeviceInfo(), e);
-            closeConnection();
-        }
+        LOG.info("Registered clustering services for node {}", deviceInfo.getLOGValue());
+
     }
 
     @Override
-    public void registerDeviceRemovedHandler(final DeviceRemovedHandler deviceRemovedHandler) {
+    public void registerDeviceRemovedHandler(@Nonnull final DeviceRemovedHandler deviceRemovedHandler) {
         if (!deviceRemovedHandlers.contains(deviceRemovedHandler)) {
             deviceRemovedHandlers.add(deviceRemovedHandler);
         }
     }
 
-    @Override
-    public void setDeviceContext(final DeviceContext deviceContext) {
-        this.deviceContext = deviceContext;
-    }
-
-    @Override
-    public void setRpcContext(final RpcContext rpcContext) {
-        this.rpcContext = rpcContext;
-    }
-
-    @Override
-    public void setStatContext(final StatisticsContext statContext) {
-        this.statContext = statContext;
-    }
-
-    @Override
-    public DeviceContext getDeviceContext() {
-        return this.deviceContext;
-    }
-
-    @Override
-    public void closeConnection() {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Closing connection for node {}.", getDeviceInfo().getLOGValue());
-        }
-
-        this.deviceContext.shutdownConnection();
-    }
-
-    private void fillDeviceFlowRegistry() {
-        final ListenableFuture<List<Optional<FlowCapableNode>>> deviceFlowRegistryFill = deviceContext.getDeviceFlowRegistry().fill();
-        Futures.addCallback(deviceFlowRegistryFill, new DeviceFlowRegistryCallback(deviceFlowRegistryFill));
-    }
-
-    @Override
-    public void setLifecycleInitializationPhaseHandler(final ClusterInitializationPhaseHandler handler) {
-        this.clusterInitializationPhaseHandler = handler;
-    }
-
-    @Override
-    public boolean onContextInstantiateService(final ConnectionContext connectionContext) {
-        if (CONNECTION_STATE.RIP.equals(connectionContext.getConnectionState())) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Connection to the device {} was interrupted.", getDeviceInfo().getLOGValue());
+    private ClusterSingletonServiceRegistration retrySingletonServiceRegistration(
+            @Nonnull final ClusterSingletonServiceProvider singletonServiceProvider,
+            int retryAttempt) {
+        if (retryAttempt > 0) {
+            try {
+                return singletonServiceProvider.registerClusterSingletonService(this);
+            } catch (RuntimeException e) {
+                LOG.warn("Runtime exception occurred while registering service for device {}.Retrying service " +
+                        "registration again", deviceInfo.getLOGValue());
             }
-
-            return false;
+            return retrySingletonServiceRegistration(singletonServiceProvider, retryAttempt--);
         }
-
-        fillDeviceFlowRegistry();
-        return true;
-    }
-
-    private class DeviceFlowRegistryCallback implements FutureCallback<List<Optional<FlowCapableNode>>> {
-        private final ListenableFuture<List<Optional<FlowCapableNode>>> deviceFlowRegistryFill;
-
-        DeviceFlowRegistryCallback(ListenableFuture<List<Optional<FlowCapableNode>>> deviceFlowRegistryFill) {
-            this.deviceFlowRegistryFill = deviceFlowRegistryFill;
-        }
-
-        @Override
-        public void onSuccess(@Nullable List<Optional<FlowCapableNode>> result) {
-            if (LOG.isDebugEnabled()) {
-                // Count all flows we read from datastore for debugging purposes.
-                // This number do not always represent how many flows were actually added
-                // to DeviceFlowRegistry, because of possible duplicates.
-                long flowCount = Optional.fromNullable(result).asSet().stream()
-                        .flatMap(Collection::stream)
-                        .filter(Objects::nonNull)
-                        .flatMap(flowCapableNodeOptional -> flowCapableNodeOptional.asSet().stream())
-                        .filter(Objects::nonNull)
-                        .filter(flowCapableNode -> Objects.nonNull(flowCapableNode.getTable()))
-                        .flatMap(flowCapableNode -> flowCapableNode.getTable().stream())
-                        .filter(Objects::nonNull)
-                        .filter(table -> Objects.nonNull(table.getFlow()))
-                        .flatMap(table -> table.getFlow().stream())
-                        .filter(Objects::nonNull)
-                        .count();
-
-                LOG.debug("Finished filling flow registry with {} flows for node: {}", flowCount, getDeviceInfo().getLOGValue());
-            }
-        }
-
-        @Override
-        public void onFailure(Throwable t) {
-            if (deviceFlowRegistryFill.isCancelled()) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Cancelled filling flow registry with flows for node: {}", getDeviceInfo().getLOGValue());
-                }
-            } else {
-                LOG.warn("Failed filling flow registry with flows for node: {} with exception: {}", getDeviceInfo().getLOGValue(), t);
-            }
-        }
+        return null;
     }
 }
