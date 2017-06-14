aadd747f3b7a3c7c5b6af4dccdd95410fc50fe05
miroslav.macko <miroslav.macko@pantheon.tech>
2017-06-14 14:06:30 +0200

    Fix port update
    - Update yang models. Add grouping common port with mask.
    - Use config mask in the PortConvertor and PortMessageSerializer.
    - Update unit tests.
    
    Resolves: bug 4747
    
    Change-Id: If558fea34f86c026ca45ba2853107c2cd82c7f50
    Signed-off-by: miroslav.macko <miroslav.macko@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/singlelayer/SingleLayerPortService.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/singlelayer/SingleLayerPortService.java
@@ -13,12 +13,12 @@ import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
 import org.opendaylight.openflowplugin.api.openflow.device.Xid;
 import org.opendaylight.openflowplugin.impl.services.AbstractSimpleService;
 import org.opendaylight.openflowplugin.impl.services.util.ServiceException;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.CommonPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.CommonPortWithMask;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.PortMessageBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 
-public final class SingleLayerPortService<O extends DataObject> extends AbstractSimpleService<CommonPort, O> {
+public final class SingleLayerPortService<O extends DataObject> extends AbstractSimpleService<CommonPortWithMask, O> {
 
     public SingleLayerPortService(
         final RequestContextStack requestContextStack,
@@ -28,7 +28,7 @@ public final class SingleLayerPortService<O extends DataObject> extends Abstract
     }
 
     @Override
-    protected OfHeader buildRequest(final Xid xid, final CommonPort input) throws ServiceException {
+    protected OfHeader buildRequest(final Xid xid, final CommonPortWithMask input) throws ServiceException {
         return new PortMessageBuilder(input)
                 .setVersion(getVersion())
                 .setXid(xid.getValue())
