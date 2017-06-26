3966f1e9437587094fcd21b736dbac8d227d2c6b
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-06-26 16:34:09 +0200

    Integrate OpenFlowJava into OpenFlowPlugin build
    - Integrate OpenFlowJava artifacts building into OpenFlowPlugin maven
    build
    - Merge openflowjava-artifacts with openflowplugin-artifacts
    - Remove odl-openflowjava-all feature, because it was unused and
    contained only odl-openflowjava-protocol
    - Change org.opendaylight.openflowjava groupId to
    org.opendaylight.openflowplugin.openflowjava
    
    Resolves: bug 8747
    
    Change-Id: I5cff2e93bcbe00aa7543a03c990aa4d61b0878b8
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- samples/simple-client/src/main/java/org/opendaylight/openflowjava/protocol/impl/clients/Scenario.java
+++ samples/simple-client/src/main/java/org/opendaylight/openflowjava/protocol/impl/clients/Scenario.java
@@ -26,25 +26,25 @@ public class Scenario {
 
     /**
      * Gets the value of the step property.
-     * 
+     *
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
      * This is why there is not a <CODE>set</CODE> method for the step property.
-     * 
+     *
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
      *    getStep().add(newItem);
      * </pre>
-     * 
-     * 
+     *
+     *
      * <p>
      * Objects of the following type(s) are allowed in the list
      * {@link Step }
-     * 
-     * 
+     *
+     *
      */
     public List<Step> getStep() {
         if (step == null) {
@@ -55,11 +55,11 @@ public class Scenario {
 
     /**
      * Gets the value of the name property.
-     * 
+     *
      * @return
      *     possible object is
      *     {@link String }
-     *     
+     *
      */
     public String getName() {
         return name;
@@ -67,11 +67,11 @@ public class Scenario {
 
     /**
      * Sets the value of the name property.
-     * 
+     *
      * @param value
      *     allowed object is
      *     {@link String }
-     *     
+     *
      */
     public void setName(String value) {
         this.name = value;
--- samples/simple-client/src/main/java/org/opendaylight/openflowjava/protocol/impl/clients/Step.java
+++ samples/simple-client/src/main/java/org/opendaylight/openflowjava/protocol/impl/clients/Step.java
@@ -82,13 +82,13 @@ public class Step {
 
     /**
      * Gets the value of the bytes property.
-     * 
+     *
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
      * This is why there is not a <CODE>set</CODE> method for the bytes property.
-     * 
+     *
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
