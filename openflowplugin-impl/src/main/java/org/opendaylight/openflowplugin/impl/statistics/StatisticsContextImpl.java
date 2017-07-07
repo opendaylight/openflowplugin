1f6a6e59d4c9388cff8d08caf5a30b3223284162
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-07-07 15:15:09 +0200

    Fix context chain initialization and SLAVE change
    - Merge ContextChain and LifecycleService
    - Let ClusterSingletonService handle stopping of mastership services in
    closeServiceInstance method
    - Store cluster singleton initialization future and terminate it in case
    of failure
    - Handle initialization chain order in ContextChain instead of storing
    each next phase in contexts
    - Remove SLAVE task from scheduler in case we acquired SLAVE role
    already
    - Prevent adding exactly same auxiliary connection twice to list of
    auxiliary connections in ContextChain
    - Do closing/unregistration from cluster singleton service in separate thread
    instead of initialization/registration to cluster singleton service
    - When SLAVE role is obtained from cluster singleton service, notify
    MastershipChangeListener about it
    - Log exceptions from handshake as warning instead of error (as only
    problem is that device gets disconnected and reconnected, what is not
    critical)
    - Use ServiceGroupIndetifier only in ContextChain and not in other
    contexts
    
    See also: bug 8805
    
    Change-Id: If14da19899087018c5e6cc42d195af43c1e89bfd
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
@@ -9,7 +9,6 @@
 package org.opendaylight.openflowplugin.impl.statistics;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterators;
@@ -28,7 +27,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.GuardedBy;
 import org.opendaylight.mdsal.common.api.TransactionChainClosedException;
-import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
 import org.opendaylight.openflowplugin.api.ConnectionException;
 import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
@@ -77,7 +75,6 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
 
     private volatile boolean schedulingEnabled;
     private volatile ContextState state;
-    private ClusterInitializationPhaseHandler clusterInitializationPhaseHandler;
     private ClusterInitializationPhaseHandler initialSubmitHandler;
 
     private volatile ListenableFuture<Boolean> lastDataGathering;
@@ -308,30 +305,16 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
     }
 
     @Override
-    public ServiceGroupIdentifier getServiceIdentifier() {
-        return this.deviceInfo.getServiceIdentifier();
-    }
-
-    @Override
     public DeviceInfo getDeviceInfo() {
         return this.deviceInfo;
     }
 
     @Override
     public ListenableFuture<Void> stopClusterServices() {
-        if (ContextState.TERMINATION.equals(this.state)) {
-            return Futures.immediateCancelledFuture();
-        }
-
-        return Futures.transform(Futures.immediateFuture(null), new Function<Object, Void>() {
-            @Nullable
-            @Override
-            public Void apply(@Nullable Object input) {
-                schedulingEnabled = false;
-                stopGatheringData();
-                return null;
-            }
-        });
+        LOG.info("Stopping statistics context cluster services for node {}", deviceInfo.getLOGValue());
+        schedulingEnabled = false;
+        stopGatheringData();
+        return Futures.immediateFuture(null);
     }
 
     @Override
@@ -355,11 +338,6 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         if (Objects.nonNull(pollTimeout) && !pollTimeout.isExpired()) {
             pollTimeout.cancel();
         }
-    }
-
-    @Override
-    public void setLifecycleInitializationPhaseHandler(final ClusterInitializationPhaseHandler handler) {
-        this.clusterInitializationPhaseHandler = handler;
     }
 
     @Override
@@ -401,7 +379,7 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
             }
         });
 
-        return this.clusterInitializationPhaseHandler.onContextInstantiateService(mastershipChangeListener);
+        return true;
     }
 
     @Override
