0f2c8e27ac5f16fa6aea29ac4e89941e13521c78
Gobinath <gobinath@ericsson.com>
2017-07-10 14:45:25 +0530

    Reconciliation Framework changes
    Changes related to the Reconciliation Framework are done in this patch.
    The APIs needed to be implemented by the services registering to the
    Reconciliation framework are present in the interface
    IReconciliationTaskFactory. The APIs to register the service to
    Reconciliation framework and the APIs to start the reconciliation /
    cancellation of the ongoing reconciliation of a node are present in
    IReconciliationManager.
    
    Change-Id: I8bd1845db8b86f4a4314977e05c6b699592f5658
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/reconciliation-framework/src/main/java/org/opendaylight/openflowplugin/applications/reconciliation/ReconciliationTaskFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.reconciliation;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.reconciliation.framework.config.rev170712.IntentType;
+
+import java.util.concurrent.Future;
+
+public interface ReconciliationTaskFactory extends AutoCloseable {
+    Future<NodeId> startReconcileTask(NodeId nodeId);
+
+    Future<NodeId> cancelReconcileTask(NodeId nodeId);
+
+    int getPriority();
+
+    String getServiceName();
+
+    IntentType getIntent();
+
+}
---
+++ applications/reconciliation-framework/src/main/java/org/opendaylight/openflowplugin/applications/reconciliation/cli/GetRegisteredServices.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.reconciliation.cli;
+
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationManager;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationTaskFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+@Command(scope = "reconciliation", name = "getRegisteredServices", description = "displaying services registered to "
+        + "Reconciliation Framework")
+public class GetRegisteredServices extends OsgiCommandSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GetRegisteredServices.class);
+    public static final String CLI_FORMAT = "%d %-20s ";
+
+    private ReconciliationManager reconciliationManager;
+
+    public void setReconciliationmgr(ReconciliationManager reconciliationManager) {
+        this.reconciliationManager = reconciliationManager;
+    }
+
+    @Override 
+    protected Object doExecute() throws Exception {
+        LOG.debug("Executing getRegisteredServices to Reconciliation Framework command");
+        for (Map.Entry<Integer, List<ReconciliationTaskFactory>> registeredService : reconciliationManager
+                .getRegisteredServices().entrySet()) {
+            List<ReconciliationTaskFactory> services = registeredService.getValue();
+            for (ReconciliationTaskFactory service : services) {
+                session.getConsole().println(
+                        String.format(CLI_FORMAT, service.getPriority(), service.getServiceName()));
+            }
+        }
+        return null;
+    }
+
+}
+
+
