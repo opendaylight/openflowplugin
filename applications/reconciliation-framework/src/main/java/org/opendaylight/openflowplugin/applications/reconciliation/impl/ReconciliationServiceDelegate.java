36f4249f330166f16a54b00f173438de531b05e3
Gobinath <gobinath@ericsson.com>
2017-07-10 14:45:25 +0530

    Bug 8902 - Reconciliation Framework changes
    Changes related to the Reconciliation Framework are done in this patch.
    The APIs needed to be implemented by the services registering to the
    Reconciliation framework are present in the interface
    IReconciliationTaskFactory. The APIs to register the service to
    Reconciliation framework and the APIs to start the reconciliation /
    cancellation of the ongoing reconciliation of a node are present in
    IReconciliationManager.
    
    Change-Id: I8bd1845db8b86f4a4314977e05c6b699592f5658
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/reconciliation-framework/src/main/java/org/opendaylight/openflowplugin/applications/reconciliation/impl/ReconciliationServiceDelegate.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.reconciliation.impl;
+
+import org.opendaylight.openflowplugin.applications.reconciliation.NotificationRegistration;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationManager;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationNotificationListener;
+
+public class ReconciliationServiceDelegate implements NotificationRegistration {
+
+    private ReconciliationManager reconciliationManager;
+    private ReconciliationNotificationListener reconciliationNotificationListener;
+
+    public ReconciliationServiceDelegate(ReconciliationManager reconciliationManager,
+            ReconciliationNotificationListener reconciliationNotificationListener) {
+        this.reconciliationManager = reconciliationManager;
+        this.reconciliationNotificationListener = reconciliationNotificationListener;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.reconciliationManager.unregisterService(reconciliationNotificationListener);
+    }
+}
