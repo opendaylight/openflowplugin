ae607e0fd74f21aaf231de2039c6a31461f6e42f
Gobinath <gobinath@ericsson.com>
2017-07-10 14:45:25 +0530

    Bug 8902 - Reconciliation Framework changes
    Changes related to the Reconciliation Framework are done in this patch.
    The APIs needed to be implemented by the services registering to the
    Reconciliation framework are present in the interface
    IReconciliationTaskFactory. The APIs to register the service to
    Reconciliation framework and the APIs to start the reconciliation /
    cancellation of the ongoing reconciliation of a node are present in
    IReconciliationManager.
    
    Change-Id: I8bd1845db8b86f4a4314977e05c6b699592f5658
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>
    Signed-off-by: Gobinath <gobinath@ericsson.com>

---
+++ applications/reconciliation-framework/src/main/java/org/opendaylight/openflowplugin/applications/reconciliation/ReconciliationManager.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.reconciliation;
+
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
+
+/*
+ * Provider to register the service to reconciliation framework
+ *
+ */
+public interface ReconciliationManager {
+    /*
+     * Application who are interested in reconciliation should use this API to
+     * register their services to the Reconciliation Framework
+     *
+     * @param object - the reference to the ReconciliationNotificationListener
+     */
+    void registerService(ReconciliationNotificationListener service);
+
+    /*
+     * API to unregister the service from Reconciliation Framework
+     */
+    void unregisterService(ReconciliationNotificationListener service);
+    /*
+     * Plugin will use this api to stop reconciliation when dpn disconnects
+     *
+     * @param nodeId - Node InstanceIdentifier
+     */
+    void haltReconciliation(DeviceInfo node);
+
+    /*
+     * API exposed by RF for get list of registered services
+     *
+     * @return the Map containing registered services with priority as Key
+     */
+    Map<Integer, List<ReconciliationNotificationListener>> getRegisteredServices();
+
+}
---
+++ applications/reconciliation-framework/src/main/java/org/opendaylight/openflowplugin/applications/reconciliation/cli/GetRegisteredServices.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.reconciliation.cli;
+
+import com.google.common.base.Preconditions;
+import java.util.List;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationManager;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationNotificationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Command(scope = "reconciliation", name = "getRegisteredServices", description = "displaying services registered to "
+        + "Reconciliation Framework")
+/*
+ * CLI to display the service priority, service name and service status TODO
+ * service status
+ */
+public class GetRegisteredServices extends OsgiCommandSupport {
+    private static final Logger LOG = LoggerFactory.getLogger(GetRegisteredServices.class);
+    public static final String CLI_FORMAT = "%d %-20s ";
+
+    private ReconciliationManager reconciliationManager;
+
+    public void setReconciliationmgr(ReconciliationManager reconciliationManager) {
+        this.reconciliationManager = Preconditions.checkNotNull(reconciliationManager, "ReconciliationManager can not be null!");
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        LOG.debug("Executing getRegisteredServices to Reconciliation Framework command");
+        if (reconciliationManager.getRegisteredServices().isEmpty()) {
+            session.getConsole().println("No Services have registered to Reconciliation Framework");
+        } else {
+            for (List<ReconciliationNotificationListener> services : reconciliationManager.getRegisteredServices()
+                    .values()) {
+                for (ReconciliationNotificationListener service : services) {
+                    session.getConsole().println(String.format(CLI_FORMAT, service.getPriority(), service.getName()));
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
