f942eb374f97cfbe835fcee7197044f3310ebb89
Jozef Bacigal <jozef.bacigal@pantheon.tech>
2017-07-10 10:42:25 +0200

    Mastership service provider API
    - create new API for mastership listener
    - rename MastershipChangeListener to ContextChainMastershipWatcher
    
    Change-Id: I45e919494039180df895602b6536af37f679cb58
    Signed-off-by: Jozef Bacigal <jozef.bacigal@pantheon.tech>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/handlers/ClusterInitializationPhaseHandler.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/device/handlers/ClusterInitializationPhaseHandler.java
@@ -7,7 +7,7 @@
  */
 package org.opendaylight.openflowplugin.api.openflow.device.handlers;
 
-import org.opendaylight.openflowplugin.api.openflow.lifecycle.MastershipChangeListener;
+import org.opendaylight.openflowplugin.api.openflow.lifecycle.ContextChainMastershipWatcher;
 
 /**
  * Interface handles MASTER initialization on ownership change.
@@ -16,9 +16,9 @@ public interface ClusterInitializationPhaseHandler {
 
     /**
      * Method for initialization cycle between contexts.
-     * @param mastershipChangeListener - listener if something goes wrong with initialization
+     * @param contextChainMastershipWatcher - listener if something goes wrong with initialization
      */
-    boolean onContextInstantiateService(MastershipChangeListener mastershipChangeListener);
+    boolean onContextInstantiateService(ContextChainMastershipWatcher contextChainMastershipWatcher);
 
     /**
      * Method for initial submit transaction after successful initial gathering.
