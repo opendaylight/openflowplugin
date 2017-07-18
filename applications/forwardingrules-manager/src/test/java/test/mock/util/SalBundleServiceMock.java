c4db3a0a5c86d7371c05af108f5df9fe191ed224
eeiillu <suja.t@ericsson.com>
2017-07-18 10:43:00 +0530

    Review for bundle based reconciliation
    Change-Id: Ida09badca9a16619fec6e6f293042956ce8ec4e4
    Signed-off-by: eeiillu <suja.t@ericsson.com>

---
+++ applications/forwardingrules-manager/src/test/java/test/mock/util/SalBundleServiceMock.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package test.mock.util;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.AddBundleMessagesInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.ControlBundleInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.bundle.service.rev170124.SalBundleService;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+public class SalBundleServiceMock implements SalBundleService {
+
+    private List<ControlBundleInput> controlBundleInput = new ArrayList<>();
+    private List<AddBundleMessagesInput>  addBundleMessagesInput = new ArrayList<>();
+
+
+    @Override
+    public Future<RpcResult<java.lang.Void>> controlBundle(ControlBundleInput input) {
+        getControlBundleInput().add(input);
+        return null;
+    }
+
+    @Override
+    public Future<RpcResult<java.lang.Void>> addBundleMessages(AddBundleMessagesInput input) {
+        getAddBundleMessagesInput().add(input);
+        return null;
+    }
+
+    public List<ControlBundleInput> getControlBundleInput() {
+        return controlBundleInput;
+    }
+
+    public List<AddBundleMessagesInput> getAddBundleMessagesInput() {
+        return addBundleMessagesInput;
+    }
+
+
+}
