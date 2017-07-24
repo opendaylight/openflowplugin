db907f4b82905b47dc55560cec457e388227a32a
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-07-24 17:44:48 +0200

    Update unit tests
    Add unit tests for:
    - Arp target transport address serializer
    - Packet in message deserializer
    - All serializers for multipart request messages
    - All serializers for table feature properties and match fields in table
    features
    - Configuration service and OpenFlowProviderConfigImpl
    - Device initializers (OF10 and OF13)
    - Single-layer direct statistics
    
    Remove custom comparators for sorting of actions and instructions and
    use OrderComparator.build() instead. Also, add missing sorting of table
    feature instructions and actions properties.
    
    See also: bug 8607
    
    Change-Id: I83e0ad9a3469c80f4fa45df4de78c1e0fd53954b
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/initialization/OF13DeviceInitializerTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.device.initialization;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.Future;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.openflowplugin.api.openflow.connection.ConnectionContext;
+import org.opendaylight.openflowplugin.api.openflow.connection.OutboundQueueProvider;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
+import org.opendaylight.openflowplugin.api.openflow.device.MessageTranslator;
+import org.opendaylight.openflowplugin.api.openflow.device.RequestContext;
+import org.opendaylight.openflowplugin.api.openflow.device.TranslatorLibrary;
+import org.opendaylight.openflowplugin.api.openflow.device.Xid;
+import org.opendaylight.openflowplugin.api.openflow.statistics.ofpspecific.MessageSpy;
+import org.opendaylight.openflowplugin.impl.datastore.MultipartWriterProvider;
+import org.opendaylight.openflowplugin.impl.datastore.multipart.AbstractMultipartWriter;
+import org.opendaylight.openflowplugin.impl.util.DeviceStateUtil;
+import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.ConvertorExecutor;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.multipart.reply.multipart.reply.body.MultipartReplyDescBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.multipart.types.rev170112.MultipartReplyBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.Capabilities;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FeaturesReply;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReplyMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.multipart.reply.multipart.reply.body.MultipartReplyDescCaseBuilder;
+import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OF13DeviceInitializerTest {
+    @Mock
+    private DeviceContext deviceContext;
+    @Mock
+    private MultipartWriterProvider multipartWriterProvider;
+    @Mock
+    private AbstractMultipartWriter abstractMultipartWriter;
+    @Mock
+    private ConvertorExecutor convertorExecutor;
+    @Mock
+    private ConnectionContext connectionContext;
+    @Mock
+    private FeaturesReply featuresReply;
+    @Mock
+    private Capabilities capabilities;
+    @Mock
+    private DeviceInfo deviceInfo;
+    @Mock
+    private DeviceState deviceState;
+    @Mock
+    private MessageSpy messageSpy;
+    @Mock
+    private TranslatorLibrary translatorLibrary;
+    @Mock
+    private MessageTranslator messageTranslator;
+    @Mock
+    private RequestContext requestContext;
+    @Mock
+    private OutboundQueueProvider outboundQueueProvider;
+    private AbstractDeviceInitializer deviceInitializer;
+
+    @Before
+    public void setUp() throws Exception {
+        final KeyedInstanceIdentifier<Node, NodeKey> nodeInstanceIdentifier = DeviceStateUtil
+                .createNodeInstanceIdentifier(new NodeId("openflow:1"));
+
+        deviceInitializer = new OF13DeviceInitializer();
+        when(multipartWriterProvider.lookup(any())).thenReturn(Optional
+                .of(abstractMultipartWriter));
+        when(featuresReply.getCapabilities()).thenReturn(capabilities);
+        when(connectionContext.getFeatures()).thenReturn(featuresReply);
+        when(connectionContext.getOutboundQueueProvider()).thenReturn(outboundQueueProvider);
+        when(deviceContext.getDeviceState()).thenReturn(deviceState);
+        when(deviceInfo.getNodeInstanceIdentifier()).thenReturn(nodeInstanceIdentifier);
+        when(deviceContext.getDeviceInfo()).thenReturn(deviceInfo);
+        when(deviceContext.getMessageSpy()).thenReturn(messageSpy);
+        when(translatorLibrary.lookupTranslator(any())).thenReturn(messageTranslator);
+        when(deviceContext.oook()).thenReturn(translatorLibrary);
+        when(requestContext.getXid()).thenReturn(new Xid(42L));
+        when(deviceContext.createRequestContext()).thenReturn(requestContext);
+        when(deviceContext.getPrimaryConnectionContext()).thenReturn(connectionContext);
+    }
+
+    @Test
+    public void initializeSingleLayer() throws Exception {
+        when(deviceContext.canUseSingleLayerSerialization()).thenReturn(true);
+        when(requestContext.getFuture()).thenReturn(RpcResultBuilder
+                .success(Collections.singletonList(new MultipartReplyBuilder()
+                        .setMultipartReplyBody(new MultipartReplyDescBuilder().build())
+                        .build()))
+                .buildFuture());
+
+        final Future<Void> initialize = deviceInitializer.initialize(deviceContext,
+                true,
+                false,
+                multipartWriterProvider,
+                convertorExecutor);
+
+        initialize.get();
+        verify(messageSpy, times(5)).spyMessage(eq(MultipartType.class), any());
+        verify(requestContext, times(5)).getFuture();
+        verify(featuresReply).getCapabilities();
+        verify(deviceContext).writeToTransaction(eq(LogicalDatastoreType.OPERATIONAL), any(), any());
+    }
+
+    @Test
+    public void initializeMultiLayer() throws Exception {
+        when(deviceContext.canUseSingleLayerSerialization()).thenReturn(false);
+        when(requestContext.getFuture()).thenReturn(RpcResultBuilder
+                .success(Collections.singletonList(new MultipartReplyMessageBuilder()
+                        .setType(MultipartType.OFPMPDESC)
+                        .setMultipartReplyBody(new MultipartReplyDescCaseBuilder()
+                                .setMultipartReplyDesc(new org.opendaylight.yang.gen.v1.urn.opendaylight.openflow
+                                        .protocol.rev130731.multipart.reply.multipart.reply.body.multipart.reply
+                                        .desc._case.MultipartReplyDescBuilder()
+                                        .build())
+                                .build())
+                        .build()))
+
+                .buildFuture());
+        final Future<Void> initialize = deviceInitializer.initialize(deviceContext,
+                true,
+                false,
+                multipartWriterProvider,
+                convertorExecutor);
+
+        initialize.get();
+        verify(messageSpy, times(5)).spyMessage(eq(MultipartType.class), any());
+        verify(requestContext, times(5)).getFuture();
+        verify(featuresReply).getCapabilities();
+        verify(deviceContext).writeToTransaction(eq(LogicalDatastoreType.OPERATIONAL), any(), any());
+    }
+}
---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/MultipartRequestPortDescSerializerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.protocol.serialization.multipart;
+
+import static org.junit.Assert.assertEquals;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.keys.MessageTypeKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.impl.protocol.serialization.AbstractSerializerTest;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.multipart.request.multipart.request.body.MultipartRequestPortDesc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.port.rev130925.multipart.request.multipart.request.body.MultipartRequestPortDescBuilder;
+
+public class MultipartRequestPortDescSerializerTest extends AbstractSerializerTest {
+    private static final MultipartRequestPortDesc BODY = new MultipartRequestPortDescBuilder()
+            .build();
+
+    private MultipartRequestPortDescSerializer serializer;
+
+    @Override
+    protected void init() {
+        serializer = getRegistry().getSerializer(new MessageTypeKey<>(EncodeConstants.OF13_VERSION_ID,
+                MultipartRequestPortDesc.class));
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        final ByteBuf out = UnpooledByteBufAllocator.DEFAULT.buffer();
+        serializer.serialize(BODY, out);
+        assertEquals(out.readableBytes(), 0);
+    }
+
+}
