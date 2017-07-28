02ab846d5306a5133f79d2d94dad1f4994f47455
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-07-28 15:35:33 +0200

    Guard lifecycle of contexts
    - Add GuardedContext wrapper for OFPContext that will guard each
    phase of context lifecycle (new instance, instantiateServiceInstance,
    closeServiceInstance, close)
    - Move setting of SLAVE/MASTER role on device to context chain so it
    will be called only after each context is succesfully initialized
    - Cleanup statistics gathering logic (remove context dependency on
    manager, add separate service just for periodic statistics polling)
    
    Resolves: bug 8913
    
    Change-Id: I6c054575c9f93f03e45dcc23feefa0e7dae6fa88
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsManagerImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsManagerImpl.java
@@ -8,32 +8,17 @@
 
 package org.opendaylight.openflowplugin.impl.statistics;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterators;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import io.netty.util.HashedWheelTimer;
-import io.netty.util.Timeout;
-import io.netty.util.TimerTask;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
-import org.opendaylight.openflowplugin.api.ConnectionException;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
-import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
 import org.opendaylight.openflowplugin.api.openflow.lifecycle.ReconciliationFrameworkRegistrar;
-import org.opendaylight.openflowplugin.api.openflow.rpc.ItemLifeCycleSource;
 import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsContext;
 import org.opendaylight.openflowplugin.api.openflow.statistics.StatisticsManager;
 import org.opendaylight.openflowplugin.impl.datastore.MultipartWriterProvider;
@@ -56,253 +41,56 @@ public class StatisticsManagerImpl implements StatisticsManager, StatisticsManag
 
     private static final Logger LOG = LoggerFactory.getLogger(StatisticsManagerImpl.class);
 
-    private static final long DEFAULT_STATS_TIMEOUT_SEC = 50L;
-    @Nonnull
     private final OpenflowProviderConfig config;
     private final ConvertorExecutor converterExecutor;
-
     private final ConcurrentMap<DeviceInfo, StatisticsContext> contexts = new ConcurrentHashMap<>();
-
-    private long currentTimerDelay;
-    private StatisticsWorkMode workMode = StatisticsWorkMode.COLLECTALL;
     private final Semaphore workModeGuard = new Semaphore(1, true);
-    private BindingAwareBroker.RpcRegistration<StatisticsManagerControlService> controlServiceRegistration;
-    private final HashedWheelTimer hashedWheelTimer;
-    private boolean istStatisticsFullyDisabled;
+    private final BindingAwareBroker.RpcRegistration<StatisticsManagerControlService> controlServiceRegistration;
+    private StatisticsWorkMode workMode = StatisticsWorkMode.COLLECTALL;
     private ReconciliationFrameworkRegistrar reconciliationFrameworkRegistrar;
+    private boolean isStatisticsFullyDisabled;
 
     public StatisticsManagerImpl(@Nonnull final OpenflowProviderConfig config,
                                  @Nonnull final RpcProviderRegistry rpcProviderRegistry,
-                                 final HashedWheelTimer hashedWheelTimer,
                                  final ConvertorExecutor convertorExecutor) {
         this.config = config;
-        currentTimerDelay = config.getBasicTimerDelay().getValue();
         this.converterExecutor = convertorExecutor;
         this.controlServiceRegistration = Preconditions.checkNotNull(rpcProviderRegistry
                 .addRpcImplementation(StatisticsManagerControlService.class, this));
-
-        this.hashedWheelTimer = hashedWheelTimer;
-    }
-
-    @VisibleForTesting
-    void pollStatistics(final DeviceState deviceState,
-                        final StatisticsContext statisticsContext,
-                        final TimeCounter timeCounter,
-                        final DeviceInfo deviceInfo) {
-
-        if (!statisticsContext.isSchedulingEnabled()) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Disabled statistics scheduling for device: {}", deviceInfo.getNodeId().getValue());
-            }
-            return;
-        }
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("POLLING ALL STATISTICS for device: {}", deviceInfo.getNodeId());
-        }
-
-        timeCounter.markStart();
-        final ListenableFuture<Boolean> deviceStatisticsCollectionFuture = statisticsContext.gatherDynamicData();
-        Futures.addCallback(deviceStatisticsCollectionFuture, new FutureCallback<Boolean>() {
-            @Override
-            public void onSuccess(final Boolean o) {
-                timeCounter.addTimeMark();
-                calculateTimerDelay(timeCounter);
-                scheduleNextPolling(deviceState, deviceInfo, statisticsContext, timeCounter);
-            }
-
-            @Override
-            public void onFailure(@Nonnull final Throwable throwable) {
-                timeCounter.addTimeMark();
-                calculateTimerDelay(timeCounter);
-
-                if (throwable instanceof ConnectionException) {
-                    // ConnectionException is raised by StatisticsContextImpl class when the connections
-                    // move to RIP state. In this particular case, there is no need to reschedule
-                    // because this statistics manager should be closed soon
-                    LOG.warn("Device {} is no more connected, stopping the statistics collection. Reason: {}",
-                            deviceInfo, throwable.getMessage());
-                    stopScheduling(deviceInfo);
-                } else if (throwable instanceof CancellationException) {
-                    LOG.info("Statistics gathering for device {} was cancelled.", deviceInfo);
-                } else {
-                    LOG.warn("Unexpected error occurred during statistics collection for device {}, rescheduling " +
-                            "statistics collections", deviceInfo, throwable);
-
-                    scheduleNextPolling(deviceState, deviceInfo, statisticsContext, timeCounter);
-                }
-            }
-        });
-
-        final long averageTime = TimeUnit.MILLISECONDS.toSeconds(timeCounter.getAverageTimeBetweenMarks());
-        final long statsTimeoutSec = averageTime > 0 ? 3 * averageTime : DEFAULT_STATS_TIMEOUT_SEC;
-        final TimerTask timerTask = timeout -> {
-            if (!deviceStatisticsCollectionFuture.isDone()) {
-                LOG.info("Statistics collection for node {} still in progress even after {} secs", deviceInfo, statsTimeoutSec);
-                deviceStatisticsCollectionFuture.cancel(true);
-            }
-        };
-
-        hashedWheelTimer.newTimeout(timerTask, statsTimeoutSec, TimeUnit.SECONDS);
-    }
-
-    private void scheduleNextPolling(final DeviceState deviceState,
-                                     final DeviceInfo deviceInfo,
-                                     final StatisticsContext statisticsContext,
-                                     final TimeCounter timeCounter) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("SCHEDULING NEXT STATISTICS POLLING for device: {}", deviceInfo.getNodeId());
-        }
-
-        if (isStatisticsEnabled()) {
-            final Timeout pollTimeout = hashedWheelTimer.newTimeout(
-                    timeout -> pollStatistics(
-                            deviceState,
-                            statisticsContext,
-                            timeCounter,
-                            deviceInfo),
-                    currentTimerDelay,
-                    TimeUnit.MILLISECONDS);
-
-            statisticsContext.setPollTimeout(pollTimeout);
-        }
-    }
-
-    private boolean isStatisticsEnabled() {
-        return !istStatisticsFullyDisabled && config.isIsStatisticsPollingOn();
-    }
-
-    @VisibleForTesting
-    void calculateTimerDelay(final TimeCounter timeCounter) {
-        final long averageStatisticsGatheringTime = timeCounter.getAverageTimeBetweenMarks();
-        if (averageStatisticsGatheringTime > currentTimerDelay) {
-            currentTimerDelay *= 2;
-            if (currentTimerDelay > config.getMaximumTimerDelay().getValue()) {
-                currentTimerDelay = config.getMaximumTimerDelay().getValue();
-            }
-        } else {
-            if (currentTimerDelay > config.getBasicTimerDelay().getValue()) {
-                currentTimerDelay /= 2;
-            } else {
-                currentTimerDelay = config.getBasicTimerDelay().getValue();
-            }
-        }
-    }
-
-    @VisibleForTesting
-    long getCurrentTimerDelay() {
-        return currentTimerDelay;
     }
 
     @Override
     public Future<RpcResult<GetStatisticsWorkModeOutput>> getStatisticsWorkMode() {
-        final GetStatisticsWorkModeOutputBuilder smModeOutputBld = new GetStatisticsWorkModeOutputBuilder();
-        smModeOutputBld.setMode(workMode);
-        return RpcResultBuilder.success(smModeOutputBld.build()).buildFuture();
+        return RpcResultBuilder.success(new GetStatisticsWorkModeOutputBuilder()
+                .setMode(workMode)
+                .build()).buildFuture();
     }
 
     @Override
     public Future<RpcResult<Void>> changeStatisticsWorkMode(ChangeStatisticsWorkModeInput input) {
-        final Future<RpcResult<Void>> result;
-        // acquire exclusive access
         if (workModeGuard.tryAcquire()) {
             final StatisticsWorkMode targetWorkMode = input.getMode();
-            if (!workMode.equals(targetWorkMode)) {
-                istStatisticsFullyDisabled = StatisticsWorkMode.FULLYDISABLED.equals(targetWorkMode);
+            isStatisticsFullyDisabled = StatisticsWorkMode.FULLYDISABLED.equals(targetWorkMode);
 
-                // iterate through stats-ctx: propagate mode
-                for (Map.Entry<DeviceInfo, StatisticsContext> entry : contexts.entrySet()) {
-                    final DeviceInfo deviceInfo = entry.getKey();
-                    final StatisticsContext statisticsContext = entry.getValue();
-                    final DeviceContext deviceContext = statisticsContext.gainDeviceContext();
-                    switch (targetWorkMode) {
-                        case COLLECTALL:
-                            scheduleNextPolling(statisticsContext.gainDeviceState(), deviceInfo, statisticsContext, new TimeCounter());
-                            for (final ItemLifeCycleSource lifeCycleSource : deviceContext.getItemLifeCycleSourceRegistry().getLifeCycleSources()) {
-                                lifeCycleSource.setItemLifecycleListener(null);
-                            }
-                            break;
-                        case FULLYDISABLED:
-                            statisticsContext.stopGatheringData();
-                            for (final ItemLifeCycleSource lifeCycleSource : deviceContext.getItemLifeCycleSourceRegistry().getLifeCycleSources()) {
-                                lifeCycleSource.setItemLifecycleListener(statisticsContext.getItemLifeCycleListener());
-                            }
-                            break;
-                        default:
-                            LOG.warn("Statistics work mode not supported: {}", targetWorkMode);
-                    }
+            contexts.values().forEach(context -> {
+                switch (targetWorkMode) {
+                    case COLLECTALL:
+                        context.enableGathering();
+                    case FULLYDISABLED:
+                        context.disableGathering();
+                        break;
+                    default:
+                        LOG.warn("Statistics work mode not supported: {}", targetWorkMode);
                 }
-                workMode = targetWorkMode;
-            }
+            });
+
             workModeGuard.release();
-            result = RpcResultBuilder.<Void>success().buildFuture();
-        } else {
-            result = RpcResultBuilder.<Void>failed()
-                    .withError(RpcError.ErrorType.APPLICATION, "mode change already in progress")
-                    .buildFuture();
-        }
-        return result;
-    }
-
-    @Override
-    public void startScheduling(final DeviceInfo deviceInfo) {
-        if (!isStatisticsEnabled()) {
-            LOG.info("Statistics are shutdown for device: {}", deviceInfo.getNodeId());
-            return;
+            return RpcResultBuilder.<Void>success().buildFuture();
         }
 
-        final StatisticsContext statisticsContext = contexts.get(deviceInfo);
-
-        if (statisticsContext == null) {
-            LOG.warn("Statistics context not found for device: {}", deviceInfo.getNodeId());
-            return;
-        }
-
-        if (statisticsContext.isSchedulingEnabled()) {
-            LOG.debug("Statistics scheduling is already enabled for device: {}", deviceInfo.getNodeId());
-            return;
-        }
-
-        LOG.info("Scheduling statistics poll for device: {}", deviceInfo.getNodeId());
-
-        statisticsContext.setSchedulingEnabled(true);
-        scheduleNextPolling(
-                statisticsContext.gainDeviceState(),
-                deviceInfo,
-                statisticsContext,
-                new TimeCounter()
-        );
-    }
-
-    @Override
-    public void stopScheduling(final DeviceInfo deviceInfo) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Stopping statistics scheduling for device: {}", deviceInfo.getNodeId());
-        }
-
-        final StatisticsContext statisticsContext = contexts.get(deviceInfo);
-
-        if (statisticsContext == null) {
-            LOG.warn("Statistics context not found for device: {}", deviceInfo.getNodeId());
-            return;
-        }
-
-        statisticsContext.setSchedulingEnabled(false);
-    }
-
-    @Override
-    public void close() {
-        istStatisticsFullyDisabled = true;
-
-        if (controlServiceRegistration != null) {
-            controlServiceRegistration.close();
-            controlServiceRegistration = null;
-        }
-
-        for (final Iterator<StatisticsContext> iterator = Iterators.consumingIterator(contexts.values().iterator());
-             iterator.hasNext();) {
-            iterator.next().close();
-        }
+        return RpcResultBuilder.<Void>failed()
+                .withError(RpcError.ErrorType.APPLICATION, "mode change already in progress")
+                .buildFuture();
     }
 
     @Override
@@ -312,37 +100,48 @@ public class StatisticsManagerImpl implements StatisticsManager, StatisticsManag
 
     @Override
     public StatisticsContext createContext(@Nonnull final DeviceContext deviceContext) {
-
         final MultipartWriterProvider statisticsWriterProvider = MultipartWriterProviderFactory
                 .createDefaultProvider(deviceContext);
 
         final StatisticsContext statisticsContext =
                 deviceContext.canUseSingleLayerSerialization() ?
                         new StatisticsContextImpl<MultipartReply>(
-                                isStatisticsEnabled(),
                                 deviceContext,
                                 converterExecutor,
-                                this,
                                 statisticsWriterProvider,
-                                reconciliationFrameworkRegistrar.isReconciliationFrameworkRegistered()) :
+                                !isStatisticsFullyDisabled && config.isIsStatisticsPollingOn(),
+                                reconciliationFrameworkRegistrar.isReconciliationFrameworkRegistered(),
+                                config.getBasicTimerDelay().getValue(),
+                                config.getMaximumTimerDelay().getValue()) :
                         new StatisticsContextImpl<org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
                                 .MultipartReply>(
-                                isStatisticsEnabled(),
                                 deviceContext,
                                 converterExecutor,
-                                this,
                                 statisticsWriterProvider,
-                                reconciliationFrameworkRegistrar.isReconciliationFrameworkRegistered());
-        contexts.putIfAbsent(deviceContext.getDeviceInfo(), statisticsContext);
+                                !isStatisticsFullyDisabled && config.isIsStatisticsPollingOn(),
+                                reconciliationFrameworkRegistrar.isReconciliationFrameworkRegistered(),
+                                config.getBasicTimerDelay().getValue(),
+                                config.getMaximumTimerDelay().getValue());
 
+        contexts.put(deviceContext.getDeviceInfo(), statisticsContext);
         return statisticsContext;
     }
 
     @Override
     public void onDeviceRemoved(final DeviceInfo deviceInfo) {
         contexts.remove(deviceInfo);
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Statistics context removed for node {}", deviceInfo);
+        LOG.debug("Statistics context removed for node {}", deviceInfo);
+    }
+
+    @Override
+    public void close() {
+        isStatisticsFullyDisabled = true;
+        controlServiceRegistration.close();
+
+        for (StatisticsContext context : contexts.values()) {
+            context.close();
         }
+
+        contexts.clear();
     }
 }
