f8112dd1a1bcf1b304eebda7599dca2cfe7aee35
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-08-02 13:15:42 +0200

    Add RoleManager and RoleContext
    - Move sending of MASTER and SLAVE role from DeviceContext
      and ContextChainHolder to new RoleContext and RoleManager
    - Use hashed wheel timer for setting SLAVE role on device in
      case MASTER role was not propagated instead of ItemScheduler
    - Remove retries from SalRoleService
    
    See also: bug 8913
    
    Change-Id: I808efef843ed936035fbf7759ae22d8976c80ca7
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/role/RoleManager.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.api.openflow.role;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.openflowplugin.api.openflow.OFPManager;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+
+/**
+ * Manages creation and termination of role contexts.
+ * @see org.opendaylight.openflowplugin.api.openflow.role.RoleContext
+ */
+public interface RoleManager extends OFPManager {
+    /**
+     * Create role context.
+     *
+     * @param deviceContext the device context
+     * @return the role context
+     */
+    RoleContext createContext(@Nonnull DeviceContext deviceContext);
+}
