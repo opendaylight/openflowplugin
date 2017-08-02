54d23f185f71a1cce35e6e659449b2c5707b1cce
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-08-02 09:48:37 +0200

    Redesign statistics context and manager
    - To simplify statistics context and statistics manager code,
    have better control over statistics polling and to remove
    referencing of statistics manager from statistics context,
    remove current method of statistics scheduling and replace
    it with Guava scheduled service that will handle starting,
    periodic gathering and stopping of statistics.
    - Simplify chain of steps that continues with statistics initialization
    with reconciliation framework enabled
    
    See also: bug 8913
    
    Change-Id: I0f3dcdd5292c6c7180ea80afc6d4d3df16b05ff9
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsPollingServiceTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.statistics;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.function.Supplier;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StatisticsPollingServiceTest {
+    @Mock
+    private TimeCounter timeCounter;
+    @Mock
+    private Supplier<ListenableFuture<Boolean>> gatheringSupplier;
+
+    private StatisticsPollingService statisticsPollingService;
+
+    @Before
+    public void setUp() throws Exception {
+        when(timeCounter.getAverageTimeBetweenMarks()).thenReturn(15000L);
+        when(gatheringSupplier.get()).thenReturn(Futures.immediateFuture(true));
+        statisticsPollingService = new StatisticsPollingService(
+                timeCounter, 10000, 12000,
+                gatheringSupplier);
+    }
+
+    @Test
+    public void startUp() throws Exception {
+        statisticsPollingService.startUp();
+        verify(timeCounter).markStart();
+    }
+
+    @Test
+    public void runOneIteration() throws Exception {
+        statisticsPollingService.runOneIteration();
+        verify(gatheringSupplier).get();
+        verify(timeCounter).addTimeMark();
+    }
+
+    @Test
+    public void scheduler() throws Exception {
+        statisticsPollingService.scheduler();
+        verify(timeCounter).getAverageTimeBetweenMarks();
+    }
+
+}
