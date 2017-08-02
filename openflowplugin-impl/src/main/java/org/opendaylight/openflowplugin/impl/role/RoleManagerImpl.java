be86ba24794fee60eab3c2f403c761f34b1f52d0
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-08-02 13:15:42 +0200

    Add RoleManager and RoleContext
    - Move sending of MASTER and SLAVE role from DeviceContext
      and ContextChainHolder to new RoleContext and RoleManager
    - Use hashed wheel timer for setting SLAVE role on device in
      case MASTER role was not propagated instead of ItemScheduler
    - Remove retries from SalRoleService
    
    See also: bug 8913
    
    Change-Id: I808efef843ed936035fbf7759ae22d8976c80ca7
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/role/RoleManagerImpl.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.impl.role;
+
+import io.netty.util.HashedWheelTimer;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import javax.annotation.Nonnull;
+import org.opendaylight.openflowplugin.api.openflow.OFPContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
+import org.opendaylight.openflowplugin.api.openflow.device.DeviceInfo;
+import org.opendaylight.openflowplugin.api.openflow.role.RoleContext;
+import org.opendaylight.openflowplugin.api.openflow.role.RoleManager;
+import org.opendaylight.openflowplugin.impl.services.sal.SalRoleServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoleManagerImpl implements RoleManager {
+    private static final Logger LOG = LoggerFactory.getLogger(RoleManagerImpl.class);
+
+    private final ConcurrentMap<DeviceInfo, RoleContext> contexts = new ConcurrentHashMap<>();
+    private final HashedWheelTimer timer;
+
+    public RoleManagerImpl(final HashedWheelTimer timer) {
+        this.timer = timer;
+    }
+
+    @Override
+    public RoleContext createContext(@Nonnull final DeviceContext deviceContext) {
+        final DeviceInfo deviceInfo = deviceContext.getDeviceInfo();
+        final RoleContextImpl roleContext = new RoleContextImpl(
+                deviceContext.getDeviceInfo(),
+                new SalRoleServiceImpl(deviceContext, deviceContext),
+                timer);
+
+        contexts.put(deviceInfo, roleContext);
+        return roleContext;
+    }
+
+    @Override
+    public void onDeviceRemoved(final DeviceInfo deviceInfo) {
+        contexts.remove(deviceInfo);
+    }
+
+    @Override
+    public void close() {
+        contexts.values().forEach(OFPContext::close);
+        contexts.clear();
+    }
+}
