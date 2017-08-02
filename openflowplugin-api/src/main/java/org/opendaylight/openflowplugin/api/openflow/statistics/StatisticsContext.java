a9d941d2b0da079bb2a2ea91e352cb326dbed3f8
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-08-02 09:48:37 +0200

    Redesign statistics context and manager
    - To simplify statistics context and statistics manager code,
    have better control over statistics polling and to remove
    referencing of statistics manager from statistics context,
    remove current method of statistics scheduling and replace
    it with Guava scheduled service that will handle starting,
    periodic gathering and stopping of statistics.
    - Simplify chain of steps that continues with statistics initialization
    with reconciliation framework enabled
    
    See also: bug 8913
    
    Change-Id: I0f3dcdd5292c6c7180ea80afc6d4d3df16b05ff9
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/statistics/StatisticsContext.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/statistics/StatisticsContext.java
@@ -8,83 +8,21 @@
 
 package org.opendaylight.openflowplugin.api.openflow.statistics;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import io.netty.util.Timeout;
 import org.opendaylight.openflowplugin.api.openflow.OFPContext;
-import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
-import org.opendaylight.openflowplugin.api.openflow.device.DeviceState;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
-import org.opendaylight.openflowplugin.api.openflow.lifecycle.OwnershipChangeListener;
-import org.opendaylight.openflowplugin.api.openflow.rpc.listener.ItemLifecycleListener;
+import org.opendaylight.openflowplugin.api.openflow.lifecycle.ReconciliationFrameworkStep;
 
 /**
  * Context for statistics.
  */
-public interface StatisticsContext extends RequestContextStack, OFPContext {
+public interface StatisticsContext extends RequestContextStack, OFPContext, ReconciliationFrameworkStep {
+    /**
+     * Enables statistics gathering.
+     */
+    void enableGathering();
 
     /**
-     * Gather data from device.
-     * @return true if gathering was successful
+     * Disables statistics gathering.
      */
-    ListenableFuture<Boolean> gatherDynamicData();
-
-    /**
-     * Method has to be called from DeviceInitialization Method, otherwise
-     * we are not able to poll anything. Statistics Context normally initialize
-     * this part by initialization process but we don't have this information
-     * in initialization phase and we have to populate whole list after every
-     * device future collecting. Because device future collecting set DeviceState
-     * and we creating marks for the correct kind of stats from DeviceState.
-     */
-    void statListForCollectingInitialization();
-
-    /**
-     * Setter.
-     * @param pollTimeout handle to nearest scheduled statistics poll
-     */
-    void setPollTimeout(Timeout pollTimeout);
-
-    /**
-     * Getter.
-     * @return dedicated item life cycle change listener (per device)
-     */
-    ItemLifecycleListener getItemLifeCycleListener();
-
-    /**
-     * On / Off scheduling.
-     * @param schedulingEnabled true if scheduling should be enabled
-     */
-    void setSchedulingEnabled(boolean schedulingEnabled);
-
-    /**
-     * Check status.
-     * @return true if scheduling is enabled
-     */
-    boolean isSchedulingEnabled();
-
-    /**
-     * Gain device state.
-     * @return device state from device context from lifecycle service
-     */
-    DeviceState gainDeviceState();
-
-    /**
-     * Gain device context.
-     * @return device context from lifecycle service
-     */
-    DeviceContext gainDeviceContext();
-
-    /**
-     * In case to change mastership to slave or connection interrupted stop the future and release thread.
-     */
-    void stopGatheringData();
-
-    /**
-     * In case of using reconciliation framework need to be initialization submit handled separately.
-     * @return true if submitting was ok
-     * @since 0.5.0 Nitrogen
-     * @see OwnershipChangeListener#isReconciliationFrameworkRegistered()
-     * @see org.opendaylight.openflowplugin.api.openflow.mastership.MastershipChangeService
-     */
-    boolean initialSubmitAfterReconciliation();
+    void disableGathering();
 }
