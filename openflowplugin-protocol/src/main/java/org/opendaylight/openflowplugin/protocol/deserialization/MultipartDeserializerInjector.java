15f562267823932c512bd037a931297d0c46bf13
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-08-22 15:03:07 +0200

    Move serializers and deserializers to protocol
    - Move serializers and deserializers to openflowplugin-protocol
    - Add new blueprint service that registers serializers into
    switch connection provider
    
    See also: bug 8653
    
    Change-Id: I8cbd1acf6fcda982f9379fc667640936dc55a481
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

---
+++ openflowplugin-protocol/src/main/java/org/opendaylight/openflowplugin/protocol/deserialization/MultipartDeserializerInjector.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2016 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.protocol.deserialization;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowplugin.extension.api.core.extension.ExtensionConverterProvider;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyDescDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyExperimenterDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyFlowAggregateStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyFlowStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyFlowTableStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyGroupDescDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyGroupFeaturesDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyGroupStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyMeterConfigDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyMeterFeaturesDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyMeterStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyPortDescDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyPortStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyQueueStatsDeserializer;
+import org.opendaylight.openflowplugin.protocol.deserialization.multipart.MultipartReplyTableFeaturesDeserializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.multipart.types.rev170112.multipart.reply.MultipartReplyBody;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.MultipartType;
+
+class MultipartDeserializerInjector {
+
+    /**
+     * Injects message deserializers into provided {@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     */
+    static void injectDeserializers(final DeserializerExtensionProvider provider,
+                                    final ExtensionConverterProvider extensionConverterProvider) {
+        final short version = EncodeConstants.OF13_VERSION_ID;
+
+        // Inject new multipart body deserializers here using injector created by createInjector method
+        final Function<Integer, Consumer<OFDeserializer<? extends MultipartReplyBody>>> injector =
+                createInjector(provider, version);
+        injector.apply(MultipartType.OFPMPDESC.getIntValue()).accept(new MultipartReplyDescDeserializer());
+        injector.apply(MultipartType.OFPMPFLOW.getIntValue()).accept(new MultipartReplyFlowStatsDeserializer());
+        injector.apply(MultipartType.OFPMPAGGREGATE.getIntValue()).accept(new MultipartReplyFlowAggregateStatsDeserializer());
+        injector.apply(MultipartType.OFPMPTABLE.getIntValue()).accept(new MultipartReplyFlowTableStatsDeserializer());
+        injector.apply(MultipartType.OFPMPPORTSTATS.getIntValue()).accept(new MultipartReplyPortStatsDeserializer());
+        injector.apply(MultipartType.OFPMPQUEUE.getIntValue()).accept(new MultipartReplyQueueStatsDeserializer());
+        injector.apply(MultipartType.OFPMPGROUP.getIntValue()).accept(new MultipartReplyGroupStatsDeserializer());
+        injector.apply(MultipartType.OFPMPGROUPDESC.getIntValue()).accept(new MultipartReplyGroupDescDeserializer(extensionConverterProvider));
+        injector.apply(MultipartType.OFPMPGROUPFEATURES.getIntValue()).accept(new MultipartReplyGroupFeaturesDeserializer());
+        injector.apply(MultipartType.OFPMPMETER.getIntValue()).accept(new MultipartReplyMeterStatsDeserializer());
+        injector.apply(MultipartType.OFPMPMETERCONFIG.getIntValue()).accept(new MultipartReplyMeterConfigDeserializer());
+        injector.apply(MultipartType.OFPMPMETERFEATURES.getIntValue()).accept(new MultipartReplyMeterFeaturesDeserializer());
+        injector.apply(MultipartType.OFPMPTABLEFEATURES.getIntValue()).accept(new MultipartReplyTableFeaturesDeserializer(extensionConverterProvider));
+        injector.apply(MultipartType.OFPMPPORTDESC.getIntValue()).accept(new MultipartReplyPortDescDeserializer());
+        injector.apply(MultipartType.OFPMPEXPERIMENTER.getIntValue()).accept(new MultipartReplyExperimenterDeserializer(extensionConverterProvider));
+    }
+
+    /**
+     * Create injector that will inject new deserializers into #{@link org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerExtensionProvider}
+     * @param provider OpenflowJava deserializer extension provider
+     * @param version Openflow version
+     * @return injector
+     */
+    @VisibleForTesting
+    static Function<Integer, Consumer<OFDeserializer<? extends MultipartReplyBody>>> createInjector(
+            final DeserializerExtensionProvider provider,
+            final short version) {
+        return code -> deserializer -> {
+            provider.registerDeserializer(
+                    new MessageCodeKey(version, code, MultipartReplyBody.class),
+                    deserializer);
+        };
+    }
+
+}
