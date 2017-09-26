b9322636bde236d59e7ef7523ee9bdfc024fbb32
Gobinath <gobinath@ericsson.com>
2017-09-26 15:07:45 +0530

    Cli to display all the connected DPNs
    Change-Id: I519450772b670750ccb38391a3c633ba572340d1
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/util/ShellUtil.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.southboundcli.util;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShellUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(ShellUtil.class);
+
+    public static final String NODE_PREFIX = "openflow:";
+
+    public static List<OFNode> getAllNodes(final DataBroker broker) {
+        List<Node> nodes = null;
+        ReadOnlyTransaction tx = broker.newReadOnlyTransaction();
+        InstanceIdentifier<Nodes> path = InstanceIdentifier.builder(Nodes.class).build();
+        try {
+            CheckedFuture<Optional<Nodes>, ReadFailedException> checkedFuture =
+                    tx.read(LogicalDatastoreType.OPERATIONAL, path);
+            Optional<Nodes> result = checkedFuture.get();
+            if (result.isPresent()) {
+                nodes = result.get().getNode();
+            }
+        } catch (ExecutionException | InterruptedException | NullPointerException e) {
+            LOG.error("Error reading nodes from Inventory DS", e);
+        }
+        if (nodes != null) {
+            List<OFNode> nodeList = new ArrayList<>();
+            for (Node node : nodes) {
+                String[] nodeId = node.getId().getValue().split(":");
+                String name = null;
+                FlowCapableNode flowCapableNode = node.<FlowCapableNode>getAugmentation(FlowCapableNode.class);
+                if (flowCapableNode != null) {
+                    name = node.<FlowCapableNode>getAugmentation(FlowCapableNode.class).getDescription();
+                } else {
+                    LOG.error("Error while converting OFNode: {} to FlowCapableNode", node.getId());
+                    return null;
+                }
+                OFNode ofNode = new OFNode(Long.parseLong(nodeId[1]), name);
+                if (ofNode != null) {
+                    LOG.trace("Added OFNode: {} to the list", ofNode.getNodeId());
+                    nodeList.add(ofNode);
+                }
+            }
+            Collections.sort(nodeList);
+            return nodeList;
+        }
+        return Collections.emptyList();
+    }
+
+    public static OFNode getNode(final long nodeId, final DataBroker broker) {
+        OFNode nodeInfo = getNodeInfo(nodeId, broker);
+        if (nodeInfo == null) {
+            LOG.info("No ports exist for this node with nodeId {}", nodeId);
+            return null;
+        } else {
+            List<String> ports = new ArrayList<>();
+            // OFNode State is not provided by plugin, hence using null
+            if (nodeInfo.getPorts() == null) {
+                LOG.info("No ports exist for this node with nodeId {}", nodeId);
+                return null;
+            } else {
+                for (String port : nodeInfo.getPorts()) {
+                    ports.add(port);
+                }
+                return new OFNode(nodeId, nodeInfo.getNodeName(), ports);
+            }
+        }
+    }
+
+    public static OFNode getNodeInfo(final Long nodeId, final DataBroker broker) {
+        OFNode ofNode = null;
+        ReadOnlyTransaction tx = broker.newReadOnlyTransaction();
+        InstanceIdentifier<Node> path = InstanceIdentifier.builder(Nodes.class)
+                .child(Node.class, new NodeKey(new NodeId(NODE_PREFIX + nodeId))).build();
+        Optional<Node> result;
+        try {
+            CheckedFuture<Optional<Node>, ReadFailedException> checkedFuture =
+                    tx.read(LogicalDatastoreType.OPERATIONAL, path);
+            result = checkedFuture.get();
+            if (result.isPresent()) {
+                Node node = result.get();
+                String name = null;
+                List<NodeConnector> nodeConnectors = null;
+                List<String> portList = new ArrayList<>();
+                FlowCapableNode flowCapableNode = node.<FlowCapableNode>getAugmentation(FlowCapableNode.class);
+                if (flowCapableNode != null) {
+                    name = node.<FlowCapableNode>getAugmentation(FlowCapableNode.class).getDescription();
+                } else {
+                    LOG.error("Error while converting OFNode:{} to FlowCapableNode: {}", node.getId());
+                    return null;
+                }
+                nodeConnectors = node.getNodeConnector();
+                for (NodeConnector nodeConnector : nodeConnectors) {
+                    FlowCapableNodeConnector flowCapableNodeConnector =
+                            nodeConnector.getAugmentation(FlowCapableNodeConnector.class);
+                    if (flowCapableNodeConnector == null) {
+                        LOG.error("Error for OFNode:{} while reading nodeConnectors {}", node.getId());
+                        return null;
+                    } else {
+                        MacAddress hardwareAddress = flowCapableNodeConnector.getHardwareAddress();
+                        String portName = flowCapableNodeConnector.getName();
+                        portList.add(portName);
+                    }
+                }
+                ofNode = new OFNode(nodeId, name, portList);
+            } else {
+                LOG.error("OFNode with nodeId {} not present Inventory DS: {}", nodeId);
+                return null;
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            LOG.error("Error reading node {} from Inventory DS: {}", nodeId, e);
+        }
+        return ofNode;
+    }
+}
