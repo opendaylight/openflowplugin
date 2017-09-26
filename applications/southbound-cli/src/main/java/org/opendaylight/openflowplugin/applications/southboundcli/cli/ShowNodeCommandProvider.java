44c1fef45fecf86a3782aca51c49324e0df4146f
Gobinath <gobinath@ericsson.com>
2017-09-26 15:07:45 +0530

    Cli to display all the connected DPNs
    Change-Id: I519450772b670750ccb38391a3c633ba572340d1
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/cli/ShowNodeCommandProvider.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.applications.southboundcli.cli;
+
+import java.util.Formatter;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.commands.Option;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.openflowplugin.applications.southboundcli.util.OFNode;
+import org.opendaylight.openflowplugin.applications.southboundcli.util.ShellUtil;
+
+@Command(scope = "openflow", name = "shownode", description = "shownode -d <NodeID>")
+public class ShowNodeCommandProvider extends OsgiCommandSupport {
+    public static final String OUTPUT_FORMAT = "%-24s %-20s %-15s";
+    public static final String HEADER_SEPARATOR = "---------------------------------------------"
+            + "---------------------------------------";
+
+    @Option(name = "-d", description = "Node Id", required = true, multiValued = false)
+    String nodeId;
+
+    private DataBroker dataBroker;
+
+    public void setDataBroker(final DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        if (nodeId == null) {
+            session.getConsole().println("NodeID not specified");
+            return null;
+        }
+        OFNode node = ShellUtil.getNode(Long.parseLong(nodeId), dataBroker);
+        if (node != null) {
+            printNodeHeaderOutput();
+            printHeaderSeparator();
+            printNodeOutput(node);
+        } else {
+            session.getConsole().println("No node available for this NodeID");
+        }
+        return null;
+    }
+
+    private void printNodeHeaderOutput() {
+        Formatter formatter = new Formatter();
+        String header = formatter.format(OUTPUT_FORMAT, "OFNode", "Name", "Ports").toString();
+        formatter.close();
+        session.getConsole().println(header);
+    }
+
+    private void printHeaderSeparator() {
+        session.getConsole().println(HEADER_SEPARATOR);
+    }
+
+    private void printNodeOutput(final OFNode ofNode) {
+        Formatter formatter = new Formatter();
+        Set<String> portNames = new HashSet<>();
+        for (String port : ofNode.getPorts()) {
+            if (port != null) {
+                portNames.add(port);
+            }
+        }
+        String portOutput = formatter.format(OUTPUT_FORMAT, ofNode.getNodeId(), ofNode.getNodeName(), portNames)
+                .toString();
+        formatter.close();
+        session.getConsole().println(portOutput);
+    }
+}
