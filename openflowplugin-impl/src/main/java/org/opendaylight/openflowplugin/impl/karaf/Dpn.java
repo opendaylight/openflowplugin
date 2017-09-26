3cd035998c2144f8e9299c9da8e4b4c8cad80b55
Gobinath <gobinath@ericsson.com>
2017-09-26 15:07:45 +0530

    Cli to display all the connected DPNs
    Change-Id: I519450772b670750ccb38391a3c633ba572340d1
    Signed-off-by: Gobinath <gobinath@ericsson.com>

---
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/karaf/Dpn.java
@@ -0,0 +1,58 @@
+package org.opendaylight.openflowplugin.impl.karaf;
+
+import java.io.Serializable;
+
+public class Dpn implements Comparable<Dpn>,Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long m_nDpId;
+    private String m_sDpnName;
+
+    // TODO dpnState
+
+    public Dpn(long nDpId, String sDpnName) {
+        super();
+        m_nDpId = nDpId;
+        m_sDpnName = sDpnName;
+    }
+
+    public long getDpId() {
+        return m_nDpId;
+    }
+
+    public String getDpnName() {
+        return m_sDpnName;
+    }
+
+    public void setDpId(long m_nDpId) {
+        this.m_nDpId = m_nDpId;
+    }
+
+    public void setDpnName(String m_sDpnName) {
+        this.m_sDpnName = m_sDpnName;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [m_nDpId=" + m_nDpId + ", m_sDpnName=" + m_sDpnName + "]";
+    }
+
+    @Override
+    public int compareTo(Dpn dpn) {
+        int res = 0;
+        if (m_sDpnName == null && dpn.getDpnName() == null) {
+            res = 0;
+        } else if (m_sDpnName == null) {
+            res = 1;
+        } else if (dpn.getDpnName() == null) {
+            res = -1;
+        } else {
+            res = m_sDpnName.compareTo(dpn.getDpnName());
+        }
+        if (res == 0) {
+            return Long.compare(m_nDpId, dpn.getDpId());
+        }
+        return res;
+    }
+
+}
