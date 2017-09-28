17b3e9bc3459189ad0906c31ac8904b18108e74e
sunilkumarms <sunil.g.kumar@ericsson.com>
2017-09-28 13:44:28 +0530

    Bug 7826 Identified the root cause for issue TransactionCommitFailed for group during statistic collection.
    Change-Id: I8fe639e6102629fb903689114c617e76a61d68db
    Signed-off-by: sunilkumarms <sunil.g.kumar@ericsson.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsContextImpl.java
@@ -75,8 +75,10 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
     StatisticsContextImpl(@Nonnull final DeviceContext deviceContext,
                           @Nonnull final ConvertorExecutor convertorExecutor,
                           @Nonnull final MultipartWriterProvider statisticsWriterProvider,
-                          @Nonnull final ListeningExecutorService executorService, boolean isStatisticsPollingOn,
-                          boolean isUsingReconciliationFramework, long statisticsPollingInterval,
+                          @Nonnull final ListeningExecutorService executorService,
+                          boolean isStatisticsPollingOn,
+                          boolean isUsingReconciliationFramework,
+                          long statisticsPollingInterval,
                           long maximumPollingDelay) {
         this.deviceContext = deviceContext;
         this.devState = Preconditions.checkNotNull(deviceContext.getDeviceState());
@@ -90,9 +92,8 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         this.isUsingReconciliationFramework = isUsingReconciliationFramework;
 
         statisticsGatheringService = new StatisticsGatheringService<>(this, deviceContext);
-        statisticsGatheringOnTheFlyService = new StatisticsGatheringOnTheFlyService<>(this, deviceContext,
-                                                                                      convertorExecutor,
-                                                                                      statisticsWriterProvider);
+        statisticsGatheringOnTheFlyService = new StatisticsGatheringOnTheFlyService<>(this,
+                deviceContext, convertorExecutor, statisticsWriterProvider);
     }
 
     @Override
@@ -137,12 +138,15 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
     @Override
     public void continueInitializationAfterReconciliation() {
         if (deviceContext.initialSubmitTransaction()) {
-            contextChainMastershipWatcher.onMasterRoleAcquired(deviceInfo, ContextChainMastershipState.INITIAL_SUBMIT);
+            contextChainMastershipWatcher.onMasterRoleAcquired(
+                    deviceInfo,
+                    ContextChainMastershipState.INITIAL_SUBMIT);
 
             startGatheringData();
         } else {
-            contextChainMastershipWatcher
-                    .onNotAbleToStartMastershipMandatory(deviceInfo, "Initial transaction cannot be submitted.");
+            contextChainMastershipWatcher.onNotAbleToStartMastershipMandatory(
+                    deviceInfo,
+                    "Initial transaction cannot be submitted.");
         }
     }
 
@@ -195,7 +199,7 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
             }
 
             @Override
-            public void onFailure(final Throwable throwable) {
+            public void onFailure(final Throwable t) {
                 requestContexts.forEach(requestContext -> RequestContextUtil
                         .closeRequestContextWithRpcError(requestContext, CONNECTION_CLOSED));
             }
@@ -213,14 +217,17 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
             StatisticsGatheringUtils.markDeviceStateSnapshotStart(deviceInfo, deviceContext);
 
             // recreate gathering future if it should be recreated
-            final ListenableFuture<Boolean> lastDataGathering =
-                    Objects.isNull(future) || future.isCancelled() || future.isDone() ? Futures
-                            .immediateFuture(Boolean.TRUE) : future;
+            final ListenableFuture<Boolean> lastDataGathering = Objects.isNull(future) ||
+                    future.isCancelled() ||
+                    future.isDone() ?
+                    Futures.immediateFuture(Boolean.TRUE) :
+                    future;
 
             // build statistics gathering future
-            final ListenableFuture<Boolean> newDataGathering = collectingStatType.stream()
-                    .reduce(lastDataGathering, this::statChainFuture,
-                        (listenableFuture, asyn) -> Futures.transformAsync(listenableFuture, result -> asyn));
+            final ListenableFuture<Boolean> newDataGathering = collectingStatType.stream().reduce(
+                    lastDataGathering,
+                    this::statChainFuture,
+                    (a, b) -> Futures.transformAsync(a, result -> b));
 
             // write end timestamp to state snapshot container
             Futures.addCallback(newDataGathering, new FutureCallback<Boolean>() {
@@ -230,8 +237,8 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
                 }
 
                 @Override
-                public void onFailure(final Throwable throwable) {
-                    if (!(throwable instanceof TransactionChainClosedException)) {
+                public void onFailure(final Throwable t) {
+                    if (!(t instanceof TransactionChainClosedException)) {
                         StatisticsGatheringUtils.markDeviceStateSnapshotEnd(deviceInfo, deviceContext, false);
                     }
                 }
@@ -241,10 +248,8 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         });
     }
 
-    private ListenableFuture<Boolean> statChainFuture(final ListenableFuture<Boolean> prevFuture,
-                                                      final MultipartType multipartType) {
-        if (ConnectionContext.CONNECTION_STATE.RIP
-                .equals(deviceContext.getPrimaryConnectionContext().getConnectionState())) {
+    private ListenableFuture<Boolean> statChainFuture(final ListenableFuture<Boolean> prevFuture, final MultipartType multipartType) {
+        if (ConnectionContext.CONNECTION_STATE.RIP.equals(deviceContext.getPrimaryConnectionContext().getConnectionState())) {
             final String errMsg = String
                     .format("Device connection for node %s doesn't exist anymore. Primary connection status : %s",
                             getDeviceInfo().getNodeId(),
@@ -254,17 +259,21 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         }
 
         return Futures.transformAsync(prevFuture, result -> {
-            LOG.debug("Status of previous stat iteration for node {}: {}", deviceInfo, result);
+            LOG.debug("Status of previous stat iteration for node {}: {}", deviceInfo.getLOGValue(), result);
             LOG.debug("Stats iterating to next type for node {} of type {}", deviceInfo, multipartType);
             final boolean onTheFly = MultipartType.OFPMPFLOW.equals(multipartType);
             final boolean supported = collectingStatType.contains(multipartType);
 
             // TODO: Refactor twice sending deviceContext into gatheringStatistics
-            return supported ? StatisticsGatheringUtils
-                    .gatherStatistics(onTheFly ? statisticsGatheringOnTheFlyService : statisticsGatheringService,
-                                      getDeviceInfo(), multipartType, deviceContext, deviceContext, convertorExecutor,
-                                      statisticsWriterProvider, executorService) : Futures
-                    .immediateFuture(Boolean.FALSE);
+            return supported ? StatisticsGatheringUtils.gatherStatistics(
+                    onTheFly ? statisticsGatheringOnTheFlyService : statisticsGatheringService,
+                    getDeviceInfo(),
+                    multipartType,
+                    deviceContext,
+                    deviceContext,
+                    convertorExecutor,
+                    statisticsWriterProvider,
+                    executorService) : Futures.immediateFuture(Boolean.FALSE);
         });
     }
 
@@ -274,11 +283,9 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         }
 
         LOG.info("Starting statistics gathering for node {}", deviceInfo);
-        final StatisticsPollingService statisticsPollingService =
-                new StatisticsPollingService(timeCounter,
-                                             statisticsPollingInterval,
-                                             maximumPollingDelay,
-                                             StatisticsContextImpl.this::gatherDynamicData);
+        final StatisticsPollingService statisticsPollingService = new StatisticsPollingService(timeCounter,
+                statisticsPollingInterval, maximumPollingDelay,
+                StatisticsContextImpl.this::gatherDynamicData);
 
         schedulingEnabled.set(true);
         statisticsPollingService.startAsync();
@@ -289,7 +296,9 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         LOG.info("Stopping running statistics gathering for node {}", deviceInfo);
         cancelLastDataGathering();
 
-        return Optional.ofNullable(statisticsPollingService.getAndSet(null)).map(StatisticsPollingService::stop)
+        return Optional
+                .ofNullable(statisticsPollingService.getAndSet(null))
+                .map(StatisticsPollingService::stop)
                 .orElseGet(() -> Futures.immediateFuture(null));
     }
 
@@ -307,16 +316,17 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
     }
 
     @VisibleForTesting
-    void setStatisticsGatheringOnTheFlyService(
-            final StatisticsGatheringOnTheFlyService<T> statisticsGatheringOnTheFlyService) {
+    void setStatisticsGatheringOnTheFlyService(final StatisticsGatheringOnTheFlyService<T> statisticsGatheringOnTheFlyService) {
         this.statisticsGatheringOnTheFlyService = statisticsGatheringOnTheFlyService;
     }
 
     private final class InitialSubmitCallback implements FutureCallback<Boolean> {
         @Override
         public void onSuccess(@Nullable final Boolean result) {
-            contextChainMastershipWatcher
-                    .onMasterRoleAcquired(deviceInfo, ContextChainMastershipState.INITIAL_GATHERING);
+            contextChainMastershipWatcher.onMasterRoleAcquired(
+                    deviceInfo,
+                    ContextChainMastershipState.INITIAL_GATHERING
+            );
 
             if (!isUsingReconciliationFramework) {
                 continueInitializationAfterReconciliation();
@@ -324,11 +334,10 @@ class StatisticsContextImpl<T extends OfHeader> implements StatisticsContext {
         }
 
         @Override
-        public void onFailure(@Nonnull final Throwable throwable) {
-            contextChainMastershipWatcher.onNotAbleToStartMastershipMandatory(deviceInfo,
-                                                                              "Initial gathering statistics "
-                                                                                      + "unsuccessful: "
-                                                                                      + throwable.getMessage());
+        public void onFailure(@Nonnull final Throwable t) {
+            contextChainMastershipWatcher.onNotAbleToStartMastershipMandatory(
+                    deviceInfo,
+                    "Initial gathering statistics unsuccessful: " + t.getMessage());
         }
     }
 }
