790176b071697d4113257d127bfd8ea42ddda572
D Arunprakash <d.arunprakash@ericsson.com>
2017-10-25 21:46:27 +0530

    Property change listener for lldp discovery config properties
    Runtime update for properties in openflowplugin.cfg is failing
    
    Change-Id: I87bcba910f8d51a5b940b82c588a06d39092404a
    Signed-off-by: D Arunprakash <d.arunprakash@ericsson.com>

---
+++ applications/topology-lldp-discovery/src/main/java/org/opendaylight/openflowplugin/applications/topology/lldp/TopologyLLDPDiscoveryProperty.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.topology.lldp;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+public enum TopologyLLDPDiscoveryProperty {
+    LLDP_SECURE_KEY,
+    TOPOLOGY_LLDP_INTERVAL,
+    TOPOLOGY_LLDP_EXPIRATION_INTERVAL;
+
+    private static final Map<String, TopologyLLDPDiscoveryProperty> KEY_VALUE_MAP;
+
+    /**
+     * Get property type from property key.
+     *
+     * @param key the property key
+     * @return the property type
+     */
+    public static TopologyLLDPDiscoveryProperty forValue(final String key) {
+        return KEY_VALUE_MAP.get(key);
+    }
+
+    static {
+        final TopologyLLDPDiscoveryProperty[] values = values();
+        final ImmutableMap.Builder<String, TopologyLLDPDiscoveryProperty> builder = ImmutableMap.builder();
+
+        for (final TopologyLLDPDiscoveryProperty value : values) {
+            builder.put(value.toString(), value);
+        }
+
+        KEY_VALUE_MAP = builder.build();
+    }
+
+    /**
+     * Converts enum name to property key.
+     *
+     * @return the property key
+     */
+    @Override
+    public String toString() {
+        return this.name().toLowerCase().replace('_', '-');
+    }
+}
