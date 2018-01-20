7ed4e7c0c54868d4d604c84f9b83e3345615d1b7
Tom Pantelis <tompantelis@gmail.com>
2018-01-20 16:45:52 -0500

    Fix checkstyle violations in openflow-protocol-impl - part 4
    Many violations - more to follow
    
    Change-Id: I9107de913adef9e19cdf7583b996598d4b9a63b0
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13SetNwTtlActionSerializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13SetNwTtlActionSerializer.java
@@ -9,14 +9,14 @@
 package org.opendaylight.openflowjava.protocol.impl.serialization.action;
 
 import io.netty.buffer.ByteBuf;
-
 import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.SetNwTtlCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
 
 /**
- * @author michal.polkorab
+ * Serializes OF 1.3 SetNwTtl actions.
  *
+ * @author michal.polkorab
  */
 public class OF13SetNwTtlActionSerializer extends AbstractActionSerializer {
 
