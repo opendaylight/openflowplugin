376984eec69926f0ef81e39323c0b3cc109bf320
eeiillu <suja.t@ericsson.com>
2018-02-15 11:29:32 +0530

    Draft review for admin based reync
    Change-Id: I11255685b12c73f1c7cb20a27b5176d6aad51199
    Signed-off-by: eeiillu <suja.t@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/ResyncServiceImpl.java
@@ -0,0 +1,126 @@
+package org.opendaylight.openflowplugin.applications.southboundcli;
+
+import com.google.common.util.concurrent.SettableFuture;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.openflowplugin.applications.southboundcli.util.OFNode;
+import org.opendaylight.openflowplugin.applications.southboundcli.util.ShellUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.reconciliation.service.rev180227.InitReconciliationInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.reconciliation.service.rev180227.InitReconciliationInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.reconciliation.service.rev180227.InitReconciliationOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.reconciliation.service.rev180227.ReconciliationService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.resync.service.rev180227.ExecResyncInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.resync.service.rev180227.ExecResyncOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.resync.service.rev180227.ResyncService;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import static org.opendaylight.yangtools.yang.common.RpcResultBuilder.success;
+
+/**
+ * Created by eeiillu on 2/28/2018.
+ */
+public class ResyncServiceImpl implements ResyncService  {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ResyncServiceImpl.class);
+    private final DataBroker broker;
+    private final ReconciliationService reconciliationService;
+
+    public ResyncServiceImpl(DataBroker broker,  ReconciliationService reconciliationService) {
+        this.broker = broker;
+        this.reconciliationService = reconciliationService;
+    }
+
+
+    @Override
+    public Future<RpcResult<ExecResyncOutput>> execResync(ExecResyncInput input) {
+        SettableFuture<RpcResult<ExecResyncOutput>> result = SettableFuture.create();
+        boolean reconcileAllNodes = input.isReconcileAllNodes();
+        List<BigInteger> inputNodes = input.getNodes();
+        if (inputNodes == null) {
+            inputNodes = new ArrayList<>();
+        }
+        if (reconcileAllNodes && inputNodes.size() > 0) {
+            return buildErrorResponse("Error executing command execResync." +
+                    "If 'all' option is enabled, no Node must be specified as input parameter.");
+        }
+        if (!reconcileAllNodes && inputNodes.size() == 0) {
+            return buildErrorResponse("Error executing command execResync. No Node information was specified.");
+        }
+        List<Long> nodeList = getAllNodes();
+        LOG.info("The list of nodes: {}", nodeList);
+        List<Long> nodesToReconcile = reconcileAllNodes ? nodeList :
+                inputNodes.stream().distinct().map(node -> node.longValue()).collect(Collectors.toList());
+
+        if (nodesToReconcile.size() > 0) {
+            List<Long> unresolvedNodes =
+                    nodesToReconcile.stream().filter(node -> !nodeList.contains(node)).collect(Collectors.toList());
+            if (!unresolvedNodes.isEmpty()) {
+                return buildErrorResponse("Node(s) not found: " + String.join(", ", unresolvedNodes.toString()));
+            }
+            for (Long nodeId : nodesToReconcile) {
+                LOG.info("Executing admin reconciliation for node {}", nodeId);
+                System.out.println("Executing admin reconciliation for node " + nodeId);
+                BigInteger node = new BigInteger(String.valueOf(nodeId));
+                NodeKey nodeKey = new NodeKey(new NodeId("openflow:" + nodeId));
+                LOG.info("The value of initReconInput");
+                InitReconciliationInput initReconInput = new InitReconciliationInputBuilder().
+                        setNodeId(node).setNode(new NodeRef(InstanceIdentifier.builder(Nodes.class).
+                        child(Node.class, nodeKey).build())).build();
+                LOG.info("The value of initReconInput: {} ",initReconInput);
+                Future<RpcResult<InitReconciliationOutput>> initReconOutput = reconciliationService.initReconciliation(initReconInput);
+                try {
+                    RpcResult<InitReconciliationOutput> rpcResult = initReconOutput.get();
+                    if (rpcResult.isSuccessful()) {
+                        System.out.println("Reconciliation successfully completed for node " + nodeId);
+                        LOG.info("Reconciliation successfully completed for node {}", nodeId);
+                    } else {
+                        System.out.println("Reconciliation failed for node " + nodeId + ", please check logs");
+                        LOG.error("Reconciliation failed for node {} with error {}", nodeId, rpcResult.getErrors());
+                    }
+                } catch (Exception e) {
+                    LOG.error("Error occurred while invoking execresync RPC for node {}", nodeId, e);
+                }
+            }
+        } else {
+            return buildErrorResponse("No node found");
+        }
+        result.set(RpcResultBuilder.<ExecResyncOutput>success().build());
+        return result;
+    }
+
+    private Future<RpcResult<ExecResyncOutput>> buildErrorResponse(String msg) {
+        SettableFuture<RpcResult<ExecResyncOutput>> result = SettableFuture.create();
+        LOG.error(msg);
+        System.out.println(msg);
+        RpcError error = RpcResultBuilder.newError(RpcError.ErrorType.PROTOCOL, "execResync", msg);
+        List<RpcError> errors = Collections.singletonList(error);
+        result.set(RpcResultBuilder.<ExecResyncOutput> failed().withRpcErrors(errors).build());
+        return result;
+    }
+
+    public List<Long> getAllNodes() {
+        List<OFNode> nodeList = ShellUtil.getAllNodes(broker);
+        List<Long> nodes = (nodeList == null)
+                ? new ArrayList<>()
+                : nodeList.stream().distinct().map(node -> node.getNodeId()).collect(Collectors.toList());
+        LOG.info("The list of nodes: {}", nodes);
+        return nodes;
+    }
+}
+
