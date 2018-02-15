8a4583c9753cd6e8aae3f78e6e5822e4ea5b38de
eeiillu <suja.t@ericsson.com>
2018-02-15 11:29:32 +0530

    Administrative Reconciliation for multiple/all Nodes
    The mechanism that is currently used for switch reconciliation implies
    either a switch or a controller restart.To avoid this force restarting
    technique and to make the user more flexible administrative reconciliation
    can be used.Here, the implementation is done to trigger switch reconciliation
    operation between controller and switch through CLI command and
    RESTCONF operation.
    
    Change-Id: I11255685b12c73f1c7cb20a27b5176d6aad51199
    Signed-off-by: eeiillu <suja.t@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/cli/ExecReconciliation.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.southboundcli.cli;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.commands.Option;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.admin.reconciliation.service.rev180227.AdminReconciliationService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.admin.reconciliation.service.rev180227.ExecReconciliationInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.admin.reconciliation.service.rev180227.ExecReconciliationInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.admin.reconciliation.service.rev180227.ExecReconciliationOutput;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Command(scope = "openflow", name = "execreconciliation", description = "Launch an admin reconciliation")
+public class ExecReconciliation extends OsgiCommandSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExecReconciliation.class);
+    private AdminReconciliationService adminReconciliationService;
+
+    public void setAdminReconciliationService(AdminReconciliationService adminReconciliationService) {
+        this.adminReconciliationService = adminReconciliationService;
+    }
+
+    @Argument(name = "nodeId", description = "The NODE Id", multiValued = true)
+    List<Long> nodeIds;
+
+    @Option(name = "-a", aliases = "--all", description = "All operative NODEs")
+    boolean reconcileAllNodes;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        LOG.info("execReconciliation: doExecute()");
+        List<BigInteger> nodes = (nodeIds == null)
+                ? new ArrayList<>()
+                : nodeIds.stream().distinct().map(node -> BigInteger.valueOf(node)).collect(Collectors.toList());
+        LOG.debug("Triggering admin reconciliation for node {}", nodes);
+        ExecReconciliationInput rpcInput = new ExecReconciliationInputBuilder().setNodes(nodes)
+                .setReconcileAllNodes(reconcileAllNodes).build();
+        Future<RpcResult<ExecReconciliationOutput>> rpcOutput = adminReconciliationService.execReconciliation(rpcInput);
+        try {
+            RpcResult<ExecReconciliationOutput> rpcResult = rpcOutput.get();
+            if (rpcResult.isSuccessful()) {
+                session.getConsole().println("execReconciliation successfully completed for the nodes");
+            } else {
+                LOG.error("execReconciliation fail with error {}", rpcResult.getErrors());
+            }
+        } catch (ExecutionException e) {
+            LOG.error("Error occurred while invoking execReconciliation RPC for node {}", nodes, e);
+        }
+        return null;
+    }
+}
