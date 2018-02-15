3b4764d2d3340f3781d03cadcdb7a593430b50b9
gobinath <gobinath@ericsson.com>
2018-02-15 17:45:39 +0530

    OPNFLWPLUG-945
    Change-Id: If139d8552bbaa3b9972882271eb1ae3f425b139f
    Signed-off-by: gobinath <gobinath@ericsson.com>

--- openflowplugin-common/src/main/java/org/opendaylight/openflowplugin/common/txchain/TransactionChainManager.java
+++ openflowplugin-common/src/main/java/org/opendaylight/openflowplugin/common/txchain/TransactionChainManager.java
@@ -16,9 +16,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.GuardedBy;
 import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
@@ -32,6 +30,7 @@ import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionChainClosedException;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.openflowplugin.common.wait.SimpleTaskRetryLooper;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
@@ -174,18 +173,16 @@ public class TransactionChainManager implements TransactionChainListener, AutoCl
             final CheckedFuture<Void, TransactionCommitFailedException> submitFuture = wTx.submit();
             lastSubmittedFuture = submitFuture;
             wTx = null;
-
             if (initCommit) {
                 try {
-                    submitFuture.get(5L, TimeUnit.SECONDS);
-                } catch (InterruptedException | ExecutionException | TimeoutException ex) {
-                    LOG.error("Exception during INITIAL transaction submitting. ", ex);
+                    SimpleTaskRetryLooper looper = new SimpleTaskRetryLooper(500, 4);
+                    looper.loopUntilNoException(() -> submitFuture.get(5L, TimeUnit.SECONDS));
+                } catch (Exception ex) {
                     return false;
                 }
                 initCommit = false;
                 return true;
             }
-
             Futures.addCallback(submitFuture, new FutureCallback<Void>() {
                 @Override
                 public void onSuccess(final Void result) {
@@ -296,6 +293,7 @@ public class TransactionChainManager implements TransactionChainListener, AutoCl
 
     private void enableSubmit() {
         synchronized (txLock) {
+            LOG.debug("Enabling submit for node {}", this.nodeId);
             /* !!!IMPORTANT: never set true without transactionChain */
             submitIsEnabled = transactionChain != null;
         }
