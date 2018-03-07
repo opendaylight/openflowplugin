18989446b8e22dd8764f2160303b9e95abd9a79b
SujaT <suja.t@ericsson.com>
2018-03-07 17:25:46 +0530

    Log Enhancement to analyze the port reason flag:
    The logs are added to identify the port-reason when the
    node is available and for transaction completion during
    node update and delete event.
    
    Change-Id: I565aa64ba20b292780c4cc1c92bb0ee493260012
    Signed-off-by: SujaT <suja.t@ericsson.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImpl.java
@@ -332,7 +332,8 @@ public class DeviceContextImpl implements DeviceContext, ExtensionConverterProvi
                                 deviceInfo.getDatapathId(),
                                 portStatusMessage.getPortNo(),
                                 OpenflowVersion.get(deviceInfo.getVersion()))));
-
+        LOG.error("The received port name {} and the port reason {}", flowCapableNodeConnector.getName(),
+                 flowCapableNodeConnector.getReason());
         writeToTransaction(LogicalDatastoreType.OPERATIONAL, iiToNodeConnector, new NodeConnectorBuilder()
                 .setKey(iiToNodeConnector.getKey())
                 .addAugmentation(FlowCapableNodeConnectorStatisticsData.class, new
@@ -340,9 +341,13 @@ public class DeviceContextImpl implements DeviceContext, ExtensionConverterProvi
                 .addAugmentation(FlowCapableNodeConnector.class, flowCapableNodeConnector)
                 .build());
         submitTransaction();
+        LOG.error("Transaction is completed for node update event with reason {} for port {}", flowCapableNodeConnector.getReason(),
+                 flowCapableNodeConnector.getName());
         if (PortReason.OFPPRDELETE.equals(portStatusMessage.getReason())) {
             addDeleteToTxChain(LogicalDatastoreType.OPERATIONAL, iiToNodeConnector);
             submitTransaction();
+            LOG.error("Transaction is completed for node delete event with reason {} for port {}", flowCapableNodeConnector.getReason(),
+                     flowCapableNodeConnector.getName());
         }
     }
 
