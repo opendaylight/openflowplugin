3f153c132b76efbdecdf7d067ef903537b5a90bf
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2018-03-13 10:23:05 +0530

    OPNFLWPLUG-990: Administrative Reconciliation alarm
    The reconciliation can be triggerd in several ways. Once the reconciliation
    is triggered on switches connected to a control node, the alarm will
    be raised for each switch, which is implemented using JMX framework.
    Once the reconciliation is complete for a particular switch, the alarm for
    the particular switch is cleared.
    
    Change-Id: I530ce7708c792ceea8d368ea8c30c9e8944957fc
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/alarm/AlarmAgent.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.southboundcli.alarm;
+
+import java.lang.management.ManagementFactory;
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class AlarmAgent {
+    private static final Logger LOG = LoggerFactory.getLogger(AlarmAgent.class);
+    private final MBeanServer mbs;
+    private ObjectName alarmName;
+    private static final String BEAN_NAME = "SDNC.FM:name=NodeReconciliationOperationOngoing";
+    private final ReconciliationAlarm alarmBean = new ReconciliationAlarm();
+
+    /**
+     * constructor get the instance of platform MBeanServer.
+     */
+    public AlarmAgent() {
+        mbs = ManagementFactory.getPlatformMBeanServer();
+    }
+
+    @PostConstruct
+    public void start() {
+        try {
+            alarmName = new ObjectName(BEAN_NAME);
+            if (!mbs.isRegistered(alarmName)) {
+                mbs.registerMBean(alarmBean, alarmName);
+                LOG.info("Registered Mbean {} successfully", alarmName);
+            }
+        } catch (MalformedObjectNameException e) {
+            LOG.error("ObjectName instance creation failed for bean {}", BEAN_NAME, e);
+        } catch (InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException e) {
+            LOG.error("Registeration failed for Mbean {}", alarmName, e);
+        }
+    }
+
+    /**
+     * Method invoke raise alarm JMX API in platform MbeanServer with alarm
+     * details.
+     *
+     * @param alarmId alarm to be raised
+     * @param text Additional details describing about the alarm on which dpnId
+     * @param src Source of the alarm ex: dpnId=openflow:1 the source node that
+     *            caused this alarm
+     */
+    public void invokeFMraisemethod(String alarmId, String text, String src) {
+        try {
+            mbs.invoke(alarmName, "raiseAlarm", new Object[] { alarmId, text, src },
+                    new String[] { String.class.getName(), String.class.getName(), String.class.getName() });
+            LOG.debug("Invoked raiseAlarm function for Mbean {} with source {}", BEAN_NAME, src);
+        } catch (InstanceNotFoundException | ReflectionException | MBeanException e) {
+            LOG.error("Invoking raiseAlarm method failed for Mbean {}", alarmName, e);
+        }
+    }
+
+    /**
+     * Method invoke clear alarm JMX API in platform MbeanServer with alarm
+     * details.
+     *
+     * @param alarmId alarm to be cleared
+     * @param text Additional details describing about the alarm on which dpnId
+     * @param src Source of the alarm ex: dpn=openflow:1 the source node that
+     *            caused this alarm
+     */
+    public void invokeFMclearmethod(String alarmId, String text, String src) {
+        try {
+            mbs.invoke(alarmName, "clearAlarm", new Object[] { alarmId, text, src },
+                    new String[] { String.class.getName(), String.class.getName(), String.class.getName() });
+            LOG.debug("Invoked clearAlarm function for Mbean {} with source {}", BEAN_NAME, src);
+        } catch (InstanceNotFoundException | ReflectionException | MBeanException e) {
+            LOG.error("Invoking clearAlarm method failed for Mbean {}", alarmName, e);
+        }
+    }
+
+    /**
+     * Method gets the alarm details to be raised and construct the alarm
+     * objects.
+     *
+     * @param nodeId Source of the alarm dpnId
+     */
+    public void raiseAdminReconciliationAlarm(long nodeId) {
+        String alarmText = getAlarmText(nodeId,  " started reconciliation");
+        String source = getSourceText(nodeId);
+
+        LOG.debug("Raising AdminReconciliation alarm... alarmText {} source {}", alarmText, source);
+        // Invokes JMX raiseAlarm method
+        invokeFMraisemethod("AdminReconciliation", alarmText, source);
+    }
+
+    /**
+     * Method gets the alarm details to be cleared and construct the alarm
+     * objects.
+     *
+     * @param nodeId Source of the alarm dpnId
+     */
+    public void clearAdminReconciliationAlarm(long nodeId) {
+        String alarmText = getAlarmText(nodeId, " finished reconciliation");
+        String source = getSourceText(nodeId);
+
+        LOG.debug("Clearing AdminReconciliation alarm of source {}", source);
+        // Invokes JMX clearAlarm method
+        invokeFMclearmethod("AdminReconciliationAlarm", alarmText, source);
+    }
+
+    /**
+     * Method gets the alarm text for the nodeId
+     *
+     * @param nodeId Source of the alarm nodeId
+     * @param event reason for alarm invoke/clear
+     */
+    private String getAlarmText(long nodeId, String event) {
+        return new StringBuffer("OF Switch ").append(nodeId).append(event).toString();
+    }
+
+    /**
+     * Method gets the source text for the nodeId
+     *
+     * @param nodeId Source of the alarm nodeId
+     */
+    private String getSourceText(long nodeId) {
+        return new StringBuffer("Dpn=").append(nodeId).toString();
+    }
+}
