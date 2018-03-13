91254491d14f35090de4d7f276ee41a814a62816
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2018-03-13 10:23:05 +0530

    OPNFLWPLUG-990: Administrative Reconciliation alarm
    The reconciliation can be triggerd in several ways. Once the reconciliation
    is triggered on switches connected to a control node, the alarm will
    be raised for each switch, which is implemented using JMX framework.
    Once the reconciliation is complete for a particular switch, the alarm for
    the particular switch is cleared.
    
    Change-Id: I530ce7708c792ceea8d368ea8c30c9e8944957fc
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/alarm/ReconciliationAlarmMBean.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.southboundcli.alarm;
+
+import java.util.List;
+
+/**
+ * ReconciliationAlarmMBean is the generic interface for
+ * providing alarm services by maintaning the alarm object.
+ */
+public interface ReconciliationAlarmMBean {
+
+    /**
+     * This method is called to set the list of raiseAlarmObjets.
+     * This includes the alarm objects which are to be raised
+     *
+     * @param raiseAlarmObject holds the list of alarm objects
+     */
+    void setRaiseAlarmObject(java.util.List<String> raiseAlarmObject);
+
+    /**
+     * This method is called to retrieve the list of alarm objects which are to be raised.
+     *
+     * @return List of string containing the alarm objects
+     */
+    List<String> getRaiseAlarmObject();
+
+    /**
+     * This method is called to set the list of alarm objects to be cleared.
+     *
+     * @param clearAlarmObject maintains the list of clearable alarm objects
+     */
+    void setClearAlarmObject(List<String> clearAlarmObject);
+
+    /**
+     * This method is called to retrieve the list of the alarm objects to be cleared.
+     *
+     * @return List of clearable alarm objects
+     */
+    List<String> getClearAlarmObject();
+
+    /**
+     * This method is called to raise the alarm with the specified alarm name.
+     *
+     * @param alarmName name of the alarm to be raised
+     * @param additionalText description of alarm event
+     * @param source source of alarm
+     */
+    void raiseAlarm(String alarmName, String additionalText, String source);
+
+    /**
+     * This method is called to clear the raised alarm with the specified alarm name.
+     *
+     * @param alarmName name of the alarm to be cleared
+     * @param additionalText description of alarm event
+     * @param source source of alarm
+     */
+    void clearAlarm(String alarmName, String additionalText, String source);
+}
