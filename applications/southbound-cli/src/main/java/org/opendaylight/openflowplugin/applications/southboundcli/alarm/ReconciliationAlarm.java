91e006bd033f07b6f49d5cda626119fa0d27a5f4
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2018-03-13 10:23:05 +0530

    Draft review for Administrative Reconciliation alarm
    Admin can trigger reconciliation for several switches to a control node 
    using CLI command. Once admin triggers reconciliation, the alarm will be 
    raised for each switch, which is implemented using JMX framework. Once 
    the reconciliation is complete for a particular switch, the alarm for 
    the particular switch is  cleared.
    
    Change-Id: I530ce7708c792ceea8d368ea8c30c9e8944957fc
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

---
+++ applications/southbound-cli/src/main/java/org/opendaylight/openflowplugin/applications/southboundcli/alarm/ReconciliationAlarm.java
@@ -0,0 +1,65 @@
+package org.opendaylight.openflowplugin.applications.southboundcli.alarm;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+import javax.management.AttributeChangeNotification;
+import javax.management.NotificationBroadcasterSupport;
+
+/**
+ * Implementation of the Reconciliation Alarm MBean. It can basically allow to
+ * rise and clear alarms occurred on the Data Path.
+ *
+ * @author Ericsson India Global Services Pvt Ltd. and others
+ *
+ */
+public class ReconciliationAlarm extends NotificationBroadcasterSupport implements ReconciliationAlarmMBean {
+
+    private long sequenceNumber = 1;
+
+    private volatile java.util.List<String> raiseAlarmObject = new ArrayList<>();
+    private volatile List<String> clearAlarmObject = new ArrayList<>();
+
+    @Override
+    public void setRaiseAlarmObject(List<String> raiseAlarmObject) {
+        this.raiseAlarmObject = raiseAlarmObject;
+
+        sendRaiseAlarmNotification(this.raiseAlarmObject);
+    }
+
+    private void sendRaiseAlarmNotification(List<String> alarmObject) {
+        sendNotification(new AttributeChangeNotification(this, sequenceNumber++, System.currentTimeMillis(),
+                "raise alarm object notified ", "raiseAlarmObject", "ArrayList", "", alarmObject));
+    }
+
+    @Override
+    public List<String> getRaiseAlarmObject() {
+        return raiseAlarmObject;
+    }
+
+    @Override
+    public void setClearAlarmObject(List<String> clearAlarmObject) {
+        this.clearAlarmObject = clearAlarmObject;
+        sendClearAlarmNotification(this.clearAlarmObject);
+    }
+
+    private void sendClearAlarmNotification(List<String> alarmObject) {
+        sendNotification(new AttributeChangeNotification(this, sequenceNumber++, System.currentTimeMillis(),
+                "clear alarm object notified ", "clearAlarmObject", "ArrayList", "", alarmObject));
+    }
+
+    @Override
+    public List<String> getClearAlarmObject() {
+        return clearAlarmObject;
+    }
+
+    @Override
+    public void raiseAlarm(String alarmName, String additionalText, String source) {
+        sendRaiseAlarmNotification(ImmutableList.of(alarmName, additionalText, source));
+    }
+
+    @Override
+    public void clearAlarm(String alarmName, String additionalText, String source) {
+        sendClearAlarmNotification(ImmutableList.of(alarmName, additionalText, source));
+    }
+}
