83571ed75db2ed71d50cade432b26e47b2fea4eb
sunilkumarms <sunil.g.kumar@ericsson.com>
2018-03-22 11:12:55 +0530

    Deserializer test
    Change-Id: I241a45bb71f0a1f475f8972363c147a18803f72f
    Signed-off-by: sunilkumarms <sunil.g.kumar@ericsson.com>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/DeserializationFactory.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/DeserializationFactory.java
@@ -17,6 +17,8 @@ import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
 import org.opendaylight.openflowjava.protocol.api.keys.TypeToClassKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Factory for deserialization.
@@ -28,9 +30,12 @@ import org.opendaylight.yangtools.yang.binding.DataObject;
 public class DeserializationFactory {
 
     private final Map<TypeToClassKey, Class<?>> messageClassMap = new ConcurrentHashMap<>();
-    private DeserializerRegistry registry;
+    private final DeserializerRegistry registry;
+    private Logger LOG = LoggerFactory.getLogger(DeserializationFactory.class);
 
-    public DeserializationFactory() {
+    public DeserializationFactory(final DeserializerRegistry registry) {
+        this.registry = registry;
+
         TypeToClassMapInitializer.initializeTypeToClassMap(messageClassMap);
 
         // Register type to class map for additional deserializers
@@ -50,7 +55,9 @@ public class DeserializationFactory {
         int type = rawMessage.readUnsignedByte();
         Class<?> clazz = messageClassMap.get(new TypeToClassKey(version, type));
         rawMessage.skipBytes(EncodeConstants.SIZE_OF_SHORT_IN_BYTES);
+        LOG.info("Type={} , class={}", type, clazz);
         OFDeserializer<DataObject> deserializer = registry.getDeserializer(new MessageCodeKey(version, type, clazz));
+        LOG.info("deserializer for above mentioned class and type {}", deserializer);
         dataObject = deserializer.deserialize(rawMessage);
         return dataObject;
     }
@@ -75,12 +82,7 @@ public class DeserializationFactory {
         if (key == null) {
             throw new IllegalArgumentException("TypeToClassKey is null");
         }
-
+        LOG.info("unregisterMapping for key {}", key);
         return messageClassMap.remove(key) != null;
     }
-
-    public void setRegistry(final DeserializerRegistry registry) {
-        this.registry = registry;
-    }
-
 }
