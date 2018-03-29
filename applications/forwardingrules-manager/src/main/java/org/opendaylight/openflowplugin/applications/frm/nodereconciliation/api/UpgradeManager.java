4083e8667242bc7b38a197603aa291f4b4b706fd
gobinath <gobinath@ericsson.com>
2018-03-29 16:23:13 +0530

    Replay based Upgrade
    Change-Id: Ib71b9ed7a846ccd4a4592684cfd74fbd6106c986
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/nodereconciliation/api/UpgradeManager.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frm.nodereconciliation.api;
+
+import java.util.concurrent.Future;
+import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationNotificationListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.onf.rev170124.BundleId;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+
+/**
+ * Provider of the APIs getActiveBundle() and getUpgradeMode()
+ * Implementation found in UpgradeManagerImpl.
+ */
+public interface UpgradeManager extends ReconciliationNotificationListener, AutoCloseable {
+
+    void start();
+
+    /**
+     * Method returns the bundleId active,ie,open for a node.
+     *
+     * @param node - node instance identifier
+     *
+     * @return BundleId - the bundleId active for the node
+     */
+    BundleId getActiveBundle(InstanceIdentifier<FlowCapableNode> node);
+
+    /**
+     * Commits the active bundle open for a node.
+     *
+     * @param node - node instance identifier
+     *
+     * @return RpcResult of action
+     */
+    Future<RpcResult<?>> commitActiveBundle(InstanceIdentifier<FlowCapableNode> node);
+
+}
