44d65dbc39ac66be7eefeb2730a4a7e8639d00aa
gobinath <gobinath@ericsson.com>
2018-03-29 16:23:13 +0530

    OPNFLWPLUG-1005 : Implementation of arbitrator reconciliation
    This change adds a new module "arbitratorreconciliation" within the frm. This
    module contains the ArbitratorReconciliationManager API and its implementation
    ArbitratorReconciliationManagerImpl which performs the arbitrator
    reconciliation. A new configurable flag "arbitrator-reconcile-enabled" has been
    added which could be used to enable/disable this. Also a new RPC for
    completing the arbitrator reconciliation(by committing the bundles) and
    its implementation has also been added.
    
    Change-Id: Ib71b9ed7a846ccd4a4592684cfd74fbd6106c986
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ applications/arbitratorreconciliation/api/src/main/java/org/opendaylight/openflowplugin/applications/arbitratorreconciliation/api/ArbitraryReconciliationProperty.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.arbitratorreconciliation.api;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Locale;
+import java.util.Map;
+
+public enum ArbitraryReconciliationProperty {
+    ARBITRARY_RECONCILIATION_ENABLED;
+
+    private static final Map<String, ArbitraryReconciliationProperty> KEY_VALUE_MAP;
+
+    /**
+     * Get property type from property key.
+     *
+     * @param key the property key
+     * @return the property type
+     */
+    public static ArbitraryReconciliationProperty forValue(final String key) {
+        return KEY_VALUE_MAP.get(key);
+    }
+
+    static {
+        final ArbitraryReconciliationProperty[] values = values();
+        final ImmutableMap.Builder<String, ArbitraryReconciliationProperty> builder = ImmutableMap.builder();
+
+        for (final ArbitraryReconciliationProperty value : values) {
+            builder.put(value.toString(), value);
+        }
+
+        KEY_VALUE_MAP = builder.build();
+    }
+
+    /**
+     * Converts enum name to property key.
+     *
+     * @return the property key
+     */
+    @Override
+    public String toString() {
+        return this.name().toLowerCase(Locale.ROOT).replace('_', '-');
+    }
+}
