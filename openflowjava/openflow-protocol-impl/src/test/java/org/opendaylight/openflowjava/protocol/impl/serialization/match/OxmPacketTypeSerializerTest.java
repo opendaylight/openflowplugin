e7d13f9237375a850b2761631f8a303e6d607afe
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-04-18 14:42:54 +0200

    Support for packet type match field
    Packet type support is required to allow packets without an ethernet
    header through the pipeline. This condition is likely to happen in nsh
    use cases with vxlan + nsh tunnel traffic.
    
    Reference:
    - See '7.2.3.11 Packet Type Match Field' on OF 1.5 spec [1].
    - See 'Packet Type Field' on [2].
    
    [1] www.opennetworking.org/software-defined-standards/specifications/
    [2] www.openvswitch.org/support/dist-docs/ovs-fields.7.txt
    
    Change-Id: I4a48a3e06dbd06c75d3e7ae470f03b6d3d5d9f39
    JIRA: OPNFLWPLUG-1017
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

---
+++ openflowjava/openflow-protocol-impl/src/test/java/org/opendaylight/openflowjava/protocol/impl/serialization/match/OxmPacketTypeSerializerTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2018 SUSE LINUX GmbH.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.protocol.impl.serialization.match;
+
+import static org.junit.Assert.assertEquals;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.OpenflowBasicClass;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.PacketType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.PacketTypeCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.PacketTypeCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entry.value.grouping.match.entry.value.packet.type._case.PacketTypeBuilder;
+
+public class OxmPacketTypeSerializerTest {
+
+    private ByteBuf buffer;
+    private OxmPacketTypeSerializer serializer;
+
+    @Before
+    public void setup() {
+        buffer = PooledByteBufAllocator.DEFAULT.buffer();
+        serializer = new OxmPacketTypeSerializer();
+    }
+
+    @Test
+    public void serializeTest() {
+        final long packetType = 0x1894f;
+        MatchEntry matchEntry = createMatchEntry(packetType);
+
+        serializer.serialize(matchEntry, buffer);
+
+        assertEquals(OxmMatchConstants.OPENFLOW_BASIC_CLASS, buffer.readUnsignedShort());
+        short fieldMask = buffer.readUnsignedByte();
+        assertEquals(OxmMatchConstants.PACKET_TYPE, fieldMask >> 1);
+        assertEquals(0, fieldMask & 1);
+        assertEquals(EncodeConstants.SIZE_OF_INT_IN_BYTES, buffer.readUnsignedByte());
+        assertEquals(packetType, buffer.readUnsignedInt());
+    }
+
+    private MatchEntry createMatchEntry(long packetType) {
+        PacketTypeCase packetTypeCase = new PacketTypeCaseBuilder()
+                .setPacketType(new PacketTypeBuilder().setPacketType(packetType).build())
+                .build();
+        return new MatchEntryBuilder()
+                .setOxmClass(OpenflowBasicClass.class)
+                .setOxmMatchField(PacketType.class)
+                .setHasMask(false)
+                .setMatchEntryValue(packetTypeCase)
+                .build();
+    }
+}
