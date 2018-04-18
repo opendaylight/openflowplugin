685de4cbad68db323417645e1c49523828896a27
gobinath <gobinath@ericsson.com>
2018-04-19 01:46:49 +0530

    Replay based Upgrade changes
    This patch adds new forwarder types BundleFlowForwarder and
    BundleGroupForwarder which would would be used to push the flows during
    the upgrade process.
    
    Change-Id: I7681019236aa5674c6242e39b967fda9757913b1
    Signed-off-by: gobinath <gobinath@ericsson.com>

--- applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/impl/ForwardingRulesManagerImpl.java
+++ applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/impl/ForwardingRulesManagerImpl.java
@@ -264,6 +264,11 @@ public class ForwardingRulesManagerImpl implements ForwardingRulesManager {
     }
 
     @Override
+    public UpgradeManager getUpgradeManager() {
+        return upgradeManager;
+    }
+
+    @Override
     public boolean isReconciliationDisabled() {
         return disableReconciliation;
     }
