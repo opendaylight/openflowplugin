7f168b7d098304b930c794330747810c768c7b1e
D Arunprakash <d.arunprakash@ericsson.com>
2018-05-05 11:56:33 +0530

    OPNFLWPLUG-1009 : Bind ports 6653/6633 on specified address
    When user specifies ip address on config xml, the ports
    should bind on the specified ip address.
    
    Currently this piece of code was broken and this review
    fixes the same.
    
    Change-Id: I5def69bde14548682f4fa625acaefeb7feaa596f
    Signed-off-by: D Arunprakash <d.arunprakash@ericsson.com>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/SwitchConnectionProviderFactoryImpl.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/SwitchConnectionProviderFactoryImpl.java
@@ -12,6 +12,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Objects;
 import org.opendaylight.openflowjava.protocol.api.connection.ConnectionConfiguration;
 import org.opendaylight.openflowjava.protocol.api.connection.ThreadConfiguration;
 import org.opendaylight.openflowjava.protocol.api.connection.TlsConfiguration;
@@ -38,26 +39,10 @@ public class SwitchConnectionProviderFactoryImpl implements SwitchConnectionProv
         return switchConnectionProviderImpl;
     }
 
-    private static InetAddress extractIpAddressBin(final IpAddress address) throws UnknownHostException {
-        byte[] addressBin = null;
-        if (address != null) {
-            if (address.getIpv4Address() != null) {
-                addressBin = address2bin(address.getIpv4Address().getValue());
-            } else if (address.getIpv6Address() != null) {
-                addressBin = address2bin(address.getIpv6Address().getValue());
-            }
+    private static InetAddress getInetAddress(final IpAddress address) throws UnknownHostException {
+        if (Objects.nonNull(address)) {
+            return InetAddress.getByName(address.getIpv4Address().getValue());
         }
-
-        if (addressBin == null) {
-            return null;
-        } else {
-            return InetAddress.getByAddress(addressBin);
-        }
-    }
-
-    @SuppressFBWarnings("PZLA_PREFER_ZERO_LENGTH_ARRAYS")
-    private static byte[] address2bin(final String value) {
-        //TODO: translate ipv4 or ipv6 into byte[]
         return null;
     }
 
@@ -69,7 +54,7 @@ public class SwitchConnectionProviderFactoryImpl implements SwitchConnectionProv
             this.config = config;
 
             try {
-                address = extractIpAddressBin(config.getAddress());
+                address = getInetAddress(config.getAddress());
             } catch (UnknownHostException e) {
                 throw new RuntimeException(e);
             }
