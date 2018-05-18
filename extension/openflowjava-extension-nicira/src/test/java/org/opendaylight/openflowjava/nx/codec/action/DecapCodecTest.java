20f0968b7c537cee56c8822523dc3f340ee982bd
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-05-18 14:26:44 +0200

    Add support for encap/decap nicira actions
    Encap action is used to encapsulate the packet with a new packet header,
    while decap action is used to decapsulate the outer packet header. Both
    actions are particularly useful for application using nsh encapsulation.
    
    From [1], the structure of encap action is:
    
    struct nx_action_encap {
        ovs_be16 type;         // OFPAT_VENDOR = EXPERIMENTER = 0xFFFF
        ovs_be16 len;
        ovs_be32 vendor;       // NX_VENDOR_ID = NXOXM_ET = 0x00002320
        ovs_be16 subtype;      // NXAST_ENCAP = 46
        ovs_be16 hdr_size;     // Header size in bytes, 0 = 'not specified'
        ovs_be32 new_pkt_type; // Header type to add
        struct ofp_ed_prop_header props[];  // Encap TLV properties
    };
    
    Properties are specific to the new packet type, such as the md type or
    type 2 metadata in case of nsh. This patch does not include support for
    properties. In case of nsh, when no properties are present,
    md type defaults to 1.
    
    From [1], the structure of decap action is:
    
    struct nx_action_encap {
        ovs_be16 type;         // OFPAT_VENDOR = EXPERIMENTER = 0xFFFF
        ovs_be16 len;
        ovs_be32 vendor;       // NX_VENDOR_ID = NXOXM_ET = 0x00002320
        ovs_be16 subtype;      // NXAST_ENCAP = 47
        uint8_t pad[2];        // 2 bytes padding
        ovs_be32 new_pkt_type; // new packet type, 0xFFFE = USE NEXT PROTO
    };
    
    new_pkt_type is the desired packet type required after decapsulation.
    The special value 0xFFFE sets the packet type automatically based on the
    outermost header type of the remaining packet.
    
    See [1] and [2] for reference.
    
    [1] https://github.com/openvswitch/ovs/blob/master/lib/ofp-actions.c
    [2] http://www.openvswitch.org/support/dist-docs/ovs-ofctl.8.txt
    
    Change-Id: Ia313c68ac17618f89c5e64c9351a0033facf9066
    JIRA: OPNFLWPLUG-1017
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

---
+++ extension/openflowjava-extension-nicira/src/test/java/org/opendaylight/openflowjava/nx/codec/action/DecapCodecTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018 SUSE LINUX GmbH.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.nx.codec.action;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.openflowjava.nx.api.NiciraConstants;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.ExperimenterId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionDecap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionDecapBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.decap.grouping.NxActionDecapBuilder;
+
+public class DecapCodecTest {
+
+    private static final int LENGTH = 16;
+    private static final int SUBTYPE = 47;
+    private static final long PACKET_TYPE = 0xFFFEL;
+
+    DecapCodec decapCodec;
+    ByteBuf buffer;
+    Action action;
+
+    @Before
+    public void setUp() {
+        decapCodec = new DecapCodec();
+        buffer = ByteBufAllocator.DEFAULT.buffer();
+    }
+
+    @Test
+    public void deserialize() {
+        createBuffer(buffer);
+
+        action = decapCodec.deserialize(buffer);
+
+        ActionDecap result = (ActionDecap) action.getActionChoice();
+
+        assertEquals(PACKET_TYPE, result.getNxActionDecap().getPacketType().longValue());
+        assertFalse(buffer.isReadable());
+    }
+
+    @Test
+    public void serialize() {
+        action = createAction();
+        decapCodec.serialize(action, buffer);
+
+        assertEquals(LENGTH, buffer.readableBytes());
+        //SerializeHeader part
+        assertEquals(EncodeConstants.EXPERIMENTER_VALUE, buffer.readUnsignedShort());
+        assertEquals(LENGTH, buffer.readUnsignedShort());
+        assertEquals(NiciraConstants.NX_VENDOR_ID.intValue(), buffer.readUnsignedInt());
+        assertEquals(SUBTYPE, buffer.readUnsignedShort());
+        //Serialize part
+        assertEquals(0, buffer.readUnsignedShort());
+        assertEquals(PACKET_TYPE, buffer.readUnsignedInt());
+        assertFalse(buffer.isReadable());
+    }
+
+    private void createBuffer(ByteBuf message) {
+        message.writeShort(EncodeConstants.EXPERIMENTER_VALUE);
+        message.writeShort(LENGTH);
+        message.writeInt(NiciraConstants.NX_VENDOR_ID.intValue());
+        message.writeShort(SUBTYPE);
+        message.writeZero(2);
+        message.writeInt((int) PACKET_TYPE);
+    }
+
+    private Action createAction() {
+        ExperimenterId experimenterId = new ExperimenterId(NiciraConstants.NX_VENDOR_ID);
+        ActionBuilder actionBuilder = new ActionBuilder();
+        actionBuilder.setExperimenterId(experimenterId);
+        final ActionDecapBuilder actionDecapBuilder = new ActionDecapBuilder();
+        NxActionDecapBuilder nxActionDecapBuilder = new NxActionDecapBuilder();
+
+        nxActionDecapBuilder.setPacketType(PACKET_TYPE);
+
+        actionDecapBuilder.setNxActionDecap(nxActionDecapBuilder.build());
+        actionBuilder.setActionChoice(actionDecapBuilder.build());
+
+        return actionBuilder.build();
+    }
+}
