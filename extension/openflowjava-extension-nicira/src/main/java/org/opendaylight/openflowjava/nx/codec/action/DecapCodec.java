3cdc9ca78c657f360d87ea3e623aba6083d2e687
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-05-18 14:26:44 +0200

    Add support for encap/decap nicira actions
    Encap action is used to encapsulate the packet with a new packet header,
    while decap action is used to decapsulate the outer packet header. Both
    actions are particularly useful for application using nsh encapsulation.
    
    From [1], the structure of encap action is:
    
    struct nx_action_encap {
        ovs_be16 type;         // OFPAT_VENDOR = EXPERIMENTER = 0xFFFF
        ovs_be16 len;
        ovs_be32 vendor;       // NX_VENDOR_ID = NXOXM_ET = 0x00002320
        ovs_be16 subtype;      // NXAST_ENCAP = 46
        ovs_be16 hdr_size;     // Header size in bytes, 0 = 'not specified'
        ovs_be32 new_pkt_type; // Header type to add
        struct ofp_ed_prop_header props[];  // Encap TLV properties
    };
    
    Properties are specific to the new packet type, such as the md type or
    type 2 metadata in case of nsh. This patch does not include support for
    properties. In case of nsh, when no properties are present,
    md type defaults to 1.
    
    From [1], the structure of decap action is:
    
    struct nx_action_encap {
        ovs_be16 type;         // OFPAT_VENDOR = EXPERIMENTER = 0xFFFF
        ovs_be16 len;
        ovs_be32 vendor;       // NX_VENDOR_ID = NXOXM_ET = 0x00002320
        ovs_be16 subtype;      // NXAST_ENCAP = 47
        uint8_t pad[2];        // 2 bytes padding
        ovs_be32 new_pkt_type; // new packet type, 0xFFFE = USE NEXT PROTO
    };
    
    new_pkt_type is the desired packet type required after decapsulation.
    The special value 0xFFFE sets the packet type automatically based on the
    outermost header type of the remaining packet.
    
    See [1] and [2] for reference.
    
    [1] https://github.com/openvswitch/ovs/blob/master/lib/ofp-actions.c
    [2] http://www.openvswitch.org/support/dist-docs/ovs-ofctl.8.txt
    
    Change-Id: Ia313c68ac17618f89c5e64c9351a0033facf9066
    JIRA: OPNFLWPLUG-1017
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

---
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/action/DecapCodec.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 SUSE LINUX GmbH.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.nx.codec.action;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.nx.api.NiciraActionDeserializerKey;
+import org.opendaylight.openflowjava.nx.api.NiciraActionSerializerKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionDecap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionDecapBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.decap.grouping.NxActionDecap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.decap.grouping.NxActionDecapBuilder;
+
+public class DecapCodec extends AbstractActionCodec {
+
+    public static final int LENGTH = 16;
+    private static final int NXAST_DECAP_SUBTYPE = 47;
+    private static final int PADDING = 2;
+
+    public static final NiciraActionSerializerKey SERIALIZER_KEY =
+            new NiciraActionSerializerKey(EncodeConstants.OF13_VERSION_ID, ActionDecap.class);
+    public static final NiciraActionDeserializerKey DESERIALIZER_KEY =
+            new NiciraActionDeserializerKey(EncodeConstants.OF13_VERSION_ID, NXAST_DECAP_SUBTYPE);
+
+    @Override
+    public Action deserialize(ByteBuf message) {
+        final ActionBuilder actionBuilder = deserializeHeader(message);
+        // skip padding
+        message.skipBytes(PADDING);
+        long packetType = message.readUnsignedInt();
+        NxActionDecap nxActionDecap = new NxActionDecapBuilder().setPacketType(packetType).build();
+        ActionDecap actionDecap = new ActionDecapBuilder().setNxActionDecap(nxActionDecap).build();
+        actionBuilder.setActionChoice(actionDecap);
+        return actionBuilder.build();
+    }
+
+    @Override
+    public void serialize(Action input, ByteBuf outBuffer) {
+        serializeHeader(LENGTH, NXAST_DECAP_SUBTYPE, outBuffer);
+        // add padding
+        outBuffer.writeZero(PADDING);
+        ActionDecap actionDecap = (ActionDecap) input.getActionChoice();
+        Long packetType = actionDecap.getNxActionDecap().getPacketType();
+        outBuffer.writeInt(packetType.intValue());
+    }
+}
---
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/action/EncapCodec.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2018 SUSE LINUX GmbH.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.nx.codec.action;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.nx.api.NiciraActionDeserializerKey;
+import org.opendaylight.openflowjava.nx.api.NiciraActionSerializerKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionEncap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionEncapBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.encap.grouping.NxActionEncap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.encap.grouping.NxActionEncapBuilder;
+
+public class EncapCodec extends AbstractActionCodec {
+
+    public static final int LENGTH = 16;
+    private static final int NXAST_ENCAP_SUBTYPE = 46;
+    private static final int HEADER_SIZE_NOT_SPECIFIED = 0;
+
+    public static final NiciraActionSerializerKey SERIALIZER_KEY =
+            new NiciraActionSerializerKey(EncodeConstants.OF13_VERSION_ID, ActionEncap.class);
+    public static final NiciraActionDeserializerKey DESERIALIZER_KEY =
+            new NiciraActionDeserializerKey(EncodeConstants.OF13_VERSION_ID, NXAST_ENCAP_SUBTYPE);
+
+    @Override
+    public Action deserialize(ByteBuf message) {
+        final ActionBuilder actionBuilder = deserializeHeader(message);
+        // skip header size, not used
+        message.skipBytes(EncodeConstants.SIZE_OF_SHORT_IN_BYTES);
+        final long packetType = message.readUnsignedInt();
+        NxActionEncap nxActionEncap = new NxActionEncapBuilder().setPacketType(packetType).build();
+        ActionEncap actionEncap = new ActionEncapBuilder().setNxActionEncap(nxActionEncap).build();
+        actionBuilder.setActionChoice(actionEncap);
+        return actionBuilder.build();
+    }
+
+    @Override
+    public void serialize(Action input, ByteBuf outBuffer) {
+        serializeHeader(LENGTH, NXAST_ENCAP_SUBTYPE, outBuffer);
+        outBuffer.writeShort(HEADER_SIZE_NOT_SPECIFIED);
+        ActionEncap actionEncap = (ActionEncap) input.getActionChoice();
+        Long packetType = actionEncap.getNxActionEncap().getPacketType();
+        outBuffer.writeInt(packetType.intValue());
+    }
+}
