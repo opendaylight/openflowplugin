fd7570b68a3ea801e7be20c2cf45de90e5ab081e
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-05-23 18:00:27 +0200

    Output reg support for nicira experimenter fields
    Implement NXAST_OUTPUT_REG2 which supports experimenter fields vs the
    existing NXAST_OUTPUT_REG that does not. The openflowplugin API will
    still expose a single output reg action, but this will be converted to
    openflowjava output reg2 action if the source choice is a experimenter
    field or to the existing output reg action otherwise.
    
    From [1], this is the structure of the output reg2 action:
    
    struct nx_action_output_reg2 {
        ovs_be16 type;            // OFPAT_EXPERIMENTER = 0xFFFF
        ovs_be16 len;             // 24.
        ovs_be32 vendor;          // NX_VENDOR_ID = 0x00002320
        ovs_be16 subtype;         // NXAST_OUTPUT_REG2 = 32
    
        ovs_be16 ofs_nbits;       // (ofs << 6) | (n_bits - 1).
        ovs_be16 max_len;         // Max length to send to controller.
        /* Followed by:
         * - 'src', as an OXM/NXM header (either 4 or 8 bytes).
         * - Enough 0-bytes to pad the action out to 24 bytes. */
    };
    
    [1] https://github.com/openvswitch/ovs/blob/master/lib/ofp-actions.c
    
    Change-Id: I6865ea6b8b3191895f2c548e7acf6707d94402e0
    JIRA: OPNFLWPLUG-1017
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

---
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/action/OutputReg2Codec.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 SUSE LINUX GmbH.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.nx.codec.action;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.openflowjava.nx.api.NiciraActionDeserializerKey;
+import org.opendaylight.openflowjava.nx.api.NiciraActionSerializerKey;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionOutputReg2;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.action.container.action.choice.ActionOutputReg2Builder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.action.rev140421.ofj.nx.action.output.reg2.grouping.NxActionOutputReg2Builder;
+
+public class OutputReg2Codec extends AbstractActionCodec {
+
+    public static final byte SUBTYPE = 32; // NXAST_OUTPUT_REG2
+    public static final NiciraActionSerializerKey SERIALIZER_KEY =
+            new NiciraActionSerializerKey(EncodeConstants.OF13_VERSION_ID, ActionOutputReg2.class);
+    public static final NiciraActionDeserializerKey DESERIALIZER_KEY =
+            new NiciraActionDeserializerKey(EncodeConstants.OF13_VERSION_ID, SUBTYPE);
+
+    @Override
+    public Action deserialize(ByteBuf message) {
+        final int startIndex = message.readerIndex();
+        final ActionBuilder actionBuilder = deserializeHeader(message);
+        final ActionOutputReg2Builder builder = new ActionOutputReg2Builder();
+        NxActionOutputReg2Builder nxActionOutputReg2Builder = new NxActionOutputReg2Builder();
+        nxActionOutputReg2Builder.setNBits(message.readUnsignedShort());
+        nxActionOutputReg2Builder.setMaxLen(message.readUnsignedShort());
+        nxActionOutputReg2Builder.setSrc(readNxmHeader(message));
+        skipPadding(message, startIndex);
+        builder.setNxActionOutputReg2(nxActionOutputReg2Builder.build());
+        actionBuilder.setActionChoice(builder.build());
+        return actionBuilder.build();
+    }
+
+    @Override
+    public void serialize(Action input, ByteBuf outBuffer) {
+        ActionOutputReg2 action = (ActionOutputReg2) input.getActionChoice();
+        final int startIndex = outBuffer.writerIndex();
+        serializeHeader(EncodeConstants.EMPTY_LENGTH, SUBTYPE, outBuffer);
+        outBuffer.writeShort(action.getNxActionOutputReg2().getNBits().shortValue());
+        outBuffer.writeShort(action.getNxActionOutputReg2().getMaxLen().shortValue());
+        writeNxmHeader(action.getNxActionOutputReg2().getSrc(), outBuffer);
+        writePaddingAndSetLength(outBuffer, startIndex);
+    }
+}
