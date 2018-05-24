83f5bb4ad52ef3c748a98819b8edd5d61212c869
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-05-24 18:09:39 +0200

    Refactor nsh fields to new encoding
    This path refactors the nsh fields encoding from the patched OVS
    2.5/2.6 implementation to the official OVS 2.8/2.9 implementation.
    Main difference on the encoding is that now the fields are
    experimenter under new code points.
    
    The fields affected are: nsh_mdtype, nsh_np, nsp, nsi, nsh_c1, nsh_c2,
    nsh_c3 and nsh_c4.
    
    Added mask for match maskable fields nsh_c1, nsh_c2, nsh_c3 and nsh_c4.
    
    This patch holds no breaking API changes for northbound applications
    but it does break compatibily with patched OVS 2.5/2.6 for the
    mentioned fields.
    
    Change-Id: Id70bcb0386592c47f76d28ff8a4f82267bb86571
    JIRA: OPNFLWPLUG-1017
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

--- extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Nshc4Codec.java
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Nshc4Codec.java
@@ -8,47 +8,27 @@
 
 package org.opendaylight.openflowjava.nx.codec.match;
 
-import io.netty.buffer.ByteBuf;
-
+import org.opendaylight.openflowjava.nx.api.NiciraConstants;
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntrySerializerKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
-import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.ExperimenterClass;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.MatchField;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm1Class;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.OxmClassBase;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntryBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxNshc4;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.ofj.nxm.nx.match.nshc._4.grouping.Nshc4ValuesBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.Nshc4CaseValue;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.Nshc4CaseValueBuilder;
 
-public class Nshc4Codec extends AbstractMatchCodec {
+public class Nshc4Codec extends AbstractNshcCodec {
 
-    private static final int VALUE_LENGTH = 4;
-    private static final int NXM_FIELD_CODE = 118;
-    public static final MatchEntrySerializerKey<Nxm1Class, NxmNxNshc4> SERIALIZER_KEY = new MatchEntrySerializerKey<>(
-            EncodeConstants.OF13_VERSION_ID, Nxm1Class.class, NxmNxNshc4.class);
-    public static final MatchEntryDeserializerKey DESERIALIZER_KEY = new MatchEntryDeserializerKey(
-            EncodeConstants.OF13_VERSION_ID, OxmMatchConstants.NXM_1_CLASS, NXM_FIELD_CODE);
-
-    @Override
-    public void serialize(MatchEntry input, ByteBuf outBuffer) {
-        serializeHeader(input, outBuffer);
-        Nshc4CaseValue csc1CaseValue = ((Nshc4CaseValue) input.getMatchEntryValue());
-        outBuffer.writeInt(csc1CaseValue.getNshc4Values().getNshc().intValue());
-    }
-
-    @Override
-    public MatchEntry deserialize(ByteBuf message) {
-        MatchEntryBuilder matchEntryBuilder = deserializeHeaderToBuilder(message);
-        Nshc4CaseValueBuilder nsc4CaseValueBuilder = new Nshc4CaseValueBuilder();
-        nsc4CaseValueBuilder.setNshc4Values(new Nshc4ValuesBuilder().setNshc(message.readUnsignedInt()).build());
-        matchEntryBuilder.setMatchEntryValue(nsc4CaseValueBuilder.build());
-
-        return matchEntryBuilder.build();
-    }
+    private static final int NXM_FIELD_CODE = 9;
+    public static final MatchEntrySerializerKey<ExperimenterClass, NxmNxNshc4> SERIALIZER_KEY =
+            createSerializerKey(
+                    EncodeConstants.OF13_VERSION_ID,
+                    NiciraConstants.NX_NSH_VENDOR_ID,
+                    NxmNxNshc4.class);
+    public static final MatchEntryDeserializerKey DESERIALIZER_KEY =
+            createDeserializerKey(
+                    EncodeConstants.OF13_VERSION_ID,
+                    NiciraConstants.NX_NSH_VENDOR_ID,
+                    NXM_FIELD_CODE);
 
     @Override
     public int getNxmFieldCode() {
@@ -56,22 +36,7 @@ public class Nshc4Codec extends AbstractMatchCodec {
     }
 
     @Override
-    public int getOxmClassCode() {
-        return OxmMatchConstants.NXM_1_CLASS;
-    }
-
-    @Override
-    public int getValueLength() {
-        return VALUE_LENGTH;
-    }
-
-    @Override
     public Class<? extends MatchField> getNxmField() {
         return NxmNxNshc4.class;
-    }
-
-    @Override
-    public Class<? extends OxmClassBase> getOxmClass() {
-        return Nxm1Class.class;
     }
 }
--- extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/NsiConvertor.java
+++ extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/NsiConvertor.java
@@ -9,16 +9,19 @@
 package org.opendaylight.openflowplugin.extension.vendor.nicira.convertor.match;
 
 import com.google.common.base.Optional;
+import org.opendaylight.openflowjava.nx.api.NiciraConstants;
 import org.opendaylight.openflowplugin.extension.api.ConvertorFromOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorToOFJava;
 import org.opendaylight.openflowplugin.extension.api.ExtensionAugment;
 import org.opendaylight.openflowplugin.extension.api.path.MatchPath;
 import org.opendaylight.openflowplugin.extension.vendor.nicira.convertor.CodecPreconditionException;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm1Class;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev150225.oxm.container.match.entry.value.ExperimenterIdCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.OfjAugNxExpMatch;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.ofj.nxm.nx.match.nsi.grouping.NsiValues;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.ofj.nxm.nx.match.nsi.grouping.NsiValuesBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.NsiCaseValue;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.NsiCaseValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.experimenter.id._case.nx.exp.match.entry.value.NsiCaseValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.oxm.container.match.entry.value.experimenter.id._case.nx.exp.match.entry.value.NsiCaseValueBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.ExtensionKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.grouping.Extension;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlow;
@@ -40,8 +43,13 @@ import org.opendaylight.yangtools.yang.binding.Augmentation;
 public class NsiConvertor implements ConvertorToOFJava<MatchEntry>, ConvertorFromOFJava<MatchEntry, MatchPath> {
     @Override
     public ExtensionAugment<? extends Augmentation<Extension>> convert(MatchEntry input, MatchPath path) {
-        NsiCaseValue nsiCaseValue = (NsiCaseValue) input.getMatchEntryValue();
-        return resolveAugmentation(new NxmNxNsiBuilder().setNsi(nsiCaseValue.getNsiValues().getNsi()).build(), path,
+        ExperimenterIdCase experimenterIdCase = (ExperimenterIdCase) input.getMatchEntryValue();
+        OfjAugNxExpMatch ofjAugNxExpMatch = experimenterIdCase.augmentation(OfjAugNxExpMatch.class);
+        NsiCaseValue nshNsiCaseValue = (NsiCaseValue) ofjAugNxExpMatch.getNxExpMatchEntryValue();
+
+        return resolveAugmentation(
+                new NxmNxNsiBuilder().setNsi(nshNsiCaseValue.getNsiValues().getNsi()).build(),
+                path,
                 NxmNxNsiKey.class);
     }
 
@@ -51,14 +59,18 @@ public class NsiConvertor implements ConvertorToOFJava<MatchEntry>, ConvertorFro
         if (!matchGrouping.isPresent()) {
             throw new CodecPreconditionException(extension);
         }
-        Short value = matchGrouping.get().getNxmNxNsi().getNsi();
+        Short nsiValue = matchGrouping.get().getNxmNxNsi().getNsi();
+        MatchEntry matchEntry = buildMatchEntry(nsiValue, null);
+        return matchEntry;
+    }
 
-        NsiCaseValueBuilder nsiCaseValueBuilder = new NsiCaseValueBuilder();
-        nsiCaseValueBuilder.setNsiValues(new NsiValuesBuilder()
-                .setNsi(value).build());
-        return MatchUtil.createDefaultMatchEntryBuilder(
+    public static MatchEntry buildMatchEntry(Short nsi, Short mask) {
+        NsiValues nsiValues = new NsiValuesBuilder().setNsi(nsi).setMask(mask).build();
+        NsiCaseValue nsiCaseValue = new NsiCaseValueBuilder().setNsiValues(nsiValues).build();
+        return MatchUtil.createExperimenterMatchEntryBuilder(
                 org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxNsi.class,
-                Nxm1Class.class, nsiCaseValueBuilder.build()).build();
+                NiciraConstants.NX_NSH_VENDOR_ID,
+                nsiCaseValue).setHasMask(mask != null).build();
     }
 
     private static ExtensionAugment<? extends Augmentation<Extension>> resolveAugmentation(NxmNxNsi value,
