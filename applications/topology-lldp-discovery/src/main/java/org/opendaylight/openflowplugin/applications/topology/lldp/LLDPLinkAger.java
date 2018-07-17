d8e4e03be2d47ec3a2e3b4c6f851db7c0fa5f932
Andrej Leitner <andrej.leitner@gmail.com>
2018-07-18 00:23:30 +0200

    OPNFLWPLUG-1027 : Topology manager writes link information everytime topology-lldp-discovery publish the linkAdded/linkRemove notificaiton
     Send new notifications only when the links are discovered
     and ignore already present links.
    
    Change-Id: I5ec6ae4d2cd8d2c83eb6e918f12cf12927056633
    JIRA: OPNFLWPLUG-1027
    Signed-off-by: Gobinath <gobinath@ericsson.com>

--- applications/topology-lldp-discovery/src/main/java/org/opendaylight/openflowplugin/applications/topology/lldp/LLDPLinkAger.java
+++ applications/topology-lldp-discovery/src/main/java/org/opendaylight/openflowplugin/applications/topology/lldp/LLDPLinkAger.java
@@ -8,6 +8,8 @@
 package org.opendaylight.openflowplugin.applications.topology.lldp;
 
 import com.google.common.annotations.VisibleForTesting;
+
+import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -20,44 +22,79 @@ import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.aries.blueprint.annotation.service.Reference;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.mdsal.binding.api.ClusteredDataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataObjectModification;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
 import org.opendaylight.openflowplugin.api.openflow.configuration.ConfigurationListener;
 import org.opendaylight.openflowplugin.api.openflow.configuration.ConfigurationService;
 import org.opendaylight.openflowplugin.applications.topology.lldp.utils.LLDPDiscoveryUtils;
+import org.opendaylight.openflowplugin.common.wait.SimpleTaskRetryLooper;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.topology.discovery.rev130819.LinkDiscovered;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.topology.discovery.rev130819.LinkRemoved;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.topology.discovery.rev130819.LinkRemovedBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.lldp.discovery.config.rev160511.TopologyLldpDiscoveryConfig;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-public class LLDPLinkAger implements ConfigurationListener, AutoCloseable {
+public class LLDPLinkAger implements ConfigurationListener, ClusteredDataTreeChangeListener<Link>, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(LLDPLinkAger.class);
+    private static final long STARTUP_LOOP_TICK = 500L;
+    private static final int STARTUP_LOOP_MAX_RETRIES = 8;
+    static final String TOPOLOGY_ID = "flow:1";
+    static final InstanceIdentifier<Link> II_TO_LINK = InstanceIdentifier.create(NetworkTopology.class)
+            .child(Topology.class, new TopologyKey(new TopologyId(TOPOLOGY_ID))).child(Link.class);
+
     private final long linkExpirationTime;
     private final Map<LinkDiscovered, Date> linkToDate;
     private final Timer timer;
     private final NotificationPublishService notificationService;
     private final AutoCloseable configurationServiceRegistration;
     private final EntityOwnershipService eos;
+    private final ListenerRegistration<DataTreeChangeListener> listenerRegistration;
 
     /**
      * default ctor - start timer.
      */
     @Inject
+    @SuppressWarnings("checkstyle:IllegalCatch")
     public LLDPLinkAger(final TopologyLldpDiscoveryConfig topologyLldpDiscoveryConfig,
-            @Reference final NotificationPublishService notificationService,
-            @Reference final ConfigurationService configurationService,
-            @Reference final EntityOwnershipService entityOwnershipService) {
+                        @Reference final NotificationPublishService notificationService,
+                        @Reference final ConfigurationService configurationService,
+                        @Reference final EntityOwnershipService entityOwnershipService,
+                        @Reference final DataBroker dataBroker) {
         this.linkExpirationTime = topologyLldpDiscoveryConfig.getTopologyLldpExpirationInterval().getValue();
         this.notificationService = notificationService;
         this.configurationServiceRegistration = configurationService.registerListener(this);
         this.eos = entityOwnershipService;
         linkToDate = new ConcurrentHashMap<>();
         timer = new Timer();
+        final DataTreeIdentifier dtiToNodeConnector = DataTreeIdentifier.create(LogicalDatastoreType.OPERATIONAL,
+                II_TO_LINK);
+        final SimpleTaskRetryLooper looper = new SimpleTaskRetryLooper(STARTUP_LOOP_TICK, STARTUP_LOOP_MAX_RETRIES);
+        try {
+            listenerRegistration = looper.loopUntilNoException(() ->
+                    dataBroker.registerDataTreeChangeListener(dtiToNodeConnector, LLDPLinkAger.this));
+        } catch (Exception e) {
+            LOG.error("DataTreeChangeListeners registration failed:", e);
+            throw new IllegalStateException("LLDPLinkAger startup failed!", e);
+        }
         timer.schedule(new LLDPAgingTask(), 0, topologyLldpDiscoveryConfig.getTopologyLldpInterval().getValue());
     }
 
@@ -73,6 +110,36 @@ public class LLDPLinkAger implements ConfigurationListener, AutoCloseable {
         timer.cancel();
         linkToDate.clear();
         configurationServiceRegistration.close();
+    }
+
+    @Override
+    public void onDataTreeChanged(@NonNull Collection<DataTreeModification<Link>> changes) {
+        for (DataTreeModification modification : changes) {
+            switch (modification.getRootNode().getModificationType()) {
+                case WRITE:
+                    break;
+                case SUBTREE_MODIFIED:
+                    // NOOP
+                    break;
+                case DELETE:
+                    processLinkDeleted(modification.getRootNode());
+                    break;
+                default:
+                    throw new IllegalArgumentException(
+                            "Unhandled modification type: {}" + modification.getRootNode().getModificationType());
+            }
+        }
+    }
+
+    private void processLinkDeleted(DataObjectModification rootNode) {
+        Link link = (Link) rootNode.getDataBefore();
+        if (LLDPDiscoveryUtils.isEntityOwned(eos, link.getDestination().getDestNode().getValue())) {
+            LinkDiscovered linkDiscovered = LLDPDiscoveryUtils
+                    .toLLDPLinkDiscovered(link);
+            LOG.info("linktoDate contains {}", linkToDate.keySet());
+            linkToDate.remove(linkDiscovered);
+            LOG.info("linktoDate contains after deletion {}", linkToDate.keySet());
+        }
     }
 
     private class LLDPAgingTask extends TimerTask {
@@ -100,13 +167,17 @@ public class LLDPLinkAger implements ConfigurationListener, AutoCloseable {
                                 LOG.warn("Interrupted while publishing notification {}", lr, e);
                             }
                         } else {
-                            LOG.trace("Skip publishing Link Remove event for the link {} because link destination "
+                            LOG.info("Skip publishing Link Remove event for the link {} because link destination "
                                     + "node is not owned by the controller", link);
                         }
                     }
                 }
             }
         }
+    }
+
+    public boolean isLinkPresent(final LinkDiscovered linkDiscovered) {
+        return linkToDate.containsKey(linkDiscovered);
     }
 
     @VisibleForTesting
