942c33beba4db0846976f2e2da5a9b6c62e6eb9e
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2018-08-01 11:47:28 +0530

    JIRA: OPNFLWPLUG-1028 Fix for Table Miss Entry failed to program in 3 node netvirt CSIT issue
    After openflowplugin-impl choosing the nodeOwner, it was publishing the
    Master role to the forwardingrules-manager through yang notification, at
    the same time, node information is written to the operational datastore.
    
    Post node information written to operational datastore, applications
    immediately start pushing the flows to forwardingrules-manager, by that
    time yang notification was failing to reach forwardingrules-manager.
    Which was causing the dealy in proving the mastership and consequesnt
    flows were getting rejected.
    
    Making DeviceMastershipManager to register for MastershipChangeService,
    so that it can quickly set master role before any flow arrives
    
    Change-Id: Id530846c8a8ca75425ccfebfe9742043bdcfb08d
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

--- applications/forwardingrules-manager/src/test/java/test/mock/TableFeaturesListenerTest.java
+++ applications/forwardingrules-manager/src/test/java/test/mock/TableFeaturesListenerTest.java
@@ -19,9 +19,9 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.openflowplugin.api.openflow.mastership.MastershipChangeServiceManager;
 import org.opendaylight.openflowplugin.applications.frm.impl.DeviceMastershipManager;
 import org.opendaylight.openflowplugin.applications.frm.impl.ForwardingRulesManagerImpl;
 import org.opendaylight.openflowplugin.applications.reconciliation.ReconciliationManager;
@@ -51,14 +51,15 @@ public class TableFeaturesListenerTest extends FRMTest {
     @Mock
     DeviceMastershipManager deviceMastershipManager;
     @Mock
-    private NotificationProviderService notificationService;
-    @Mock
     private ReconciliationManager reconciliationManager;
+    @Mock
+    private MastershipChangeServiceManager mastershipChangeServiceManager;
 
     @Before
     public void setUp() {
         forwardingRulesManager = new ForwardingRulesManagerImpl(getDataBroker(), rpcProviderRegistryMock, getConfig(),
-                clusterSingletonService, notificationService, getConfigurationService(), reconciliationManager);
+                mastershipChangeServiceManager, clusterSingletonService, getConfigurationService(),
+                reconciliationManager);
 
         forwardingRulesManager.start();
         // TODO consider tests rewrite (added because of complicated access)
