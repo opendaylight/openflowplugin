321cfb1d32750fe44867130f24b07749bbc2aa33
Robert Varga <robert.varga@pantheon.tech>
2018-11-15 14:46:09 +0100

    Replace GroupingResolver with AugmentationGroupingResolver
    AugmentationGroupingResolver provides the same functionality with
    an immutable implementation, geared towards low memory overhead
    both in terms of static allocation and memory usage during traversal.
    
    GroupingResolver.getExtension() functionality is exposed as
    findExtension(), returning java.util.Optional -- thus allowing
    for a clean option to retrofit a @Nullable getter if the need
    arises.
    
    JIRA: OPNFLWPLUG-1050
    Change-Id: I7346ff41c6b1425a9f95dccb11547836f0a15c53
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/CtZoneConvertor.java
+++ extension/openflowplugin-extension-nicira/src/main/java/org/opendaylight/openflowplugin/extension/vendor/nicira/convertor/match/CtZoneConvertor.java
@@ -5,10 +5,9 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.extension.vendor.nicira.convertor.match;
 
-import com.google.common.base.Optional;
+import java.util.Optional;
 import org.opendaylight.openflowplugin.extension.api.ConvertorFromOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorToOFJava;
 import org.opendaylight.openflowplugin.extension.api.ExtensionAugment;
@@ -56,7 +55,7 @@ public class CtZoneConvertor implements ConvertorToOFJava<MatchEntry>, Convertor
 
     @Override
     public MatchEntry convert(Extension extension) {
-        Optional<NxmNxCtZoneGrouping> matchGrouping = MatchUtil.CT_ZONE_RESOLVER.getExtension(extension);
+        Optional<NxmNxCtZoneGrouping> matchGrouping = MatchUtil.CT_ZONE_RESOLVER.findExtension(extension);
         if (!matchGrouping.isPresent()) {
             throw new CodecPreconditionException(extension);
         }
