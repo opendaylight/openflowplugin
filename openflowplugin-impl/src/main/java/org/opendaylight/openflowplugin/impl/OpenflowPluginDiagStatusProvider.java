1f9ee539070799534b5fee3d407f03179892c505
Michael Vorburger <vorburger@redhat.com>
2018-11-29 00:04:36 +0100

    remove "polling" from OpenflowPluginDiagStatusProvider
    diagstatus offers two ways if integrating: Either a module knows it
    status changes and can therefore report it (push from module to
    diagstatus infra).  Or a module has no clear status change points, and
    wants to be asked when status is required to be provided (pull from
    infra to a module; the DatastoreServiceStatusProvider is a good example
    of where this is required).
    
    The OpenflowPluginDiagStatusProvider however implemented a mix of those
    two modes, which IMHO is not required - OpenFlowPluginProviderImpl seems
    to know exactly when it is done starting switch connections.
    
    We add report ERROR when shutting down, emulating what polling would
    have reported if one were to check diagstatus during shutdown.
    
    If there are additional scenarios where whatever is started to listen on
    those ports (TcpHandler ?) closes, or dies, then those places should
    simply also use report ERROR via openflowPluginStatusMonitor.
    
    This reverts I8b41cd32990e7c4ea14cabbf862008fe77c7cc91, and is an
    alternative to I4ece338a4f0cf01c98e2b9b02557f0406faf27ae.
    
    JIRA: OPNFLWPLUG-1053
    Change-Id: Ieb78f225c1fcdd2ebf72ad1a99a49d4993a6a5b6
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/OpenflowPluginDiagStatusProvider.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/OpenflowPluginDiagStatusProvider.java
@@ -7,52 +7,27 @@
  */
 package org.opendaylight.openflowplugin.impl;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.aries.blueprint.annotation.service.Reference;
-import org.apache.aries.blueprint.annotation.service.Service;
 import org.opendaylight.infrautils.diagstatus.DiagStatusService;
 import org.opendaylight.infrautils.diagstatus.ServiceDescriptor;
 import org.opendaylight.infrautils.diagstatus.ServiceState;
-import org.opendaylight.infrautils.diagstatus.ServiceStatusProvider;
-import org.opendaylight.openflowjava.protocol.spi.connection.SwitchConnectionProvider;
-import org.opendaylight.openflowjava.protocol.spi.connection.SwitchConnectionProviderList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Service(classes = ServiceStatusProvider.class)
-public class OpenflowPluginDiagStatusProvider implements ServiceStatusProvider {
+public class OpenflowPluginDiagStatusProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(OpenflowPluginDiagStatusProvider.class);
     private static final String OPENFLOW_SERVICE_NAME = "OPENFLOW";
-    private static final int OF_PORT_11 = 6633;
-    private static final int OF_PORT_13 = 6653;
 
     private final DiagStatusService diagStatusService;
-    private InetAddress defaultInetAddres;
-    private InetAddress legacyInetAddress;
 
     @Inject
-    public OpenflowPluginDiagStatusProvider(final @Reference DiagStatusService diagStatusService,
-                                            final SwitchConnectionProviderList switchConnectionProviders) {
+    public OpenflowPluginDiagStatusProvider(final @Reference DiagStatusService diagStatusService) {
         this.diagStatusService = diagStatusService;
-        setSwitchConnectionInetAddress(switchConnectionProviders);
         diagStatusService.register(OPENFLOW_SERVICE_NAME);
-    }
-
-    private void setSwitchConnectionInetAddress(final List<SwitchConnectionProvider> switchConnectionProviders) {
-        switchConnectionProviders.forEach(switchConnectionProvider -> {
-            if (switchConnectionProvider.getConfiguration().getPort() == OF_PORT_11) {
-                legacyInetAddress = switchConnectionProvider.getConfiguration().getAddress();
-            } else if (switchConnectionProvider.getConfiguration().getPort() == OF_PORT_13) {
-                defaultInetAddres = switchConnectionProvider.getConfiguration().getAddress();
-            }
-        });
     }
 
     public void reportStatus(ServiceState serviceState) {
@@ -68,40 +43,5 @@ public class OpenflowPluginDiagStatusProvider implements ServiceStatusProvider {
     public void reportStatus(ServiceState serviceState, String description) {
         LOG.debug("reporting status as {} for {}", serviceState, OPENFLOW_SERVICE_NAME);
         diagStatusService.report(new ServiceDescriptor(OPENFLOW_SERVICE_NAME, serviceState, description));
-    }
-
-    @Override
-    public ServiceDescriptor getServiceDescriptor() {
-        if (getApplicationNetworkState(OF_PORT_13, defaultInetAddres)
-                && getApplicationNetworkState(OF_PORT_11, legacyInetAddress)) {
-            return new ServiceDescriptor(OPENFLOW_SERVICE_NAME, ServiceState.OPERATIONAL,
-                    "OF::PORTS:: 6653 and 6633 are up.");
-        } else {
-            return new ServiceDescriptor(OPENFLOW_SERVICE_NAME, ServiceState.ERROR,
-                    "OF::PORTS:: 6653 and 6633 are not up yet");
-        }
-    }
-
-    private boolean getApplicationNetworkState(int port, InetAddress inetAddress) {
-        Socket socket = null;
-        try {
-            if (inetAddress == null) {
-                socket = new Socket("localhost", port);
-            } else {
-                socket = new Socket(inetAddress, port);
-            }
-            LOG.debug("Socket connection established");
-            return true;
-        } catch (IOException e) {
-            return false;
-        } finally {
-            try {
-                if (socket != null) {
-                    socket.close();
-                }
-            } catch (IOException ex) {
-                LOG.error("Failed to close socket : {}", socket, ex);
-            }
-        }
     }
 }
