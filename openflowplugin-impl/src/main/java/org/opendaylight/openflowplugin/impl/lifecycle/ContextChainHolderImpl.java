3d469e56308afc055d4d071cae63b4e6dd360fe5
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2018-11-28 16:42:14 +0530

    Fix for DPNs not connecting to controller
    Change-Id: If708df53a0102bb85fe0040b260f821b4d35773c
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/ContextChainHolderImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/ContextChainHolderImpl.java
@@ -380,7 +380,7 @@ public class ContextChainHolderImpl implements ContextChainHolder, MasterChecker
             public void onFailure(final Throwable throwable) {
                 OF_EVENT_LOG.debug("Reconciliation framework failure for device {} with error {}", deviceInfo,
                         throwable.getMessage());
-                LOG.warn("Reconciliation framework failure.");
+                LOG.warn("Reconciliation framework onFailure for device {}", deviceInfo, throwable);
                 destroyContextChain(deviceInfo);
             }
         };
