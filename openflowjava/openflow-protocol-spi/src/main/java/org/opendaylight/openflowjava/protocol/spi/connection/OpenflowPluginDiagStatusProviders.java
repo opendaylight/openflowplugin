ac8c76d0578cb8acfabb1c63b250b310cd64c79e
Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>
2019-01-21 17:44:40 +0530

    Checking
    Change-Id: I6d192ca72153f48892d8ee14c6e0d10f41c40dfc
    Signed-off-by: Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>

---
+++ openflowjava/openflow-protocol-spi/src/main/java/org/opendaylight/openflowjava/protocol/spi/connection/OpenflowPluginDiagStatusProviders.java
@@ -0,0 +1,17 @@
+
+package org.opendaylight.openflowjava.protocol.spi.connection;
+
+import org.opendaylight.infrautils.diagstatus.ServiceState;
+
+public interface OpenflowPluginDiagStatusProviders {
+
+    public void reportStatus(ServiceState serviceState);
+
+
+    public void reportStatus(ServiceState serviceState, Throwable throwable);
+
+    public void reportStatus(ServiceState serviceState, String description);
+
+}
+
+
