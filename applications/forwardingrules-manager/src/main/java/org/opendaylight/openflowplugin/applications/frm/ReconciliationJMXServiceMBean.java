b83a894c79580dcae3ce640150b6642a92be132a
Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>
2019-02-07 15:08:45 +0530

    CSC  to provide a view of DPN re-sync status from CLI
    Change-Id: I6153f394803e43abf77e871f993d9ca7a1bedd45
    Signed-off-by: Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>

---
+++ applications/forwardingrules-manager/src/main/java/org/opendaylight/openflowplugin/applications/frm/ReconciliationJMXServiceMBean.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.applications.frm;
+
+import java.util.Map;
+
+public interface ReconciliationJMXServiceMBean {
+    /* The MapKey is datapathId
+       The MapValue is ReconciliationStates of the respective nodes.
+     */
+    Map<String, String> acquireReconciliationStates();
+}
