a183a74d2dd182e9f8d934a7797cc1b30b2093dc
Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>
2019-02-07 15:08:45 +0530

    CSC  to provide a view of DPN re-sync status from CLI
    Change-Id: I6153f394803e43abf77e871f993d9ca7a1bedd45
    Signed-off-by: Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>

--- applications/forwardingrules-manager/src/test/java/test/mock/TableFeaturesListenerTest.java
+++ applications/forwardingrules-manager/src/test/java/test/mock/TableFeaturesListenerTest.java
@@ -20,6 +20,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.mdsal.binding.api.WriteTransaction;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.openflowplugin.api.openflow.FlowGroupCacheManager;
 import org.opendaylight.openflowplugin.api.openflow.mastership.MastershipChangeServiceManager;
 import org.opendaylight.openflowplugin.applications.frm.impl.DeviceMastershipManager;
 import org.opendaylight.openflowplugin.applications.frm.impl.ForwardingRulesManagerImpl;
@@ -59,14 +60,15 @@ public class TableFeaturesListenerTest extends FRMTest {
     private ServiceRecoveryRegistry serviceRecoveryRegistry;
     @Mock
     private MastershipChangeServiceManager mastershipChangeServiceManager;
+    @Mock
+    private FlowGroupCacheManager flowGroupCacheManager;
 
     @Before
     public void setUp() {
         forwardingRulesManager = new ForwardingRulesManagerImpl(getDataBroker(), rpcProviderRegistryMock,
                 rpcProviderRegistryMock, getConfig(), mastershipChangeServiceManager, clusterSingletonService,
                 getConfigurationService(), reconciliationManager, openflowServiceRecoveryHandler,
-                serviceRecoveryRegistry);
-
+                serviceRecoveryRegistry, flowGroupCacheManager);
         forwardingRulesManager.start();
         // TODO consider tests rewrite (added because of complicated access)
         forwardingRulesManager.setDeviceMastershipManager(deviceMastershipManager);
