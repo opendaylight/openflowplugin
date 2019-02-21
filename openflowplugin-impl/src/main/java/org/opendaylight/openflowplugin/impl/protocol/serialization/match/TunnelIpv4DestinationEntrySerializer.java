a54f35ca6d3f65bbb7a27dd34252c08ceb484d3f
Robert Varga <robert.varga@pantheon.tech>
2019-02-21 11:46:00 +0100

    Rework MatchEntrySerializer
    MatchEntrySerializer has a filter, which is being applied before
    main serialization, and that is the only way it is being used --
    we check if the match is present and if it is we run serialize().
    
    This leads to quite a bit of code duplication as well as inefficient
    execution, MatchSerializer performs two invocations and each of them
    performs at least one traversal of the match.
    
    Change matchTypeCheck() to serializeIfPresent(), which means
    MatchSerializer does not invoke two method. This also allows us to
    optimize the serialization process in AbstractMatchEntrySerializer
    and its subclasses by traversing the match only once.
    
    Resulting code structure removes quite a bit of duplicated code
    from the individual EntrySerializer classes and lends itself to
    sharing functionality through common subclasses.
    
    The second part moves oxmField/oxmClass/valueLength to
    AbstractMatchEntrySerializer as fields, so that we get monomorphic
    invocation of these methods, which is readily inlineable and
    devirtualizable. The resulting memory overhead is insignificant as
    we only have one instance of each serializer and even that cost is
    offset by having less bytecode.
    
    Change-Id: I49b7a3c5770e939d082db8479f6d70a689d4c944
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/match/TunnelIpv4DestinationEntrySerializer.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/match/TunnelIpv4DestinationEntrySerializer.java
@@ -7,47 +7,21 @@
  */
 package org.opendaylight.openflowplugin.impl.protocol.serialization.match;
 
-import io.netty.buffer.ByteBuf;
-import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
-import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.IpConversionUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.Layer3Match;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.TunnelIpv4Match;
 
-public class TunnelIpv4DestinationEntrySerializer extends AbstractMatchEntrySerializer {
-
-    @Override
-    public void serialize(Match match, ByteBuf outBuffer) {
-        super.serialize(match, outBuffer);
-        // TODO: Why is this serialization exactly same as in Ipv4DestinationEntrySerializer?
-        writeIpv4Prefix(((TunnelIpv4Match) match.getLayer3Match()).getTunnelIpv4Destination(), outBuffer);
+// TODO: Why is this serialization exactly same as in Ipv4DestinationEntrySerializer?
+public class TunnelIpv4DestinationEntrySerializer extends AbstractIpv4PrefixEntrySerializer {
+    public TunnelIpv4DestinationEntrySerializer() {
+        super(OxmMatchConstants.OPENFLOW_BASIC_CLASS, OxmMatchConstants.IPV4_DST);
     }
 
     @Override
-    public boolean matchTypeCheck(Match match) {
-        return match.getLayer3Match() != null
-                && match.getLayer3Match() instanceof TunnelIpv4Match
-                && ((TunnelIpv4Match) match.getLayer3Match()).getTunnelIpv4Destination() != null;
-    }
-
-    @Override
-    protected boolean getHasMask(Match match) {
-        return IpConversionUtil.hasIpv4Prefix(((TunnelIpv4Match) match.getLayer3Match()).getTunnelIpv4Destination())
-                != null;
-    }
-
-    @Override
-    protected int getOxmFieldCode() {
-        return OxmMatchConstants.IPV4_DST;
-    }
-
-    @Override
-    protected int getOxmClassCode() {
-        return OxmMatchConstants.OPENFLOW_BASIC_CLASS;
-    }
-
-    @Override
-    protected int getValueLength() {
-        return EncodeConstants.SIZE_OF_INT_IN_BYTES;
+    protected Ipv4Prefix extractEntry(final Match match) {
+        final Layer3Match l3Match = match.getLayer3Match();
+        return l3Match instanceof TunnelIpv4Match ? ((TunnelIpv4Match) l3Match).getTunnelIpv4Destination() : null;
     }
 }
