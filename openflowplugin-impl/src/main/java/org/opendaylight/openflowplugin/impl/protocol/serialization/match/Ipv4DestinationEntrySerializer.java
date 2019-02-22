0ce17dfe91f24e2f5ddaa30b344b6d789300c4d6
Robert Varga <robert.varga@pantheon.tech>
2019-02-22 13:09:35 +0100

    Add IpConversionUtil.hasIpv4Prefix()
    This is a mirror method to the IPv6 case. The IPv6 version is
    also optimized to prevent unnecessary reboxing via a shared internal
    utility method.
    
    Change-Id: Ic32b95a52a197e5735f1740cbd7b2cb3530baa99
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/match/Ipv4DestinationEntrySerializer.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/match/Ipv4DestinationEntrySerializer.java
@@ -8,7 +8,6 @@
 package org.opendaylight.openflowplugin.impl.protocol.serialization.match;
 
 import io.netty.buffer.ByteBuf;
-import java.util.Iterator;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.common.IpConversionUtil;
@@ -52,13 +51,7 @@ public class Ipv4DestinationEntrySerializer extends AbstractMatchEntrySerializer
     @Override
     protected boolean getHasMask(Match match) {
         if (isPrefix(match)) {
-            // Split address to IP and mask
-            final Iterator<String> addressParts = IpConversionUtil.splitToParts(
-                    ((Ipv4Match) match.getLayer3Match()).getIpv4Destination());
-            addressParts.next();
-
-            // Check if we have mask
-            return addressParts.hasNext() && Integer.parseInt(addressParts.next()) < 32;
+            return IpConversionUtil.hasIpv4Prefix(((Ipv4Match) match.getLayer3Match()).getIpv4Destination()) != null;
         } else if (isArbitrary(match)) {
             return ((Ipv4MatchArbitraryBitMask) match.getLayer3Match()).getIpv4DestinationArbitraryBitmask() != null;
         }
