5091ba4439b7fc74bf544e50ddc96b5c21164f4d
nithi thomas <nithi.t@altencalsoftlabs.com>
2019-03-29 16:41:06 +0530

    NETVIRT-1586
    OVS Based NA Responder for IPv6 default Gateway
    
    Change-Id: I7e53b54097c6678046f5b216537129157081913c
    Signed-off-by: nithi thomas <nithi.t@altencalsoftlabs.com>

---
+++ extension/openflowjava-extension-eric/src/test/java/org/opendaylight/openflowjava/eric/codec/match/EricHeaderTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.eric.codec.match;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.opendaylight.openflowjava.eric.api.EricConstants;
+
+public class EricHeaderTest {
+
+    private static final int ERIC_FIELD_CODE = 4;
+    private static final int VALUE_LENGTH = 14;
+    private static long header;
+
+    private EricHeader ericHeader;
+
+    @Test
+    public void ericHeaderTest() {
+        header = createHeader();
+        ericHeader = new EricHeader(header);
+        assertEquals(EricConstants.ERICOXM_OF_EXPERIMENTER_ID, ericHeader.getOxmClass());
+        assertEquals(ERIC_FIELD_CODE, ericHeader.getEricField());
+        assertEquals(false, ericHeader.isHasMask());
+        assertEquals(VALUE_LENGTH, ericHeader.getLength());
+    }
+
+    @Test
+    public void equalsTest() {
+        Object notHeader = new Object();
+        header = createHeader();
+        ericHeader = new EricHeader(header);
+        assertFalse(ericHeader.equals(notHeader));
+    }
+
+    @Test
+    public void equalsTest1() {
+        header = createHeader();
+        ericHeader = new EricHeader(header);
+
+        assertTrue(ericHeader.equals(ericHeader));
+    }
+
+    @Test
+    public void toStringTest() {
+        header = createHeader();
+        ericHeader = new EricHeader(header);
+
+        String shouldBe = new String("EricHeader " + "[headerAsLong=" + header + ", " + "oxmClass="
+               + EricConstants.ERICOXM_OF_EXPERIMENTER_ID + "," + " ericField=" + ERIC_FIELD_CODE + "," + " hasMask="
+                + false + "," + " length=" + VALUE_LENGTH + "]");
+        assertEquals(shouldBe, ericHeader.toString());
+    }
+
+    private long createHeader() {
+        long result = 0;
+        int oxmClass = 4096 << 16;
+        result = result | oxmClass;
+        int oxmField = ERIC_FIELD_CODE << 9;
+        result = result | oxmField;
+        int mask = 0 << 8;
+        result = result | mask;
+        int length = VALUE_LENGTH;
+        result = result | length;
+        return result;
+    }
+
+}
---
+++ extension/openflowplugin-extension-eric/src/test/java/org/opendaylight/openflowplugin/extension/vendor/eric/convertor/match/Icmpv6NDOptionsTypeConvertorTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowplugin.extension.vendor.eric.convertor.match;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.openflowplugin.extension.api.ExtensionAugment;
+import org.opendaylight.openflowplugin.extension.api.GroupingLooseResolver;
+import org.opendaylight.openflowplugin.extension.api.path.MatchPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetField;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.set.field._case.SetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.match.entries.grouping.MatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.eric.match.rev180730.icmpv6.nd.options.type.grouping.Icmpv6NdOptionsTypeValuesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.eric.match.rev180730.oxm.container.match.entry.value.Icmpv6NdOptionsTypeCaseValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.eric.match.rev180730.oxm.container.match.entry.value.Icmpv6NdOptionsTypeCaseValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.EricAugMatchNodesNodeTableFlow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.EricAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.EricAugMatchNotifPacketIn;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.EricAugMatchNotifSwitchFlowRemoved;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.EricAugMatchRpcGetFlowStats;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.Icmpv6NdOptionsTypeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.eric.match.rev180730.eric.of.icmpv6.nd.options.type.grouping.EricOfIcmpv6NdOptionsTypeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.GeneralAugMatchNodesNodeTableFlowWriteActionsSetField;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.GeneralAugMatchNodesNodeTableFlowWriteActionsSetFieldBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.GeneralExtensionListGrouping;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.grouping.Extension;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.grouping.ExtensionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.list.grouping.ExtensionListBuilder;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+
+/**
+ * Test for {@link Icmpv6NDOptionsTypeConvertor}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class Icmpv6NDOptionsTypeConvertorTest {
+
+    @Mock
+    private Extension extension;
+    @Mock
+    private MatchEntry matchEntry;
+
+    private Icmpv6NDOptionsTypeConvertor icmpv6NDOptionsTypeConvertor;
+
+    @Before
+    public void setUp() throws Exception {
+        final EricOfIcmpv6NdOptionsTypeBuilder ericOfIcmpv6NdOptionsTypeBuilder = new EricOfIcmpv6NdOptionsTypeBuilder()
+                .setIcmpv6NdOptionsType((short)1);
+        final EricAugMatchNodesNodeTableFlowBuilder ericAugMatchNotifUpdateFlowStatsBuilder =
+                new EricAugMatchNodesNodeTableFlowBuilder();
+        ericAugMatchNotifUpdateFlowStatsBuilder.setEricOfIcmpv6NdOptionsType(ericOfIcmpv6NdOptionsTypeBuilder.build());
+
+        final Augmentation<Extension> extensionAugmentation = ericAugMatchNotifUpdateFlowStatsBuilder.build();
+        when(extension.augmentation(Matchers.<Class<Augmentation<Extension>>>any()))
+                .thenReturn(extensionAugmentation);
+
+        icmpv6NDOptionsTypeConvertor = new Icmpv6NDOptionsTypeConvertor();
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+        final MatchEntry converted = icmpv6NDOptionsTypeConvertor.convert(extension);
+        assertEquals(1, ((Icmpv6NdOptionsTypeCaseValue)converted.getMatchEntryValue())
+                .getIcmpv6NdOptionsTypeValues().getIcmpv6NdOptionsType().intValue());
+    }
+
+    @Test
+    public void testConvert1() throws Exception {
+        final Icmpv6NdOptionsTypeValuesBuilder icmpv6NdOptionsTypeValuesBuilder = new Icmpv6NdOptionsTypeValuesBuilder()
+                .setIcmpv6NdOptionsType((short)10);
+        final Icmpv6NdOptionsTypeCaseValueBuilder icmpv6NdOptionsTypeCaseValueBuilder
+                = new Icmpv6NdOptionsTypeCaseValueBuilder()
+                .setIcmpv6NdOptionsTypeValues(icmpv6NdOptionsTypeValuesBuilder.build());
+
+        final Icmpv6NdOptionsTypeCaseValue icmpv6NdOptionsTypeCaseValue = icmpv6NdOptionsTypeCaseValueBuilder.build();
+        when(matchEntry.getMatchEntryValue()).thenReturn(icmpv6NdOptionsTypeCaseValue);
+
+        final ExtensionAugment<? extends Augmentation<Extension>> extensionAugment
+                = icmpv6NDOptionsTypeConvertor.convert(matchEntry, MatchPath.PACKET_RECEIVED_MATCH);
+        assertEquals(10, ((EricAugMatchNotifPacketIn) extensionAugment.getAugmentationObject())
+                .getEricOfIcmpv6NdOptionsType().getIcmpv6NdOptionsType().intValue());
+        assertEquals(extensionAugment.getKey(), Icmpv6NdOptionsTypeKey.class);
+
+        final ExtensionAugment<? extends Augmentation<Extension>> extensionAugment1
+                = icmpv6NDOptionsTypeConvertor.convert(matchEntry, MatchPath.SWITCH_FLOW_REMOVED_MATCH);
+        assertEquals(10, ((EricAugMatchNotifSwitchFlowRemoved) extensionAugment1.getAugmentationObject())
+                .getEricOfIcmpv6NdOptionsType() .getIcmpv6NdOptionsType().intValue());
+        assertEquals(extensionAugment.getKey(), Icmpv6NdOptionsTypeKey.class);
+
+        final ExtensionAugment<? extends Augmentation<Extension>> extensionAugment2
+                = icmpv6NDOptionsTypeConvertor.convert(matchEntry, MatchPath.FLOWS_STATISTICS_UPDATE_MATCH);
+        assertEquals(10, ((EricAugMatchNodesNodeTableFlow) extensionAugment2.getAugmentationObject())
+                .getEricOfIcmpv6NdOptionsType().getIcmpv6NdOptionsType().intValue());
+        assertEquals(extensionAugment.getKey(), Icmpv6NdOptionsTypeKey.class);
+
+        final ExtensionAugment<? extends Augmentation<Extension>> extensionAugment3
+                = icmpv6NDOptionsTypeConvertor.convert(matchEntry, MatchPath.FLOWS_STATISTICS_RPC_MATCH);
+        assertEquals(10, ((EricAugMatchRpcGetFlowStats) extensionAugment3.getAugmentationObject())
+                .getEricOfIcmpv6NdOptionsType().getIcmpv6NdOptionsType().intValue());
+        assertEquals(extensionAugment.getKey(), Icmpv6NdOptionsTypeKey.class);
+    }
+
+    @Test
+    public void testSetFieldExtension() throws Exception {
+        GroupingLooseResolver<GeneralExtensionListGrouping> eqGroup =
+                new GroupingLooseResolver<>(GeneralExtensionListGrouping.class);
+        eqGroup.add(GeneralAugMatchNodesNodeTableFlowWriteActionsSetField.class);
+
+        ExtensionAugment<? extends Augmentation<Extension>> extensionMatch
+                =  new ExtensionAugment<>(EricAugMatchNodesNodeTableFlow.class,
+                new EricAugMatchNodesNodeTableFlowBuilder().setEricOfIcmpv6NdOptionsType(
+                        new EricOfIcmpv6NdOptionsTypeBuilder().setIcmpv6NdOptionsType((short)1).build()).build(),
+                Icmpv6NdOptionsTypeKey.class);
+
+        ExtensionListBuilder extListBld = null;
+        ExtensionBuilder extBld = new ExtensionBuilder();
+        extBld.addAugmentation(extensionMatch.getAugmentationClass(), extensionMatch.getAugmentationObject());
+
+        extListBld = new ExtensionListBuilder();
+        extListBld.setExtension(extBld.build());
+        extListBld.setExtensionKey(extensionMatch.getKey());
+
+        GeneralAugMatchNodesNodeTableFlowWriteActionsSetField ndOptionsTypeSetField =
+                 new GeneralAugMatchNodesNodeTableFlowWriteActionsSetFieldBuilder()
+                         .setExtensionList(Collections.singletonList(extListBld.build())).build();
+
+        SetFieldBuilder sb = new SetFieldBuilder();
+        SetField setField = sb.addAugmentation(GeneralAugMatchNodesNodeTableFlowWriteActionsSetField.class,
+                 ndOptionsTypeSetField).build();
+
+        Assert.assertEquals(Icmpv6NdOptionsTypeKey.class, eqGroup.getExtension(setField).get().getExtensionList().get(0)
+                 .getExtensionKey());
+    }
+
+}
