ec47bee41dc253a4d00d978925aaa46a60ce7efe
Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>
2019-06-07 12:15:08 +0530

    Creating Empty Tables in datastore only when statistics polling is enabled.
    Change-Id: Ia651042e35ceba5d2aea5f0579573cc68d81c90b
    Signed-off-by: Somashekhar Javalagi <somashekhar.manohara.javalagi@ericsson.com>

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/device/DeviceContextImplTest.java
@@ -258,8 +258,8 @@ public class DeviceContextImplTest {
                 deviceInitializerProvider,
                 true, false,
                 contextChainHolder,
-                queuedNotificationManager);
-
+                queuedNotificationManager,
+                false);
         ((DeviceContextImpl) deviceContext).lazyTransactionManagerInitialization();
         deviceContextSpy = Mockito.spy(deviceContext);
 
