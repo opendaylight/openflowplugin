04d54fe8e26c775fc9d1025852f2fa0e913883b3
Robert Varga <robert.varga@pantheon.tech>
2019-09-06 09:09:43 +0200

    Fix checkstyle
    Upgraded checkstyle is reporting issues, fix them up.
    
    Change-Id: I5ef8369369265ee31c151376694d8fceb37fb813
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 28f2bced3075329036dff49feaa771c8f7e24a9e)

--- openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/rpc/RpcContextImplTest.java
+++ openflowplugin-impl/src/test/java/org/opendaylight/openflowplugin/impl/rpc/RpcContextImplTest.java
@@ -190,5 +190,7 @@ public class RpcContextImplTest {
     }
 
     //Stub for RpcService class.
-    public class TestRpcService implements RpcService {}
+    public class TestRpcService implements RpcService {
+
+    }
 }
