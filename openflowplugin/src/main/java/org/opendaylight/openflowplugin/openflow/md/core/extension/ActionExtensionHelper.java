1b98df571b8206f7d1b3c748968ca83d2eb3c273
Ning Zhang <zhangninglc@inspur.com>
2019-10-30 18:01:57 +0800

    Read action throwing NPE
    Issue Description:
    When process alien action throwing NullPointerException:
    java.lang.NullPointerException: null
            at org.opendaylight.openflowplugin.openflow.md.core.extension.ActionExtensionHelper.processAlienAction(ActionExtensionHelper.java:46)
            at org.opendaylight.openflowplugin.impl.protocol.deserialization.util.ActionUtil.readAction(ActionUtil.java:66)
    
    Signed-off-by: Ning Zhang <zhangninglc@inspur.com>
    Change-Id: I4008eef616bb08de9333442a4f21c3f21b12c65e

--- openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/extension/ActionExtensionHelper.java
+++ openflowplugin/src/main/java/org/opendaylight/openflowplugin/openflow/md/core/extension/ActionExtensionHelper.java
@@ -42,6 +42,9 @@ public final class ActionExtensionHelper {
         if (extensionConvertorProvider == null) {
             return null;
         }
+        if (action == null) {
+            return null;
+        }
 
         if (action.getActionChoice() instanceof ExperimenterIdCase) {
             ExperimenterIdCase actionCase = (ExperimenterIdCase) action.getActionChoice();
