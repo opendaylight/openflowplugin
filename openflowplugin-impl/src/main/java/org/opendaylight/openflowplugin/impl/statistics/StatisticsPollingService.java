d06d7169912216e2fe40b1ffc77e344ae8401e93
VenkataSatya Jonnadula <rsankar@luminanetworks.com>
2020-02-10 14:26:03 +0530

    OPNFLWPLUG-1083: Stats frozen after applying 2 sec delay in OF channel
    Signed-off-by: VenkataSatya Jonnadula <rsankar@luminanetworks.com>
    Change-Id: I3a34ed327237ec2f765291cc45305683f561ae88

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsPollingService.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/StatisticsPollingService.java
@@ -14,14 +14,20 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.SettableFuture;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.openflowplugin.api.ConnectionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StatisticsPollingService extends AbstractScheduledService {
+    private static final Logger LOG = LoggerFactory.getLogger(StatisticsPollingService.class);
     private static final long DEFAULT_STATS_TIMEOUT = 50000;
 
     private final TimeCounter counter;
@@ -51,6 +57,9 @@ public class StatisticsPollingService extends AbstractScheduledService {
         counter.markStart();
     }
 
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    @SuppressFBWarnings(value = "REC_CATCH_EXCEPTION",
+            justification = "WaitFuture wait exception should not result in the statistics executor getting killed.")
     @Override
     protected void runOneIteration() throws Exception {
         final long averageTime = counter.getAverageTimeBetweenMarks();
@@ -71,6 +80,8 @@ public class StatisticsPollingService extends AbstractScheduledService {
 
         try {
             waitFuture.get(statsTimeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            LOG.error("Exception occured while waiting for the stats collection.", e);
         } finally {
             counter.addTimeMark();
         }
