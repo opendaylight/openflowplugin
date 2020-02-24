9178ab38f523a31701fa2fe801429d530c5bc8cf
dheenadayalan b <dhinua@gmail.com>
2020-02-24 15:32:56 +0530

    CSC  to provide a view of DPN re-sync status from CLI
    Signed-off-by: dheenadayalan b <dhinua@gmail.com>
    Change-Id: Ibf3e02fe4352a2bd559b838f8788ca18bc2ab83d
    Signed-off-by: dheenadayalan b <dhinua@gmail.com>

---
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/FlowGroupCacheManager.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.openflowplugin.api.openflow;
+
+import java.util.Map;
+
+public interface FlowGroupCacheManager {
+
+    Map<String, ReconciliationState> getReconciliationStates();
+}
