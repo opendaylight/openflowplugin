4e6394c5e895e248ac3d0f5dd88d0bb4016f6ed0
Gobinath <gobinath@ericsson.com>
2020-02-27 11:24:41 +0530

    OPNFLWPLUG-1086: Reconciliation framework failure when starting cbench tool for the first time
    Issue:
    
    CSIT job using cbench had consistent failure with the error message
    starting starting with a "closed connection ... exiting" message.
    The karaf logs indicated the switches getting disconnected because of
    "reconciliation framework" failure.
    
    Observation and Fix:
    
    On analyzing the logs it was found that the "flownodereconciliation"
    service of the reconciliation framework was throwing an uncaught
    exception (ConcurrentModificationException) which had resulted in
    the reconciliation framework failure.
    
    This has been fixed and logs have been enhanced to debug similar issues.
    
    Change-Id: I3cbcea2da32c342c83846f2041d82e921d7acf08
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    (cherry picked from commit 1fdc1a25f9703550c678b4cd83188de26ad9b19d)

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/ContextChainHolderImpl.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/lifecycle/ContextChainHolderImpl.java
@@ -357,7 +357,7 @@ public class ContextChainHolderImpl implements ContextChainHolder, MasterChecker
                     contextChain.continueInitializationAfterReconciliation();
                 } else {
                     OF_EVENT_LOG.debug("Reconciliation framework failure for device {}", deviceInfo);
-                    LOG.warn("Reconciliation framework failure for device {}", deviceInfo);
+                    LOG.warn("Reconciliation framework failure for device {} with result {}", deviceInfo, result);
                     destroyContextChain(deviceInfo);
                 }
             }
@@ -366,7 +366,7 @@ public class ContextChainHolderImpl implements ContextChainHolder, MasterChecker
             public void onFailure(Throwable throwable) {
                 OF_EVENT_LOG.debug("Reconciliation framework failure for device {} with error {}", deviceInfo,
                         throwable.getMessage());
-                LOG.warn("Reconciliation framework failure.");
+                LOG.warn("Reconciliation framework failure.", throwable);
                 destroyContextChain(deviceInfo);
             }
         };
