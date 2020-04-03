38bd458555bf86f9793b1690d7bdc8e8bc83c568
Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>
2020-04-03 20:47:42 +0530

    GetFlowNodeCache cli
    Signed-off-by: Shweta Chaturvedi <shweta.chaturvedi@ericsson.com>
    Change-Id: I81c5f411a50a91dba08b8e6d46555faf36c6aa7a

--- applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/impl/strategy/FlowForwarder.java
+++ applications/forwardingrules-sync/src/main/java/org/opendaylight/openflowplugin/applications/frsync/impl/strategy/FlowForwarder.java
@@ -110,7 +110,7 @@ public class FlowForwarder implements ForwardingRulesCommitter<Flow, AddFlowOutp
                 identifier, nodeIdent, addDataObj);
 
         final ListenableFuture<RpcResult<AddFlowOutput>> output;
-        final TableKey tableKey = identifier.firstKeyOf(Table.class);
+        final TableKey tableKey =identifier.firstKeyOf(Table.class);
         if (tableIdValidationPrecondition(tableKey, addDataObj)) {
             final AddFlowInputBuilder builder = new AddFlowInputBuilder(addDataObj);
 
