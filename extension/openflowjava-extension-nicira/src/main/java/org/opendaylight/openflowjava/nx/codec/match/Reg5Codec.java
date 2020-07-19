ae33fb22afbb807b6b5595cf4a75eefc746ad6f0
Robert Varga <robert.varga@pantheon.tech>
2020-07-19 13:25:58 +0200

    WIP: refactor AbstractMatchCodec
    This is a preliminary, unfinished, conversion.
    
    Change-Id: I3ff7f5837de226fe598da3a38a881ef24fdd0c46
    JIRA: OPNFLWPLUG-1100
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Reg5Codec.java
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Reg5Codec.java
@@ -5,33 +5,28 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowjava.nx.codec.match;
 
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntrySerializerKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.MatchField;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm1Class;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg5;
 
 public class Reg5Codec extends AbstractRegCodec {
-
     private static final int NXM_FIELD_CODE = 5;
     public static final MatchEntrySerializerKey<Nxm1Class, NxmNxReg5> SERIALIZER_KEY = new MatchEntrySerializerKey<>(
             EncodeConstants.OF13_VERSION_ID, Nxm1Class.class, NxmNxReg5.class);
     public static final MatchEntryDeserializerKey DESERIALIZER_KEY = new MatchEntryDeserializerKey(
             EncodeConstants.OF13_VERSION_ID, OxmMatchConstants.NXM_1_CLASS, NXM_FIELD_CODE);
 
+    public Reg5Codec(NxmHeader headerWithMask, NxmHeader headerWithoutMask) {
+        super(NxmNxReg5.class, headerWithMask, headerWithoutMask);
+    }
+
     @Override
     public int getNxmFieldCode() {
         return NXM_FIELD_CODE;
     }
-
-    @Override
-    public Class<? extends MatchField> getNxmField() {
-        return NxmNxReg5.class;
-    }
-
 }
--- extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Reg6Codec.java
+++ extension/openflowjava-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/Reg6Codec.java
@@ -5,33 +5,28 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowjava.nx.codec.match;
 
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntryDeserializerKey;
 import org.opendaylight.openflowjava.protocol.api.keys.MatchEntrySerializerKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.MatchField;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev150225.Nxm1Class;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg6;
 
 public class Reg6Codec extends AbstractRegCodec {
-
     private static final int NXM_FIELD_CODE = 6;
     public static final MatchEntrySerializerKey<Nxm1Class, NxmNxReg6> SERIALIZER_KEY = new MatchEntrySerializerKey<>(
             EncodeConstants.OF13_VERSION_ID, Nxm1Class.class, NxmNxReg6.class);
     public static final MatchEntryDeserializerKey DESERIALIZER_KEY = new MatchEntryDeserializerKey(
             EncodeConstants.OF13_VERSION_ID, OxmMatchConstants.NXM_1_CLASS, NXM_FIELD_CODE);
 
+    public Reg6Codec(NxmHeader headerWithMask, NxmHeader headerWithoutMask) {
+        super(NxmNxReg6.class, headerWithMask, headerWithoutMask);
+    }
+
     @Override
     public int getNxmFieldCode() {
         return NXM_FIELD_CODE;
     }
-
-    @Override
-    public Class<? extends MatchField> getNxmField() {
-        return NxmNxReg6.class;
-    }
-
 }
