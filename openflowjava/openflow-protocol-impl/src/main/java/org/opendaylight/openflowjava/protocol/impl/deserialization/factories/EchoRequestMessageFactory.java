73099b3ffa1362f9572399c75b521ebd0974de09
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 19:40:10 +0200

    Make VersionAssignableFactory implement OFDeserializer
    All subclasses are implementing OFSerializer, hence it makes sense
    to encode this in the base class. This ends up sharing the offset
    of the implementation.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I53342f0f0ec5ffe37440ee29f6e06c20be5add5d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/EchoRequestMessageFactory.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/EchoRequestMessageFactory.java
@@ -5,13 +5,11 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowjava.protocol.impl.deserialization.factories;
 
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
-import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
 import org.opendaylight.openflowjava.protocol.impl.util.VersionAssignableFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoRequestMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoRequestMessageBuilder;
@@ -22,10 +20,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
  * @author michal.polkorab
  * @author timotej.kubas
  */
-public class EchoRequestMessageFactory extends VersionAssignableFactory implements OFDeserializer<EchoRequestMessage> {
-
+public class EchoRequestMessageFactory extends VersionAssignableFactory<EchoRequestMessage> {
     @Override
-    public EchoRequestMessage deserialize(ByteBuf rawMessage) {
+    public EchoRequestMessage deserialize(final ByteBuf rawMessage) {
         EchoRequestMessageBuilder builder = new EchoRequestMessageBuilder();
         builder.setVersion(getVersion());
         builder.setXid(readUint32(rawMessage));
