44f3a549a445abb882e9844942c50736b850c8bc
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 18:09:10 +0200

    Make AbstractActionSerializer.serialize() final
    We want to reduce the number of megamorphic calls in the serialization
    path. Rather than overriding the serialize() method in abstract base
    class and call into it, make sure the base class calls out to subclasses
    to serialize the body.
    
    This reduces the number of serialize() implementations, peeling common
    code into the caller. It also makes it clear there are opportunities
    to further reduction.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I0cb421be9f84fd278039a07107254bfac9ddd9a2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF10SetNwSrcActionSerializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF10SetNwSrcActionSerializer.java
@@ -24,8 +24,7 @@ public class OF10SetNwSrcActionSerializer extends AbstractActionSerializer {
     }
 
     @Override
-    public void serialize(final Action action, final ByteBuf outBuffer) {
-        super.serialize(action, outBuffer);
+    protected void serializeBody(final Action action, final ByteBuf outBuffer) {
         outBuffer.writeBytes(IetfInetUtil.INSTANCE.ipv4AddressBytes(
             ((SetNwSrcCase) action.getActionChoice()).getSetNwSrcAction().getIpAddress()));
     }
