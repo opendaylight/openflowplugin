cd519f7870043252655c5d3ead71370279c5e7ed
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 17:20:35 +0200

    Refactor AbstractInstructionSerializer
    AbstractInstructionSerializer can be made more efficient with
    inlining types -- which means we will issue a simple read/load
    instead of having to go through virtual dispatch.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I8c325718715a928a1b24fc75a6de11a6b80eada1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/instruction/GoToTableInstructionSerializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/instruction/GoToTableInstructionSerializer.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowjava.protocol.impl.serialization.instruction;
 
 import io.netty.buffer.ByteBuf;
@@ -19,19 +18,16 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.instruction
  * @author michal.polkorab
  */
 public class GoToTableInstructionSerializer extends AbstractInstructionSerializer {
+    public GoToTableInstructionSerializer() {
+        super(InstructionConstants.GOTO_TABLE_TYPE);
+    }
 
     @Override
-    public void serialize(Instruction instruction, ByteBuf outBuffer) {
-        outBuffer.writeShort(getType());
+    public void serialize(final Instruction instruction, final ByteBuf outBuffer) {
+        outBuffer.writeShort(InstructionConstants.GOTO_TABLE_TYPE);
         outBuffer.writeShort(InstructionConstants.STANDARD_INSTRUCTION_LENGTH);
         outBuffer.writeByte(((GotoTableCase) instruction.getInstructionChoice())
-                .getGotoTable().getTableId().toJava());
+                .getGotoTable().getTableId().intValue());
         outBuffer.writeZero(InstructionConstants.PADDING_IN_GOTO_TABLE);
     }
-
-    @Override
-    protected int getType() {
-        return InstructionConstants.GOTO_TABLE_TYPE;
-    }
-
 }
