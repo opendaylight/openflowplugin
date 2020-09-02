28e95bc11cafb8a4ef4d621f608c38a8d3093d84
Robert Varga <robert.varga@pantheon.tech>
2020-09-03 00:52:46 +0200

    WIP: remove dedicated table features serializers
    // FIXME: convert subclasses
    // FIXME: make AbstractMatchFieldSerializer final
    
    Change-Id: Ia337c5f3999a90034e6bab3454797032c10489e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/tablefeatures/matchfield/AbstractMatchFieldSerializer.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/tablefeatures/matchfield/AbstractMatchFieldSerializer.java
@@ -5,21 +5,34 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.impl.protocol.serialization.multipart.tablefeatures.matchfield;
 
 import io.netty.buffer.ByteBuf;
 import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.types.rev131026.set.field.match.SetFieldMatch;
 
 public abstract class AbstractMatchFieldSerializer implements OFSerializer<SetFieldMatch> {
+    private final int oxmClass;
+    private final int oxmField;
+    private final int valueLength;
+
+    protected AbstractMatchFieldSerializer(final int oxmField, final int valueLength) {
+        this(OxmMatchConstants.OPENFLOW_BASIC_CLASS, oxmField, valueLength);
+    }
+
+    protected AbstractMatchFieldSerializer(final int oxmClass, final int oxmField, final int valueLength) {
+        this.oxmClass = oxmClass;
+        this.oxmField = oxmField;
+        this.valueLength = valueLength;
+    }
 
     @Override
-    public void serialize(final SetFieldMatch setFieldMatch, final ByteBuf byteBuf) {
-        byteBuf.writeShort(getOxmClassCode());
+    public final void serialize(final SetFieldMatch setFieldMatch, final ByteBuf byteBuf) {
+        byteBuf.writeShort(oxmClass);
 
-        int fieldAndMask = getOxmFieldCode() << 1;
-        int length = getValueLength();
+        int fieldAndMask = oxmField << 1;
+        int length = valueLength;
 
         if (setFieldMatch.isHasMask()) {
             fieldAndMask |= 1;
@@ -29,11 +42,4 @@ public abstract class AbstractMatchFieldSerializer implements OFSerializer<SetFi
         byteBuf.writeByte(fieldAndMask);
         byteBuf.writeByte(length);
     }
-
-    protected abstract int getOxmClassCode();
-
-    protected abstract int getOxmFieldCode();
-
-    protected abstract int getValueLength();
-
 }
