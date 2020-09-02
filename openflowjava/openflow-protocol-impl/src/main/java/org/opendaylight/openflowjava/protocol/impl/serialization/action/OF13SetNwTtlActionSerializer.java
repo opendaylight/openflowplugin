78bc36cbcffbd53da4ff70418a8d648e0c127e99
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 17:48:58 +0200

    Refactor AbstractActionSerializer
    Current structure is causing multiple megamorphic invocations in
    the serialization paths. This first step eliminates
    getType()/getLength() methods, storing their values in the object
    instead.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: Ie806a6a4a011b9235c9b47005927cfc5704f018b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13SetNwTtlActionSerializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13SetNwTtlActionSerializer.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowjava.protocol.impl.serialization.action;
 
 import io.netty.buffer.ByteBuf;
@@ -19,22 +18,14 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev1
  * @author michal.polkorab
  */
 public class OF13SetNwTtlActionSerializer extends AbstractActionSerializer {
+    public OF13SetNwTtlActionSerializer() {
+        super(ActionConstants.SET_NW_TTL_CODE, ActionConstants.GENERAL_ACTION_LENGTH);
+    }
 
     @Override
-    public void serialize(Action action, ByteBuf outBuffer) {
+    public void serialize(final Action action, final ByteBuf outBuffer) {
         super.serialize(action, outBuffer);
         outBuffer.writeByte(((SetNwTtlCase) action.getActionChoice()).getSetNwTtlAction().getNwTtl().toJava());
         outBuffer.writeZero(ActionConstants.SET_NW_TTL_PADDING);
     }
-
-    @Override
-    protected int getType() {
-        return ActionConstants.SET_NW_TTL_CODE;
-    }
-
-    @Override
-    protected int getLength() {
-        return ActionConstants.GENERAL_ACTION_LENGTH;
-    }
-
 }
