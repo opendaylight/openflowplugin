5683c6a7dd73139f99fd0ce7491c8c0e19511d73
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 19:40:10 +0200

    Make VersionAssignableFactory implement OFDeserializer
    All subclasses are implementing OFSerializer, hence it makes sense
    to encode this in the base class. This ends up sharing the offset
    of the implementation.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I53342f0f0ec5ffe37440ee29f6e06c20be5add5d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/BarrierInputMessageFactory.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/BarrierInputMessageFactory.java
@@ -10,7 +10,6 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.factories;
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
-import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
 import org.opendaylight.openflowjava.protocol.impl.util.VersionAssignableFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInputBuilder;
@@ -20,10 +19,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
  * OF protocol versions: 1.0, 1.3, 1.4, 1.5.
  * @author giuseppex.petralia@intel.com
  */
-public class BarrierInputMessageFactory extends VersionAssignableFactory implements OFDeserializer<BarrierInput> {
-
+public class BarrierInputMessageFactory extends VersionAssignableFactory<BarrierInput> {
     @Override
-    public BarrierInput deserialize(ByteBuf rawMessage) {
+    public BarrierInput deserialize(final ByteBuf rawMessage) {
         return new BarrierInputBuilder()
                 .setVersion(getVersion())
                 .setXid(readUint32(rawMessage))
