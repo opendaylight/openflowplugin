093b9a92b16811ba363b5f24a79db2c599b2f606
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 16:34:31 +0200

    Refactor AbstractActionDeserializer
    Looking at AbstractActionDeserializer, we can increase performance
    of parseHeader() through by eagerly instantiating choice/header
    objects.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: Ia71bb2d865eaf83627945b1a7039a0978b76be80
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13GroupActionDeserializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13GroupActionDeserializer.java
@@ -10,7 +10,7 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.action;
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.ActionChoice;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.GroupCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.GroupCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.group._case.GroupActionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
@@ -21,19 +21,18 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev1
  *
  * @author michal.polkorab
  */
-public class OF13GroupActionDeserializer extends AbstractActionDeserializer {
+public class OF13GroupActionDeserializer extends AbstractActionDeserializer<GroupCase> {
+    public OF13GroupActionDeserializer() {
+        super(new GroupCaseBuilder().build());
+    }
+
     @Override
-    public Action deserialize(ByteBuf input) {
+    public Action deserialize(final ByteBuf input) {
         input.skipBytes(2 * Short.BYTES);
         return new ActionBuilder()
                 .setActionChoice(new GroupCaseBuilder()
                     .setGroupAction(new GroupActionBuilder().setGroupId(readUint32(input)).build())
                     .build())
                 .build();
-    }
-
-    @Override
-    protected ActionChoice getType() {
-        return new GroupCaseBuilder().build();
     }
 }
--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13SetQueueActionDeserializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13SetQueueActionDeserializer.java
@@ -10,7 +10,7 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.action;
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.ActionChoice;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.SetQueueCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.SetQueueCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.set.queue._case.SetQueueActionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
@@ -21,9 +21,13 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev1
  *
  * @author michal.polkorab
  */
-public class OF13SetQueueActionDeserializer extends AbstractActionDeserializer {
+public class OF13SetQueueActionDeserializer extends AbstractActionDeserializer<SetQueueCase> {
+    public OF13SetQueueActionDeserializer() {
+        super(new SetQueueCaseBuilder().build());
+    }
+
     @Override
-    public Action deserialize(ByteBuf input) {
+    public Action deserialize(final ByteBuf input) {
         final ActionBuilder builder = new ActionBuilder();
         input.skipBytes(2 * Short.BYTES);
         SetQueueCaseBuilder caseBuilder = new SetQueueCaseBuilder();
@@ -32,10 +36,5 @@ public class OF13SetQueueActionDeserializer extends AbstractActionDeserializer {
         caseBuilder.setSetQueueAction(actionBuilder.build());
         builder.setActionChoice(caseBuilder.build());
         return builder.build();
-    }
-
-    @Override
-    protected ActionChoice getType() {
-        return new SetQueueCaseBuilder().build();
     }
 }
