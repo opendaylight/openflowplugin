972500bf58418ccf7fb0f34d47e7344bca7cc85d
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 19:40:10 +0200

    Make VersionAssignableFactory implement OFDeserializer
    All subclasses are implementing OFSerializer, hence it makes sense
    to encode this in the base class. This ends up sharing the offset
    of the implementation.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I53342f0f0ec5ffe37440ee29f6e06c20be5add5d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/GetConfigInputMessageFactory.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/GetConfigInputMessageFactory.java
@@ -10,7 +10,6 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.factories;
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
-import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
 import org.opendaylight.openflowjava.protocol.impl.util.VersionAssignableFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigInputBuilder;
@@ -20,9 +19,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731
  * OF protocol versions: 1.0, 1.3, 1.4, 1.5.
  * @author giuseppex.petralia@intel.com
  */
-public class GetConfigInputMessageFactory extends VersionAssignableFactory implements OFDeserializer<GetConfigInput> {
+public class GetConfigInputMessageFactory extends VersionAssignableFactory<GetConfigInput> {
     @Override
-    public GetConfigInput deserialize(ByteBuf rawMessage) {
+    public GetConfigInput deserialize(final ByteBuf rawMessage) {
         return new GetConfigInputBuilder()
                 .setVersion(getVersion())
                 .setXid(readUint32(rawMessage))
