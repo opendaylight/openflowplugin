dc2cd58959b3112d893410548690d9b9a07012c6
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 18:18:10 +0200

    Add AbstractEmptyActionSerializer
    Similar to AbstractEmptyActionDeserializer, we can centralize
    common body reads in a single class, eliminating duplicate code.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: I91e02ac81d7f631bc7237e4ea770fad40ca5eb0d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13PopPbbActionSerializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/action/OF13PopPbbActionSerializer.java
@@ -7,22 +7,15 @@
  */
 package org.opendaylight.openflowjava.protocol.impl.serialization.action;
 
-import io.netty.buffer.ByteBuf;
 import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
 
 /**
  * Serializes OF 1.3 PopPbb actions.
  *
  * @author michal.polkorab
  */
-public class OF13PopPbbActionSerializer extends AbstractActionSerializer {
+public class OF13PopPbbActionSerializer extends AbstractEmptyActionSerializer {
     public OF13PopPbbActionSerializer() {
-        super(ActionConstants.POP_PBB_CODE, ActionConstants.GENERAL_ACTION_LENGTH);
-    }
-
-    @Override
-    protected void serializeBody(final Action action, final ByteBuf outBuffer) {
-        outBuffer.writeZero(ActionConstants.PADDING_IN_ACTION_HEADER);
+        super(ActionConstants.POP_PBB_CODE);
     }
 }
