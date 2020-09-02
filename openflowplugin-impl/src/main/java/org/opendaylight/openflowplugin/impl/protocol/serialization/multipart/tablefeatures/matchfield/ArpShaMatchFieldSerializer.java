33777708e46ae751dfbc0ab5304eadbb519fac23
Robert Varga <robert.varga@pantheon.tech>
2020-09-03 00:52:46 +0200

    WIP: remove dedicated table features serializers
    // FIXME: convert subclasses
    // FIXME: make AbstractMatchFieldSerializer final
    
    Change-Id: Ia337c5f3999a90034e6bab3454797032c10489e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/tablefeatures/matchfield/ArpShaMatchFieldSerializer.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/tablefeatures/matchfield/ArpShaMatchFieldSerializer.java
@@ -5,25 +5,13 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.impl.protocol.serialization.multipart.tablefeatures.matchfield;
 
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
 
 public class ArpShaMatchFieldSerializer extends AbstractMatchFieldSerializer {
-    @Override
-    protected int getOxmClassCode() {
-        return OxmMatchConstants.OPENFLOW_BASIC_CLASS;
-    }
-
-    @Override
-    protected int getOxmFieldCode() {
-        return OxmMatchConstants.ARP_SHA;
-    }
-
-    @Override
-    protected int getValueLength() {
-        return EncodeConstants.MAC_ADDRESS_LENGTH;
+    public ArpShaMatchFieldSerializer() {
+        super(OxmMatchConstants.ARP_SHA, EncodeConstants.MAC_ADDRESS_LENGTH);
     }
 }
