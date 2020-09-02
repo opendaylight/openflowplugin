e20b047b867c249de9c2f2b2a021c7a1815dfac5
Robert Varga <robert.varga@pantheon.tech>
2020-09-02 16:34:31 +0200

    Refactor AbstractActionDeserializer
    Looking at AbstractActionDeserializer, we can increase performance
    of parseHeader() by eagerly instantiating choice/header objects.
    
    JIRA: OPNFLWPLUG-1102
    Change-Id: Ia71bb2d865eaf83627945b1a7039a0978b76be80
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13PopVlanActionDeserializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13PopVlanActionDeserializer.java
@@ -7,30 +7,16 @@
  */
 package org.opendaylight.openflowjava.protocol.impl.deserialization.action;
 
-import io.netty.buffer.ByteBuf;
-import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.ActionChoice;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.PopVlanCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.PopVlanCaseBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.Action;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.actions.grouping.ActionBuilder;
 
 /**
  * OF13PopVlanActionDeserializer.
  *
  * @author michal.polkorab
  */
-public class OF13PopVlanActionDeserializer extends AbstractActionDeserializer {
-    @Override
-    public Action deserialize(ByteBuf input) {
-        ActionBuilder builder = new ActionBuilder();
-        input.skipBytes(2 * Short.BYTES);
-        builder.setActionChoice(getType());
-        input.skipBytes(ActionConstants.PADDING_IN_ACTION_HEADER);
-        return builder.build();
-    }
-
-    @Override
-    protected ActionChoice getType() {
-        return new PopVlanCaseBuilder().build();
+public class OF13PopVlanActionDeserializer extends AbstractEmptyActionDeserializer<PopVlanCase> {
+    public OF13PopVlanActionDeserializer() {
+        super(new PopVlanCaseBuilder().build());
     }
 }
