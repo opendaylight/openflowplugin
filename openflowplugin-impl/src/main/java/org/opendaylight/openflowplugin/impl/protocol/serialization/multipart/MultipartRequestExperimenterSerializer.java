b857c71e7d99e8a52c5a408eecdd9fa6ba79ed0b
Robert Varga <robert.varga@pantheon.tech>
2021-01-03 10:49:05 +0100

    Cleanup MultipartRequestExperimenterSerializer
    Split fallback serialization into its own method and remove use
    of Optional.ofNullable().ifPresent() in favor of null checks.
    
    Change-Id: Ic4581efbc383de4b2c6028c6be654280fae0605d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/MultipartRequestExperimenterSerializer.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/protocol/serialization/multipart/MultipartRequestExperimenterSerializer.java
@@ -5,12 +5,11 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.impl.protocol.serialization.multipart;
 
-import com.google.common.base.Preconditions;
+import static java.util.Objects.requireNonNull;
+
 import io.netty.buffer.ByteBuf;
-import java.util.Optional;
 import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
 import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerRegistry;
 import org.opendaylight.openflowjava.protocol.api.extensibility.SerializerRegistryInjector;
@@ -18,6 +17,7 @@ import org.opendaylight.openflowjava.protocol.api.keys.MessageTypeKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.openflowjava.util.ExperimenterSerializerKeyFactory;
 import org.opendaylight.openflowplugin.api.OFConstants;
+import org.opendaylight.openflowplugin.extension.api.ConverterMessageToOFJava;
 import org.opendaylight.openflowplugin.extension.api.ConvertorData;
 import org.opendaylight.openflowplugin.extension.api.TypeVersionKey;
 import org.opendaylight.openflowplugin.extension.api.exception.ConversionException;
@@ -25,50 +25,52 @@ import org.opendaylight.openflowplugin.openflow.md.core.session.OFSessionUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.experimenter.core.ExperimenterDataOfChoice;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.experimenter.types.rev151020.experimenter.core.message.ExperimenterMessageOfChoice;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.experimenter.types.rev151020.multipart.request.multipart.request.body.MultipartRequestExperimenter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MultipartRequestExperimenterSerializer implements OFSerializer<MultipartRequestExperimenter>,
         SerializerRegistryInjector {
+    private static final Logger LOG = LoggerFactory.getLogger(MultipartRequestExperimenterSerializer.class);
 
-    private SerializerRegistry registry;
+    private SerializerRegistry registry = null;
 
     @Override
-    @SuppressWarnings("unchecked")
-    public void serialize(final MultipartRequestExperimenter multipartRequestExperimenter, final ByteBuf byteBuf) {
+    public void serialize(final MultipartRequestExperimenter input, final ByteBuf byteBuf) {
+        final OFSerializer<ExperimenterMessageOfChoice> serializer = registry.getSerializer(new MessageTypeKey<>(
+            EncodeConstants.OF13_VERSION_ID,
+            input.getExperimenterMessageOfChoice().implementedInterface()));
+
         try {
-            final OFSerializer<ExperimenterMessageOfChoice> serializer = Preconditions.checkNotNull(registry)
-                    .getSerializer(new MessageTypeKey<>(
-                            EncodeConstants.OF13_VERSION_ID,
-                            multipartRequestExperimenter.getExperimenterMessageOfChoice().implementedInterface()));
+            serializer.serialize(input.getExperimenterMessageOfChoice(), byteBuf);
+        } catch (ClassCastException | IllegalStateException e) {
+            LOG.debug("Failed to serialize {}, attempting to convert", e);
+            serializeConverted(input, byteBuf);
+        }
+    }
 
-            serializer.serialize(multipartRequestExperimenter.getExperimenterMessageOfChoice(), byteBuf);
-        } catch (ClassCastException | IllegalStateException ex) {
-            Optional
-                    .ofNullable(OFSessionUtil.getExtensionConvertorProvider().<ExperimenterMessageOfChoice,
-                            ExperimenterDataOfChoice, ConvertorData>getMessageConverter(new TypeVersionKey<>(
-                            (Class<ExperimenterMessageOfChoice>) multipartRequestExperimenter
-                                    .getExperimenterMessageOfChoice().implementedInterface(),
-                            OFConstants.OFP_VERSION_1_3)))
-                    .ifPresent(converter -> {
-                        final OFSerializer<ExperimenterDataOfChoice> serializer = Preconditions.checkNotNull(registry)
-                                .getSerializer(ExperimenterSerializerKeyFactory
-                                        .createMultipartRequestSerializerKey(
-                                                EncodeConstants.OF13_VERSION_ID,
-                                                converter.getExperimenterId().getValue().toJava(),
-                                                converter.getType()));
+    private void serializeConverted(final MultipartRequestExperimenter input, final ByteBuf byteBuf) {
+        @SuppressWarnings("unchecked")
+        final ConverterMessageToOFJava<ExperimenterMessageOfChoice, ExperimenterDataOfChoice, ConvertorData> converter =
+            OFSessionUtil.getExtensionConvertorProvider().
+                <ExperimenterMessageOfChoice, ExperimenterDataOfChoice, ConvertorData>getMessageConverter(
+                    new TypeVersionKey<>((Class<ExperimenterMessageOfChoice>)
+                        input.getExperimenterMessageOfChoice().implementedInterface(), OFConstants.OFP_VERSION_1_3));
+        if (converter != null) {
+            final OFSerializer<ExperimenterDataOfChoice> serializer = registry.getSerializer(
+                ExperimenterSerializerKeyFactory.createMultipartRequestSerializerKey(
+                    EncodeConstants.OF13_VERSION_ID, converter.getExperimenterId().getValue().toJava(),
+                    converter.getType()));
 
-                        try {
-                            serializer.serialize(converter.convert(multipartRequestExperimenter
-                                    .getExperimenterMessageOfChoice(), null), byteBuf);
-                        } catch (ConversionException e) {
-                            throw new IllegalStateException(e);
-                        }
-                    });
+            try {
+                serializer.serialize(converter.convert(input.getExperimenterMessageOfChoice(), null), byteBuf);
+            } catch (ConversionException e) {
+                throw new IllegalStateException(e);
+            }
         }
     }
 
     @Override
     public void injectSerializerRegistry(final SerializerRegistry serializerRegistry) {
-        registry = serializerRegistry;
+        registry = requireNonNull(serializerRegistry);
     }
-
 }
