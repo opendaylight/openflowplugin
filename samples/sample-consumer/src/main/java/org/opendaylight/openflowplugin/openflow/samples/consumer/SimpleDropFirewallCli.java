a2bfc401dfd0bd7fb1e20326e6b4da1fd017f0e6
Robert Varga <robert.varga@pantheon.tech>
2021-01-03 18:01:06 +0100

    Cleanup sample-consumer
    We have a few warnings, fix them up by refactoring to current
    best practices.
    
    Change-Id: I85acd2d7494b861f7d313af92b896f4d40bbb032
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- samples/sample-consumer/src/main/java/org/opendaylight/openflowplugin/openflow/samples/consumer/SimpleDropFirewallCli.java
+++ samples/sample-consumer/src/main/java/org/opendaylight/openflowplugin/openflow/samples/consumer/SimpleDropFirewallCli.java
@@ -7,12 +7,9 @@
  */
 package org.opendaylight.openflowplugin.openflow.samples.consumer;
 
-import java.util.Collections;
 import java.util.List;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.DropActionCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.DropActionCaseBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInputBuilder;
@@ -20,63 +17,47 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.I
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.ApplyActionsCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.apply.actions._case.ApplyActionsBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.InstructionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv4MatchBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._4.match.TcpMatchBuilder;
+import org.opendaylight.yangtools.yang.binding.util.BindingMap;
 
 public class SimpleDropFirewallCli {
 
     /**
      * Form of input is: node name node-connector number source ip-address destinatinon ip-address.
      *
-     * @param cliInput
-     *            Parsed input from CLI
+     * @param cliInput Parsed input from CLI
      */
     public AddFlowInput createTcpFlow(final List<String> cliInput) {
-        AddFlowInputBuilder ret = new AddFlowInputBuilder();
-        ret.setNode(nodeFromString(cliInput.get(0)));
-
-        // We construct a match
-        MatchBuilder match = new MatchBuilder();
-
-        Ipv4MatchBuilder ipv4Match = new Ipv4MatchBuilder();
-
-        ipv4Match.setIpv4Source(new Ipv4Prefix(cliInput.get(3)));
-        ipv4Match.setIpv4Destination(new Ipv4Prefix(cliInput.get(4)));
-
-        match.setLayer3Match(ipv4Match.build());
-
-        match.setLayer4Match(new TcpMatchBuilder().build());
-
-        ret.setMatch(match.build());
-
-        DropActionCase dropAction = new DropActionCaseBuilder().build();
-
-        ActionBuilder action = new ActionBuilder();
-        action.setAction(dropAction);
-
-        List<Action> actions = Collections.singletonList(action.build());
-        //
-        ApplyActionsCaseBuilder aaBldr = new ApplyActionsCaseBuilder();
-        aaBldr.setApplyActions(new ApplyActionsBuilder().setAction(actions).build());
-
-        InstructionBuilder instructionBldr = new InstructionBuilder();
-        instructionBldr.setInstruction(aaBldr.build());
-
-        List<Instruction> isntructions = Collections.singletonList(instructionBldr.build());
-        InstructionsBuilder instructionsBldr = new InstructionsBuilder();
-        instructionsBldr.setInstruction(isntructions);
-
-        ret.setInstructions(instructionsBldr.build());
-
-        return ret.build();
+        return new AddFlowInputBuilder()
+            .setNode(nodeFromString(cliInput.get(0)))
+            // We construct a match
+            .setMatch(new MatchBuilder()
+                .setLayer3Match(new Ipv4MatchBuilder()
+                    .setIpv4Source(new Ipv4Prefix(cliInput.get(3)))
+                    .setIpv4Destination(new Ipv4Prefix(cliInput.get(4)))
+                    .build())
+                .setLayer4Match(new TcpMatchBuilder().build())
+                .build())
+            .setInstructions(new InstructionsBuilder()
+                .setInstruction(BindingMap.of(new InstructionBuilder()
+                    .setInstruction(new ApplyActionsCaseBuilder()
+                        .setApplyActions(new ApplyActionsBuilder()
+                            .setAction(BindingMap.of(new ActionBuilder()
+                                .setOrder(0)
+                                .setAction(new DropActionCaseBuilder().build())
+                                .build()))
+                            .build())
+                        .build())
+                    .build()))
+                .build())
+            .build();
     }
 
-    private NodeRef nodeFromString(final String string) {
+    private static NodeRef nodeFromString(final String string) {
         // TODO Auto-generated method stub
         return null;
     }
-
 }
