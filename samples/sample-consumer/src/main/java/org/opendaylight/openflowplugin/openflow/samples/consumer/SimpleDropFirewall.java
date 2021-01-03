39f877f79fb00b79c01c1532fe314a0c3839d433
Robert Varga <robert.varga@pantheon.tech>
2021-01-03 18:01:06 +0100

    Cleanup sample-consumer
    We have a few warnings, fix them up by refactoring to current
    best practices.
    
    Change-Id: I85acd2d7494b861f7d313af92b896f4d40bbb032
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- samples/sample-consumer/src/main/java/org/opendaylight/openflowplugin/openflow/samples/consumer/SimpleDropFirewall.java
+++ samples/sample-consumer/src/main/java/org/opendaylight/openflowplugin/openflow/samples/consumer/SimpleDropFirewall.java
@@ -8,26 +8,20 @@
 package org.opendaylight.openflowplugin.openflow.samples.consumer;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 
 public class SimpleDropFirewall {
 
     private final SalFlowService flowService;
 
-    public SimpleDropFirewall(SalFlowService flowService) {
+    public SimpleDropFirewall(final SalFlowService flowService) {
         this.flowService = flowService;
     }
 
-    public boolean addFlow(AddFlowInput flow) throws InterruptedException,
-            ExecutionException, TimeoutException {
-        Future<RpcResult<AddFlowOutput>> result = flowService.addFlow(flow);
-
-        return result.get(5, TimeUnit.SECONDS).isSuccessful();
+    public boolean addFlow(final AddFlowInput flow) throws InterruptedException, ExecutionException, TimeoutException {
+        return flowService.addFlow(flow).get(5, TimeUnit.SECONDS).isSuccessful();
     }
 }
