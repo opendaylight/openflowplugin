838624e77ac6348d20a2032ed73a7440898d8f69
Robert Varga <robert.varga@pantheon.tech>
2021-01-21 03:57:20 +0100

    WIP: Rework action lookup
    // FIXME: requires Match registry split off
    // FIXME: consider fusing simple providers
    // FIXME: finish conversion of all lookups
    
    Change-Id: I91b1847267195a17cc2bea31423c2bab5a72d6bf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF10SetDlDstActionDeserializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF10SetDlDstActionDeserializer.java
@@ -7,19 +7,32 @@
  */
 package org.opendaylight.openflowjava.protocol.impl.deserialization.action;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.openflowjava.protocol.api.extensibility.ActionDeserializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.SetDlDstCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.SetDlDstCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.set.dl.dst._case.SetDlDstActionBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * OF10SetDlDstActionDeserializer.
  *
  * @author michal.polkorab
  */
+@Singleton
+@Component(service = ActionDeserializer.OFProvider.class)
+@MetaInfServices(value = ActionDeserializer.OFProvider.class)
 public final class OF10SetDlDstActionDeserializer extends AbstractOF10SetDlActionDeserializer<SetDlDstCase> {
+    @Inject
     public OF10SetDlDstActionDeserializer() {
-        super(new SetDlDstCaseBuilder().build());
+        super(new SetDlDstCaseBuilder().build(), EncodeConstants.OF_VERSION_1_0,
+            Uint16.valueOf(ActionConstants.SET_DL_DST_CODE));
     }
 
     @Override
--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13GroupActionDeserializer.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/action/OF13GroupActionDeserializer.java
@@ -10,18 +10,31 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.action;
 import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint32;
 
 import io.netty.buffer.ByteBuf;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.openflowjava.protocol.api.extensibility.ActionDeserializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.impl.util.ActionConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.GroupCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.GroupCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev150203.action.grouping.action.choice.group._case.GroupActionBuilder;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * OF13GroupActionDeserializer.
  *
  * @author michal.polkorab
  */
-public class OF13GroupActionDeserializer extends AbstractActionCaseDeserializer<GroupCase> {
+@Singleton
+@Component(service = ActionDeserializer.OFProvider.class)
+@MetaInfServices(value = ActionDeserializer.OFProvider.class)
+public class OF13GroupActionDeserializer extends AbstractSimpleActionCaseDeserializer<GroupCase> {
+    @Inject
     public OF13GroupActionDeserializer() {
-        super(new GroupCaseBuilder().build());
+        super(new GroupCaseBuilder().build(), EncodeConstants.OF_VERSION_1_3,
+            Uint16.valueOf(ActionConstants.GROUP_CODE));
     }
 
     @Override
