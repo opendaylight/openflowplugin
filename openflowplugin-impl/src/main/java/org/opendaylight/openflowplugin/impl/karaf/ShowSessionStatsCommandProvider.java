d9adc4846967aef9371ea0492f27c47a757f12d4
Sangwook Ha <sangwook.ha@verizon.com>
2023-06-24 01:12:33 +0000

    Modernize openflowplugin-impl CLI
    There is a more modern way of implementing CLIs than through Blueprint,
    make sure we use it.
    
    Change-Id: Icdeab1e6c985db560e88bbbbb6345bfc2af75298
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Sangwook Ha <sangwook.ha@verizon.com>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/karaf/ShowSessionStatsCommandProvider.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/karaf/ShowSessionStatsCommandProvider.java
@@ -5,32 +5,26 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.impl.karaf;
 
 import java.io.PrintStream;
-import java.util.List;
-import org.apache.karaf.shell.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.opendaylight.openflowplugin.impl.statistics.ofpspecific.SessionStatistics;
 
 /**
  * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 21.5.2015.
  */
-
+@Service
 @Command(scope = "ofp", name = "show-session-stats", description = "Show session statistics.")
-public class ShowSessionStatsCommandProvider extends OsgiCommandSupport {
-
+public class ShowSessionStatsCommandProvider extends AbstractAction {
     @Override
-    protected Object doExecute() {
-        PrintStream out = session.getConsole();
-        final List<String> statistics = SessionStatistics.provideStatistics();
-        final StringBuilder result = new StringBuilder();
-        for (String line : statistics) {
-            result.append(line);
-            result.append("\n");
+    void execute(final PrintStream out) {
+        final var sb = new StringBuilder();
+        // FIXME: static wiring
+        for (var line : SessionStatistics.provideStatistics()) {
+            sb.append(line).append('\n');
         }
-        out.print(result.toString());
-        return null;
+        out.print(sb.toString());
     }
 }
