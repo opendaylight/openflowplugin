3320f18096b90360e53d6ae95958b6185fea99af
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-09-25 16:31:17 +0300

    Remove dependency on RpcService for openflow-protocol
    JIRA: OPNFLWPLUG-1125
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Change-Id: Ib74c20c08c338088de2cff3ad23de83811a5bc53

--- openflowjava/openflow-protocol-api/src/main/java/org/opendaylight/openflowjava/protocol/api/connection/ConnectionAdapter.java
+++ openflowjava/openflow-protocol-api/src/main/java/org/opendaylight/openflowjava/protocol/api/connection/ConnectionAdapter.java
@@ -8,6 +8,7 @@
 package org.opendaylight.openflowjava.protocol.api.connection;
 
 import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.ListenableFuture;
 import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.security.cert.X509Certificate;
@@ -16,9 +17,47 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.openflowjava.protocol.api.extensibility.AlienMessageListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoReplyInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.EchoReplyOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ExperimenterInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ExperimenterOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetAsyncInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetAsyncOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetFeaturesOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetQueueConfigInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetQueueConfigOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GroupModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GroupModOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.HelloInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.HelloOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MeterModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MeterModOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartRequestInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartRequestOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OpenflowProtocolListener;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OpenflowProtocolService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketOutInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketOutOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PortModOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.RoleRequestInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.RoleRequestOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetAsyncInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetAsyncOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetConfigInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetConfigOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.TableModInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.TableModOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.system.rev130927.SystemNotificationsListener;
+import org.opendaylight.yangtools.yang.common.RpcResult;
 
 /**
  * Manages a switch connection.
@@ -26,7 +65,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.system.rev130927.S
  * @author mirehak
  * @author michal.polkorab
  */
-public interface ConnectionAdapter extends OpenflowProtocolService {
+public interface ConnectionAdapter {
 
     /**
      * Disconnect corresponding switch.
@@ -139,4 +178,164 @@ public interface ConnectionAdapter extends OpenflowProtocolService {
      * @param executorService for all dpns
      */
     void setExecutorService(ExecutorService executorService);
+
+    /*
+     FIXME: Below methods are legacy RPC methods (of openflow-protocol module) inherited from auto-generated
+      RpcService which is deprecated for removal. These methods seems never used. It requires validation and
+      further cleanup.
+     */
+
+    /**
+     * Invoke {@code echo} RPC.
+     *
+     * @param input of {@code echo}
+     * @return output of {@code echo}
+     */
+    ListenableFuture<RpcResult<EchoOutput>> echo(EchoInput input);
+
+    /**
+     * Invoke {@code echo-reply} RPC.
+     *
+     * @param input of {@code echo-reply}
+     * @return output of {@code echo-reply}
+     */
+    ListenableFuture<RpcResult<EchoReplyOutput>> echoReply(EchoReplyInput input);
+
+    /**
+     * Invoke {@code hello} RPC.
+     *
+     * @param input of {@code hello}
+     * @return output of {@code hello}
+     *
+     */
+    ListenableFuture<RpcResult<HelloOutput>> hello(HelloInput input);
+
+    /**
+     * Invoke {@code experimenter} RPC.
+     * Sends experimenter message to device, reply is not solicited.
+     *
+     * @param input of {@code experimenter}
+     * @return output of {@code experimenter}
+     */
+    ListenableFuture<RpcResult<ExperimenterOutput>> experimenter(ExperimenterInput input);
+
+    /**
+     * Invoke {@code get-features} RPC.
+     *
+     * @param input of {@code get-features}
+     * @return output of {@code get-features}
+     */
+    ListenableFuture<RpcResult<GetFeaturesOutput>> getFeatures(GetFeaturesInput input);
+
+    /**
+     * Invoke {@code get-config} RPC.
+     *
+     * @param input of {@code get-config}
+     * @return output of {@code get-config}
+     */
+    ListenableFuture<RpcResult<GetConfigOutput>> getConfig(GetConfigInput input);
+
+    /**
+     * Invoke {@code set-config} RPC.
+     *
+     * @param input of {@code set-config}
+     * @return output of {@code set-config}
+     */
+    ListenableFuture<RpcResult<SetConfigOutput>> setConfig(SetConfigInput input);
+
+    /**
+     * Invoke {@code packet-out} RPC.
+     *
+     * @param input of {@code packet-out}
+     * @return output of {@code packet-out}
+     */
+    ListenableFuture<RpcResult<PacketOutOutput>> packetOut(PacketOutInput input);
+
+    /**
+     * Invoke {@code flow-mod} RPC.
+     *
+     * @param input of {@code flow-mod}
+     * @return output of {@code flow-mod}
+     */
+    ListenableFuture<RpcResult<FlowModOutput>> flowMod(FlowModInput input);
+
+    /**
+     * Invoke {@code group-mod} RPC.
+     *
+     * @param input of {@code group-mod}
+     * @return output of {@code group-mod}
+     */
+    ListenableFuture<RpcResult<GroupModOutput>> groupMod(GroupModInput input);
+
+    /**
+     * Invoke {@code port-mod} RPC.
+     *
+     * @param input of {@code port-mod}
+     * @return output of {@code port-mod}
+     */
+    ListenableFuture<RpcResult<PortModOutput>> portMod(PortModInput input);
+
+    /**
+     * Invoke {@code table-mod} RPC.
+     *
+     * @param input of {@code table-mod}
+     * @return output of {@code table-mod}
+     */
+    ListenableFuture<RpcResult<TableModOutput>> tableMod(TableModInput input);
+
+    /**
+     * Invoke {@code multipart-request} RPC.
+     *
+     * @param input of {@code multipart-request}
+     * @return output of {@code multipart-request}
+     */
+    ListenableFuture<RpcResult<MultipartRequestOutput>> multipartRequest(MultipartRequestInput input);
+
+    /**
+     * Invoke {@code barrier} RPC.
+     *
+     * @param input of {@code barrier}
+     * @return output of {@code barrier}
+     */
+    ListenableFuture<RpcResult<BarrierOutput>> barrier(BarrierInput input);
+
+    /**
+     * Invoke {@code get-queue-config} RPC.
+     *
+     * @param input of {@code get-queue-config}
+     * @return output of {@code get-queue-config}
+     */
+    ListenableFuture<RpcResult<GetQueueConfigOutput>> getQueueConfig(GetQueueConfigInput input);
+
+    /**
+     * Invoke {@code role-request} RPC.
+     *
+     * @param input of {@code role-request}
+     * @return output of {@code role-request}
+     */
+    ListenableFuture<RpcResult<RoleRequestOutput>> roleRequest(RoleRequestInput input);
+
+    /**
+     * Invoke {@code get-async} RPC.
+     *
+     * @param input of {@code get-async}
+     * @return output of {@code get-async}
+     */
+    ListenableFuture<RpcResult<GetAsyncOutput>> getAsync(GetAsyncInput input);
+
+    /**
+     * Invoke {@code set-async} RPC.
+     *
+     * @param input of {@code set-async}
+     * @return output of {@code set-async}
+     */
+    ListenableFuture<RpcResult<SetAsyncOutput>> setAsync(SetAsyncInput input);
+
+    /**
+     * Invoke {@code meter-mod} RPC.
+     *
+     * @param input of {@code meter-mod}
+     * @return output of {@code meter-mod}
+     */
+    ListenableFuture<RpcResult<MeterModOutput>> meterMod(MeterModInput input);
 }
