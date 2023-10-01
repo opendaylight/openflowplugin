c691778123c25210501e94d3163ca9dca72f345b
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-01 22:50:09 +0200

    Do not use RpcService in test-provider
    Migrated usage of RpcService to Rpc<?,?> for test-provider.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: I02bdb491b517bc5898bfa43bab7430fa69c28b6a
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginTestRpcProvider.java
+++ test-provider/src/main/java/org/opendaylight/openflowplugin/test/OpenflowpluginTestRpcProvider.java
@@ -7,16 +7,20 @@
  */
 package org.opendaylight.openflowplugin.test;
 
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.AddFlowOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.RemoveFlowOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.UpdateFlowOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
@@ -24,23 +28,23 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yangtools.concepts.AbstractObjectRegistration;
-import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OpenflowpluginTestServiceProvider implements AutoCloseable,
-        SalFlowService {
+public class OpenflowpluginTestRpcProvider implements AutoCloseable {
 
     private static final Logger LOG = LoggerFactory
-            .getLogger(OpenflowpluginTestServiceProvider.class);
+            .getLogger(OpenflowpluginTestRpcProvider.class);
 
     private final DataBroker dataService;
-    private ObjectRegistration<SalFlowService> flowRegistration;
+    private Registration flowRegistration;
     private final NotificationPublishService notificationProviderService;
 
-    public OpenflowpluginTestServiceProvider(final DataBroker dataService,
+    public OpenflowpluginTestRpcProvider(final DataBroker dataService,
             final NotificationPublishService notificationProviderService) {
         this.dataService = dataService;
         this.notificationProviderService = notificationProviderService;
@@ -60,14 +64,14 @@ public class OpenflowpluginTestServiceProvider implements AutoCloseable,
      *
      * @return {@link #flowRegistration}
      */
-    public ObjectRegistration<SalFlowService> getFlowRegistration() {
+    public Registration getFlowRegistration() {
         return flowRegistration;
     }
 
     /**
      * Set {@link #flowRegistration}.
      */
-    public void setFlowRegistration(final ObjectRegistration<SalFlowService> flowRegistration) {
+    public void setFlowRegistration(final Registration flowRegistration) {
         this.flowRegistration = flowRegistration;
     }
 
@@ -87,58 +91,36 @@ public class OpenflowpluginTestServiceProvider implements AutoCloseable,
      */
     @Override
     public void close() {
-        OpenflowpluginTestServiceProvider.LOG
-                .info("SalFlowServiceProvide stopped.");
+        OpenflowpluginTestRpcProvider.LOG
+                .info("SalFlowRpcProvide stopped.");
         flowRegistration.close();
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
-     * SalFlowService
-     * #addFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow.
-     * service.rev130819.AddFlowInput)
-     */
-    @Override
-    public ListenableFuture<RpcResult<AddFlowOutput>> addFlow(final AddFlowInput input) {
-        OpenflowpluginTestServiceProvider.LOG.info("addFlow - {}", input);
+    private ListenableFuture<RpcResult<AddFlowOutput>> addFlow(final AddFlowInput input) {
+        OpenflowpluginTestRpcProvider.LOG.info("addFlow - {}", input);
         return null;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
-     * SalFlowService
-     * #removeFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow
-     * .service.rev130819.RemoveFlowInput)
-     */
-    @Override
-    public ListenableFuture<RpcResult<RemoveFlowOutput>> removeFlow(final RemoveFlowInput input) {
-        OpenflowpluginTestServiceProvider.LOG.info("removeFlow - {}", input);
+    private ListenableFuture<RpcResult<RemoveFlowOutput>> removeFlow(final RemoveFlowInput input) {
+        OpenflowpluginTestRpcProvider.LOG.info("removeFlow - {}", input);
         return null;
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.
-     * SalFlowService
-     * #updateFlow(org.opendaylight.yang.gen.v1.urn.opendaylight.flow
-     * .service.rev130819.UpdateFlowInput)
-     */
-    @Override
-    public ListenableFuture<RpcResult<UpdateFlowOutput>> updateFlow(final UpdateFlowInput input) {
-        OpenflowpluginTestServiceProvider.LOG.info("updateFlow - {}", input);
+    private ListenableFuture<RpcResult<UpdateFlowOutput>> updateFlow(final UpdateFlowInput input) {
+        OpenflowpluginTestRpcProvider.LOG.info("updateFlow - {}", input);
         return null;
     }
 
-    public ObjectRegistration<OpenflowpluginTestServiceProvider> register(final RpcProviderService rpcRegistry) {
-        setFlowRegistration(rpcRegistry.registerRpcImplementation(SalFlowService.class, this, ImmutableSet.of(
+    public ClassToInstanceMap<Rpc<?,?>> getRpcClassToInstanceMap() {
+        return ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(AddFlow.class, this::addFlow)
+            .put(RemoveFlow.class, this::removeFlow)
+            .put(UpdateFlow.class, this::updateFlow)
+            .build();
+    }
+
+    public Registration register(final RpcProviderService rpcRegistry) {
+        setFlowRegistration(rpcRegistry.registerRpcImplementations(this.getRpcClassToInstanceMap(), ImmutableSet.of(
             InstanceIdentifier.create(Nodes.class)
             .child(Node.class, new NodeKey(new NodeId(OpenflowpluginTestActivator.NODE_ID))))));
 
