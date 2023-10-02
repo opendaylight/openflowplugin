63d3fb91e7e36fda7490aa64f557497521820a75
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-02 21:10:05 +0200

    Do not use RpcService in OpendaylightPortStatisticsServiceImpl
    Migrated usage of RpcService to Rpc<?,?> for
    OpendaylightPortStatisticsServiceImpl class.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: I883900233220de7587ac733055f1f12ad153e470
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/OpendaylightPortStatisticsRpcs.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/OpendaylightPortStatisticsRpcs.java
@@ -7,24 +7,29 @@
  */
 package org.opendaylight.openflowplugin.impl.statistics.services;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatistics;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatisticsInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetAllNodeConnectorsStatisticsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatistics;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatisticsInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.GetNodeConnectorStatisticsOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.OpendaylightPortStatisticsService;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public class OpendaylightPortStatisticsServiceImpl implements OpendaylightPortStatisticsService {
+public class OpendaylightPortStatisticsRpcs {
     private final AllPortStatsService allPortStats;
     private final PortStatsService portStats;
     private final NotificationPublishService notificationPublishService;
 
-    public OpendaylightPortStatisticsServiceImpl(final RequestContextStack requestContextStack,
+    public OpendaylightPortStatisticsRpcs(final RequestContextStack requestContextStack,
                                                  final DeviceContext deviceContext,
                                                  final AtomicLong compatibilityXidSeed,
                                                  final NotificationPublishService notificationPublishService) {
@@ -33,15 +38,22 @@ public class OpendaylightPortStatisticsServiceImpl implements OpendaylightPortSt
         portStats = new PortStatsService(requestContextStack, deviceContext, compatibilityXidSeed);
     }
 
-    @Override
-    public ListenableFuture<RpcResult<GetAllNodeConnectorsStatisticsOutput>> getAllNodeConnectorsStatistics(
+    @VisibleForTesting
+    ListenableFuture<RpcResult<GetAllNodeConnectorsStatisticsOutput>> getAllNodeConnectorsStatistics(
             final GetAllNodeConnectorsStatisticsInput input) {
         return allPortStats.handleAndNotify(input, notificationPublishService);
     }
 
-    @Override
-    public ListenableFuture<RpcResult<GetNodeConnectorStatisticsOutput>> getNodeConnectorStatistics(
+    @VisibleForTesting
+    ListenableFuture<RpcResult<GetNodeConnectorStatisticsOutput>> getNodeConnectorStatistics(
             final GetNodeConnectorStatisticsInput input) {
         return portStats.handleAndNotify(input, notificationPublishService);
+    }
+
+    public ClassToInstanceMap<Rpc<?,?>> getRpcClassToInstanceMap() {
+        return ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(GetAllNodeConnectorsStatistics.class, this::getAllNodeConnectorsStatistics)
+            .put(GetNodeConnectorStatistics.class, this::getNodeConnectorStatistics)
+            .build();
     }
 }
