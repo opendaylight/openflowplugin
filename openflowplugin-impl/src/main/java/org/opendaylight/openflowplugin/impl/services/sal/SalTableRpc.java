248dec2ecb7bd32bfc104f71399908cb88b1cac0
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-02 23:18:02 +0200

    Do not use RpcService in SalTableServiceImpl
    Migrated usage of RpcService to Rpc<?,?> for SalTableServiceImpl
    class.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: Ief9fa79046cc66050c055760d3ee49257cfb5218
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/SalTableRpc.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/SalTableRpc.java
@@ -14,17 +14,16 @@ import org.opendaylight.openflowplugin.impl.datastore.MultipartWriterProvider;
 import org.opendaylight.openflowplugin.impl.services.multilayer.MultiLayerTableMultipartService;
 import org.opendaylight.openflowplugin.impl.services.singlelayer.SingleLayerTableMultipartService;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.ConvertorExecutor;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableOutput;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public final class SalTableServiceImpl implements SalTableService {
+public final class SalTableRpc {
 
     private final SingleLayerTableMultipartService singleLayerService;
     private final MultiLayerTableMultipartService multiLayerService;
 
-    public SalTableServiceImpl(final RequestContextStack requestContextStack,
+    public SalTableRpc(final RequestContextStack requestContextStack,
                                final DeviceContext deviceContext,
                                final ConvertorExecutor convertorExecutor,
                                final MultipartWriterProvider multipartWriterProvider) {
@@ -37,7 +36,6 @@ public final class SalTableServiceImpl implements SalTableService {
                                                                 multipartWriterProvider);
     }
 
-    @Override
     public ListenableFuture<RpcResult<UpdateTableOutput>> updateTable(final UpdateTableInput input) {
         return singleLayerService.canUseSingleLayerSerialization()
             ? singleLayerService.handleAndReply(input)
