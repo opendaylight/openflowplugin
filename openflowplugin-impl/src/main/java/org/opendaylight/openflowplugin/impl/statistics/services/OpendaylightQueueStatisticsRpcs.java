d56cd1e40eb1417e459aacfc4f3b3264469ff91a
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-02 22:44:16 +0200

    Do not use RpcService in OpendaylightQueueStatisticsServiceImpl
    Migrated usage of RpcService to Rpc<?,?> for
    OpendaylightQueueStatisticsServiceImpl class.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: Id135cae084d63f2903c49384815969f97139880a
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/OpendaylightQueueStatisticsRpcs.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/statistics/services/OpendaylightQueueStatisticsRpcs.java
@@ -7,27 +7,33 @@
  */
 package org.opendaylight.openflowplugin.impl.statistics.services;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 import org.opendaylight.mdsal.binding.api.NotificationPublishService;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromAllPorts;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromAllPortsInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromAllPortsOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromGivenPort;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromGivenPortInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetAllQueuesStatisticsFromGivenPortOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetQueueStatisticsFromGivenPort;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetQueueStatisticsFromGivenPortInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.GetQueueStatisticsFromGivenPortOutput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.queue.statistics.rev131216.OpendaylightQueueStatisticsService;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public class OpendaylightQueueStatisticsServiceImpl implements OpendaylightQueueStatisticsService {
+public class OpendaylightQueueStatisticsRpcs {
     private final AllQueuesAllPortsService allQueuesAllPorts;
     private final AllQueuesOnePortService allQueuesOnePort;
     private final OneQueueOnePortService oneQueueOnePort;
     private final NotificationPublishService notificationPublishService;
 
-    public OpendaylightQueueStatisticsServiceImpl(final RequestContextStack requestContextStack,
+    public OpendaylightQueueStatisticsRpcs(final RequestContextStack requestContextStack,
                                                   final DeviceContext deviceContext,
                                                   final AtomicLong compatibilityXidSeed,
                                                   final NotificationPublishService notificationPublishService) {
@@ -37,21 +43,29 @@ public class OpendaylightQueueStatisticsServiceImpl implements OpendaylightQueue
         oneQueueOnePort = new OneQueueOnePortService(requestContextStack, deviceContext, compatibilityXidSeed);
     }
 
-    @Override
-    public ListenableFuture<RpcResult<GetAllQueuesStatisticsFromAllPortsOutput>> getAllQueuesStatisticsFromAllPorts(
+    @VisibleForTesting
+    ListenableFuture<RpcResult<GetAllQueuesStatisticsFromAllPortsOutput>> getAllQueuesStatisticsFromAllPorts(
             final GetAllQueuesStatisticsFromAllPortsInput input) {
         return allQueuesAllPorts.handleAndNotify(input, notificationPublishService);
     }
 
-    @Override
+    @VisibleForTesting
     public ListenableFuture<RpcResult<GetAllQueuesStatisticsFromGivenPortOutput>> getAllQueuesStatisticsFromGivenPort(
             final GetAllQueuesStatisticsFromGivenPortInput input) {
         return allQueuesOnePort.handleAndNotify(input, notificationPublishService);
     }
 
-    @Override
+    @VisibleForTesting
     public ListenableFuture<RpcResult<GetQueueStatisticsFromGivenPortOutput>> getQueueStatisticsFromGivenPort(
             final GetQueueStatisticsFromGivenPortInput input) {
         return oneQueueOnePort.handleAndNotify(input, notificationPublishService);
+    }
+
+    public ClassToInstanceMap<Rpc<?,?>> getRpcClassToInstanceMap() {
+        return ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(GetAllQueuesStatisticsFromAllPorts.class, this::getAllQueuesStatisticsFromAllPorts)
+            .put(GetAllQueuesStatisticsFromGivenPort.class, this::getAllQueuesStatisticsFromGivenPort)
+            .put(GetQueueStatisticsFromGivenPort.class, this::getQueueStatisticsFromGivenPort)
+            .build();
     }
 }
