8166a122d5cbb73d450f4ef2d6f78a26cb090810
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-02 23:18:02 +0200

    Do not use RpcService in new SalTableServiceImpl class
    Removed usage of RpcService from SalTableServiceImpl class and
    in all usages.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: Ief9fa79046cc66050c055760d3ee49257cfb5218
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/SalTableRpc.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/SalTableRpc.java
@@ -7,6 +7,9 @@
  */
 package org.opendaylight.openflowplugin.impl.services.sal;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
 import org.opendaylight.openflowplugin.api.openflow.device.RequestContextStack;
@@ -14,17 +17,18 @@ import org.opendaylight.openflowplugin.impl.datastore.MultipartWriterProvider;
 import org.opendaylight.openflowplugin.impl.services.multilayer.MultiLayerTableMultipartService;
 import org.opendaylight.openflowplugin.impl.services.singlelayer.SingleLayerTableMultipartService;
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.ConvertorExecutor;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.SalTableService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTable;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.table.service.rev131026.UpdateTableOutput;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public final class SalTableServiceImpl implements SalTableService {
+public final class SalTableRpc {
 
     private final SingleLayerTableMultipartService singleLayerService;
     private final MultiLayerTableMultipartService multiLayerService;
 
-    public SalTableServiceImpl(final RequestContextStack requestContextStack,
+    public SalTableRpc(final RequestContextStack requestContextStack,
                                final DeviceContext deviceContext,
                                final ConvertorExecutor convertorExecutor,
                                final MultipartWriterProvider multipartWriterProvider) {
@@ -37,10 +41,16 @@ public final class SalTableServiceImpl implements SalTableService {
                                                                 multipartWriterProvider);
     }
 
-    @Override
-    public ListenableFuture<RpcResult<UpdateTableOutput>> updateTable(final UpdateTableInput input) {
+    @VisibleForTesting
+    ListenableFuture<RpcResult<UpdateTableOutput>> updateTable(final UpdateTableInput input) {
         return singleLayerService.canUseSingleLayerSerialization()
             ? singleLayerService.handleAndReply(input)
             : multiLayerService.handleAndReply(input);
+    }
+
+    public ClassToInstanceMap<Rpc<?,?>> getRpcClassToInstanceMap() {
+        return ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(UpdateTable.class, this::updateTable)
+            .build();
     }
 }
