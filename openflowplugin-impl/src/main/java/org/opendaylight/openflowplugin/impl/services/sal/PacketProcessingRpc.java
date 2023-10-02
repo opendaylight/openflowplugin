97e16e518d04063f5e2c6e790b924c8babb22257
lubos-cicut <lubos.cicut@pantheon.tech>
2023-10-02 17:10:59 +0200

    Do not use RpcService in PacketProcessingServiceImpl class
    Migrated usage of RpcService to Rpc<?,?> for
    PacketProcessingServiceImpl class and in all usages.
    
    JIRA: OPNFLWPLUG-1125
    Change-Id: I36ad61f97213017c1347b1222e7f72df92528f94
    Signed-off-by: lubos-cicut <lubos.cicut@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/PacketProcessingRpc.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/services/sal/PacketProcessingRpc.java
@@ -7,6 +7,9 @@
  */
 package org.opendaylight.openflowplugin.impl.services.sal;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Optional;
 import org.opendaylight.openflowplugin.api.openflow.device.DeviceContext;
@@ -18,26 +21,32 @@ import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.PacketOutC
 import org.opendaylight.openflowplugin.openflow.md.core.sal.convertor.data.XidConvertorData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.PacketOutInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacket;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketOutput;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public final class PacketProcessingServiceImpl extends AbstractSimpleService<TransmitPacketInput, TransmitPacketOutput>
-                                               implements PacketProcessingService {
+public final class PacketProcessingRpc extends AbstractSimpleService<TransmitPacketInput, TransmitPacketOutput> {
 
     private final ConvertorExecutor convertorExecutor;
 
-    public PacketProcessingServiceImpl(final RequestContextStack requestContextStack,
+    public PacketProcessingRpc(final RequestContextStack requestContextStack,
                                        final DeviceContext deviceContext,
                                        final ConvertorExecutor convertorExecutor) {
         super(requestContextStack, deviceContext, TransmitPacketOutput.class);
         this.convertorExecutor = convertorExecutor;
     }
 
-    @Override
-    public ListenableFuture<RpcResult<TransmitPacketOutput>> transmitPacket(final TransmitPacketInput input) {
+    @VisibleForTesting
+    ListenableFuture<RpcResult<TransmitPacketOutput>> transmitPacket(final TransmitPacketInput input) {
         return handleServiceCall(input);
+    }
+
+    public ClassToInstanceMap<Rpc<?,?>> getRpcClassToInstanceMap() {
+        return ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(TransmitPacket.class, this::transmitPacket)
+            .build();
     }
 
     @Override
