3b314f06177863d9c7fa5aac5be803d112260d0b
Robert Varga <robert.varga@pantheon.tech>
2024-05-21 10:08:46 +0200

    WIP: Add openflowplugin-impl's OSGi configuration
    // FIXME: rewire blueprint
    
    JIRA: OPNFLWPLUG-1132
    Change-Id: I695d98dca8032ae91fb3a6501e2ff43ab007fb44
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/configuration/OSGiConfigurationServiceFactory.java
+++ openflowplugin-impl/src/main/java/org/opendaylight/openflowplugin/impl/configuration/OSGiConfigurationServiceFactory.java
@@ -7,69 +7,30 @@
  */
 package org.opendaylight.openflowplugin.impl.configuration;
 
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.collect.Maps;
-import java.io.IOException;
+import java.util.Map;
 import org.opendaylight.openflowplugin.api.OFConstants;
 import org.opendaylight.openflowplugin.api.openflow.configuration.ConfigurationService;
 import org.opendaylight.openflowplugin.api.openflow.configuration.ConfigurationServiceFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.provider.config.rev160510.OpenflowProviderConfig;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.osgi.service.metatype.annotations.Designate;
 
-@Component(service = ConfigurationServiceFactory.class)
+@Component(service = ConfigurationServiceFactory.class, configurationPid = OFConstants.CONFIG_FILE_ID)
+@Designate(ocd = OSGiConfiguration.class)
 public final class OSGiConfigurationServiceFactory extends ConfigurationServiceFactoryImpl {
-    private static final Logger LOG = LoggerFactory.getLogger(OSGiConfigurationServiceFactory.class);
-
-    private final BundleContext bundleContext;
+    private final Map<String, String> properties;
 
     @Activate
-    public OSGiConfigurationServiceFactory(final BundleContext bundleContext) {
-        this.bundleContext = requireNonNull(bundleContext);
+    public OSGiConfigurationServiceFactory(final Map<String, ?> properties) {
+        this.properties = Map.copyOf(Maps.transformValues(properties, Object::toString));
     }
 
     @Override
     public ConfigurationService newInstance(final OpenflowProviderConfig providerConfig) {
         var cs = super.newInstance(providerConfig);
-        update(cs);
+        cs.update(properties);
         return cs;
-    }
-
-    private void update(final ConfigurationService configurationService) {
-        LOG.info("Loading configuration from '{}' configuration file", OFConstants.CONFIG_FILE_ID);
-        final var serviceReference = bundleContext.getServiceReference(ConfigurationAdmin.class);
-        if (serviceReference == null) {
-            LOG.debug("No Configuration Admin Service available");
-            return;
-        }
-        final var configurationAdmin = bundleContext.getService(serviceReference);
-        if (configurationAdmin == null) {
-            LOG.debug("Failed to get Configuration Admin Service from {}", serviceReference);
-            return;
-        }
-
-        try {
-            final Configuration configuration;
-            try {
-                configuration = configurationAdmin.getConfiguration(OFConstants.CONFIG_FILE_ID);
-            } catch (IOException e) {
-                LOG.warn("Failed to load {} configuration file", OFConstants.CONFIG_FILE_ID, e);
-                return;
-            }
-
-            final var properties = configuration.getProperties();
-            if (properties != null) {
-                configurationService.update(Maps.transformValues(FrameworkUtil.asMap(properties), Object::toString));
-            }
-        } finally {
-            bundleContext.ungetService(serviceReference);
-        }
     }
 }
