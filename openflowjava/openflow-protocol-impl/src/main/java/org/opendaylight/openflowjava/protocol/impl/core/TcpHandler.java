c01bf6f270a14a95aa74e71dfb3e053e84a28bba
Robert Varga <robert.varga@pantheon.tech>
2024-05-23 11:55:09 +0200

    Remove ServerFacade.setThreadConfig()
    This method is not used anywhere, remove it.
    
    Change-Id: Icf4f5202bd17b9b467857e2c3eab8080d9fb46f6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/TcpHandler.java
+++ openflowjava/openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/TcpHandler.java
@@ -190,12 +190,6 @@ public class TcpHandler implements ServerFacade {
         this.channelInitializer = channelInitializer;
     }
 
-    @Override
-    @Deprecated(since = "0.17.2", forRemoval = true)
-    public void setThreadConfig(final ThreadConfiguration threadConfig) {
-        // No-op
-    }
-
     /**
      * Initiate event loop groups.
      *
