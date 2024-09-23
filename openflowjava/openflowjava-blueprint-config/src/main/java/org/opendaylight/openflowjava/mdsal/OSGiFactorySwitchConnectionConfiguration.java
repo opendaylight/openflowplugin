19a44c1e5bb12c0200f4e7cb8ba35772c79d40bb
Robert Varga <robert.varga@pantheon.tech>
2024-09-23 19:36:05 +0200

    Use configuration from XML files
    Rather than deploying fixed configuration, parse it from XML files, as
    documented.
    
    JIRA: OPNFLWPLUG-1133
    Change-Id: Ib3e9df26639e33be6414c7458d9664a5eb823237
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e20cdfd2f5a64fd5083ac6d1521386409a332e33)

--- openflowjava/openflowjava-blueprint-config/src/main/java/org/opendaylight/openflowjava/mdsal/OSGiFactorySwitchConnectionConfiguration.java
+++ openflowjava/openflowjava-blueprint-config/src/main/java/org/opendaylight/openflowjava/mdsal/OSGiFactorySwitchConnectionConfiguration.java
@@ -10,18 +10,22 @@ package org.opendaylight.openflowjava.mdsal;
 import com.google.common.base.Stopwatch;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.util.List;
-import org.opendaylight.mdsal.binding.api.DataBroker;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.xml.stream.XMLStreamException;
 import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.config.rev140630.KeystoreType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.config.rev140630.PathType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.config.rev140630.TransportProtocol;
+import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow._switch.connection.config.rev160506.SwitchConnectionConfig;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow._switch.connection.config.rev160506.SwitchConnectionConfigBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow._switch.connection.config.rev160506._switch.connection.config.TlsBuilder;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.util.xml.UntrustedXML;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.codec.xml.XmlParserStream;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizationResultHolder;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack.Inference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -34,73 +38,81 @@ import org.slf4j.LoggerFactory;
 @Component(service = { })
 public final class OSGiFactorySwitchConnectionConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(OSGiFactorySwitchConnectionConfiguration.class);
+    private static final Path INITIAL_CONFIG_DIR = Path.of("etc", "opendaylight", "datastore", "initial", "config");
 
     @Activate
-    public OSGiFactorySwitchConnectionConfiguration(@Reference final DataBroker dataBroker) {
-        // Common for both cases
-        final var builder = new SwitchConnectionConfigBuilder()
-            .setTransportProtocol(TransportProtocol.TCP)
-            .setGroupAddModEnabled(Boolean.FALSE)
-            .setChannelOutboundQueueSize(Uint16.valueOf(1024))
-            .setTls(new TlsBuilder()
-                .setKeystore("configuration/ssl/ctl.jks")
-                .setKeystoreType(KeystoreType.JKS)
-                .setKeystorePathType(PathType.PATH)
-                .setKeystorePassword("opendaylight")
-                .setTruststore("configuration/ssl/truststore.jks")
-                .setTruststoreType(KeystoreType.JKS)
-                .setTruststorePathType(PathType.PATH)
-                .setTruststorePassword("opendaylight")
-                .setCertificatePassword("opendaylight")
-                .setCipherSuites(List.of())
-                .build());
+    public OSGiFactorySwitchConnectionConfiguration(@Reference final DOMDataBroker dataBroker,
+            @Reference final DOMSchemaService schemaService) {
+        final var inference = Inference.ofDataTreePath(schemaService.getGlobalContext(), SwitchConnectionConfig.QNAME);
 
         // Create OF switch connection provider on port 6653 (default)
-        writeIfNotPresent(dataBroker, builder
-            .setInstanceName("openflow-switch-connection-provider-default-impl")
-            .setPort(Uint16.valueOf(6653))
-            .build());
+        writeIfNotPresent(dataBroker, inference, "default-openflow-connection-config.xml");
         // Create OF switch connection provider on port 6633 (legacy)
-        writeIfNotPresent(dataBroker, builder
-            .setInstanceName("openflow-switch-connection-provider-legacy-impl")
-            .setPort(Uint16.valueOf(6633))
-            .build());
+        writeIfNotPresent(dataBroker, inference, "legacy-openflow-connection-config.xml");
     }
 
-    private static void writeIfNotPresent(final DataBroker dataBroker, final SwitchConnectionConfig config) {
-        final var instanceName = config.getInstanceName();
-        LOG.info("Checking presence of configuration for {}", instanceName);
+    private static void writeIfNotPresent(final DOMDataBroker dataBroker, final Inference inference,
+            final String fileName) {
+        final var path = INITIAL_CONFIG_DIR.resolve(fileName);
+        final var resultHolder = new NormalizationResultHolder();
+        final var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
 
+        try (var xmlParser = XmlParserStream.create(writer, inference)) {
+            xmlParser.parse(UntrustedXML.createXMLStreamReader(Files.newInputStream(path)));
+        } catch (IOException | XMLStreamException e) {
+            LOG.warn("Cannot parse {}, skipping configuration deployment", path, e);
+            return;
+        }
+
+        final var result = resultHolder.getResult().data();
+        if (!(result instanceof MapNode map)) {
+            LOG.warn("Skipping configuration deployment of non-MapNode {}", result.prettyTree());
+            return;
+        }
+        final var size = map.size();
+        if (size != 1) {
+            LOG.warn("Skipping configuration deployment of multi-entry {}", map.prettyTree());
+            return;
+        }
+        final var entry = map.body().iterator().next();
+        final var name = entry.name();
+        if (!SwitchConnectionConfig.QNAME.equals(name.getNodeType())) {
+            LOG.warn("Skipping configuration deployemtn of unrecognized {}", entry.prettyTree());
+            return;
+        }
+
+        LOG.info("Checking presence of configuration for {}", name);
         final var sw = Stopwatch.createStarted();
-        final var iid = InstanceIdentifier.builder(SwitchConnectionConfig.class, config.key()).build();
+        final var iid = YangInstanceIdentifier.builder().node(SwitchConnectionConfig.QNAME).node(name).build();
+
         final var tx = dataBroker.newReadWriteTransaction();
         tx.exists(LogicalDatastoreType.CONFIGURATION, iid).addCallback(new FutureCallback<Boolean>() {
             @Override
             public void onSuccess(final Boolean result) {
-                LOG.debug("Presence of configuration for {} ascertained in {}", instanceName, sw);
+                LOG.debug("Presence of configuration for {} ascertained in {}", name, sw);
                 if (result) {
-                    LOG.info("Configuration for {} already present", instanceName);
+                    LOG.info("Configuration for {} already present", name);
                     tx.cancel();
                     return;
                 }
 
-                tx.put(LogicalDatastoreType.CONFIGURATION, iid, config);
+                tx.put(LogicalDatastoreType.CONFIGURATION, iid, entry);
                 tx.commit().addCallback(new FutureCallback<CommitInfo>() {
                     @Override
                     public void onSuccess(final CommitInfo result) {
-                        LOG.info("Configuration for {} set to factory-default", instanceName);
+                        LOG.info("Configuration for {} populated", name);
                     }
 
                     @Override
                     public void onFailure(final Throwable cause) {
-                        LOG.warn("Failed to set configuration for {} set to factory-default", instanceName, cause);
+                        LOG.warn("Failed to populated configuration for {}", name, cause);
                     }
                 }, MoreExecutors.directExecutor());
             }
 
             @Override
             public void onFailure(final Throwable cause) {
-                LOG.warn("Failed to ascertain presence of configuration for {} after {}", instanceName, sw, cause);
+                LOG.warn("Failed to ascertain presence of configuration for {} after {}", name, sw, cause);
                 tx.cancel();
             }
         }, MoreExecutors.directExecutor());
