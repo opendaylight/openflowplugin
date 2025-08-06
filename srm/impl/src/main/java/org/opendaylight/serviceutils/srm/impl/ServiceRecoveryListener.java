d2c76ac5fe3328042b926ce1c7ffbfc47bdf7c1e
Robert Varga <robert.varga@pantheon.tech>
2025-08-06 12:51:43 +0200

    Remove AbstractDataTreeChangeListener
    This class has a single superclass, integrate it into
    ServiceRecoveryListener, fixing activation/deactivation and switching to
    more modern APIs in the process of doing so.
    
    Change-Id: I9ba4f9fadcfbfcab556e680efd0039b722b5b4cc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- srm/impl/src/main/java/org/opendaylight/serviceutils/srm/impl/ServiceRecoveryListener.java
+++ srm/impl/src/main/java/org/opendaylight/serviceutils/srm/impl/ServiceRecoveryListener.java
@@ -8,9 +8,15 @@
 
 package org.opendaylight.serviceutils.srm.impl;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
+import org.opendaylight.mdsal.binding.api.DataTreeModification;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.serviceutils.srm.ServiceRecoveryRegistry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.serviceutils.srm.ops.rev180626.ServiceOps;
@@ -19,44 +25,87 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.serviceutils.srm.ops.rev180
 import org.opendaylight.yang.gen.v1.urn.opendaylight.serviceutils.srm.types.rev180626.EntityNameBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.serviceutils.srm.types.rev180626.EntityTypeBase;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.binding.DataObjectReference;
+import org.opendaylight.yangtools.concepts.Registration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.RequireServiceComponentRuntime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Component
-@RequireServiceComponentRuntime
-public final class ServiceRecoveryListener extends AbstractDataTreeChangeListener<Operations> {
+@Component(service = { })
+public final class ServiceRecoveryListener implements AutoCloseable, DataTreeChangeListener<Operations>,
+        ChainableDataTreeChangeListener<Operations> {
     private static final Logger LOG = LoggerFactory.getLogger(ServiceRecoveryListener.class);
 
+    private final ChainableDataTreeChangeListenerImpl<Operations> chainingDelegate =
+        new ChainableDataTreeChangeListenerImpl<>();
     private final ServiceRecoveryRegistry serviceRecoveryRegistry;
+    private final Registration dataChangeListenerRegistration;
 
     @Inject
     @Activate
     public ServiceRecoveryListener(@Reference final DataBroker dataBroker,
             @Reference final ServiceRecoveryRegistry serviceRecoveryRegistry) {
-        super(dataBroker, LogicalDatastoreType.OPERATIONAL, InstanceIdentifier.create(ServiceOps.class)
-                .child(Services.class).child(Operations.class));
-        this.serviceRecoveryRegistry = serviceRecoveryRegistry;
+        this.serviceRecoveryRegistry = requireNonNull(serviceRecoveryRegistry);
+        dataChangeListenerRegistration = dataBroker.registerTreeChangeListener(LogicalDatastoreType.OPERATIONAL,
+            DataObjectReference.builder(ServiceOps.class).child(Services.class).child(Operations.class).build(), this);
     }
 
     @Override
-    void add(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations operations) {
+    @PreDestroy
+    @Deactivate
+    public void close() {
+        dataChangeListenerRegistration.close();
+    }
+
+    @Override
+    @Deprecated
+    public void addBeforeListener(final DataTreeChangeListener<Operations> listener) {
+        chainingDelegate.addBeforeListener(listener);
+    }
+
+    @Override
+    @Deprecated
+    public void addAfterListener(final DataTreeChangeListener<Operations> listener) {
+        chainingDelegate.addAfterListener(listener);
+    }
+
+    @Override
+    public void onDataTreeChanged(final List<DataTreeModification<Operations>> changes) {
+        for (var dataTreeModification : changes) {
+            var instanceIdentifier = dataTreeModification.path();
+            var dataObjectModification = dataTreeModification.getRootNode();
+            var dataBefore = dataObjectModification.dataBefore();
+            var dataAfter = dataObjectModification.dataAfter();
+
+            switch (dataObjectModification.modificationType()) {
+                case null -> throw new NullPointerException();
+                case SUBTREE_MODIFIED -> update(instanceIdentifier, dataBefore, dataAfter);
+                case DELETE -> remove(instanceIdentifier, dataBefore);
+                case WRITE -> {
+                    if (dataBefore == null) {
+                        add(instanceIdentifier, dataAfter);
+                    } else {
+                        update(instanceIdentifier, dataBefore, dataAfter);
+                    }
+                }
+            }
+        }
+    }
+
+    private void add(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations operations) {
         LOG.info("Service Recovery operation triggered for service: {}", operations);
         recoverService(operations.getEntityType(), operations.getEntityName(), operations.getEntityId());
     }
 
-    @Override
-    void remove(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations removedDataObject) {
+    private void remove(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations removedDataObject) {
         // FIXME: this should be doing something, right?
     }
 
-    @Override
-    void update(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations originalDataObject,
+    private void update(final DataObjectIdentifier<Operations> instanceIdentifier, final Operations originalDataObject,
             final Operations updatedDataObject) {
         add(instanceIdentifier, updatedDataObject);
     }
