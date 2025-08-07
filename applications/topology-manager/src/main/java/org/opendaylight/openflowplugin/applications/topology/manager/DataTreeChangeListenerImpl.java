8b90cd53eacf68983152747ca4f52c86d0c534b3
Robert Varga <robert.varga@pantheon.tech>
2025-08-07 23:49:13 +0200

    Hide DataTreeChangeListenerImpl
    This class should not be visible to the outside world and its internals
    should be suitably final.
    
    Change-Id: I544126159d47daa31edf9d7d9d18214c94ee8343
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- applications/topology-manager/src/main/java/org/opendaylight/openflowplugin/applications/topology/manager/DataTreeChangeListenerImpl.java
+++ applications/topology-manager/src/main/java/org/opendaylight/openflowplugin/applications/topology/manager/DataTreeChangeListenerImpl.java
@@ -15,31 +15,31 @@ import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.DataTreeChangeListener;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier.WithKey;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-public abstract class DataTreeChangeListenerImpl<T extends DataObject> implements DataTreeChangeListener<T>,
+abstract class DataTreeChangeListenerImpl<T extends DataObject> implements DataTreeChangeListener<T>,
         AutoCloseable {
     static final WithKey<Topology, TopologyKey> II_TO_TOPOLOGY = DataObjectIdentifier.builder(NetworkTopology.class)
         .child(Topology.class, new TopologyKey(new TopologyId(FlowCapableTopologyProvider.TOPOLOGY_ID)))
         .build();
 
-    protected final Registration listenerRegistration;
-    protected OperationProcessor operationProcessor;
+    final Registration listenerRegistration;
+    final OperationProcessor operationProcessor;
 
     @SuppressFBWarnings(value = "MC_OVERRIDABLE_METHOD_CALL_IN_CONSTRUCTOR",
         justification = "'this' passed to registerDataTreeChangeListener")
-    protected DataTreeChangeListenerImpl(final OperationProcessor operationProcessor, final DataBroker dataBroker,
+    DataTreeChangeListenerImpl(final OperationProcessor operationProcessor, final DataBroker dataBroker,
             final InstanceIdentifier<T> ii) {
         this.operationProcessor = requireNonNull(operationProcessor);
         listenerRegistration = dataBroker.registerDataTreeChangeListener(
@@ -51,29 +51,18 @@ public abstract class DataTreeChangeListenerImpl<T extends DataObject> implement
         listenerRegistration.close();
     }
 
-    <O extends DataObject> void sendToTransactionChain(final O node, final DataObjectIdentifier<O> iiToTopologyNode) {
+    final <O extends DataObject> void sendToTransactionChain(final O node, final DataObjectIdentifier<O> nodePath) {
         operationProcessor.enqueueOperation(
-            manager -> manager.mergeToTransaction(LogicalDatastoreType.OPERATIONAL, iiToTopologyNode, node, true));
+            manager -> manager.mergeToTransaction(LogicalDatastoreType.OPERATIONAL, nodePath, node, true));
     }
 
-    @NonNull WithKey<
-            org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology
-            .Node,
-            org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology
-            .NodeKey> provideIIToTopologyNode(final NodeId nodeIdInTopology) {
-        return II_TO_TOPOLOGY.toBuilder()
-            .child(org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology
-                    .topology.Node.class,
-                new org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology
-                    .topology.NodeKey(nodeIdInTopology))
-            .build();
+    static final @NonNull WithKey<Node, NodeKey> provideIIToTopologyNode(final NodeId nodeIdInTopology) {
+        return II_TO_TOPOLOGY.toBuilder().child(Node.class, new NodeKey(nodeIdInTopology)).build();
     }
 
-    NodeId provideTopologyNodeId(final InstanceIdentifier<T> iiToNodeInInventory) {
-        final NodeKey inventoryNodeKey = iiToNodeInInventory.firstKeyOf(Node.class);
-        if (inventoryNodeKey != null) {
-            return new NodeId(inventoryNodeKey.getId().getValue());
-        }
-        return null;
+    final NodeId provideTopologyNodeId(final InstanceIdentifier<T> iiToNodeInInventory) {
+        final var key = iiToNodeInInventory.firstKeyOf(org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819
+            .nodes.Node.class);
+        return key == null ? null : new NodeId(key.getId().getValue());
     }
 }
