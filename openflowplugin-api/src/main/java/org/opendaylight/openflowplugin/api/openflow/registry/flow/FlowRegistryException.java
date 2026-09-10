4ff43b244ec10744fb00dc84c24327fde31ac5b7
Robert Varga <robert.varga@pantheon.tech>
2026-09-10 17:34:56 +0200

    WIP: Remove use of HTML entities
    Replace HTML entities with their equivalents and drop long-defunct email
    addresses.
    
    Change-Id: I86d194c9329905fd63b2e2aa777c862cc263780a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/flow/FlowRegistryException.java
+++ openflowplugin-api/src/main/java/org/opendaylight/openflowplugin/api/openflow/registry/flow/FlowRegistryException.java
@@ -5,11 +5,10 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.openflowplugin.api.openflow.registry.flow;
 
 /**
- * Created by Martin Bobak &lt;mbobak@cisco.com&gt; on 8.4.2015.
+ * Created by Martin Bobak on 8.4.2015.
  */
 @Deprecated
 public class FlowRegistryException extends Exception {
